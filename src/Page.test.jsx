96c9ce911e09e5c3d18013cce3aa78acc72ec9a7
eonsang <2eonhworking@gmail.com>
2020-06-16 05:53:33 +0900

    page test 컴포넌트 render 중복 제거

--- src/Page.test.jsx
+++ src/Page.test.jsx
@@ -4,18 +4,35 @@ import { render } from '@testing-library/react';
 import Page from './Page';
 
 describe('<Page />', () => {
+  const dom = (props = {}) => {
+    const {
+      taskTitle,
+      onChangeTitle,
+      onClickAddTask,
+      onClickDeleteTask,
+    } = props;
+    return render(
+      <Page
+        tasks={[]}
+        taskTitle={taskTitle}
+        onChangeTitle={onChangeTitle}
+        onClickAddTask={onClickAddTask}
+        onClickDeleteTask={onClickDeleteTask}
+      />,
+    );
+  };
 
   context('로딩 되면', () => {
     it('"To-do" 타이틀이 있다.', () => {
-      const { getByText } = render(<Page tasks={[]} />);
+      const { getByText } = dom();
       expect(getByText('To-do')).toBeTruthy();
     });
     it('<Input />이 렌더링 됐다.', () => {
-      const { getByText } = render(<Page tasks={[]} />);
+      const { getByText } = dom();
       expect(getByText('할 일')).toBeTruthy();
     });
     it('<List />가 렌더링 됐다.', () => {
-      const { getByText } = render(<Page tasks={[]} />);
+      const { getByText } = dom();
       expect(getByText('할 일이 없어요!')).toBeTruthy();
     });
   });
