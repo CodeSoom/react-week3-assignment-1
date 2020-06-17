ecbe3d9447184278c71d2d6c46f08c446cd654dc
Paul An <anpaul0615@gmail.com>
2020-06-18 01:06:50 +0900

    List 컴포넌트 테스트 수정
    - 버튼클릭이벤트 테스트 추가

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import List from './List';
 
@@ -19,17 +19,23 @@ test('List', () => {
       title: '코드숨 과제하기',
     },
   ];
+  const handleClickDeleteButton = jest.fn();
 
-  const { container } = render((
+  const { container, getAllByText } = render((
     <List
       tasks={tasks}
-      onClickDelete={null}
+      onClickDelete={handleClickDeleteButton}
     />
   ));
 
   expect(container).toHaveTextContent('뭐라도 하기');
   expect(container).toHaveTextContent('아무것도 하지 않기');
   expect(container).toHaveTextContent('코드숨 과제하기');
+
+  const confirmButtons = getAllByText('완료');
+  expect(handleClickDeleteButton).not.toBeCalled();
+  confirmButtons.forEach((button) => fireEvent.click(button));
+  expect(handleClickDeleteButton).toBeCalledTimes(tasks.length);
 });
 
 
