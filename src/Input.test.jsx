0882f2039f52734bb41e88a0f80ff65e7cadeba1
Kwakcena <rhkrgudwh@gmail.com>
2020-06-18 01:30:31 +0900

    Input component test
    - 수정사항
    리액트 라이브러리와 테스팅 라이브러리의 구분을 위해 빈 줄을 추가함.
    render()의 결과를 바로 디스트럭처링 적용.
    모두 Input 컴포넌트에 대한 테스트이므로 describe 를 이용해 테스트 그룹화
    
    - 깨달은 점
    Input 컴포넌트의 테스트를 작성하면서 많은 어려움이 있었다.
    Input 컴포넌트에서 사용하는 onClick 이벤트에 대해 테스트를 하려고 했던
    점에서 막혀 있었다.
    
    onChange를 통해 value 값을 바꾸고 추가 버튼을 누르면 할 일이
    추가 된 뒤, value는 공백으로 초기화되는 테스트를 하고 싶었다.
    
    하지만 우리는 구현을 테스트 하는게 아닌 컴포넌트를 테스트 하는 것.
    
    Input 컴포넌트는 화면에 어떻게 보여줄지 구현되어 있고, 이에 대한 상태를
    바꾸는 기능은 App 컴포넌트에 구현되어 있다.
    
    Input 컴포넌트에서 기능 테스트를 수행했다고 치자. 만약 App.jsx에서
    value 초기화 하는 것을 안하게 되면 테스트가 우루루 깨지게 된다.
    
    즉 테스트가 다른 컴포넌트에 의존하게 되는 것이다.
    
    마찬가지로, 강의에서 Item 컴포넌트를 테스트 하던 과정을 생각해보자.
    Item 컴포넌트도 할 일 목록 버튼을 눌러 삭제되는 기능을 보고자 테스트
    하지 않고, Item 컴포넌트에서 onClickDelete(id) 가 사용되는 것 처럼
    실제로 매개변수가 잘 전달되며 함수가 불리는지 toBeCalledWith() 를
    사용하는 모습을 확인할 수 있다.
    
    이는 관심사의 분리가 매우 중요함을 다시한번 알려주는 내용인 것 같다.

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -1,49 +1,37 @@
 import React from 'react';
+
 import { render, fireEvent } from '@testing-library/react';
+
 import Input from './Input';
 
-test('input과 button으로 이루어져 있는지 확인한다.', () => {
-  const utils = render(<Input />);
-  const { getByText, getByPlaceholderText } = utils;
-
-  getByText('할 일');
-  getByText('추가');
-  getByPlaceholderText('할 일을 입력해 주세요');
-});
-
-test('change 이벤트가 발생하면 value의 값이 바뀐다.', () => {
-  const utils = render(<Input />);
-  const { getByPlaceholderText } = utils;
-
-  const input = getByPlaceholderText('할 일을 입력해 주세요');
-
-  fireEvent.change(input, {
-    target: {
-      value: '자바스크립트 공부하기',
-    },
+describe('<Input /> ', () => {
+  it('input과 button으로 이루어져 있는지 확인한다.', () => {
+    const { getByText, getByPlaceholderText } = render(<Input />);
+    getByText('할 일');
+    getByText('추가');
+    getByPlaceholderText('할 일을 입력해 주세요');
   });
 
-  expect(input.value).toBe('자바스크립트 공부하기');
-});
+  it('change 이벤트가 발생하면 value의 값이 바뀐다.', () => {
+    const { getByPlaceholderText } = render(<Input />);
+    const input = getByPlaceholderText('할 일을 입력해 주세요');
 
-test('onClick 이벤트가 발생하면 할 일이 추가되고, value는 비어진다.', () => {
-  const handleClickAddTask = jest.fn();
-  const utils = render(<Input onClick={handleClickAddTask} />);
-
-  const { getByPlaceholderText, getByText } = utils;
-
-  const input = getByPlaceholderText('할 일을 입력해 주세요');
-  const button = getByText('추가');
-
-  // value를 바꾼다.
-  fireEvent.change(input, {
-    target: {
-      value: '자바스크립트 공부하기',
-    },
+    fireEvent.change(input, {
+      target: {
+        value: '자바스크립트 공부하기',
+      },
+    });
+    expect(input.value).toBe('자바스크립트 공부하기');
   });
-  // '추가' 버튼을 클릭한다.
-  fireEvent.click(button);
-  expect(handleClickAddTask).toBeCalled();
-  expect(utils.container).toHaveTextContent('자바스크립트 공부하기');
-  expect(input.value).toBe('');
+
+  it('추가 버튼을 누르면 handleClickAddTask 이벤트가 호출된다.', () => {
+    const handleClickAddTask = jest.fn();
+    const { getByText } = render((
+      <Input onClick={handleClickAddTask} />
+    ));
+
+    expect(handleClickAddTask).not.toBeCalled();
+    fireEvent.click(getByText('추가'));
+    expect(handleClickAddTask).toBeCalled();
+  });
 });
