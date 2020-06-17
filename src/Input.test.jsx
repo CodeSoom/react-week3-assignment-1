93fca3e6002abdeaddbd068faeb0a5a0d8313fe3
Kwakcena <rhkrgudwh@gmail.com>
2020-06-17 18:34:26 +0900

    Input test) 추가 버튼이 동작하지 않음.
    value를 바꾸고 추가 버튼을 눌렀을 때 '자바스크립트 공부하기' 텍스트가
    포함되어있는지, 그리고 value는 비어지는지 테스트를 진행함
    
    하지만 추가 버튼이 눌리는 이벤트가 동작하지 않음.

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -25,3 +25,25 @@ test('change 이벤트가 발생하면 value의 값이 바뀐다.', () => {
 
   expect(input.value).toBe('자바스크립트 공부하기');
 });
+
+test('onClick 이벤트가 발생하면 할 일이 추가되고, value는 비어진다.', () => {
+  const handleClickAddTask = jest.fn();
+  const utils = render(<Input onClick={handleClickAddTask} />);
+
+  const { getByPlaceholderText, getByText } = utils;
+
+  const input = getByPlaceholderText('할 일을 입력해 주세요');
+  const button = getByText('추가');
+
+  // value를 바꾼다.
+  fireEvent.change(input, {
+    target: {
+      value: '자바스크립트 공부하기',
+    },
+  });
+  // '추가' 버튼을 클릭한다.
+  fireEvent.click(button);
+  expect(handleClickAddTask).toBeCalled();
+  expect(utils.container).toHaveTextContent('자바스크립트 공부하기');
+  expect(input.value).toBe('');
+});
