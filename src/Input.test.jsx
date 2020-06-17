eea326de939ac6b6a3d6a3d8d585e7602a92b830
Kwakcena <rhkrgudwh@gmail.com>
2020-06-17 18:03:57 +0900

    Input test) change event test
    Change 이벤트가 발생하면 Input의 value가 바뀌어야 한다.
    
    - 이벤트를 발생시키기 위해서는 @testing-library/react 에서 fireEvent를
      import 해야 한다.
    
    - fireEvent.change 의 사용법은 아래의 공식 문서 예제를 참고했다.
    see also : https://testing-library.com/docs/example-input-event

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import Input from './Input';
 
 test('input과 button으로 이루어져 있는지 확인한다.', () => {
@@ -9,4 +9,19 @@ test('input과 button으로 이루어져 있는지 확인한다.', () => {
   getByText('할 일');
   getByText('추가');
   getByPlaceholderText('할 일을 입력해 주세요');
+});
+
+test('change 이벤트가 발생하면 value의 값이 바뀐다.', () => {
+  const utils = render(<Input />);
+  const { getByPlaceholderText } = utils;
+
+  const input = getByPlaceholderText('할 일을 입력해 주세요');
+
+  fireEvent.change(input, {
+    target: {
+      value: '자바스크립트 공부하기',
+    },
+  });
+
+  expect(input.value).toBe('자바스크립트 공부하기');
 });
