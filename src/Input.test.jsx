5f270b6d9af16920075ba7a68c778f140d116230
daadaadaah <daadaadaah@gmail.com>
2020-06-18 03:33:37 +0900

    Update value

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -10,36 +10,31 @@ describe('<Input />', () => {
 
   context('사용자가 할 일을 입력하면', () => {
     it('input 값이 해당 글자로 값이 바뀐다.', () => {
-      const container = render(
-        <Input
-          onChange={handleOnChangeTitle}
-          onClick={handleOnClickAddTask}
-        />,
+      const { getByLabelText } = render(
+        <Input onChange={handleOnChangeTitle} onClick={handleOnClickAddTask} />,
       );
 
       expect(handleOnChangeTitle).not.toBeCalled();
-      fireEvent.change(container.getByLabelText('할 일'), {
+      fireEvent.change(getByLabelText('할 일'), {
         target: {
           value: '바뀐다',
         },
       });
       expect(handleOnChangeTitle).toBeCalledTimes(1);
-      expect(container.getByLabelText('할 일').value).toBe('바뀐다');
+      expect(getByLabelText('할 일').value).toBe('바뀐다');
     });
   });
   context('사용자가 할 일을 입력한 후 추가를 누르면', () => {
     it('input의 텍스트가 지워진다.', () => {
-      const taskTitle = 'a';
-
       const container = render(
         <Input
-          value={taskTitle}
+          value="추가될 할일"
           onChange={handleOnChangeTitle}
           onClick={handleOnClickAddTask}
         />,
       );
 
-      expect(container.getByLabelText('할 일').value).toBe('a');
+      expect(container.getByLabelText('할 일').value).toBe('추가될 할일');
 
       expect(handleOnClickAddTask).not.toBeCalled();
       fireEvent.click(container.getByText('추가'));
