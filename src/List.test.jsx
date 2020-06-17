e43522d7b951cef2918bb66f2393eb15421132c6
Kwakcena <rhkrgudwh@gmail.com>
2020-06-18 02:08:52 +0900

    render 의 debug를 사용하면 해당 DOM node를 확인.
    완료 버튼이 여러개 있는걸 테스트 하려고 하니 getByText를 쓰지 말라고
    한다.
    
    공식 문서에 따르면 쿼리에 대한 첫 번째 일치 노드를 반환하고 일치하는
    요소가 없거나 일치하는 요소가 두 개 이상이면 오류를 발생시킨다고 한다.
    
    대신 getAllBy* 를 사용하라고 한다.
    
    그럼 getAllByText('완료') 라고 한줄만 쓰면 테스트를 수행했다고 생각할 수
    있는 것일까?
    잘 모르겠어서 검색으로 찾아보다가 debug를 알게 되었다. getAllByText를
    수행하면 동일한 text를 가진 node가 배열로 들어오는데, 이를 forEach를
    돌려서 node에 대해 debug 인자로 전달하니 콘솔에 해당 DOM element 가
    나타나는 것을 볼 수 있었다.

---
+++ src/List.test.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import List from './List';
+
+describe('<List /> ', () => {
+  it('tasks 가 없으면 할 일이 없어요! 를 화면에 보여준다.', () => {
+    const tasks = [];
+    const { container } = render(<List tasks={tasks} />);
+    expect(container).toHaveTextContent(/^할 일이 없어요!$/);
+  });
+
+  it('tasks 가 있으면 해당 tasks를 화면에 보여준다.', () => {
+    const tasks = [
+      { id: 1, title: '자바스크립트 공부' },
+      { id: 2, title: 'git 공부' },
+      { id: 3, title: '운동하기' },
+    ];
+
+    const { container, getAllByText, debug } = render(<List tasks={tasks} />);
+    getAllByText('완료').forEach((node) => debug(node));
+    expect(container).toHaveTextContent('자바스크립트 공부');
+    expect(container).toHaveTextContent('git 공부');
+    expect(container).toHaveTextContent('운동하기');
+  });
+});
