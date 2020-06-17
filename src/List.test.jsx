94b270debb91d82ffe35493312953ea19aaf2ed0
Law Woo <law@cupist.com>
2020-06-18 02:10:47 +0900

    Add test code about List component

---
+++ src/List.test.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import List from './List';
+
+describe('List', () => {
+  it('No todo', () => {
+    const todos = [];
+
+    const { container } = render((
+      <List
+        tasks={todos}
+      />
+    ));
+
+    expect(container).toHaveTextContent('할 일이 없어요!');
+  });
+
+  it('Have any todos', () => {
+    const todos = [
+      {
+        id: 1,
+        title: 'Distribute new version',
+      }, {
+        id: 2,
+        title: 'Fix critical error',
+      },
+    ];
+
+    const { container } = render((
+      <List
+        tasks={todos}
+      />
+    ));
+
+    expect(container).toHaveTextContent('Distribute new version');
+    expect(container).toHaveTextContent('Fix critical error');
+  });
+});
