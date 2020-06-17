187b8f5e740a2679444724fb5457ac0256920d62
Paul An <anpaul0615@gmail.com>
2020-06-18 00:04:42 +0900

    Input 컴포넌트 테스트 수정
    - 재사용하지않는 taskTitle 상수 삭제
    - 컴포넌트의 하위 엘리먼트를 조회한다음, 각 하위 엘리먼트가 컴포넌트에 포함되어있는지 재확인하는 테스트 삭제
    - 입력박스이벤트, 버튼클릭이벤트 동작 확인 테스트순서 조정
    - 입력박스이벤트, 버튼클릭이벤트 동작 이후 컴포넌트 props 변화여부 확인하는 테스트 삭제

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -5,38 +5,26 @@ import { render, fireEvent } from '@testing-library/react';
 import Input from './Input';
 
 test('Input', () => {
-  const taskTitle = '';
-
   const handleChangeInput = jest.fn();
-
   const handleClickButton = jest.fn();
 
-  const {
-    container, getByLabelText, getByPlaceholderText, getByText,
-  } = render((
+  const { getByLabelText, getByPlaceholderText, getByText } = render((
     <Input
-      value={taskTitle}
+      value=""
       onChange={handleChangeInput}
       onClick={handleClickButton}
     />
   ));
 
-  const label = getByLabelText('할 일');
+  getByLabelText('할 일');
+
   const input = getByPlaceholderText('할 일을 입력해 주세요');
-  const button = getByText('추가');
-
-  expect(container).toContainElement(label);
-  expect(container).toContainElement(input);
-  expect(container).toContainElement(button);
-
   expect(handleChangeInput).not.toBeCalled();
-  expect(handleClickButton).not.toBeCalled();
-
   fireEvent.change(input, { target: { value: '뭐라도 하기' } });
   expect(handleChangeInput).toBeCalled();
-  expect(input).toHaveTextContent('뭐라도 하기');
 
+  const button = getByText('추가');
+  expect(handleClickButton).not.toBeCalled();
   fireEvent.click(button);
   expect(handleClickButton).toBeCalled();
-  expect(input).toHaveTextContent('');
 });
