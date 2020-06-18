80b056baa67c58ffe5d1721b3a539ec10fa71aff
sxhyxnchxng <sxhyxnchxng@gmail.com>
2020-06-19 06:55:25 +0900

    feat: add Input test

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from 'react';
 
-import {render, fireEvent} from '@testing-library/react'
+import { render, fireEvent, screen } from '@testing-library/react';
 
-import Input from './Input'
+import Input from './Input';
 
-test('', () => {})
+test('input에 텍스트가 입력된다.', () => {
+  const inputText = 'hello';
+
+  const mockedOnChange = jest.fn();
+  const mockedOnClick = jest.fn();
+
+  render(<Input value={undefined} onChange={mockedOnChange} onClick={mockedOnClick} />);
+  fireEvent.change(screen.getByPlaceholderText(/할 일을 입력해 주세요/), { target: { value: inputText } });
+  fireEvent.click(screen.getByText(/추가/));
+
+  expect(mockedOnChange).toBeCalledTimes(1);
+  expect(mockedOnClick).toBeCalledTimes(1);
+});
