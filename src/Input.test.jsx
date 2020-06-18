86258715cd812d53dca4c3b5162cdfe902b67385
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-06-18 09:23:57 +0900

    tests(Input)
    - 컴포넌트 렌더링 요소 테스트 추가

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -19,13 +19,23 @@ function createInput(value) {
 
 describe('Input', () => {
   context('할 일을 입력하지 않았을 때', () => {
+    const todoTitle = '';
+    const inputPalceHodler = '할 일을 입력해 주세요';
+
     it('제목을 볼 수 있다.', () => {
-      const { container } = createInput('');
+      const { container } = createInput(todoTitle);
 
       expect(container).toHaveTextContent('할 일');
     });
+
+    it('입력창을 볼 수 있다.', () => {
+      const { getByPlaceholderText } = createInput(todoTitle);
+
+      expect(getByPlaceholderText(inputPalceHodler)).toContainHTML('input');
+    });
+
     it('추가 버튼을 볼 수 있다.', () => {
-      const { container } = createInput('');
+      const { container } = createInput(todoTitle);
 
       expect(container).toHaveTextContent('추가');
     });
