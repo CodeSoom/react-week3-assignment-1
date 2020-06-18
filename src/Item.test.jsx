ae473d649c8250d2a977630327ea1caea18fd22a
Kwakcena <rhkrgudwh@gmail.com>
2020-06-18 20:58:01 +0900

    Item test refactoring
    Item 컴포넌트 리팩토링

--- src/Item.test.jsx
+++ src/Item.test.jsx
@@ -4,27 +4,29 @@ import { render, fireEvent } from '@testing-library/react';
 
 import Item from './Item';
 
-test('Item', () => {
-  const task = {
-    id: 1,
-    title: '뭐라도 하기',
-  };
+describe('<Item />', () => {
+  context('아이템이 존재하면', () => {
+    const task = {
+      id: 1,
+      title: '뭐라도 하기',
+    };
 
-  const handleClick = jest.fn();
+    it('해당 task의 title을 render 한다.', () => {
+      const { container } = render(<Item task={task} />);
+      expect(container).toHaveTextContent(task.title);
+    });
 
-  const { container, getByText } = render((
-    <Item
-      task={task}
-      onClickDelete={handleClick}
-    />
-  ));
-
-  expect(container).toHaveTextContent('뭐라도 하기');
-  expect(container).toHaveTextContent('완료');
-
-  expect(handleClick).not.toBeCalled();
-
-  fireEvent.click(getByText('완료'));
-
-  expect(handleClick).toBeCalledWith(1);
+    it('완료 버튼을 눌러 handleClic에 id를 인자로 전달한다.', () => {
+      const handleClick = jest.fn();
+      const { getByText } = render((
+        <Item
+          task={task}
+          onClickDelete={handleClick}
+        />
+      ));
+      expect(handleClick).not.toBeCalled();
+      fireEvent.click(getByText('완료'));
+      expect(handleClick).toBeCalledWith(1);
+    });
+  });
 });
