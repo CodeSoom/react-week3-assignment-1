9dc892a6b4390274b2dcb35a97005fdb96c61fa2
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-06-18 09:11:32 +0900

    test(Input)
    - 할 일을 입력하지 않은 경우 렌더링에 대한 테스트 추가

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -4,10 +4,30 @@ import { render } from '@testing-library/react';
 
 import Input from './Input';
 
-test('Input', () => {
-  const { container } = render((
-    <Input />
-  ));
+const onChange = jest.fn();
+const onClick = jest.fn();
 
-  expect(container).toHaveTextContent('할 일');
+function createInput(value) {
+  return render((
+    <Input
+      value={value}
+      onChange={onChange}
+      onClick={onClick}
+    />
+  ));
+}
+
+describe('Input', () => {
+  context('할 일을 입력하지 않았을 때', () => {
+    it('제목을 볼 수 있다.', () => {
+      const { container } = createInput('');
+
+      expect(container).toHaveTextContent('할 일');
+    });
+    it('추가 버튼을 볼 수 있다.', () => {
+      const { container } = createInput('');
+
+      expect(container).toHaveTextContent('추가');
+    });
+  });
 });
