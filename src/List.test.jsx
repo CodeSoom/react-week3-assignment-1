81b3dcdc57a10b69a07e54f6ffdcd7c5a97b96ff
Kwakcena <rhkrgudwh@gmail.com>
2020-06-18 20:48:40 +0900

    List test refactoring
    List 컴포넌트 테스트 리팩토링

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -5,23 +5,27 @@ import { render } from '@testing-library/react';
 import List from './List';
 
 describe('<List /> ', () => {
-  it('tasks 가 없으면 할 일이 없어요! 를 화면에 보여준다.', () => {
-    const tasks = [];
-    const { container } = render(<List tasks={tasks} />);
-    expect(container).toHaveTextContent(/^할 일이 없어요!$/);
+  context('tasks가 없으면', () => {
+    it('할 일이 없어요! 를 화면에 보여준다.', () => {
+      const tasks = [];
+      const { container } = render(<List tasks={tasks} />);
+      expect(container).toHaveTextContent(/^할 일이 없어요!$/);
+    });
   });
 
-  it('tasks 가 있으면 해당 tasks를 화면에 보여준다.', () => {
-    const tasks = [
-      { id: 1, title: '자바스크립트 공부' },
-      { id: 2, title: 'git 공부' },
-      { id: 3, title: '운동하기' },
-    ];
+  context('tasks가 있으면', () => {
+    it('tasks 가 있으면 해당 tasks를 화면에 보여준다.', () => {
+      const tasks = [
+        { id: 1, title: '자바스크립트 공부' },
+        { id: 2, title: 'git 공부' },
+        { id: 3, title: '운동하기' },
+      ];
 
-    const { container, getAllByText } = render(<List tasks={tasks} />);
-    getAllByText('완료');
-    expect(container).toHaveTextContent('자바스크립트 공부');
-    expect(container).toHaveTextContent('git 공부');
-    expect(container).toHaveTextContent('운동하기');
+      const { container, getAllByText } = render(<List tasks={tasks} />);
+      getAllByText('완료').forEach((button) => expect(button).toHaveAttribute('type', 'button'));
+      expect(container).toHaveTextContent(tasks[0].title);
+      expect(container).toHaveTextContent(tasks[1].title);
+      expect(container).toHaveTextContent(tasks[2].title);
+    });
   });
 });
