bea8c71a81b304b48aa24cec6c7aa8ab834d18fe
Kwakcena <rhkrgudwh@gmail.com>
2020-06-18 20:35:11 +0900

    Page test) 표현력 명확히 하기
    context와 expect를 추가하여 표현력을 명확히 한다.

--- src/Page.test.jsx
+++ src/Page.test.jsx
@@ -5,20 +5,23 @@ import { render } from '@testing-library/react';
 import Page from './Page';
 
 describe('<Page /> ', () => {
-  it('할 일 목록이 없을 때 render page', () => {
-    const tasks = [];
-    const { getByText } = render(<Page tasks={tasks} />);
-    getByText('할 일');
-    getByText('할 일이 없어요!');
+  context('할 일 목록이 없으면', () => {
+    it('할 일이 없음을 보여준다.', () => {
+      const tasks = [];
+      const { container } = render(<Page tasks={tasks} />);
+      expect(container).toHaveTextContent('할 일이 없어요!');
+    });
   });
 
-  it('할 일 목록이 있을 때 render page', () => {
-    const tasks = [
-      { id: 1, title: '뭐라도 하자' },
-    ];
+  context('할 일 목록이 있다면 ', () => {
+    it('목록을 보여준다.', () => {
+      const tasks = [
+        { id: 1, title: '뭐라도 하자' },
+      ];
 
-    const { getByText, getByTestId } = render(<Page tasks={tasks} />);
-    getByText('완료');
-    expect(getByTestId('todo-list')).toHaveTextContent('뭐라도 하자');
+      const { getByText, getByTestId } = render(<Page tasks={tasks} />);
+      expect(getByText('완료')).toHaveAttribute('type', 'button');
+      expect(getByTestId('todo-list')).toHaveTextContent('뭐라도 하자');
+    });
   });
 });
