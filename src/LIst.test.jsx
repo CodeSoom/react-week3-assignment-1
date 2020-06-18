cd7ee33b50488c895e21b9f17cc8753a4c710b07
sxhyxnchxng <sxhyxnchxng@gmail.com>
2020-06-19 07:00:25 +0900

    feat: add List test

--- src/LIst.test.jsx
+++ src/LIst.test.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from 'react';
 
-import {render, fireEvent} from '@testing-library/react'
+import { render, screen } from '@testing-library/react';
 
-import List from './List'
+import List from './List';
 
-test('', () => {})
+test('todo item이 없으면 할 일이 없어요! 를 보여준다.', () => {
+  const { getByText } = render(<List tasks={[]} />);
+  getByText('할 일이 없어요!');
+});
+
+test('tasks props로 task 데이터를 넘기면 리스트로 보여준다.', () => {
+  const todos = [
+    { id: 1, title: 'hello' },
+    { id: 2, title: 'world' },
+  ];
+
+  render(<List tasks={todos} />);
+  screen.getByText('hello');
+  screen.getByText('world');
+});
