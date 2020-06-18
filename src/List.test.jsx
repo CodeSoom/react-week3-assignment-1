3acd7741954f228d9617980c5a3c11a0b747f3ba
daadaadaah <daadaadaah@gmail.com>
2020-06-19 01:22:15 +0900

    Update List.test.jsx

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -5,14 +5,14 @@ import { render, fireEvent } from '@testing-library/react';
 import List from './List';
 
 describe('<List />', () => {
-  const handleOnClickDeleteTask = jest.fn();
+  const handleClickDeleteTask = jest.fn();
 
   context('할 일이 없다면', () => {
     const tasks = [];
 
     it(' "할 일이 없어요!" 문구가 보인다.', () => {
       const { container } = render(
-        <List tasks={tasks} onClickDelete={handleOnClickDeleteTask} />,
+        <List tasks={tasks} onClickDelete={handleClickDeleteTask} />,
       );
       expect(container).toHaveTextContent('할 일이 없어요!');
     });
@@ -29,32 +29,39 @@ describe('<List />', () => {
         title: '할 일2',
       },
     ];
-    it(' 추가된 할 일들이 보인다.', () => {
+    it('추가된 할 일과 완료버튼이 보인다.', () => {
       const { container } = render(
-        <List tasks={tasks} onClickDelete={handleOnClickDeleteTask} />,
+        <List tasks={tasks} onClickDelete={handleClickDeleteTask} />,
       );
       expect(container).toHaveTextContent('할 일1');
       expect(container).toHaveTextContent('할 일2');
+
+      expect(container).toHaveTextContent('완료');
     });
-  });
 
-  context('할 일에서 완료 버튼을 클릭하면', () => {
-    const tasks = [
-      {
-        id: 1,
-        title: '할 일1',
-      },
-    ];
-    it('onClickDelete이 실행된다.', () => {
-      const { getByText } = render(
-        <List tasks={tasks} onClickDelete={handleOnClickDeleteTask} />,
-      );
+    context('할 일에서 완료 버튼을 클릭하면', () => {
+      const tasks2 = [
+        {
+          id: 1,
+          title: '할 일1',
+        },
+      ];
+      it('할 일이 보이지 않는다.', () => {
+        const { container, getByText } = render(
+          <List tasks={tasks2} onClickDelete={handleClickDeleteTask} />,
+        );
+        
+        expect(container).toHaveTextContent('할 일1');
+        
+        expect(handleClickDeleteTask).not.toBeCalled();
+        
+        fireEvent.click(getByText('완료'));
 
-      expect(handleOnClickDeleteTask).not.toBeCalled();
+        expect(handleClickDeleteTask).toBeCalledWith(1);
+        
+        expect(container).not.toHaveTextContent('할 일1');
 
-      fireEvent.click(getByText('완료'));
-
-      expect(handleOnClickDeleteTask).toBeCalledWith(1);
+      });
     });
   });
 });
