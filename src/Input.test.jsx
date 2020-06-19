4601d72e07065e2dd64df4c1f1a54fad74037c3a
Hwang <iamhwang9@gmail.com>
2020-06-19 18:09:59 +0900

    TDD Test

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -7,20 +7,16 @@ import Input from './Input';
 describe('<Input /> ', () => {
   context('테스트가 시작하면', () => {
     it('input과 button으로 이루어져 있는지 확인한다.', () => {
-
-      const { container,getByPlaceholderText } = render((
+      const { container, getByPlaceholderText } = render((
         <Input />
       ));
-
       expect(getByPlaceholderText('할 일을 입력해 주세요'));
       expect(container).toHaveTextContent('추가');
-
     });
   });
 
   context('input에 값을 입력하면', () => {
     it('onchange가 호출된다', () => {
-
       const value = 'input value test';
 
       const handleChange = jest.fn();
@@ -31,6 +27,8 @@ describe('<Input /> ', () => {
           onChange={handleChange}
         />
       ));
+
+      expect(container).toHaveTextContent('추가');
 
       // onChage 검증 아이디어가 필요합니다...
     });
@@ -47,8 +45,6 @@ describe('<Input /> ', () => {
       expect(handleClick).not.toBeCalled();
       fireEvent.click(getByText('추가'));
       expect(handleClick).toBeCalled();
-
     });
   });
-
 });
--- src/List.test.jsx
+++ src/List.test.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import List from './List';
 
 describe('<List /> ', () => {
   context('할 일에 데이터가 없으면', () => {
     it('할 일이 없어요', () => {
-
       const tasks = [];
 
       const { container } = render((
@@ -13,23 +12,21 @@ describe('<List /> ', () => {
       ));
 
       expect(container).toHaveTextContent('할 일이 없어요');
-    
     });
   });
 
   context('할 일에 데이터가 있으면', () => {
     it('task 표출', () => {
-      
       const tasks = [
-        {id:1, title:'component'},
-        {id:2, title:'list'},
-        {id:3, title:'test'},
+        { id: 1, title: 'component' },
+        { id: 2, title: 'list' },
+        { id: 3, title: 'test' },
       ];
-    
+
       const { container } = render((
         <List tasks={tasks} />
       ));
-    
+
       tasks.map((task) => (
         expect(container).toHaveTextContent(task.title)
       ));
