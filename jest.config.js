3f10a22d4a186a571da21cfed1f0ee18a4ede1c2
YONGJAE LEE <yongjae.lee@nanocellect.com>
2020-06-18 17:33:58 -0700

    fix: revise mistake when merge

--- jest.config.js
+++ jest.config.js
@@ -5,10 +5,10 @@ module.exports = {
   ],
   coverageThreshold: {
     global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80,
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
     },
     './src/App.jsx': {
     },
