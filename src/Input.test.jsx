ee04ab94f93126cfa811308d78c784d17587f655
sxhyxnchxng <sxhyxnchxng@gmail.com>
2020-06-20 20:31:01 +0900

    fix: fix Input.test.jsx
    - edit test title
    - remove `mock` from `onChange`, `onClick`
    - remove `screen`

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 
-import { render, fireEvent, screen } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import Input from './Input';
 
-test('input에 텍스트가 입력된다.', () => {
+test('Test Input component', () => {
   const inputText = 'hello';
+  const onChange = jest.fn();
+  const onClick = jest.fn();
 
-  const mockedOnChange = jest.fn();
-  const mockedOnClick = jest.fn();
+  const { getByText, getByPlaceholderText } = render(
+    <Input value="" onChange={onChange} onClick={onClick} />,
+  );
 
-  render(<Input value={undefined} onChange={mockedOnChange} onClick={mockedOnClick} />);
-  fireEvent.change(screen.getByPlaceholderText(/할 일을 입력해 주세요/), { target: { value: inputText } });
-  fireEvent.click(screen.getByText(/추가/));
+  fireEvent.change(getByPlaceholderText(/할 일을 입력해 주세요/), { target: { value: inputText } });
+  fireEvent.click(getByText(/추가/));
 
-  expect(mockedOnChange).toBeCalledTimes(1);
-  expect(mockedOnClick).toBeCalledTimes(1);
+  expect(onChange).toBeCalledTimes(1);
+  expect(onClick).toBeCalledTimes(1);
 });
