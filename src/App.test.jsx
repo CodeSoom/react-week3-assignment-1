71f990da35e3b66e456fa23bd7b10259587f40f3
sogoagain <imyong0@gmail.com>
2020-06-20 19:32:51 +0900

    Refactor - App 컴포넌트 테스트를 개선한다

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -4,44 +4,50 @@ import { render, fireEvent, screen } from '@testing-library/react';
 
 import App from './App';
 
-function setup() {
+function renderApp() {
   render(<App />);
-  const changeInput = (text) => fireEvent.change(screen.getByLabelText(/할 일/i, { selector: 'input' }),
-    { target: { value: text } });
-  const clickAddButton = () => fireEvent.click(screen.getByRole('button', { name: /추가/i }));
-  const clickDoneButton = () => fireEvent.click(screen.getAllByRole('button', { name: /완료/i })[0]);
-  return { changeInput, clickAddButton, clickDoneButton };
+  return {
+    taskInput: screen.getByLabelText(/할 일/i, { selector: 'input' }),
+    taskAddButton: screen.getByRole('button', { name: /추가/i }),
+    getTaskDoneButtons: () => screen.queryAllByRole('button', { name: /완료/i }),
+  };
 }
 
 test('아무런 To-do가 등록되어 있지 않으면 "할 일이 없어요!"라는 메시지가 보인다', () => {
-  setup();
-  expect(screen.getByText('할 일이 없어요!')).toBeInTheDocument();
+  // when
+  renderApp();
+  // then
+  expect(screen.getByText(/할 일이 없어요!/i)).toBeInTheDocument();
 });
 
 test('할 일을 입력하고 추가를 누르면 할 일 목록에 추가한 목록이 보인다', () => {
-  const { changeInput, clickAddButton } = setup();
-  changeInput('아무것도 하지 않기');
-  clickAddButton();
+  // when
+  const { taskInput, taskAddButton } = renderApp();
+  fireEvent.change(taskInput, { target: { value: '아무것도 하지 않기' } });
+  fireEvent.click(taskAddButton);
+  // then
   expect(screen.getByText('아무것도 하지 않기')).toBeInTheDocument();
 });
 
 test('할 일을 입력하고 추가를 누르면 input의 텍스트가 지워진다', () => {
-  const { changeInput, clickAddButton } = setup();
-  changeInput('아무것도 하지 않기');
-  clickAddButton();
-  const inputElement = screen.getByLabelText(/할 일/i, { selector: 'input' });
-  expect(inputElement.value).toBe('');
+  // when
+  const { taskInput, taskAddButton } = renderApp();
+  fireEvent.change(taskInput, { target: { value: '아무것도 하지 않기' } });
+  fireEvent.click(taskAddButton);
+  // then
+  expect(taskInput.value).toBe('');
 });
 
 test('할 일을 완료하면 할 일이 목록에서 보이지 않는다', () => {
+  // given
   const tasks = ['코드숨 과제하기', '아무것도 하지 않기'];
-  const { changeInput, clickAddButton, clickDoneButton } = setup();
-
+  // when
+  const { taskInput, taskAddButton, getTaskDoneButtons } = renderApp();
   tasks.forEach((task) => {
-    changeInput(task);
-    clickAddButton();
+    fireEvent.change(taskInput, { target: { value: task } });
+    fireEvent.click(taskAddButton);
   });
-
-  tasks.forEach(() => clickDoneButton());
+  // then
+  tasks.forEach(() => fireEvent.click(getTaskDoneButtons()[0]));
   tasks.forEach((task) => expect(screen.queryByText(task)).not.toBeInTheDocument());
 });
