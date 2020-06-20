caf29357c91d0b285c730868782fc1ab23d60c00
sogoagain <imyong0@gmail.com>
2020-06-20 17:55:35 +0900

    Refactor - Input 테스트 코드를 개선한다

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -4,58 +4,75 @@ import { render, fireEvent, screen } from '@testing-library/react';
 
 import Input from './Input';
 
-function setup(value, handleChange, handleClick) {
+function renderInput({ value, onChange, onClick }) {
   render(<Input
     value={value}
-    onChange={handleChange}
-    onClick={handleClick}
+    onChange={onChange}
+    onClick={onClick}
   />);
-  const changeTaskInput = (text) => fireEvent.change(screen.getByLabelText(/할 일/i, { selector: 'input' }),
-    { target: { value: text } });
-  const clickAddButton = () => fireEvent.click(screen.getByRole('button', { name: /추가/i }));
-  return { changeTaskInput, clickAddButton };
+
+  const taskTitleInput = screen.getByLabelText(/할 일/i, { selector: 'input' });
+  const addButton = screen.getByRole('button', { name: /추가/i });
+
+  return {
+    taskTitleInput,
+    changeTaskInput: (text) => fireEvent.change(taskTitleInput, { target: { value: text } }),
+    clickAddButton: () => fireEvent.click(addButton),
+  };
 }
 
-describe('Input Component는', () => {
-  const mockHandleChange = jest.fn();
-  const mockHandleClick = jest.fn();
-
-  beforeEach(() => {
-    mockHandleChange.mockClear();
-    mockHandleClick.mockClear();
+describe('<Input />', () => {
+  context('without input value', () => {
+    it('print guide message', () => {
+      // given
+      const value = '';
+      // when
+      const { taskTitleInput } = renderInput({ value, onChange: jest.fn(), onClick: jest.fn() });
+      // then
+      expect(taskTitleInput.placeholder).toBe('할 일을 입력해 주세요');
+    });
   });
 
-  describe('value 값이', () => {
-    describe('비어 있다면', () => {
-      test('입력 안내 메시지를 출력한다', () => {
-        const placeholderText = '할 일을 입력해 주세요';
-        setup('', mockHandleChange, mockHandleClick);
-        expect(screen.getByPlaceholderText(placeholderText)).toBeInTheDocument();
+  context('with input value', () => {
+    it('print input value', () => {
+      // given
+      const value = '어제보다 열심히 하기';
+      // when
+      const { taskTitleInput } = renderInput({ value, onChange: jest.fn(), onClick: jest.fn() });
+      // then
+      expect(taskTitleInput.value).toBe(value);
+    });
+  });
+
+  context('when clicked button', () => {
+    it('notify that it has been clicked', () => {
+      // given
+      const handleClick = jest.fn();
+      // when
+      const { clickAddButton } = renderInput({
+        value: '오늘 할 일',
+        onChange: jest.fn(),
+        onClick: handleClick,
       });
-    });
-
-    describe('비어 있지 않다면', () => {
-      test('value 값을 출력한다.', () => {
-        const value = '어제보다 열심히 하기';
-        setup(value, mockHandleChange, mockHandleClick);
-        expect(screen.getByDisplayValue(value)).toBeInTheDocument();
-      });
-    });
-  });
-
-  describe('텍스트를 입력하면', () => {
-    test('onChange를 실행한다', () => {
-      const { changeTaskInput } = setup('어제보다 열심히 하기', mockHandleChange, mockHandleClick);
-      changeTaskInput('새롭게 해야할 일');
-      expect(mockHandleChange).toBeCalledTimes(1);
-    });
-  });
-
-  describe('추가 버튼을 누르면', () => {
-    test('onClick을 실행한다', () => {
-      const { clickAddButton } = setup('어제보다 열심히 하기', mockHandleChange, mockHandleClick);
       clickAddButton();
-      expect(mockHandleClick).toBeCalledTimes(1);
+      // then
+      expect(handleClick).toBeCalledTimes(1);
+    });
+  });
+
+  context('when an input field is entered', () => {
+    it('notify that it has been entered', () => {
+      // given
+      const handleChange = jest.fn();
+      // when
+      const { changeTaskInput } = renderInput({
+        value: '오늘 할 일',
+        onChange: handleChange,
+        onClick: jest.fn(),
+      });
+      changeTaskInput('내일 할 일');
+      // then
+      expect(handleChange).toBeCalledTimes(1);
     });
   });
 });
