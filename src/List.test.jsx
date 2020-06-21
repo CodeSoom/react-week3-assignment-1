6608b09ee35c6ae8737111bf23f8f8a78254694f
Paul An <anpaul0615@gmail.com>
2020-06-21 21:55:21 +0900

    List 컴포넌트 테스트코드 수정
    - 목록표출 테스트케이스에 있는 클릭이벤트 테스트케이스를 별도로 분리
    - 중복되는 컴포넌트 렌더링 코드를 별도로 분리

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -7,41 +7,39 @@ import List from './List';
 
 
 describe('<List />', () => {
-  const handleClickDeleteButton = jest.fn();
-  const tasks = Tasks;
+  const handleClickDeleteTask = jest.fn();
+
+  const renderComponent = (tasks) => render((
+    <List
+      tasks={tasks}
+      onClickDelete={handleClickDeleteTask}
+    />
+  ));
 
   context('empty list', () => {
     it('display empty list', () => {
-      const { container } = render((
-        <List
-          tasks={[]}
-          onClickDelete={null}
-        />
-      ));
+      const { container } = renderComponent([]);
       expect(container).toHaveTextContent('할 일이 없어요!');
     });
   });
 
   context('fully list', () => {
     it('display list', () => {
-      const { container, getAllByRole } = render((
-        <List
-          tasks={tasks}
-          onClickDelete={handleClickDeleteButton}
-        />
-      ));
-
-      tasks.forEach((task) => {
+      const { container } = renderComponent(Tasks);
+      Tasks.forEach((task) => {
         expect(container).toHaveTextContent(task.title);
       });
-
-      const confirmButtons = getAllByRole('button');
-      expect(handleClickDeleteButton).not.toBeCalled();
-      confirmButtons.forEach((button) => fireEvent.click(button));
-      expect(handleClickDeleteButton).toBeCalledTimes(tasks.length);
     });
-  });
 
-  context('click event each list items', () => {
+    it('click event each list items buttons', () => {
+      const { getAllByRole } = renderComponent(Tasks);
+      const confirmButtons = getAllByRole('button');
+
+      expect(handleClickDeleteTask).not.toBeCalled();
+
+      confirmButtons.forEach((button) => fireEvent.click(button));
+
+      expect(handleClickDeleteTask).toBeCalledTimes(Tasks.length);
+    });
   });
 });
