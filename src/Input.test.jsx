abbde91d17033133e75b22186624a097d4f27751
YONGJAE LEE <yongjae.lee@nanocellect.com>
2020-06-21 01:08:31 -0700

    fix: finished Input.test.jsx

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -4,37 +4,55 @@ import { render, fireEvent } from '@testing-library/react';
 import Input from './Input';
 
 describe('Input', () => {
-  it('Input', () => {
-    const handleChange = jest.fn();
-    const handleClick = jest.fn();
+  context('When text changed', () => {
+    it('hanlde change values', () => {
+      const handleChange = jest.fn();
+      const handleClick = jest.fn();
 
-    const { container, getByText, getByPlaceholderText } = render((
-      <Input
-        onChange={handleChange}
-        onClick={handleClick}
-      />
-    ));
+      const { container, getByText, getByPlaceholderText } = render((
+        <Input
+          onChange={handleChange}
+          onClick={handleClick}
+        />
+      ));
 
-    expect(handleChange).not.toBeCalled();
-    expect(handleClick).not.toBeCalled();
+      expect(handleChange).not.toBeCalled();
+      expect(handleClick).not.toBeCalled();
 
-    expect(container).toHaveTextContent('할 일');
-    expect(container).toHaveTextContent('추가');
+      expect(container).toHaveTextContent('할 일');
+      expect(container).toHaveTextContent('추가');
 
-    // [v] TODO: input태그의 placeholder "할 일을 입력해주세요" 테스트
-    const input = getByPlaceholderText('할 일을 입력해 주세요');
-    expect(input).toBeEnabled();
+      const input = getByPlaceholderText('할 일을 입력해 주세요');
+      expect(input).toBeEnabled();
 
-    // [?] TODO: input 태그에 뭔가를 적었을 경우 onChange 호출을 통해 value가 바뀌는지 테스트
-    fireEvent.change(input, { target: { value: 'something' } });
-    expect(input.value).toBe('something');
-    fireEvent.change(input, { target: { value: 'something' } });
-    expect(input.value).not.toBe('else');
-    expect(handleChange).toBeCalled();
+      fireEvent.change(input, { target: { value: 'something' } });
+      expect(input.value).toBe('something');
+      fireEvent.change(input, { target: { value: 'something' } });
+      expect(input.value).not.toBe('else');
+      expect(handleChange).toBeCalled();
 
-    // [ ] TODO: onClick을 클릭했을 경우 value가 사리지는가 테스트
-    // expect(handleClick).toBeCalled();
-    fireEvent.click(getByText('추가'));
-    expect(handleClick).toBeCalled();
+
+      fireEvent.click(getByText('추가'));
+      expect(handleClick).toBeCalled();
+    });
+  });
+  context('When click button', () => {
+    it('click add button', () => {
+      const handleChange = jest.fn();
+      const handleClick = jest.fn();
+
+      const { getByText, getByPlaceholderText } = render((
+        <Input
+          onChange={handleChange}
+          onClick={handleClick}
+        />
+      ));
+
+      const input = getByPlaceholderText('할 일을 입력해 주세요');
+      fireEvent.change(input, { target: { value: 'something' } });
+
+      fireEvent.click(getByText('추가'));
+      expect(document.querySelector('input').getAttribute('value')).not.toEqual('something');
+    });
   });
 });
