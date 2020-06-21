e65130bcc3f83c5bcec90bc1d64036800ea96e69
Paul An <anpaul0615@gmail.com>
2020-06-21 20:36:11 +0900

    List 컴포넌트 테스트코드 개선
    - 테스트 구조를 describe-context-it 으로 변경
    - 명칭기반 엘리먼트 쿼리를 역할기반 쿼리로 변경
    - 하드코딩되어있는 리터럴 삭제

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -4,7 +4,9 @@ import { render, fireEvent } from '@testing-library/react';
 
 import List from './List';
 
-test('List', () => {
+
+describe('<List />', () => {
+  const handleClickDeleteButton = jest.fn();
   const tasks = [
     {
       id: 1,
@@ -19,35 +21,39 @@ test('List', () => {
       title: '코드숨 과제하기',
     },
   ];
-  const handleClickDeleteButton = jest.fn();
 
-  const { container, getAllByText } = render((
-    <List
-      tasks={tasks}
-      onClickDelete={handleClickDeleteButton}
-    />
-  ));
+  context('empty list', () => {
+    it('display empty list', () => {
+      const { container } = render((
+        <List
+          tasks={[]}
+          onClickDelete={null}
+        />
+      ));
+      expect(container).toHaveTextContent('할 일이 없어요!');
+    });
+  });
 
-  expect(container).toHaveTextContent('뭐라도 하기');
-  expect(container).toHaveTextContent('아무것도 하지 않기');
-  expect(container).toHaveTextContent('코드숨 과제하기');
+  context('fully list', () => {
+    it('display list', () => {
+      const { container, getAllByRole } = render((
+        <List
+          tasks={tasks}
+          onClickDelete={handleClickDeleteButton}
+        />
+      ));
 
-  const confirmButtons = getAllByText('완료');
-  expect(handleClickDeleteButton).not.toBeCalled();
-  confirmButtons.forEach((button) => fireEvent.click(button));
-  expect(handleClickDeleteButton).toBeCalledTimes(tasks.length);
-});
+      tasks.forEach((task) => {
+        expect(container).toHaveTextContent(task.title);
+      });
 
+      const confirmButtons = getAllByRole('button');
+      expect(handleClickDeleteButton).not.toBeCalled();
+      confirmButtons.forEach((button) => fireEvent.click(button));
+      expect(handleClickDeleteButton).toBeCalledTimes(tasks.length);
+    });
+  });
 
-test('Empty List', () => {
-  const tasks = [];
-
-  const { container } = render((
-    <List
-      tasks={tasks}
-      onClickDelete={null}
-    />
-  ));
-
-  expect(container).toHaveTextContent('할 일이 없어요!');
+  context('click event each list items', () => {
+  });
 });
