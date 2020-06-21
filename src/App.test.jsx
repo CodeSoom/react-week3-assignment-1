4242018420cfec060bd8732848f1a207f2240053
SeaFalcon <seafalcon323@gmail.com>
2020-06-21 09:30:24 +0900

    complete 100% test coverage
    - 거의 빈 파일이었던 page와 app의 테스트를 작성했습니다.
    - descibe - context - it 표현을 사용했습니다. (제대로 사용한건지는 의문)
    - 컴포넌트가 가진 주요 관심사를 구분하는 부분이 매우 어려웠습니다. (때문에 page의 테스트를 어떻게 작성해야하는지 고민을 많이 했네요)

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,11 +1,86 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import App from './App';
 
-test('App', () => {
-  render((
-    <App />
-  ));
+describe('App', () => {
+  context('로딩 후 아무 작업을 하지 않을 경우', () => {
+    it('초기화면이 표시된다.', () => {
+      const { container, getByLabelText } = render((
+        <App />
+      ));
+
+      expect(container).toHaveTextContent('To-do');
+      expect(container).toHaveTextContent('할 일');
+      expect(container).toHaveTextContent('추가');
+      expect(container).toHaveTextContent('할 일이 없어요!');
+
+      const input = getByLabelText('할 일');
+
+      expect(input).toHaveAttribute('value', '');
+      expect(input).toHaveAttribute('placeholder', '할 일을 입력해 주세요');
+    });
+  });
+
+  context('input에 할 일을 적고 추가 버튼을 누르면', () => {
+    it('할 일이 추가된다.', () => {
+      const { container, getByText, getByLabelText } = render((
+        <App />
+      ));
+
+      expect(container).toHaveTextContent('To-do');
+      expect(container).toHaveTextContent('할 일');
+      expect(container).toHaveTextContent('추가');
+      expect(container).toHaveTextContent('할 일이 없어요!');
+
+      const input = getByLabelText('할 일');
+
+      expect(input).toHaveAttribute('value', '');
+      expect(input).toHaveAttribute('placeholder', '할 일을 입력해 주세요');
+
+      fireEvent.change(input, { target: { value: 'do something' } });
+
+      expect(input).toHaveValue('do something');
+
+      fireEvent.click(getByText('추가'));
+
+      expect(input).toHaveAttribute('value', '');
+
+      expect(container).toHaveTextContent('do something');
+    });
+  });
+
+  context('할 일이 추가된 상태에서 완료 버튼을 누르면', () => {
+    it('할 일이 제거되고 할 일이 없어요! 문구가 뜨게 된다.', () => {
+      const { container, getByText, getByLabelText } = render((
+        <App />
+      ));
+
+      expect(container).toHaveTextContent('To-do');
+      expect(container).toHaveTextContent('할 일');
+      expect(container).toHaveTextContent('추가');
+      expect(container).toHaveTextContent('할 일이 없어요!');
+
+      const input = getByLabelText('할 일');
+
+      expect(input).toHaveAttribute('value', '');
+      expect(input).toHaveAttribute('placeholder', '할 일을 입력해 주세요');
+
+      fireEvent.change(input, { target: { value: 'do something' } });
+
+      expect(input).toHaveValue('do something');
+
+      fireEvent.click(getByText('추가'));
+
+      expect(input).toHaveAttribute('value', '');
+
+      expect(container).toHaveTextContent('do something');
+
+      fireEvent.click(getByText('완료'));
+
+      expect(container).not.toHaveTextContent('do something');
+      expect(container).toHaveTextContent('할 일이 없어요!');
+    });
+  });
 });
--- src/Page.test.jsx
+++ src/Page.test.jsx
@@ -1,15 +1,165 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import Page from './Page';
 
-test('Page', () => {
-  const tasks = [];
+describe('Page', () => {
+  context('할 일 목록이 없을 경우', () => {
+    context('할 일이 없을 경우', () => {
+      it('할 일이 없어요!가 표시되고 input에는 값이 표시되지 않는다.', () => {
+        const taskTitle = '';
+        const handleChangeTitle = jest.fn();
+        const handleClickAddTask = jest.fn();
+        const tasks = [];
+        const handleClickDeleteTask = jest.fn();
 
-  const { container } = render((
-    <Page tasks={tasks} />
-  ));
+        const { container, getByLabelText } = render((
+          <Page
+            taskTitle={taskTitle}
+            onChangeTitle={handleChangeTitle}
+            onClickAddTask={handleClickAddTask}
+            tasks={tasks}
+            onClickDeleteTask={handleClickDeleteTask}
+          />
+        ));
 
-  expect(container).toHaveTextContent('To-do');
+        expect(container).toHaveTextContent('To-do');
+        expect(container).toHaveTextContent('할 일');
+        expect(container).toHaveTextContent('추가');
+        expect(container).toHaveTextContent('할 일이 없어요!');
+
+        const input = getByLabelText('할 일');
+
+        expect(input).toHaveAttribute('placeholder', '할 일을 입력해 주세요');
+        expect(input).toHaveAttribute('value', '');
+      });
+    });
+
+    context('할 일이 있을 경우', () => {
+      it('할 일이 없어요!가 표시되고, input에 값이 표시된다.', () => {
+        const taskTitle = 'do something';
+        const handleChangeTitle = jest.fn();
+        const handleClickAddTask = jest.fn();
+        const tasks = [];
+        const handleClickDeleteTask = jest.fn();
+
+        const { container, getByLabelText } = render((
+          <Page
+            taskTitle={taskTitle}
+            onChangeTitle={handleChangeTitle}
+            onClickAddTask={handleClickAddTask}
+            tasks={tasks}
+            onClickDeleteTask={handleClickDeleteTask}
+          />
+        ));
+
+        expect(container).toHaveTextContent('To-do');
+        expect(container).toHaveTextContent('할 일');
+        expect(container).toHaveTextContent('추가');
+        expect(container).toHaveTextContent('할 일이 없어요!');
+
+        const input = getByLabelText('할 일');
+
+        expect(input).toHaveAttribute('placeholder', '할 일을 입력해 주세요');
+        expect(input).toHaveAttribute('value', 'do something');
+      });
+    });
+  });
+
+  context('할 일 목록이 있을 경우', () => {
+    context('할 일이 없을 경우', () => {
+      it('할 일이 표시되고, input에는 값이 표시되지 않는다. ', () => {
+        const taskTitle = '';
+        const handleChangeTitle = jest.fn();
+        const handleClickAddTask = jest.fn();
+        const tasks = [{ id: 1, title: 'do something' }];
+        const handleClickDeleteTask = jest.fn();
+
+        const { container, getByLabelText } = render((
+          <Page
+            taskTitle={taskTitle}
+            onChangeTitle={handleChangeTitle}
+            onClickAddTask={handleClickAddTask}
+            tasks={tasks}
+            onClickDeleteTask={handleClickDeleteTask}
+          />
+        ));
+
+        expect(container).toHaveTextContent('To-do');
+        expect(container).toHaveTextContent('할 일');
+        expect(container).toHaveTextContent('추가');
+        expect(container).toHaveTextContent('do something');
+        expect(container).toHaveTextContent('완료');
+
+        const input = getByLabelText('할 일');
+
+        expect(input).toHaveAttribute('placeholder', '할 일을 입력해 주세요');
+        expect(input).toHaveAttribute('value', '');
+      });
+    });
+
+    context('할 일이 있을 경우', () => {
+      it('할 일이 표시되고, input에는 값이 표시된다.', () => {
+        const taskTitle = 'do something';
+        const handleChangeTitle = jest.fn();
+        const handleClickAddTask = jest.fn();
+        const tasks = [{ id: 1, title: 'do something' }];
+        const handleClickDeleteTask = jest.fn();
+
+        const { container, getByLabelText } = render((
+          <Page
+            taskTitle={taskTitle}
+            onChangeTitle={handleChangeTitle}
+            onClickAddTask={handleClickAddTask}
+            tasks={tasks}
+            onClickDeleteTask={handleClickDeleteTask}
+          />
+        ));
+
+        expect(container).toHaveTextContent('To-do');
+        expect(container).toHaveTextContent('할 일');
+        expect(container).toHaveTextContent('추가');
+        expect(container).toHaveTextContent('do something');
+        expect(container).toHaveTextContent('완료');
+
+        const input = getByLabelText('할 일');
+
+        expect(input).toHaveAttribute('placeholder', '할 일을 입력해 주세요');
+        expect(input).toHaveAttribute('value', 'do something');
+      });
+    });
+  });
+
+  context('input에 값을 입력하고 추가 버튼을 누르면', () => {
+    it('input에 값이 표시되었다가, 값이 사라진다.', () => {
+      const handleChangeTitle = jest.fn();
+      const handleClickAddTask = jest.fn();
+      const tasks = [];
+      const handleClickDeleteTask = jest.fn();
+
+      const { getByLabelText, getByText } = render((
+        <Page
+          onChangeTitle={handleChangeTitle}
+          onClickAddTask={handleClickAddTask}
+          tasks={tasks}
+          onClickDeleteTask={handleClickDeleteTask}
+        />
+      ));
+
+      const input = getByLabelText('할 일');
+
+      fireEvent.change(input, { target: { value: 'do something' } });
+
+      expect(input).toHaveValue('do something');
+
+      expect(handleClickAddTask).not.toBeCalled();
+
+      fireEvent.click(getByText('추가'));
+
+      expect(handleClickAddTask).toBeCalled();
+
+      expect(input).toHaveAttribute('value', '');
+    });
+  });
 });
