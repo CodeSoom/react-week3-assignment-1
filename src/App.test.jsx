4f98b0a94de331512d623429eb3fbb5d281d0d96
daadaadaah <daadaadaah@gmail.com>
2020-06-21 18:37:15 +0900

    Change test situation from single task to multi tasks

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -4,6 +4,8 @@ import { render, fireEvent } from '@testing-library/react';
 
 import App from './App';
 
+import tasks from './__fixture__/tasks';
+
 describe('<App />', () => {
   context('사용자가 처음 앱을 실행시키면', () => {
     it(' "To-do"라는 문구가 보인다. ', () => {
@@ -52,54 +54,43 @@ describe('<App />', () => {
 
       const inputTodo = getByLabelText('할 일');
 
-      fireEvent.change(inputTodo, { target: { value: '할 일1' } });
+      tasks.forEach((task) => {
+        fireEvent.change(inputTodo, { target: { value: task.title } });
+
+        fireEvent.click(getByText('추가'));
+
+        return [];
+      });
+
+      fireEvent.change(inputTodo, { target: { value: '할 일4' } });
 
       fireEvent.click(getByText('추가'));
 
       expect(getByPlaceholderText('할 일을 입력해 주세요')).toBeInTheDocument();
 
-      expect(container).toHaveTextContent('할 일1');
+      expect(container).toHaveTextContent('할 일4');
 
       expect(container).toHaveTextContent('완료');
     });
   });
 
-  context('사용자가 "할 일1" 할 일에 대한 완료 버튼을 누르면', () => {
-    const tasks = [
-      {
-        id: 1,
-        title: '할 일1',
-      },
-      {
-        id: 2,
-        title: '할 일2',
-      },
-      {
-        id: 3,
-        title: '할 일3',
-      },
-    ];
-
-    it(' "할 일1"이 보이지 않는다.', () => {
-      const { container, getByLabelText, getByText, getAllByText } = render(
+  context('사용자가 "할 일2" 할 일에 대한 완료 버튼을 누르면', () => {
+    it(' "할 일2"이 보이지 않는다.', () => {
+      const { container, getByLabelText, getByText } = render(
         <App />,
       );
 
       const inputTodo = getByLabelText('할 일');
 
-      tasks.forEach((item) => {
-        fireEvent.change(inputTodo, { target: { value: item.title } });
+      tasks.forEach((task) => {
+        fireEvent.change(inputTodo, { target: { value: task.title } });
 
         fireEvent.click(getByText('추가'));
       });
 
-      const doneButtons = getAllByText('완료');
+      fireEvent.click(getByText('할 일2').lastChild);
 
-      doneButtons.forEach((button) => fireEvent.click(button));
-
-      expect(container).not.toHaveTextContent('할 일1');
-
-      expect(container).not.toHaveTextContent('완료');
+      expect(container).not.toHaveTextContent('할 일2');
     });
   });
 });
