913e196641ed693c8a0cf4a350130a0b1b97866d
KiChang Shin <kc7851@gmail.com>
2020-06-21 16:30:13 +0900

    Change BDD style testing
    App.test.js를 describe - context - it 형태로 변경해봤습니다.
    이런 방향이 맞는건지 확인이 필요합니다.

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,87 +1,99 @@
 import React from 'react';
 
-import { render, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 import App from './App';
 
-test('App', () => {
-  const handleChangeTitle = jest.fn();
-  const handleClickAddTask = jest.fn();
-  const handleClickDeleteTask = jest.fn();
+describe('App', () => {
+  context('start application', () => {
+    it('first display', () => {
+      // When
+      const { container } = render(
+        (
+          <App />
+        ),
+      );
 
-  const { container } = render(
-    (
-      <App />
-    ),
-  );
+      // Then
+      expect(container).toHaveTextContent('To-do');
+      expect(container).toHaveTextContent('할 일');
+      expect(container).toHaveTextContent('추가');
+      expect(container).toHaveTextContent('할 일이 없어요!');
+    });
+  });
 
-  expect(handleChangeTitle).not.toBeCalled();
-  expect(handleClickAddTask).not.toBeCalled();
-  expect(handleClickDeleteTask).not.toBeCalled();
+  context('추가 button click', () => {
+    it('add task', () => {
+      // Given
+      const initState = {
+        newId: 100,
+        taskTitle: '',
+        tasks: [],
+      };
+      const { newId, tasks } = initState;
 
-  expect(container).toHaveTextContent('To-do');
-  expect(container).toHaveTextContent('할 일');
-  expect(container).toHaveTextContent('추가');
-  expect(container).toHaveTextContent('할 일이 없어요!');
-});
+      // When
+      const newTask = {
+        id: newId,
+        title: '새로운 할 일',
+      };
 
-test('Add task', () => {
-  const initState = {
-    newId: 100,
-    taskTitle: '',
-    tasks: [],
-  };
+      const newState = {
+        ...initState,
+        newId: newId + 1,
+        taskTitle: '',
+        tasks: [...tasks, newTask],
+      };
 
-  const { newId, tasks } = initState;
+      // Then
+      expect(newState.newId).toBe(101);
+      expect(newState.taskTitle).toBe('');
+      expect(newState.tasks.length).toBe(1);
+      expect(newState.tasks[0]).toEqual(newTask);
+    });
+  });
 
-  const newTask = {
-    id: newId,
-    title: '새로운 할 일',
-  };
+  context('완료 button click', () => {
+    it('delete task', () => {
+      // Given
+      const doneTaskId = 1;
+      const tasks = [{
+        id: 1,
+        title: '첫 번째 할 일',
+      }, {
+        id: 2,
+        title: '두 번째 할 일',
+      }];
 
-  const newState = {
-    ...initState,
-    newId: newId + 1,
-    taskTitle: '',
-    tasks: [...tasks, newTask],
-  };
+      expect(tasks.length).toBe(2);
 
-  expect(newState.newId).toBe(101);
-  expect(newState.taskTitle).toBe('');
-  expect(newState.tasks.length).toBe(1);
-  expect(newState.tasks[0]).toEqual(newTask);
-});
+      // When
+      const existed = tasks.filter((task) => task.id !== doneTaskId);
 
-test('Delete task', () => {
-  const doneTaskId = 1;
-  const tasks = [{
-    id: 1,
-    title: '첫 번째 할 일',
-  }, {
-    id: 2,
-    title: '두 번째 할 일',
-  }];
+      // Then
+      expect(existed.length).toBe(tasks.length - 1);
+      expect(existed[0]).toEqual(tasks[1]);
+    });
+  });
 
-  expect(tasks.length).toBe(2);
+  context('input text', () => {
+    it('change taskTitle', () => {
+      // Given
+      const input = '첫 번째 할 일';
+      const initState = {
+        newId: 100,
+        taskTitle: '',
+        tasks: [],
+      };
 
-  const existed = tasks.filter((task) => task.id !== doneTaskId);
+      // When
+      const newState = {
+        ...initState,
+        taskTitle: input,
+      };
 
-  expect(existed.length).toBe(tasks.length - 1);
-  expect(existed[0]).toEqual(tasks[1]);
-});
-
-test('Input text', () => {
-  const input = '첫 번째 할 일';
-  const initState = {
-    newId: 100,
-    taskTitle: '',
-    tasks: [],
-  };
-
-  const newState = {
-    ...initState,
-    taskTitle: input,
-  };
-
-  expect(newState.taskTitle).toBe(input);
+      // Then
+      expect(newState.taskTitle).toBe(input);
+    });
+  });
 });
