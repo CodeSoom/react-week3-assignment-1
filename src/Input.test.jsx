bc625a69e6c6e123fdefdb8c35539596f64fa948
sxhyxnchxng <sxhyxnchxng@gmail.com>
2020-06-21 13:59:08 +0900

    chore: add describe blocks

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, screen } from '@testing-library/react';
 
 import Input from './Input';
 
@@ -9,13 +9,19 @@ describe('Input 컴포넌트에', () => {
   const onChange = jest.fn();
   const onClick = jest.fn();
 
-  const { getByText, getByPlaceholderText } = render(
-    <Input value="" onChange={onChange} onClick={onClick} />,
-  );
+  beforeEach(() => {
+    render(
+      <Input value="" onChange={onChange} onClick={onClick} />,
+    );
+  });
+
+  test('\'추가\' 버튼이 있다.', () => {
+    screen.getByText(/추가/);
+  });
 
   test('할 일을 입력 후 추가할 수 있다.', () => {
-    fireEvent.change(getByPlaceholderText(/할 일을 입력해 주세요/), { target: { value: inputText } });
-    fireEvent.click(getByText(/추가/));
+    fireEvent.change(screen.getByPlaceholderText(/할 일을 입력해 주세요/), { target: { value: inputText } });
+    fireEvent.click(screen.getByText(/추가/));
 
     expect(onChange).toBeCalledTimes(1);
     expect(onClick).toBeCalledTimes(1);
--- src/List.test.jsx
+++ src/List.test.jsx
@@ -1,31 +1,31 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 import List from './List';
 
 describe('List 컴포넌트에', () => {
   describe('태스크가 없으면', () => {
+    render(<List tasks={[]} />);
+
     test('"할 일이 없어요!" 를 보여준다.', () => {
-      const todos = [];
-
-      const { getByText } = render(<List tasks={todos} />);
-
-      getByText('할 일이 없어요!');
+      screen.getByText('할 일이 없어요!');
     });
   });
 
   describe('태스크가 있으면', () => {
+    const todos = [
+      { id: 1, title: 'hello' },
+      { id: 2, title: 'world' },
+    ];
+
+    beforeEach(() => {
+      render(<List tasks={todos} />);
+    });
+
     test('태스크 리스트를 보여준다.', () => {
-      const todos = [
-        { id: 1, title: 'hello' },
-        { id: 2, title: 'world' },
-      ];
-
-      const { getByText } = render(<List tasks={todos} />);
-
-      getByText('hello');
-      getByText('world');
+      screen.getByText('hello');
+      screen.getByText('world');
     });
   });
 });
