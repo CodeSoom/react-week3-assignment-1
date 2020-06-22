bf447ad1c045f63b87dfe74e1574df9c416dadc7
yeonhwa <yeonhwa6717@gmail.com>
2020-06-22 23:55:35 +0900

    App 컴포넌트 describe-context-it 적용

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -5,52 +5,56 @@ import { render, fireEvent } from '@testing-library/react';
 import App from './App';
 
 describe('<App />', () => {
-  test('Page 컴포넌트 확인', () => {
-    const { getByText, getByPlaceholderText } = render((
-      <App />
-    ));
+  context('랜더링 확인', () => {
+    it('Page 컴포넌트 확인', () => {
+      const { getByText, getByPlaceholderText } = render((
+        <App />
+      ));
 
-    expect(getByText('할 일')).toBeTruthy();
-    expect(getByPlaceholderText('할 일을 입력해 주세요')).toBeTruthy();
-    expect(getByText('추가')).toBeTruthy();
-    expect(getByText('할 일이 없어요!')).toBeTruthy();
+      expect(getByText('할 일')).toBeTruthy();
+      expect(getByPlaceholderText('할 일을 입력해 주세요')).toBeTruthy();
+      expect(getByText('추가')).toBeTruthy();
+      expect(getByText('할 일이 없어요!')).toBeTruthy();
+    });
   });
 
-  test('할 일 입력', () => {
-    const { container, getByText, getByPlaceholderText } = render((
-      <App />
-    ));
+  context('전제 동작 확인', () => {
+    test('할 일 입력', () => {
+      const { container, getByText, getByPlaceholderText } = render((
+        <App />
+      ));
 
-    const input = getByPlaceholderText('할 일을 입력해 주세요');
-    const button = getByText('추가');
+      const input = getByPlaceholderText('할 일을 입력해 주세요');
+      const button = getByText('추가');
 
-    expect(input).toHaveAttribute('value', '');
+      expect(input).toHaveAttribute('value', '');
 
-    fireEvent.change(input, { target: { value: '뭐라도 하기' } });
-    fireEvent.click(button);
+      fireEvent.change(input, { target: { value: '뭐라도 하기' } });
+      fireEvent.click(button);
 
-    expect(input).toHaveAttribute('value', '');
-    expect(container).toHaveTextContent('뭐라도 하기');
-  });
+      expect(input).toHaveAttribute('value', '');
+      expect(container).toHaveTextContent('뭐라도 하기');
+    });
 
-  test('할 일 완료', () => {
-    const { container, getByText, getByPlaceholderText } = render((
-      <App />
-    ));
+    test('할 일 완료', () => {
+      const { container, getByText, getByPlaceholderText } = render((
+        <App />
+      ));
 
-    const input = getByPlaceholderText('할 일을 입력해 주세요');
-    const buttonAdd = getByText('추가');
+      const input = getByPlaceholderText('할 일을 입력해 주세요');
+      const buttonAdd = getByText('추가');
 
-    expect(input).toHaveAttribute('value', '');
+      expect(input).toHaveAttribute('value', '');
 
-    fireEvent.change(input, { target: { value: '뭐라도 하기' } });
-    fireEvent.click(buttonAdd);
+      fireEvent.change(input, { target: { value: '뭐라도 하기' } });
+      fireEvent.click(buttonAdd);
 
-    expect(input).toHaveAttribute('value', '');
-    expect(container).toHaveTextContent('뭐라도 하기');
+      expect(input).toHaveAttribute('value', '');
+      expect(container).toHaveTextContent('뭐라도 하기');
 
-    fireEvent.click(getByText('완료'));
+      fireEvent.click(getByText('완료'));
 
-    expect(container).not.toHaveTextContent('뭐라도 하기');
+      expect(container).not.toHaveTextContent('뭐라도 하기');
+    });
   });
 });
