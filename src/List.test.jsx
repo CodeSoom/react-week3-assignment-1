4a9c8f0571671c9556c654083e2be5a9059de67b
yeonhwa <yeonhwa6717@gmail.com>
2020-06-22 23:39:25 +0900

    List 컴포넌트 describe-context-it 적용

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -5,47 +5,40 @@ import { render, fireEvent } from '@testing-library/react';
 import Lsit from './List';
 
 describe('<List />', () => {
-  test('할 일이 없을 때', () => {
-    const tasks = [];
-    const { container } = render((
+  const onClickDeleteTask = jest.fn();
+  const renderList = (tasks) => (
+    render((
       <Lsit
         tasks={tasks}
+        onClickDelete={onClickDeleteTask}
       />
-    ));
+    ))
+  );
 
-    expect(container).toHaveTextContent('할 일이 없어요!');
+  context('할 일이 없을 때', () => {
+    it('할 일이 없어요 랜더링', () => {
+      const tasks = [];
+      const { container } = renderList(tasks);
+
+      expect(container).toHaveTextContent('할 일이 없어요!');
+    });
   });
 
-  test('할 일이 있을 때', () => {
+  context('할 일이 있을 때', () => {
     const tasks = [{
       id: 1,
       title: '뭐라도 하기',
     }];
-    const { getByText } = render((
-      <Lsit
-        tasks={tasks}
-      />
-    ));
+    it('할 일 보여주기', () => {
+      const { getByText } = renderList(tasks);
+      expect(getByText(tasks[0].title)).toBeInTheDocument();
+    });
 
-    expect(getByText(tasks[0].title)).toBeInTheDocument();
-  });
+    it('onClickDeleteTask 호출', () => {
+      const { getByText } = renderList(tasks);
+      fireEvent.click(getByText('완료'));
 
-  test('onClickDelete 호출', () => {
-    const tasks = [{
-      id: 1,
-      title: '뭐라도 하기',
-    }];
-    const onClickDelete = jest.fn();
-
-    const { getByText } = render((
-      <Lsit
-        tasks={tasks}
-        onClickDelete={onClickDelete}
-      />
-    ));
-
-    fireEvent.click(getByText('완료'));
-
-    expect(onClickDelete).toBeCalledWith(1);
+      expect(onClickDeleteTask).toBeCalledWith(1);
+    });
   });
 });
