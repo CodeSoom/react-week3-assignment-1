3ce1dad07f06b4e0ff061305af7ec1cb8470e292
yeonhwa <yeonhwa6717@gmail.com>
2020-06-22 23:52:52 +0900

    Page 컴포넌트 describe-context-it 적용

--- src/Page.test.jsx
+++ src/Page.test.jsx
@@ -5,14 +5,11 @@ import { render, fireEvent } from '@testing-library/react';
 import Page from './Page';
 
 describe('<Page />', () => {
-  test('To-do 타이틀 활인', () => {
-    const taskTitle = '';
-    const tasks = [];
-    const handleChangeTitle = jest.fn();
-    const handleClickAddTask = jest.fn();
-    const handleClickDeleteTask = jest.fn();
-
-    const { getByText } = render((
+  const handleChangeTitle = jest.fn();
+  const handleClickAddTask = jest.fn();
+  const handleClickDeleteTask = jest.fn();
+  const renderPage = (taskTitle, tasks) => (
+    render((
       <Page
         taskTitle={taskTitle}
         tasks={tasks}
@@ -20,55 +17,42 @@ describe('<Page />', () => {
         onClickAddTask={handleClickAddTask}
         onClickDeleteTask={handleClickDeleteTask}
       />
-    ));
+    ))
+  );
 
-    expect(getByText('To-do')).toBeTruthy();
+  context('랜더링 확인', () => {
+    it('To-do 타이틀 활인', () => {
+      const taskTitle = '';
+      const tasks = [];
+      const { getByText } = renderPage(taskTitle, tasks);
+
+      expect(getByText('To-do')).toBeTruthy();
+    });
+
+    it('Input, List 컴포넌트 확인', () => {
+      const taskTitle = '';
+      const tasks = [];
+      const { getByText, getByPlaceholderText } = renderPage(taskTitle, tasks);
+
+      expect(getByText('할 일')).toBeTruthy();
+      expect(getByPlaceholderText('할 일을 입력해 주세요')).toBeTruthy();
+      expect(getByText('추가')).toBeTruthy();
+      expect(getByText('할 일이 없어요!')).toBeTruthy();
+    });
   });
 
-  test('Input, List 컴포넌트 확인', () => {
-    const taskTitle = '';
-    const tasks = [];
-    const handleChangeTitle = jest.fn();
-    const handleClickAddTask = jest.fn();
-    const handleClickDeleteTask = jest.fn();
-    const { getByText, getByPlaceholderText } = render((
-      <Page
-        taskTitle={taskTitle}
-        tasks={tasks}
-        onChangeTitle={handleChangeTitle}
-        onClickAddTask={handleClickAddTask}
-        onClickDeleteTask={handleClickDeleteTask}
-      />
-    ));
+  context('핸들러 확인', () => {
+    it('onClickDeleteTask 호출', () => {
+      const taskTitle = '';
+      const tasks = [{
+        id: 1,
+        title: '뭐라도 하기',
+      }];
+      const { getByText } = renderPage(taskTitle, tasks);
 
-    expect(getByText('할 일')).toBeTruthy();
-    expect(getByPlaceholderText('할 일을 입력해 주세요')).toBeTruthy();
-    expect(getByText('추가')).toBeTruthy();
-    expect(getByText('할 일이 없어요!')).toBeTruthy();
-  });
-
-  test('onClickDeleteTask 호출', () => {
-    const taskTitle = '';
-    const tasks = [{
-      id: 1,
-      title: '뭐라도 하기',
-    }];
-    const handleChangeTitle = jest.fn();
-    const handleClickAddTask = jest.fn();
-    const handleClickDeleteTask = jest.fn();
-
-    const { getByText } = render((
-      <Page
-        taskTitle={taskTitle}
-        tasks={tasks}
-        onChangeTitle={handleChangeTitle}
-        onClickAddTask={handleClickAddTask}
-        onClickDeleteTask={handleClickDeleteTask}
-      />
-    ));
-
-    expect(handleClickDeleteTask).not.toBeCalled();
-    fireEvent.click(getByText('완료'));
-    expect(handleClickDeleteTask).toBeCalledWith(1);
+      expect(handleClickDeleteTask).not.toBeCalled();
+      fireEvent.click(getByText('완료'));
+      expect(handleClickDeleteTask).toBeCalledWith(1);
+    });
   });
 });
