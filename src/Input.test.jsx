689ecd6f173bc683325843479b510cac38bca9a3
yeonhwa <yeonhwa6717@gmail.com>
2020-06-22 23:47:09 +0900

    Input 컴포넌트 describe-context-it 적용

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -5,54 +5,46 @@ import { render, fireEvent } from '@testing-library/react';
 import Input from './Input';
 
 describe('<Input />', () => {
-  test('인풋과 버튼 확인', () => {
-    const taskTitle = '';
-    const onChangeTitle = jest.fn();
-    const onClickAddTask = jest.fn();
-    const { getByText, getByPlaceholderText } = render((
+  const onChangeTitle = jest.fn();
+  const onClickAddTask = jest.fn();
+  const renderInput = (taskTitle) => (
+    render((
       <Input
         value={taskTitle}
         onChange={onChangeTitle}
         onClick={onClickAddTask}
       />
-    ));
-    expect(getByText('할 일')).toBeTruthy();
-    expect(getByPlaceholderText('할 일을 입력해 주세요')).toBeTruthy();
-    expect(getByText('추가')).toBeTruthy();
+    ))
+  );
+
+  context('랜더 확인', () => {
+    it('인풋과 버튼 확인', () => {
+      const taskTitle = '';
+      const { getByText, getByPlaceholderText } = renderInput(taskTitle);
+      expect(getByText('할 일')).toBeTruthy();
+      expect(getByPlaceholderText('할 일을 입력해 주세요')).toBeTruthy();
+      expect(getByText('추가')).toBeTruthy();
+    });
   });
 
-  test('할 일 입력', () => {
-    const taskTitle = '';
-    const onChangeTitle = jest.fn();
-    const onClickAddTask = jest.fn();
-    const { getByPlaceholderText } = render((
-      <Input
-        value={taskTitle}
-        onChange={onChangeTitle}
-        onClick={onClickAddTask}
-      />
-    ));
-    const input = getByPlaceholderText('할 일을 입력해 주세요');
+  context('할 일 추가하기', () => {
+    it('입력하기', () => {
+      const taskTitle = '';
+      const { getByPlaceholderText } = renderInput(taskTitle);
+      const input = getByPlaceholderText('할 일을 입력해 주세요');
 
-    expect(input).toHaveValue('');
-    fireEvent.change(input, { target: { value: '뭐라도 하기' } });
-    expect(onChangeTitle).toBeCalled();
-  });
+      expect(input).toHaveValue('');
+      fireEvent.change(input, { target: { value: '뭐라도 하기' } });
+      expect(onChangeTitle).toBeCalled();
+    });
 
-  test('추가 클릭, 인풋창 초기화', () => {
-    const taskTitle = '뭐라도 하기';
-    const onChangeTitle = jest.fn();
-    const onClickAddTask = jest.fn();
-    const { getByText } = render((
-      <Input
-        value={taskTitle}
-        onChange={onChangeTitle}
-        onClick={onClickAddTask}
-      />
-    ));
-    const button = getByText('추가');
+    it('추가 클릭, 인풋창 초기화', () => {
+      const taskTitle = '뭐라도 하기';
+      const { getByText } = renderInput(taskTitle);
+      const button = getByText('추가');
 
-    fireEvent.click(button);
-    expect(onClickAddTask).toBeCalled();
+      fireEvent.click(button);
+      expect(onClickAddTask).toBeCalled();
+    });
   });
 });
