0294f2dc4a1dce95c29153120bfe2d6d2b63315d
gringrape <53764714+gringrape@users.noreply.github.com>
2020-10-05 22:47:06 +0900

    Add test for rendering given value to Input Component

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -5,12 +5,19 @@ import { render } from '@testing-library/react';
 import Input from './Input';
 
 describe('Input Component test', () => {
-  it('renders label, button text', () => {
-    const { container } = render((
-      <Input />
-    ));
+  const givenValue = '밥먹기';
 
+  const { container, getByLabelText } = render((
+    <Input value={givenValue} onChange={() => {}} />
+  ));
+  it('renders label, button text', () => {
     expect(container).toHaveTextContent('할 일');
     expect(container).toHaveTextContent('추가');
+  });
+
+  const input = getByLabelText('할 일');
+
+  it('renders given value on input', () => {
+    expect(input).toHaveValue(givenValue);
   });
 });
