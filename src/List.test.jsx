965101bb2e9429df43b5db03d3b9b8f9e0a3c101
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-05 18:27:28 +0900

    Fix the List test.

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -4,40 +4,44 @@ import { render } from '@testing-library/react';
 
 import List from './List';
 
-test('ListWithTasks', () => {
-  const tasks = [
-    {
-      id: 1,
-      title: '할 일 하나',
-    },
-    {
-      id: 2,
-      title: '할 일 두울',
-    },
-  ];
+describe('List', () => {
+  const handleClickDelete = jest.fn();
 
-  const { container } = render((
-    <List tasks={tasks} />
-  ));
+  test('tasks안에 task가 하나라도 존재한다면', () => {
+    const tasks = [
+      {
+        id: 1,
+        title: '할 일 하나',
+      },
+      {
+        id: 2,
+        title: '할 일 두울',
+      },
+    ];
 
-  expect(container).toContainHTML('ol');
-  expect(container).toContainHTML('li');
+    const { container } = render((
+      <List tasks={tasks} onClickDelete={handleClickDelete} />
+    ));
 
-  expect(container).toHaveTextContent('할 일 하나');
-  expect(container).toHaveTextContent('할 일 두울');
-  expect(container).toHaveTextContent('완료');
-});
+    expect(container).toContainHTML('ol');
+    expect(container).toContainHTML('li');
 
-test('ListWithoutTasks', () => {
-  const tasks = [];
+    expect(container).toHaveTextContent('할 일 하나');
+    expect(container).toHaveTextContent('할 일 두울');
+    expect(container).toHaveTextContent('완료');
+  });
 
-  const { container } = render((
-    <List tasks={tasks} />
-  ));
+  test('tasks안에 task가 존재하지 않으면', () => {
+    const tasks = [];
 
-  expect(container).not.toContainHTML('ol');
-  expect(container).not.toContainHTML('li');
+    const { container } = render((
+      <List tasks={tasks} onClickDelete={handleClickDelete} />
+    ));
 
-  expect(container).toHaveTextContent('할 일이 없어요!');
-  expect(container).not.toHaveTextContent('완료');
+    expect(container).not.toContainHTML('ol');
+    expect(container).not.toContainHTML('li');
+
+    expect(container).toHaveTextContent('할 일이 없어요!');
+    expect(container).not.toHaveTextContent('완료');
+  });
 });
