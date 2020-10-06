aa4842a305c215b4d94ebd1c884dc671d08df21e
Noah <mike1124@naver.com>
2020-10-06 16:04:26 +0900

    ♻️ Refactor test code about List.test.jsx

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -7,33 +7,34 @@ import List from './List';
 describe('List', () => {
   const handleClick = jest.fn();
 
-  test('length is 0', () => {
-    const tasks = [];
+  const tasks = [];
 
-    const { container } = render((
-      <List tasks={tasks} onClickDelete={handleClick} />
-    ));
+  const { container } = render((
+    <List tasks={tasks} onClickDelete={handleClick} />
+  ));
 
-    expect(container).toHaveTextContent('할 일이 없어요!');
+  context('test length is 0', () => {
+    it('할 일이 없어요! 표시', () => {
+      expect(container).toHaveTextContent('할 일이 없어요!');
+    });
   });
 
-  test('length is over 1', () => {
-    const tasks = [
+  context('test length is over 1', () => {
+    tasks.concat([
       {
         id: 1,
-        title: '할일1',
+        task: '할일1',
       },
       {
         id: 2,
-        title: '할일2',
+        task: '할일2',
       },
-    ];
+    ]);
 
-    const { container } = render((
-      <List tasks={tasks} onClickDelete={handleClick} />
-    ));
-
-    expect(container).toHaveTextContent('할일1');
-    expect(container).toHaveTextContent('할일2');
+    it('할 일 모두 표시', () => {
+      tasks.forEach((task) => {
+        expect(container).toHaveTextContent(`${task.title}`);
+      });
+    });
   });
 });
