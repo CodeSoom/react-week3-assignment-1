b9093a9414857bce8f8486432e01ec3ef140d391
saseungmin <dbd02169@naver.com>
2020-10-06 23:51:17 +0900

    Fixed Input.test

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -7,19 +7,21 @@ import {
 import Input from './Input';
 
 describe('Input', () => {
-  const taskTitle = '아무것도 하지 않기';
-  const handleChange = jest.fn();
   const handleClick = jest.fn();
+  const handleChange = jest.fn();
 
   const { container, getByText, getByLabelText } = render((
     <Input onClick={handleClick} onChange={handleChange} />
   ));
+
   const input = getByLabelText('할 일');
   const button = getByText('추가');
 
   expect(container).toHaveTextContent('추가');
 
   test('할일 입력', () => {
+    const taskTitle = '아무것도 하지 않기';
+
     expect(input).toHaveDisplayValue('');
     expect(handleChange).not.toBeCalled();
 
