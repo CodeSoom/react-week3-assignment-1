d95f4a6ac92755dea8101ebe8d3a90711af693db
Dong A Kim <dongakim90@gmail.com>
2020-10-06 19:06:00 +0900

    Input Test 수정
    Input 컴포넌트의 render와 담당하는 event를 'render', 'onClick',
    'onChange' describe로 구분하여 표현했다.
    
    각 테스트는 Given-When-Then 형태를 따랐으며, Given이 없을 시 생략했다.

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -1,35 +1,88 @@
 import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import Input from './Input';
 
-test('Input', () => {
-  const handleChange = jest.fn();
-  const handleClick = jest.fn();
+describe('<Input />', () => {
+  describe('render', () => {
+    it('할 일 label을 출력한다.', () => {
+      // When
+      render(<Input />);
 
-  render(
-    <Input
-      value=""
-      onChange={handleChange}
-      onClick={handleClick}
-    />,
-  );
+      // Then
+      expect(screen.getByText('할 일')).toBeInTheDocument();
+    });
 
-  expect(screen.getByText('할 일')).toBeInTheDocument();
-  expect(screen.getByRole('textbox')).toHaveAttribute('placeholder', '할 일을 입력해 주세요');
-  expect(screen.getByRole('button')).toHaveTextContent('추가');
+    it('input을 출력한다.', () => {
+      // When
+      render(<Input />);
 
-  expect(handleChange).not.toHaveBeenCalled();
+      // Then
+      expect(screen.getByRole('textbox')).toHaveAttribute('placeholder', '할 일을 입력해 주세요');
+    });
 
-  fireEvent.change(screen.getByRole('textbox'), {
-    target: { value: '강의듣기' },
+    it('추가 button을 출력한다.', () => {
+      // When
+      render(<Input />);
+
+      // Then
+      expect(screen.getByRole('button')).toHaveTextContent('추가');
+    });
   });
 
-  expect(handleChange).toHaveBeenCalledTimes(1);
+  describe('onClick', () => {
+    context('할 일 label을 클릭했을 때', () => {
+      it('input으로 focus된다.', () => {
+        // Given
+        render(<Input />);
 
-  expect(handleClick).not.toHaveBeenCalled();
+        // When
+        userEvent.click(screen.getByText('할 일'));
 
-  fireEvent.click(screen.getByRole('button'));
+        // Then
+        expect(screen.getByRole('textbox')).toHaveFocus();
+      });
+    });
 
-  expect(handleClick).toHaveBeenCalledTimes(1);
+    context('추가 button을 클릭했을 때', () => {
+      it('onClick props가 호출된다.', () => {
+        // Given
+        const handleClick = jest.fn();
+
+        render(
+          <Input
+            onClick={handleClick}
+          />,
+        );
+
+        // When
+        userEvent.click(screen.getByRole('button'));
+
+        // Then
+        expect(handleClick).toHaveBeenCalledTimes(1);
+      });
+    });
+  });
+
+  describe('onChange', () => {
+    context('input에 typing 했을 때', () => {
+      it('onChange props가 호출된다.', () => {
+        // Given
+        const handleChange = jest.fn();
+
+        render(
+          <Input
+            onChange={handleChange}
+          />,
+        );
+
+        // When
+        userEvent.type(screen.getByRole('textbox'), 'fix the bug');
+
+        // Then
+        expect(handleChange).toHaveBeenCalledTimes(11);
+      });
+    });
+  });
 });
