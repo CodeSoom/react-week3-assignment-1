b0b1f0a9a3472d5a5d6b81f1db8bacf49bc40412
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-06 18:47:57 +0900

    Refactoring List.test.jsx
    문구를 좀더 명확하고 매끄럽게 수정했습니다.
    중복을 제거하고 테스트내용을 더 구체화했습니다.

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -1,51 +1,72 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 import List from './List';
 
 describe('List', () => {
   const handleClickDelete = jest.fn();
 
-  context('tasks안에 task가 존재하지 않으면', () => {
+  context('할 일이 존재하지 않으면', () => {
     const tasks = [];
 
-    it('할 일이 없어요! 메세지만 노출한다.', () => {
+    it('"할 일이 없어요!" 메세지만 노출한다.', () => {
       const { container } = render((
-        <List tasks={tasks} onClickDelete={handleClickDelete} />
+        <List tasks={tasks} />
       ));
-
-      expect(container).not.toContainHTML('ol');
-      expect(container).not.toContainHTML('li');
 
       expect(container).toHaveTextContent('할 일이 없어요!');
       expect(container).not.toHaveTextContent('완료');
     });
   });
 
-  context('tasks안에 task가 하나라도 존재한다면', () => {
+  context('할 일이 존재한다면', () => {
     const tasks = [
-      {
-        id: 1,
-        title: '할 일 하나',
-      },
-      {
-        id: 2,
-        title: '할 일 두울',
-      },
+      { id: 1, title: '첫번째 할 일' },
+      { id: 2, title: '두번째 할 일' },
     ];
 
-    it('tasks 목록을 출력한다.', () => {
+    it('할 일 목록이 순서대로 보여진다', () => {
+      const { getAllByRole } = render((
+        <List tasks={tasks} />
+      ));
+      const taskTitles = getAllByRole('listitem');
+
+      taskTitles.forEach((listItem, index) => {
+        expect(listItem).toHaveTextContent(tasks[index].title);
+        expect(listItem).toHaveTextContent('완료');
+      });
+    });
+
+    it('할 일 목록에 순번이 매겨진다', () => {
       const { container } = render((
-        <List tasks={tasks} onClickDelete={handleClickDelete} />
+        <List tasks={tasks} />
       ));
 
       expect(container).toContainHTML('ol');
-      expect(container).toContainHTML('li');
+    });
+  });
 
-      expect(container).toHaveTextContent('할 일 하나');
-      expect(container).toHaveTextContent('할 일 두울');
-      expect(container).toHaveTextContent('완료');
+  context('삭제 버튼을 클릭하면', () => {
+    const tasks = [
+      { id: 1, title: '첫번째 할 일' },
+      { id: 2, title: '두번째 할 일' },
+    ];
+
+    it('클릭한 횟수 만큼 handleClickDelete 함수가 실행된다', () => {
+      const { getAllByText } = render((
+        <List tasks={tasks} onClickDelete={handleClickDelete} />
+      ));
+
+      const deleteButtons = getAllByText('완료');
+
+      expect(handleClickDelete).not.toBeCalled();
+
+      deleteButtons.forEach((button) => {
+        fireEvent.click(button);
+      });
+
+      expect(handleClickDelete).toBeCalledTimes(2);
     });
   });
 });
