239e882fb68b67a4b4ce6e57f64ad9acde6376ad
Dong A Kim <dongakim90@gmail.com>
2020-10-06 19:38:03 +0900

    Page Test 수정
    Page 컴포넌트의 구성요소인 헤딩 태그와 Input, List 컴포넌트가
    잘 출력되는지 render 테스트를 추가했다.

--- src/Page.test.jsx
+++ src/Page.test.jsx
@@ -3,28 +3,83 @@ import { render, screen } from '@testing-library/react';
 
 import Page from './Page';
 
-test('Page', () => {
-  const tasks = [{
-    id: 1,
-    title: '운동하기',
-  }];
-  const handleChangeTitle = jest.fn();
-  const handleClickAddTask = jest.fn();
-  const handleClickDeleteTask = jest.fn();
+describe('<Page />', () => {
+  describe('render', () => {
+    // Given
+    const emptyTasks = [];
 
-  render(
-    <Page
-      taskTitle
-      tasks={tasks}
-      onChangeTitle={handleChangeTitle}
-      onClickAddTask={handleClickAddTask}
-      onClickDeleteTask={handleClickDeleteTask}
-    />,
-  );
+    it('제목 To-do를 출력한다.', () => {
+      // When
+      render(
+        <Page
+          tasks={emptyTasks}
+        />,
+      );
 
-  expect(screen.getByText('To-do')).toBeInTheDocument();
+      // Then
+      expect(screen.getByText('To-do')).toBeInTheDocument();
+    });
 
-  expect(screen.getByText('할 일')).toBeInTheDocument();
+    it('input을 출력한다.', () => {
+      // When
+      render(
+        <Page
+          tasks={emptyTasks}
+        />,
+      );
 
-  expect(screen.getByRole('list')).toBeInTheDocument();
+      // Then
+      expect(screen.getByRole('textbox')).toHaveAttribute('placeholder', '할 일을 입력해 주세요');
+    });
+
+    it('추가 button을 출력한다.', () => {
+      // When
+      render(
+        <Page
+          tasks={emptyTasks}
+        />,
+      );
+
+      // Then
+      expect(screen.getByRole('button')).toHaveTextContent('추가');
+    });
+
+    context('tasks가 없을 때', () => {
+      it('빈 메세지를 출력한다.', () => {
+        // When
+        render(
+          <Page
+            tasks={emptyTasks}
+          />,
+        );
+
+        // Then
+        expect(screen.getByText('할 일이 없어요!')).toBeInTheDocument();
+        expect(screen.queryByRole('list')).not.toBeInTheDocument();
+      });
+    });
+
+    context('tasks가 있을 때', () => {
+      it('tasks 목록을 출력한다.', () => {
+        // Given
+        const title = '복습하기';
+        const tasks = [{
+          id: 1,
+          title,
+        }];
+
+        // When
+        render(
+          <Page
+            tasks={tasks}
+          />,
+        );
+
+        // Then
+        expect(screen.queryByText('할 일이 없어요!')).not.toBeInTheDocument();
+        expect(screen.getByRole('list')).toBeInTheDocument();
+        expect(screen.getByRole('list')).toHaveTextContent(title);
+      });
+    });
+  });
 });
