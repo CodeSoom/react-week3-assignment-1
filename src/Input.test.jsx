7bb9e24db0f0ae056e9c707af20692d5d646042b
gringrape <53764714+gringrape@users.noreply.github.com>
2020-10-06 18:38:47 +0900

    Fix tests of InputComponent
    Make render input function not to use jsx everywhere.
    Show given value without user action

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -5,13 +5,33 @@ import { render } from '@testing-library/react';
 import Input from './Input';
 
 describe('Input Component', () => {
+  const renderInput = ({
+    value = '',
+    onChange = () => {},
+    onClick = () => {},
+  }) => render(
+    <Input
+      value={value}
+      onChange={onChange}
+      onClick={onClick}
+    />,
+  );
+
   context('without user action', () => {
     it('show input, label, button elements', () => {
-      const { getByText, getByPlaceholderText } = render(<Input />);
+      const { getByText, getByPlaceholderText } = renderInput({});
 
       getByText('할 일');
       getByPlaceholderText('할 일을 입력해 주세요');
       getByText('추가');
+    });
+
+    it('show given value on input', () => {
+      const value = '밥 먹기';
+      const { getByLabelText } = renderInput({ value });
+      const input = getByLabelText('할 일');
+
+      expect(input).toHaveValue(value);
     });
   });
 });
