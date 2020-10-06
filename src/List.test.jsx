36d8dc322e7a528b8f3436d54e5102b0a0564905
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-06 15:06:32 +0900

    Fix List test using describe,context,it.
    -tasks안에 task가 존재하지 않으면
    -tasks안에 task가 하나라도 존재한다면
    
    으로 context를 나누어 테스트를 수정했습니다.

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -7,7 +7,23 @@ import List from './List';
 describe('List', () => {
   const handleClickDelete = jest.fn();
 
-  test('tasks안에 task가 하나라도 존재한다면', () => {
+  context('tasks안에 task가 존재하지 않으면', () => {
+    const tasks = [];
+
+    it('할 일이 없어요! 메세지만 노출한다.', () => {
+      const { container } = render((
+        <List tasks={tasks} onClickDelete={handleClickDelete} />
+      ));
+
+      expect(container).not.toContainHTML('ol');
+      expect(container).not.toContainHTML('li');
+
+      expect(container).toHaveTextContent('할 일이 없어요!');
+      expect(container).not.toHaveTextContent('완료');
+    });
+  });
+
+  context('tasks안에 task가 하나라도 존재한다면', () => {
     const tasks = [
       {
         id: 1,
@@ -19,29 +35,17 @@ describe('List', () => {
       },
     ];
 
-    const { container } = render((
-      <List tasks={tasks} onClickDelete={handleClickDelete} />
-    ));
+    it('tasks 목록을 출력한다.', () => {
+      const { container } = render((
+        <List tasks={tasks} onClickDelete={handleClickDelete} />
+      ));
 
-    expect(container).toContainHTML('ol');
-    expect(container).toContainHTML('li');
+      expect(container).toContainHTML('ol');
+      expect(container).toContainHTML('li');
 
-    expect(container).toHaveTextContent('할 일 하나');
-    expect(container).toHaveTextContent('할 일 두울');
-    expect(container).toHaveTextContent('완료');
-  });
-
-  test('tasks안에 task가 존재하지 않으면', () => {
-    const tasks = [];
-
-    const { container } = render((
-      <List tasks={tasks} onClickDelete={handleClickDelete} />
-    ));
-
-    expect(container).not.toContainHTML('ol');
-    expect(container).not.toContainHTML('li');
-
-    expect(container).toHaveTextContent('할 일이 없어요!');
-    expect(container).not.toHaveTextContent('완료');
+      expect(container).toHaveTextContent('할 일 하나');
+      expect(container).toHaveTextContent('할 일 두울');
+      expect(container).toHaveTextContent('완료');
+    });
   });
 });
