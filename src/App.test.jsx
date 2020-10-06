393e7f62bc8cfd049cbaeec404613da17da43955
Dong A Kim <dongakim90@gmail.com>
2020-10-06 19:41:36 +0900

    App Test 수정
    App 컴포넌트 하위 Page 컴포넌트의 구성요소가 잘 출력되는지,
    또한 task를 입력하고 추가하고 삭제하는 테스트를 작성했다.
    
    각 테스트는 Given-When-Then 형태를 따랐으며, Given이 없을 시 생략했다.

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -3,31 +3,105 @@ import { render, screen, fireEvent } from '@testing-library/react';
 
 import App from './App';
 
-describe('App', () => {
-  test('task를 추가할 수 있다.', () => {
-    render(<App />);
+describe('<App />', () => {
+  describe('render', () => {
+    it('제목 To-do를 출력한다.', () => {
+      // When
+      render(<App />);
 
-    fireEvent.change(screen.getByRole('textbox'), {
-      target: { value: '청소하기' },
+      // Then
+      expect(screen.getByText('To-do')).toBeInTheDocument();
     });
 
-    fireEvent.click(screen.getByText('추가'));
+    it('input을 출력한다.', () => {
+      // When
+      render(<App />);
 
-    expect(screen.getByRole('list')).toHaveTextContent('청소하기');
+      // Then
+      expect(screen.getByRole('textbox')).toHaveAttribute('placeholder', '할 일을 입력해 주세요');
+    });
+
+    it('추가 button을 출력한다.', () => {
+      // When
+      render(<App />);
+
+      // Then
+      expect(screen.getByRole('button')).toHaveTextContent('추가');
+    });
+
+    context('tasks가 없을 때', () => {
+      it('빈 메세지를 출력한다.', () => {
+        // When
+        render(<App />);
+
+        // Then
+        expect(screen.getByText('할 일이 없어요!')).toBeInTheDocument();
+        expect(screen.queryByRole('list')).not.toBeInTheDocument();
+      });
+    });
+
+    context('tasks가 있을 때', () => {
+      it('tasks 목록을 출력한다.', () => {
+        // Given
+        render(<App />);
+
+        // When
+        fireEvent.change(screen.getByRole('textbox'), {
+          target: { value: '청소하기' },
+        });
+
+        fireEvent.click(screen.getByText('추가'));
+
+        // Then
+        expect(screen.getByRole('list')).toHaveTextContent('청소하기');
+      });
+    });
   });
 
-  test('task를 삭제할 수 있다.', () => {
-    render(<App />);
+  describe('task', () => {
+    it('입력할 수 있다.', () => {
+      // Given
+      render(<App />);
 
-    fireEvent.change(screen.getByRole('textbox'), {
-      target: { value: '청소하기' },
+      // When
+      fireEvent.change(screen.getByRole('textbox'), {
+        target: { value: '청소하기' },
+      });
+
+      // Then
+      expect(screen.getByRole('textbox')).toHaveValue('청소하기');
     });
 
-    fireEvent.click(screen.getByText('추가'));
+    it('추가할 수 있다.', () => {
+      // Given
+      render(<App />);
 
-    fireEvent.click(screen.getByText('완료'));
+      // When
+      fireEvent.change(screen.getByRole('textbox'), {
+        target: { value: '선물사기' },
+      });
 
-    expect(screen.getByText('할 일이 없어요!')).toBeInTheDocument();
-    expect(screen.queryByText('청소하기')).not.toBeInTheDocument();
+      fireEvent.click(screen.getByText('추가'));
+
+      // Then
+      expect(screen.getByRole('list')).toHaveTextContent('선물사기');
+    });
+
+    it('삭제할 수 있다.', () => {
+      // Given
+      render(<App />);
+
+      fireEvent.change(screen.getByRole('textbox'), {
+        target: { value: '운동하기' },
+      });
+
+      fireEvent.click(screen.getByText('추가'));
+
+      // When
+      fireEvent.click(screen.getByText('완료'));
+
+      // Then
+      expect(screen.queryByRole('list')).not.toBeInTheDocument();
+    });
   });
 });
