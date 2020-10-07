b3450d6807670e5ff8d3341ed94c23e5f167ca0e
JamieShin0201 <65442049+JamieShin0201@users.noreply.github.com>
2020-10-07 19:38:44 +0900

    Refactoring List.test.jsx

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { render, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 import List from './List';
 
@@ -38,7 +38,7 @@ describe('List 컴포넌트', () => {
       },
     ];
 
-    it('할 일들과 완료버튼들을 보여준다', () => {
+    it('할 일들을 보여준다', () => {
       const { container } = render((
         <List
           tasks={tasks}
@@ -46,34 +46,9 @@ describe('List 컴포넌트', () => {
         />
       ));
 
-      tasks.forEach((task) => {
-        expect(container).toHaveTextContent(task.title);
-        expect(container).toHaveTextContent('완료');
-      });
-    });
-  });
-
-  context('완료를 누르면', () => {
-    const tasks = [
-      {
-        id: 1,
-        title: '할일1',
-      },
-    ];
-
-    it('onClickDelete 이벤트가 발생한다', () => {
-      const { getByText } = render((
-        <List
-          tasks={tasks}
-          onClickDelete={handleClickDelete}
-        />
-      ));
-
-      expect(handleClickDelete).not.toBeCalled();
-
-      fireEvent.click(getByText('완료'));
-
-      expect(handleClickDelete).toBeCalledTimes(1);
+      expect(container).toHaveTextContent('할일1');
+      expect(container).toHaveTextContent('할일2');
+      expect(container).toHaveTextContent('할일3');
     });
   });
 });
