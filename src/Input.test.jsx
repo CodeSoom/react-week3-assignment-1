d938b94c4d72b4d75fb67b7201f008be61756949
davidyang2149 <davidyang2149@gmail.com>
2020-10-07 21:04:55 +0900

    Fix Input.test.jsx
    3강 테스트 - 3주 차 과제 1
    - Input.test.jsx 구현(진행 중).
    - 테스트 구현 시도 중.

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -8,7 +8,6 @@ test('Input', () => {
   const value = '';
 
   const onChange = jest.fn();
-
   const onClick = jest.fn();
 
   const { container, getByText } = render((
@@ -19,11 +18,18 @@ test('Input', () => {
     />
   ));
 
-  fireEvent.onChange(getByText('어떤 할일 입력 중'));
+  // Basic Test
+  // value 값이 비어 있는 것을 체크하기
+  expect(value).toHaveValue('');
+  expect(container).toHaveTextContent('할 일');
 
+  // onChange Test
+  // onChange로 할일을 입력 후 적용되었는지 체크하기
+  fireEvent.change(getByText('어떤 할일 입력 중'));
   expect(onChange).toBeCalledWith('어떤 할일 입력 중');
 
+  // onClick Test
+  // onClick 후 value 값이 비어있는지 확인하기
   fireEvent.click(getByText('추가'));
-
-  expect(container).toHaveTextContent('');
+  expect(value).toHaveValue('');
 });
