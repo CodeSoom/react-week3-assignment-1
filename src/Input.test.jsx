73fc899a4ebc4aaca0350e1900c865cf3a55a86f
gringrape <53764714+gringrape@users.noreply.github.com>
2020-10-07 20:55:37 +0900

    Fix InputComponent tests
    fix event handler test to check not called before event happens,
    fix renderInput function by setting event handler as constant,
    add asserts to the case checking element existence

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -5,30 +5,33 @@ import { render, fireEvent } from '@testing-library/react';
 import Input from './Input';
 
 describe('Input Component', () => {
-  const renderInput = ({
-    value = '',
-    onChange = () => {},
-    onClick = () => {},
-  }) => render(
+  const handleClick = jest.fn();
+  const handleChange = jest.fn();
+
+  const renderInput = (value = '') => render(
     <Input
       value={value}
-      onChange={onChange}
-      onClick={onClick}
+      onChange={handleChange}
+      onClick={handleClick}
     />,
   );
 
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
   context('without user action', () => {
     it('show input, label, button elements', () => {
-      const { getByText, getByPlaceholderText } = renderInput({});
+      const { getByText, getByPlaceholderText } = renderInput();
 
-      getByText('할 일');
-      getByPlaceholderText('할 일을 입력해 주세요');
-      getByText('추가');
+      expect(getByText('할 일')).toBeInTheDocument();
+      expect(getByPlaceholderText('할 일을 입력해 주세요')).toBeInTheDocument();
+      expect(getByText('추가')).toBeInTheDocument();
     });
 
     it('show given value on input', () => {
       const value = '밥 먹기';
-      const { getByLabelText } = renderInput({ value });
+      const { getByLabelText } = renderInput(value);
       const input = getByLabelText('할 일');
 
       expect(input).toHaveValue(value);
@@ -37,27 +40,31 @@ describe('Input Component', () => {
 
   context('when user input', () => {
     it('call change handler', () => {
-      const onChange = jest.fn();
-      const { getByLabelText } = renderInput({ onChange });
+      const { getByLabelText } = renderInput();
       const input = getByLabelText('할 일');
+
+      expect(handleChange).not.toBeCalled();
+
       fireEvent.change(input, {
         target: {
           value: '밥 먹기',
         },
       });
 
-      expect(onChange).toBeCalled();
+      expect(handleChange).toBeCalled();
     });
   });
 
   context('when user click button', () => {
     it('call click handler', () => {
-      const onClick = jest.fn();
-      const { getByText } = renderInput({ onClick });
+      const { getByText } = renderInput();
       const button = getByText('추가');
+
+      expect(handleClick).not.toBeCalled();
+
       fireEvent.click(button);
 
-      expect(onClick).toBeCalled();
+      expect(handleClick).toBeCalledTimes(1);
     });
   });
 });
