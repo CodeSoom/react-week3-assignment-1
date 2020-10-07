074400a8bcfe42f3958a692021b0fc1eace9be58
saseungmin <dbd02169@naver.com>
2020-10-07 23:04:10 +0900

    Update App test Code - App test code 작성

---
+++ src/App.test.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+
+import {
+  fireEvent,
+  render,
+} from '@testing-library/react';
+
+import App from './App';
+
+describe('App', () => {
+  context('initial loading', () => {
+    it('label "할 일" 확인한다.', () => {
+      const { container } = render(<App />);
+
+      expect(container).toHaveTextContent('할 일');
+    });
+
+    it('input이 존재하는지 확인한다.', () => {
+      const { getByLabelText } = render(<App />);
+      const input = getByLabelText('할 일');
+
+      expect(input).toBeTruthy();
+    });
+
+    it('"할 일이 없어요!"를 화면에 나타나있는지 확인한다.', () => {
+      const { container } = render(<App />);
+
+      expect(container).toHaveTextContent('할 일이 없어요!');
+    });
+  });
+
+  context('function test', () => {
+    const task = '아무것도 하지 않기';
+
+    it('taskTitle 추가하기 후 추가 버튼 클릭 후 완료 버튼 클릭 삭제', () => {
+      const { getByLabelText, getByText, container } = render(<App />);
+      const input = getByLabelText('할 일');
+      const addButton = getByText('추가');
+
+      expect(input).toHaveDisplayValue('');
+
+      fireEvent.change(input, { target: { value: task } });
+      expect(input).toHaveDisplayValue(task);
+
+      fireEvent.click(addButton);
+      expect(input).toHaveDisplayValue('');
+      expect(container).toHaveTextContent(task);
+
+      const finishButton = getByText('완료');
+
+      fireEvent.click(finishButton);
+      expect(container).toHaveTextContent('할 일이 없어요!');
+    });
+  });
+});
