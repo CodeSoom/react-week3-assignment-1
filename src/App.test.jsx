3b1d9babc4517bb3d90774ba2512942c2de32b38
Noah <mike1124@naver.com>
2020-10-07 23:38:29 +0900

    ♻️ Refactor code App.test.jsx / List.test.jsx

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -5,19 +5,30 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 describe('App', () => {
-  const { getByLabelText, getAllByText } = render((
-    <App />
-  ));
+  context('when rendering text', () => {
+    it('verify Input visible', () => {
+      const { getByLabelText } = render((
+        <App />
+      ));
 
-  context('check rendering', () => {
-    it('verify Input', () => {
       expect(getByLabelText('할 일')).toBeVisible();
     });
-    it('verify List', () => {
-      //
+
+    it('verify List visible', () => {
+      const { getByText } = render((
+        <App />
+      ));
+
+      expect(getByText('할 일이 없어요!')).toBeVisible();
     });
   });
-  context('check click button', () => {
-    //
+  context('when rendering buttons', () => {
+    it('verify "추가" button visible', () => {
+      const { getByText } = render((
+        <App />
+      ));
+
+      expect(getByText('추가')).toBeVisible();
+    });
   });
 });
--- src/List.test.jsx
+++ src/List.test.jsx
@@ -5,22 +5,22 @@ import { render } from '@testing-library/react';
 import List from './List';
 
 describe('List', () => {
-  const handleClick = jest.fn();
-
-  const tasks = [];
-
-  const { container } = render((
-    <List tasks={tasks} onClickDelete={handleClick} />
-  ));
-
   context('test length is 0', () => {
+    const handleClick = jest.fn();
+
+    const tasks = [];
+
     it('할 일이 없어요! 표시', () => {
+      const { container } = render((
+        <List tasks={tasks} onClickDelete={handleClick} />
+      ));
+
       expect(container).toHaveTextContent('할 일이 없어요!');
     });
   });
 
   context('test length is over 1', () => {
-    tasks.concat([
+    const tasks = [
       {
         id: 1,
         title: '할일1',
@@ -33,9 +33,13 @@ describe('List', () => {
         id: 3,
         title: '할일2',
       },
-    ]);
+    ];
 
     it('할 일 모두 표시', () => {
+      const { container } = render((
+        <List tasks={tasks} />
+      ));
+
       tasks.forEach((task) => {
         expect(container).toHaveTextContent(task.title);
       });
