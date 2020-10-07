c47f9253dd81376863fcaea6859332683ffd0d75
Dokyun Lim <im.koremp@gmail.com>
2020-10-07 15:23:01 +0900

    Update List.test.jsx - tasks 유무를 분리

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -4,7 +4,7 @@ import { render, fireEvent } from '@testing-library/react';
 
 import List from './List';
 
-test('List', () => {
+test('List - no task', () => {
   const tasks = [];
 
   const handleClick = jest.fn();
@@ -17,15 +17,31 @@ test('List', () => {
   ));
 
   expect(container).toHaveTextContent('할 일이 없어요!');
+  expect(handleClick).not.toBeCalled();
+});
 
-  tasks.push({ id: 1, title: '뭐라도 하기' });
+test('List - tasks have task', () => {
+  const tasks = [
+    {
+      id: 1,
+      title: '뭐라도 하기',
+    },
+    {
+      id: 2,
+      title: '코드숨 과제하기',
+    },
+  ];
+
+  const handleClick = jest.fn();
+
+  const { container } = render((
+    <List
+      tasks={tasks}
+      onClickDelete={handleClick}
+    />
+  ));
 
   expect(container).toHaveTextContent('뭐라도 하기');
-
-  tasks.push({ id: 2, title: '코드숨 과제하기' });
-
   expect(container).toHaveTextContent('뭐라도 하기');
-  expect(container).toHaveTextContent('코드숨 과제하기');
-
   expect(handleClick).not.toBeCalled();
 });
