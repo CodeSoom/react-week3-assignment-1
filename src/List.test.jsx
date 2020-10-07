a67c1bfa6ab66fd888d35595cf2c97e8829561fb
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-07 14:31:09 +0900

    Fix List.test.jsx
    tasks가 존재하면, tasks가 존재하지 않으면
    이렇게 두개의 context로 재구성했습니다.

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 
-import { fireEvent, render } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 import List from './List';
 
 describe('List', () => {
-  const handleClickDelete = jest.fn();
-
-  context('할 일이 존재하지 않으면', () => {
+  context('tasks가 존재하지 않으면', () => {
     const tasks = [];
 
-    it('"할 일이 없어요!" 메세지만 노출한다.', () => {
+    it('"할 일이 없어요!"를 보여준다.', () => {
       const { container } = render((
         <List tasks={tasks} />
       ));
@@ -20,13 +18,13 @@ describe('List', () => {
     });
   });
 
-  context('할 일이 존재한다면', () => {
+  context('tasks가 존재한다면', () => {
     const tasks = [
       { id: 1, title: '첫번째 할 일' },
       { id: 2, title: '두번째 할 일' },
     ];
 
-    it('할 일 목록이 순서대로 보여진다', () => {
+    it('할 일 목록이 순서대로 보여준다.', () => {
       const { getAllByRole } = render((
         <List tasks={tasks} />
       ));
@@ -34,39 +32,15 @@ describe('List', () => {
 
       taskTitles.forEach((listItem, index) => {
         expect(listItem).toHaveTextContent(tasks[index].title);
-        expect(listItem).toHaveTextContent('완료');
       });
     });
 
-    it('할 일 목록에 순번이 매겨진다', () => {
+    it('삭제버튼을 보여준다.', () => {
       const { container } = render((
         <List tasks={tasks} />
       ));
 
-      expect(container).toContainHTML('ol');
-    });
-  });
-
-  context('삭제 버튼을 클릭하면', () => {
-    const tasks = [
-      { id: 1, title: '첫번째 할 일' },
-      { id: 2, title: '두번째 할 일' },
-    ];
-
-    it('클릭한 횟수 만큼 handleClickDelete 함수가 실행된다', () => {
-      const { getAllByText } = render((
-        <List tasks={tasks} onClickDelete={handleClickDelete} />
-      ));
-
-      const deleteButtons = getAllByText('완료');
-
-      expect(handleClickDelete).not.toBeCalled();
-
-      deleteButtons.forEach((button) => {
-        fireEvent.click(button);
-      });
-
-      expect(handleClickDelete).toBeCalledTimes(2);
+      expect(container).toHaveTextContent('완료');
     });
   });
 });
