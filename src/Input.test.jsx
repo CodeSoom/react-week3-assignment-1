199412dcf10b14718920bb0595df7d5c904b4d8e
김태호 <thkim@taehokim-MacBookPro.local>
2020-10-08 20:40:23 +0900

    Do something

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -9,9 +9,21 @@ test('Input', () => {
     title: '아무것도 하지 않기',
   };
 
-  const { container } = render((
-    <Input value={taskTitle} />
+  const handleClick = jest.fn();
+
+  const { container, getByText } = render((
+    <Input
+      value={taskTitle}
+      onChange={handleChange}
+      onClick={handleClick}
+    />
   ));
 
-  expect(container).toHaveTextContent('아무것도 하지 않기');
-});
+  expect(container).toHaveTextContent('할 일추가');
+})
+
+expect(handleClick).not.toBeCalled();
+
+fireEvent.click(getByText('추가'));
+
+expect(handleClick).toBeCalledWith(1);
