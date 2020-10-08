e312d6efab74b080de9f60179bbc757bf3814197
seungyup <shinyep@hanmail.net>
2020-10-09 00:04:38 +0900

    List 컴포넌트 테스트 추가

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-import { render, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 import List from './List';
 
-test('List', () => {
+test('task List', () => {
   const tasks = [
     {
       id: 1,
@@ -15,8 +15,42 @@ test('List', () => {
   const handleClick = jest.fn();
 
   const { container } = render((
-    <List tasks={tasks} onClickDelete={handleClick}></List>
+    <List tasks={tasks} onClickDelete={handleClick} />
   ));
 
   expect(container).toHaveTextContent('테스트 코드 작성');
+});
+
+test('no data List', () => {
+  const tasks = [];
+
+  const handleClick = jest.fn();
+
+  const { container } = render((
+    <List tasks={tasks} onClickDelete={handleClick} />
+  ));
+
+  expect(container).toHaveTextContent('할 일이 없어요!');
+});
+
+test('tasks List', () => {
+  const tasks = [
+    {
+      id: 1,
+      title: '테스트 코드 작성',
+    },
+    {
+      id: 2,
+      title: '공부 하기',
+    },
+  ];
+
+  const handleClick = jest.fn();
+
+  const { container } = render((
+    <List tasks={tasks} onClickDelete={handleClick} />
+  ));
+
+  expect(container).toHaveTextContent('테스트 코드 작성');
+  expect(container).toHaveTextContent('공부 하기');
 });
