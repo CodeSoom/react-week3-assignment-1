af8f86db46964ef3116e08de8c5256a1adb06888
JamieShin0201 <jamieshin1450@gmail.com>
2020-10-08 10:41:46 +0900

    Revert "Refactoring List.test.jsx"
    This reverts commit 7a1269db0dd84905aaa2af0eaf7b8d9e340c8180.

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
 
-import { render, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 import List from './List';
 
 describe('List 컴포넌트', () => {
   const handleClickDelete = jest.fn();
+
+  context('tasks에 값이 없으면', () => {
+    const tasks = [];
+
+    it('할 일이 없어요! 를 보여준다', () => {
+      const { container } = render((
+        <List
+          tasks={tasks}
+          onClickDelete={handleClickDelete}
+        />
+      ));
+
+      expect(container).toHaveTextContent('할 일이 없어요!');
+    });
+  });
 
   context('tasks에 값이 있으면', () => {
     const tasks = [
@@ -23,7 +38,7 @@ describe('List 컴포넌트', () => {
       },
     ];
 
-    it('할 일들, 완료버튼들을 보여준다', () => {
+    it('할 일들을 보여준다', () => {
       const { container } = render((
         <List
           tasks={tasks}
@@ -32,45 +47,8 @@ describe('List 컴포넌트', () => {
       ));
 
       expect(container).toHaveTextContent('할일1');
-      expect(container).toHaveTextContent('완료');
-
       expect(container).toHaveTextContent('할일2');
-      expect(container).toHaveTextContent('완료');
-
       expect(container).toHaveTextContent('할일3');
-      expect(container).toHaveTextContent('완료');
-    });
-
-    it('완료버튼들이 잘 호출된다', () => {
-      const { getAllByText } = render((
-        <List
-          tasks={tasks}
-          onClickDelete={handleClickDelete}
-        />
-      ));
-
-      expect(handleClickDelete).not.toBeCalled();
-
-      getAllByText('완료').forEach((complete) => {
-        fireEvent.click(complete);
-      });
-
-      expect(handleClickDelete).toBeCalledWith(3);
-    });
-  });
-
-  context('tasks에 값이 없으면', () => {
-    const tasks = [];
-
-    it('할 일이 없어요! 를 보여준다', () => {
-      const { container } = render((
-        <List
-          tasks={tasks}
-          onClickDelete={handleClickDelete}
-        />
-      ));
-
-      expect(container).toHaveTextContent('할 일이 없어요!');
     });
   });
 });
