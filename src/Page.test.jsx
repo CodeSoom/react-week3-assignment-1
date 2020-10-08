e7832a9b88724b9d0307ecd45fe54c083f68d466
koremp <im.koremp@gmail.com>
2020-10-09 00:07:09 +0900

    add Page.test.jsx

--- src/Page.test.jsx
+++ src/Page.test.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 import Page from './Page';
 
-test('Page', () => {
+test('Page - has no tasks', () => {
   const taskTitle = '';
   const tasks = [];
 
@@ -14,13 +14,43 @@ test('Page', () => {
 
   const { container } = render((
     <Page
-        taskTitle={taskTitle}
-        tasks={tasks}
-        onChangeTitle={onChangeTitle}
-        onClickDeleteTask={onClickDeleteTask}
-        onClickAddTask={onClickAddTask}
+      taskTitle={taskTitle}
+      tasks={tasks}
+      onChangeTitle={onChangeTitle}
+      onClickDeleteTask={onClickDeleteTask}
+      onClickAddTask={onClickAddTask}
     />
   ));
 
+  expect(onChangeTitle).not.toBeCalled();
+  expect(onClickDeleteTask).not.toBeCalled();
+  expect(onClickAddTask).not.toBeCalled();
+});
 
+test('Page - has title', () => {
+  const taskTitle = '뭐라도 하기';
+  const tasks = [];
+
+  const onChangeTitle = jest.fn();
+  const onClickDeleteTask = jest.fn();
+  const onClickAddTask = jest.fn();
+
+  const { container, getByText } = render((
+    <Page
+      taskTitle={taskTitle}
+      tasks={tasks}
+      onChangeTitle={onChangeTitle}
+      onClickDeleteTask={onClickDeleteTask}
+      onClickAddTask={onClickAddTask}
+    />
+  ));
+
+  expect(onChangeTitle).not.toBeCalled();
+  expect(onClickDeleteTask).not.toBeCalled();
+  expect(onClickAddTask).not.toBeCalled();
+
+  fireEvent.click(getByText('추가'));
+
+  expect(onChangeTitle).toBeCalled();
+  expect(onClickAddTask).toBeCalled();
 });
