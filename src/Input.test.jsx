e29dd84d84923d543c7e52dbabca5bacd5527023
saseungmin <dbd02169@naver.com>
2020-10-08 21:41:58 +0900

    Fixed Input test code
    -  describe-context-it으로 변경
    - renderInput 함수로 각 테스트별 renderInput 함수 호출
    - jest.fn을 전역으로 한번만 선언

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -7,42 +7,42 @@ import Input from './Input';
 describe('Input', () => {
   const handleChange = jest.fn();
   const handleClick = jest.fn();
-  const taskTitle = '아무것도 하지 않기';
 
-  test('할 일 입력', () => {
-    const { getByLabelText } = render((
-      <Input
-        onClick={handleClick}
-        onChange={handleChange}
-      />
-    ));
-    const input = getByLabelText('할 일');
+  const renderInput = () => render((
+    <Input
+      onClick={handleClick}
+      onChange={handleChange}
+    />
+  ));
 
-    expect(input).toHaveDisplayValue('');
-    expect(handleChange).not.toBeCalled();
+  context('exist taskTitle', () => {
+    const taskTitle = '아무것도 하지 않기';
+    const inputLabel = '할 일';
 
-    fireEvent.change(input, { target: { value: taskTitle } });
+    it('할 일을 입력한다.', () => {
+      const { getByLabelText } = renderInput();
+      const input = getByLabelText(inputLabel);
 
-    expect(handleChange).toBeCalled();
-    expect(input).toHaveDisplayValue(taskTitle);
-  });
+      expect(input).toHaveDisplayValue('');
+      expect(handleChange).not.toBeCalled();
 
-  test('추가 버튼 클릭', () => {
-    const { getByLabelText, getByText } = render((
-      <Input
-        onClick={handleClick}
-        onChange={handleChange}
-      />
-    ));
-    const input = getByLabelText('할 일');
-    const button = getByText('추가');
+      fireEvent.change(input, { target: { value: taskTitle } });
 
-    expect(handleClick).not.toBeCalled();
+      expect(handleChange).toBeCalled();
+      expect(input).toHaveDisplayValue(taskTitle);
+    });
 
-    fireEvent.change(input, { target: { value: taskTitle } });
-    fireEvent.click(button);
+    test('추가 버튼 클릭을 클릭한다.', () => {
+      const { getByLabelText, getByText } = renderInput();
+      const input = getByLabelText(inputLabel);
 
-    expect(handleClick).toBeCalled();
-    expect(input).toHaveDisplayValue('');
+      expect(handleClick).not.toBeCalled();
+
+      fireEvent.change(input, { target: { value: taskTitle } });
+      fireEvent.click(getByText('추가'));
+
+      expect(handleClick).toBeCalled();
+      expect(input).toHaveDisplayValue('');
+    });
   });
 });
