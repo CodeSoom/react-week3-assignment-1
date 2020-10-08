2ea724544da1075e0b37846fee82ece7dc02bd0f
JamieShin0201 <jamieshin1450@gmail.com>
2020-10-08 11:15:34 +0900

    Refactoring Page.test.jsx

--- src/Page.test.jsx
+++ src/Page.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import Page from './Page';
 
@@ -39,6 +39,12 @@ describe('Page 컴포넌트', () => {
       expect(container).toHaveTextContent('추가');
       expect(container).toHaveTextContent('할일1');
       expect(container).toHaveTextContent('할일2');
+
+      expect(handleChangeTitle).not.toBeCalled();
+
+      fireEvent.change(getByLabelText('할 일'), { target: { value: '입력 값' } });
+
+      expect(handleChangeTitle).toBeCalledTimes(1);
     });
   });
 });
