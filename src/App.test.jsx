407457eabeff9fc329fa423e5af0e69bc0edcf8b
wjy5446 <wjy5446@gmail.com>
2020-10-09 01:26:54 +0900

    refac) jsx 한 줄인 경우, 소괄호 제거

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -6,7 +6,7 @@ import App from './App';
 
 describe('App', () => {
   it('input에 값이 변경 되었을 때, input의 값이 변경되는 지 확인', () => {
-    const { getByDisplayValue } = render((<App />));
+    const { getByDisplayValue } = render(<App />);
 
     const inputTaskTitle = getByDisplayValue('');
 
@@ -16,7 +16,7 @@ describe('App', () => {
   });
 
   it('"추가" 버튼이 클릭 되었을 때, list에 task가 추가 되는 지 확인', () => {
-    const { getByDisplayValue, getByText } = render((<App />));
+    const { getByDisplayValue, getByText } = render(<App />);
 
     const inputTaskTitle = getByDisplayValue('');
     const addButton = getByText('추가');
@@ -30,7 +30,7 @@ describe('App', () => {
   });
 
   it('"추가" 버튼이 클릭 되었을 때, input의 값이 초기화되는 지 확인', () => {
-    const { getByDisplayValue, getByText } = render((<App />));
+    const { getByDisplayValue, getByText } = render(<App />);
 
     const inputTaskTitle = getByDisplayValue('');
     const addButton = getByText('추가');
@@ -42,7 +42,7 @@ describe('App', () => {
   });
 
   it('"삭제" 버튼이 클릭 되었을 때, list에 선택된 task가 삭제되는 지 확인', () => {
-    const { getByDisplayValue, getByText } = render((<App />));
+    const { getByDisplayValue, getByText } = render(<App />);
 
     const inputTaskTitle = getByDisplayValue('');
     const addButton = getByText('추가');
--- src/List.test.jsx
+++ src/List.test.jsx
@@ -31,7 +31,7 @@ describe('List', () => {
     ];
 
     it('tasks 목록을 출력한다.', () => {
-      const { getByText } = render((<List tasks={tasks} />));
+      const { getByText } = render(<List tasks={tasks} />);
 
       tasks.forEach((task) => expect(getByText(task.title)).toHaveTextContent(task.title));
     });
