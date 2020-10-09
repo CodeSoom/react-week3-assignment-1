727ddca076fc8ad021df8e7e1ff725788eff93fb
Noah <mike1124@naver.com>
2020-10-09 16:42:56 +0900

    ♻️ Refactor code / App.test.jsx

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -5,11 +5,13 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 describe('App', () => {
+  const renderUtil = () => render((
+    <App />
+  ));
+
   context('when rendering text', () => {
     it('verify Input visible', () => {
-      const { getByLabelText } = render((
-        <App />
-      ));
+      const { getByLabelText } = renderUtil();
 
       expect(getByLabelText('할 일')).toBeVisible();
     });
@@ -22,11 +24,10 @@ describe('App', () => {
       expect(getByText('할 일이 없어요!')).toBeVisible();
     });
   });
+
   context('when rendering buttons', () => {
     it('verify "추가" button visible', () => {
-      const { getByText } = render((
-        <App />
-      ));
+      const { getByText } = renderUtil();
 
       expect(getByText('추가')).toBeVisible();
     });
