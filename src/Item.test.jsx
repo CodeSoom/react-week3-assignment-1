54a4b3d9af48ee3dd6cd203cba5596f943c84cfb
ParkDongJo <pdj10211@gmail.com>
2020-10-09 15:01:03 +0900

    [week3-1:fix] 잘못 작성된 test 코드 수정
    * test > it 을 작성하였음 -> 이를 test 로 수정
    * 렌랭 시 확인 필요한 테스트 코드 추가가

--- src/Item.test.jsx
+++ src/Item.test.jsx
@@ -4,7 +4,16 @@ import { render, fireEvent } from '@testing-library/react';
 
 import Item from './Item';
 
-test('Item', () => {
+test('컴포넌트 렌딩 시, labal, placeholder, button 텍스트를 표시한다', () => {
+  // Given
+  const { container, getByText } = render(<Item />);
+
+  // When
+  // Then
+  expect(container).toHaveTextContent('뭐라도 하기');
+});
+
+test('제거 버튼 클릭 시, onClickDelete가 호출된다.', () => {
   // Given
   const task = {
     id: 1,
@@ -18,12 +27,9 @@ test('Item', () => {
     />
   ));
 
-  it('제거 버튼 클릭 시, onClickDelete가 호출된다.', () => {
-    expect(container).toHaveTextContent('뭐라도 하기');
-    expect(handleClick).not.toBeCalled();
-    // When
-    fireEvent.click(getByText('완료'));
-    // Then
-    expect(handleClick).toBeCalledWith(1);
-  });
+  expect(handleClick).not.toBeCalled();
+  // When
+  fireEvent.click(getByText('완료'));
+  // Then
+  expect(handleClick).toBeCalledWith(1);
 });
--- src/input.test.jsx
+++ src/input.test.jsx
@@ -4,16 +4,22 @@ import { render, fireEvent } from '@testing-library/react';
 
 import Input from './Input';
 
-test('Input', () => {
+test('컴포넌트 렌딩 시, labal, placeholder, button 텍스트를 표시한다', () => {
+  // Given
+  const { getByText, getByPlaceholderText } = render(<Input />);
+
+  // When
+  // Then
+});
+
+test('onChange 이벤트 발생 시, value 가 입력값이 된다.', () => {
   // Given
   const { getByTestId } = render(<Input />);
   const input = getByTestId('input-task');
   const inputValue = '게임하기';
 
-  it('onChange 이벤트 발생 시, value 가 입력값이 된다.', () => {
-    // When
-    fireEvent.change(input, { target: { value: inputValue } });
-    // Then
-    expect(input.value).toBe(inputValue);
-  });
+  // When
+  fireEvent.change(input, { target: { value: inputValue } });
+  // Then
+  expect(input.value).toBe(inputValue);
 });
