0b48b0ce31cc2cb125ce7e79110f89158303e0b9
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-09 17:03:57 +0900

    Fix Page.test.jsx
    중복과 불필요한 부분을 모두 제거하고 간소화했습니다.

--- src/Page.test.jsx
+++ src/Page.test.jsx
@@ -4,84 +4,33 @@ import { fireEvent, render } from '@testing-library/react';
 
 import Page from './Page';
 
-describe('Page', () => {
+test('Page', () => {
   const handleChangeTitle = jest.fn();
   const handleClickDeleteTask = jest.fn();
   const handleClickAddTask = jest.fn();
 
-  const pageRender = (tasks, taskTitle) => render((
+  const tasks = [
+    { id: 1, title: '첫번째 할 일' },
+    { id: 2, title: '두번째 할 일' },
+  ];
+
+  const { getByText } = render((
     <Page
       tasks={tasks}
-      taskTitle={taskTitle}
+      taskTitle="받은 문자"
       onClickDeleteTask={handleClickDeleteTask}
       onChangeTitle={handleChangeTitle}
       onClickAddTask={handleClickAddTask}
     />
   ));
 
-  context('with tasks & taskTitle', () => {
-    const taskTitle = '네번째 할 일';
-    const tasks = [
-      { id: 1, title: '첫번째 할 일' },
-      { id: 2, title: '두번째 할 일' },
-      { id: 3, title: '세번째 할 일' },
-    ];
+  expect(getByText('To-do')).not.toBeNull();
+  expect(getByText('할 일')).not.toBeNull();
 
-    it('show taskTitle in inputbox', () => {
-      const { container, getByPlaceholderText, getByText } = pageRender(tasks, taskTitle);
+  expect(getByText(tasks[0].title)).not.toBeNull();
+  expect(getByText(tasks[1].title)).not.toBeNull();
 
-      const input = getByPlaceholderText('할 일을 입력해 주세요');
+  fireEvent.click(getByText('추가'));
 
-      expect(container).toHaveTextContent('To-do');
-      expect(container).toHaveTextContent('할 일');
-      expect(input).toHaveDisplayValue('네번째 할 일');
-      expect(container).toHaveTextContent('추가');
-
-      expect(handleChangeTitle).not.toBeCalled();
-
-      fireEvent.change(input, { target: { value: '입력된 문자' } });
-
-      expect(handleChangeTitle).toBeCalled();
-
-      expect(handleClickAddTask).not.toBeCalled();
-
-      fireEvent.click(getByText('추가'));
-
-      expect(handleClickAddTask).toBeCalled();
-    });
-
-    it('show tasks list', () => {
-      const { container, getAllByRole } = pageRender(tasks, taskTitle);
-
-      expect(container).toHaveTextContent('To-do');
-
-      const taskItems = getAllByRole('listitem');
-      taskItems.forEach((task, index) => {
-        expect(task).toHaveTextContent(tasks[index].title);
-        expect(task).toHaveTextContent('완료');
-      });
-    });
-  });
-
-  context('without tasks & taskTitle', () => {
-    const taskTitle = '';
-    const tasks = [];
-
-    it('show placeholder in inputbox', () => {
-      const { container, getByPlaceholderText } = pageRender(tasks, taskTitle);
-
-      expect(container).toHaveTextContent('To-do');
-      expect(container).toHaveTextContent('할 일');
-      expect(getByPlaceholderText('할 일을 입력해 주세요')).toHaveDisplayValue('');
-      expect(container).toHaveTextContent('추가');
-    });
-
-    it('show "할 일이 없어요!"', () => {
-      const { container } = pageRender(tasks, taskTitle);
-
-      expect(container).toHaveTextContent('To-do');
-      expect(container).toHaveTextContent('할 일이 없어요!');
-      expect(container).not.toHaveTextContent('완료');
-    });
-  });
+  expect(handleClickAddTask).toBeCalled();
 });
