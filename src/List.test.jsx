427900681a07bda039406bfabfb72a339000d779
oceanide <oceanide@gmail.com>
2020-10-10 01:20:52 +0900

    Refactor List.test.jsx

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -6,31 +6,41 @@ import List from './List';
 describe('List', () => {
   const handleClickDelete = jest.fn();
 
-  const renderList = (tasks = []) => render((
+  const renderList = (tasks) => render((
     <List
       tasks={tasks}
       onClickDelete={handleClickDelete}
     />
   ));
 
-  context('tasks가 없을 때', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  context('tasks가 비어 있을 때', () => {
     it('빈 메시지를 표시한다', () => {
-      const { getByText } = renderList();
+      const tasks = [];
+
+      const { getByText } = renderList(tasks);
 
       expect(getByText('할 일이 없어요!')).toBeInTheDocument();
     });
   });
 
   context('tasks가 있을 때', () => {
-    it('할 일 목록과 완료 버튼을 표시한다', () => {
+    it('할 일 목록들을 표시한다', () => {
       const tasks = [
         { id: 1, title: '운동하기' },
+        { id: 2, title: '산책하기' },
       ];
 
-      const { getByText } = renderList(tasks);
+      const { getAllByRole } = renderList(tasks);
+      const taskLists = getAllByRole('listitem');
 
-      expect(getByText('운동하기')).toBeInTheDocument();
-      expect(getByText('완료')).toBeInTheDocument();
+      taskLists.forEach((task, index) => {
+        expect(task).toHaveTextContent(tasks[index].title);
+        expect(task).toHaveTextContent('완료');
+      });
     });
   });
 
@@ -38,13 +48,20 @@ describe('List', () => {
     it('handleClickDelete()를 호출한다', () => {
       const tasks = [
         { id: 1, title: '운동하기' },
+        { id: 2, title: '산책하기' },
       ];
 
-      const { getByText } = renderList(tasks);
+      const { getAllByText } = renderList(tasks);
+
       expect(handleClickDelete).not.toHaveBeenCalled();
 
-      fireEvent.click(getByText('완료'));
-      expect(handleClickDelete).toHaveBeenCalled();
+      const buttons = getAllByText('완료');
+
+      buttons.forEach((button) => {
+        fireEvent.click(button);
+      });
+
+      expect(handleClickDelete).toHaveBeenCalledTimes(2);
     });
   });
 });
