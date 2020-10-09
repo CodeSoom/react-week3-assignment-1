b5c945ddad22accc74c4b74798c5f27c5bce7275
Woong <37302935+TaeWoongKim2@users.noreply.github.com>
2020-10-09 21:13:25 +0900

    [START] Make test about Page component
    Just make test about page title named 'To-do'

---
+++ src/Page.test.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import { render, fireEvent } from '@testing-library/react';
+
+import Page from './Page';
+
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
+describe('Page Component', () => {
+  const pageTitle = 'To-do';
+
+  const onChangeTitle = jest.fn();
+  const onClickAddTask = jest.fn();
+  const onClickDeleteTask = jest.fn();
+
+  const init = ({
+    taskTitle = '',
+    handleChangeTitle = onChangeTitle,
+    handleClickAddTask = onClickAddTask,
+    tasks = [],
+    handleClickDeleteTask = onClickDeleteTask,
+  }) => {
+    const utils = render((
+      <Page
+        taskTitle={taskTitle}
+        onChangeTitle={handleChangeTitle}
+        onClickAddTask={handleClickAddTask}
+        tasks={tasks}
+        onClickDeleteTask={handleClickDeleteTask}
+      />
+    ));
+    return { ...utils };
+  };
+
+  test('has title label', () => {
+    const { container } = init({});
+    expect(container).toHaveTextContent(pageTitle);
+  });
+});
