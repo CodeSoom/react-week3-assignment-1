485e06ce62bce108eb1485c0578f6441adcec519
comejtome508 <youngnoh.goh@gmail.com>
2020-10-11 03:28:08 +0900

    Make Add test code - 추가 버튼을 누를 경우 추가 - 완료 버튼을 누를 경우 추가

--- src/Page.test.jsx
+++ src/Page.test.jsx
@@ -5,30 +5,30 @@ import { render } from '@testing-library/react';
 import Page from './Page';
 
 test('Page', () => {
-  const tasks= [
-  {
-    title: "Test value #1",
-    id: 1,
-  },
-  {
-    title: "Test value #2",
-    id: 2,
-  },
-];
-const onClick = jest.fn();
-const onChange = jest.fn();
-const onClickDelete = jest.fn();
+  const tasks = [
+    {
+      title: 'Test value #1',
+      id: 1,
+    },
+    {
+      title: 'Test value #2',
+      id: 2,
+    },
+  ];
+  const onClick = jest.fn();
+  const onChange = jest.fn();
+  const onClickDelete = jest.fn();
 
-const { container } = render((
-  <Page
-    tasks = {tasks}
-    onClick = {onClick}
-    onChange = {onChange}
-    onClickDelete= {onClickDelete}
-  />
-));
+  const { container } = render((
+    <Page
+      tasks={tasks}
+      onClick={onClick}
+      onChange={onChange}
+      onClickDelete={onClickDelete}
+    />
+  ));
 
-expect(container).toHaveTextContent("To-do");
-  expect(container).toHaveTextContent("Test value #1");
-  expect(container).toHaveTextContent("Test value #2");
+  expect(container).toHaveTextContent('To-do');
+  expect(container).toHaveTextContent('Test value #1');
+  expect(container).toHaveTextContent('Test value #2');
 });
