095ee65e11b13ce059f77fe7dfbf64a39f016d98
juunini <juuni.ni.i@gmail.com>
2020-10-10 20:13:33 +0900

    Change `App.test.jsx` test case

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -5,51 +5,62 @@ import { render, fireEvent } from '@testing-library/react';
 import App from './App';
 
 describe('App', () => {
-  const placeholderText = '할 일을 입력해 주세요';
-  const emptyTasksText = '할 일이 없어요!';
-  const addTaskButtonText = '추가';
-  const deleteTaskButtonText = '완료';
+  const renderApp = () => render(<App />);
   const tasks = [
     '아무것도 안하기',
     '더욱 더 아무것도 안하기',
     '본격적으로 아무것도 안하기',
   ];
 
-  it('check elements', () => {
-    const { getByText, getByPlaceholderText } = render(<App />);
+  it('"할 일이 없어요!" 확인', () => {
+    const { getByText } = renderApp();
 
-    getByText(emptyTasksText);
-    getByText(addTaskButtonText);
-    getByPlaceholderText(placeholderText);
+    getByText('할 일이 없어요!');
   });
 
-  context('function test', () => {
-    it('tasks 추가 후 전부 삭제하는 시나리오', () => {
-      const { getByText, getAllByText, getByRole } = render(<App />);
+  it('할 일을 입력하고 추가를 누르면 할 일 목록에 추가한 목록이 보인다.', () => {
+    const { getByRole, getByText } = renderApp();
 
-      tasks.forEach((task, i) => {
-        const input = getByRole('textbox');
+    fireEvent.change(
+      getByRole('textbox'),
+      { target: { value: tasks[0] } },
+    );
+    fireEvent.click(getByText('추가'));
 
-        fireEvent.change(input, { target: { value: task } });
+    getByText(tasks[0]);
+  });
 
-        expect(input.value).toEqual(task);
+  it('할 일을 입력하고 추가를 누르면 input의 텍스트가 지워진다.', () => {
+    const { getByRole, getByText } = renderApp();
 
-        fireEvent.click(getByText(addTaskButtonText));
+    fireEvent.change(
+      getByRole('textbox'),
+      { target: { value: tasks[0] } },
+    );
+    fireEvent.click(getByText('추가'));
 
-        getByText(task);
-        expect(input.value).toEqual('');
-        expect(getAllByText(deleteTaskButtonText))
-          .toHaveLength(i + 1);
-      });
-      tasks.forEach((task) => getByText(task));
+    expect(getByRole('textbox').value).toBe('');
+  });
 
-      const deleteTaskButtons = getAllByText(deleteTaskButtonText);
+  it('할 일을 완료하면 할 일이 목록에서 보이지 않는다.', () => {
+    const {
+      getByRole, queryByText, getByText, getAllByText,
+    } = renderApp();
 
-      expect(deleteTaskButtons).toHaveLength(3);
+    tasks.forEach((task) => {
+      fireEvent.change(
+        getByRole('textbox'),
+        { target: { value: task } },
+      );
+      fireEvent.click(getByText('추가'));
+    });
 
-      deleteTaskButtons.forEach((button) => fireEvent.click(button));
+    getAllByText('완료').forEach((button) => {
+      fireEvent.click(button);
+    });
 
-      getByText(emptyTasksText);
+    tasks.forEach((task) => {
+      expect(queryByText(task)).toBe(null);
     });
   });
 });
