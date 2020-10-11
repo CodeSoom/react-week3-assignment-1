5cda3cb60f964e4addb782592a36312a5fba8d46
goodzzong <goodzzong@gmail.com>
2020-10-12 00:50:22 +0900

    Update App.test.jsx
    App 컴포넌트 테스트 코드 작성

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,22 +1,40 @@
 /*
  * TODO:
- * 1. 처음 렌더링이 되면
- * 	 1-1. 할 일이 없을 때
- * 		1-1-1. To-do 타이틀이 보여야 한다.
- *    1-1-2. 할 일 이라는 입력창 타이틀과 입력창이 보여야 한다.
- *    1-1.3 할 일이 없어요! 라는 placeholder 값이 보여야 한다.
- *   1-2. 할 일이 있을 때
- * 		 1-2-1. 할 일 목록에 할일이 보여야 한다.
- *     1-2-2. 할 일 목록의 갯수만큼 완료 버튼이 존재 해야 한다.
- * 2. 글을 추가 할 때
- *   2-1. 글을 입력할 때
- *     2-1-1. 입력한 글이 입력창에 보여야 한다.
- *   2-2. 추가 버튼 클릭 시
- *     2-2-1. 할 일 목록에 보여야 한다.
- *     2-2-2. 추가된 글과 함께 완료 버튼도 보여야 한다.
- *     2-2-3. 입력창의 입력값이 비워지고 할 일이 없어요! 라는 placeholder 값이 보여야 한다.
- * 3. 글을 삭제 할 때
- *   3-1. 완료 버튼 클릭 시
- * 	   3-1-1. 해당 글이 삭제 되야 한다.
- *     3-1-2. 모든 글이 완료되어 삭제 되면 할 일이 없어요! 라는 placeholder 값이 보여야 한다.
+ * 1. 렌더링이 되면
+ *   1-1. To-do 타이틀이 보여야 한다.
+ *   1-2. 할 일 이라는 입력창 타이틀과 입력창이 보여야 한다.
+ *   1-3. 추가 라는 버튼이 보여야 한다.
+ *   1-4. 할 일이 없어요! 라는 글 목록이 보여야 한다.
  */
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import App from './App';
+
+describe('App', () => {
+  const handleChangeTitle = jest.fn();
+  const handleClickAddTask = jest.fn();
+  const handleClickDeleteTask = jest.fn();
+
+  const renderHelper = (taskTitle = '', tasks = []) => render((
+    <App
+      taskTitle={taskTitle}
+      tasks={tasks}
+      onChangeTitle={handleChangeTitle}
+      onClickAddTask={handleClickAddTask}
+      onClickDeleteTask={handleClickDeleteTask}
+    />
+  ));
+
+  context('렌더링 되면', () => {
+    it('초기 화면에 렌더링 되야하는 텍스트를 보여준다.', () => {
+      const { container } = renderHelper();
+
+      expect(container).toHaveTextContent('To-do');
+      expect(container).toHaveTextContent('할 일');
+      expect(container).toHaveTextContent('추가');
+      expect(container).toHaveTextContent('할 일이 없어요!');
+    });
+  });
+});
