150257e4653cba04d06a35e17383b5f440e27cb9
davidyang2149 <davidyang2149@gmail.com>
2020-10-11 12:52:26 +0900

    Refactor Input.test.jsx
    3강 테스트 - 3주 차 과제 1
    - Input.test.jsx 리팩토링.

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -5,61 +5,45 @@ import { render, fireEvent } from '@testing-library/react';
 import Input from './Input';
 
 describe('Input Component', () => {
-  context('Initialize', () => {
-    it('render', () => {
-      const taskTitle = '';
-      const handleClick = jest.fn();
-      const handleChange = jest.fn();
+  const handleClick = jest.fn();
+  const handleChange = jest.fn();
 
-      const { getByLabelText, getByText, getByPlaceholderText } = render(
-        <Input
-          value={taskTitle}
-          onChange={handleChange}
-          onClick={handleClick}
-        />,
-      );
+  const renderInput = (value) => render((
+    <Input
+      value={value}
+      onChange={handleChange}
+      onClick={handleClick}
+    />
+  ));
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  context('when render', () => {
+    it('Initialize', () => {
+      const { getByLabelText, getByText, getByPlaceholderText } = renderInput('');
 
       expect(getByLabelText('할 일')).toBeInTheDocument();
       expect(getByText('추가')).toBeInTheDocument();
       expect(getByPlaceholderText('할 일을 입력해 주세요')).toBeInTheDocument();
     });
-  });
 
-  context('onChange', () => {
-    it('taskTitle', () => {
+    it('onChange taskTitle', () => {
       const taskTitle = '추가된 할 일';
-      const handleClick = jest.fn();
-      const handleChange = jest.fn();
 
-      const { getByPlaceholderText } = render(
-        <Input
-          value={taskTitle}
-          onChange={handleChange}
-          onClick={handleClick}
-        />,
-      );
+      const { getByPlaceholderText } = renderInput(taskTitle);
 
       const input = getByPlaceholderText('할 일을 입력해 주세요');
 
       expect(handleChange).not.toHaveBeenCalled();
       fireEvent.change(input, { target: { value: taskTitle } });
-      expect(input).toHaveAttribute('value', '추가된 할 일');
+      expect(input).toHaveValue(taskTitle);
     });
-  });
 
-  context('onClick', () => {
-    it('add task', () => {
+    it('onClick addTask', () => {
       const taskTitle = '추가된 할 일';
-      const handleClick = jest.fn();
-      const handleChange = jest.fn();
-
-      const { getByText } = render(
-        <Input
-          value={taskTitle}
-          onChange={handleChange}
-          onClick={handleClick}
-        />,
-      );
+      const { getByText } = renderInput(taskTitle);
 
       expect(handleClick).not.toHaveBeenCalled();
       fireEvent.click(getByText('추가'));
