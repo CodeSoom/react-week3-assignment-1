2b5df8bd83c0e684dce04efbcda057854d81d033
davidyang2149 <davidyang2149@gmail.com>
2020-10-11 20:42:00 +0900

    Refactor Page.test.jsx
    3강 테스트 - 3주 차 과제 1
    - Page.test.jsx 리팩토링.

--- src/Page.test.jsx
+++ src/Page.test.jsx
@@ -5,24 +5,26 @@ import { render } from '@testing-library/react';
 import Page from './Page';
 
 describe('Page Component', () => {
-  it('render', () => {
-    const tasks = [];
-    const taskTitle = '';
+  context('when render', () => {
+    it('Initialize', () => {
+      const tasks = [];
+      const taskTitle = '';
 
-    const handleChangeTitle = jest.fn();
-    const handleClickAddTask = jest.fn();
-    const handleClickDeleteTask = jest.fn();
+      const handleChangeTitle = jest.fn();
+      const handleClickAddTask = jest.fn();
+      const handleClickDeleteTask = jest.fn();
 
-    const { getByText } = render(
-      <Page
-        taskTitle={taskTitle}
-        tasks={tasks}
-        onChangeTitle={handleChangeTitle}
-        onClickAddTask={handleClickAddTask}
-        onClickDeleteTask={handleClickDeleteTask}
-      />,
-    );
+      const { getByText } = render(
+        <Page
+          taskTitle={taskTitle}
+          tasks={tasks}
+          onChangeTitle={handleChangeTitle}
+          onClickAddTask={handleClickAddTask}
+          onClickDeleteTask={handleClickDeleteTask}
+        />,
+      );
 
-    expect(getByText('To-do')).toBeInTheDocument();
+      expect(getByText('To-do')).toBeInTheDocument();
+    });
   });
 });
