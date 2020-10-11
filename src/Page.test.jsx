5c6e478843096f491130407d0e7d164904379b70
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-11 14:37:23 +0900

    Refactoring Page.test.jsx
    라벨과 display value를 둘다 확인 할 수 있는 로직으로 바꾸었습니다.

--- src/Page.test.jsx
+++ src/Page.test.jsx
@@ -14,7 +14,7 @@ test('Page', () => {
     { id: 2, title: '두번째 할 일' },
   ];
 
-  const { getByText } = render((
+  const { getByText, getByLabelText } = render((
     <Page
       tasks={tasks}
       taskTitle="세번째 할 일"
@@ -25,7 +25,7 @@ test('Page', () => {
   ));
 
   expect(getByText('To-do')).not.toBeNull();
-  expect(getByText('할 일')).not.toBeNull();
+  expect(getByLabelText('할 일')).toHaveDisplayValue('세번째 할 일');
 
   expect(getByText(tasks[0].title)).not.toBeNull();
   expect(getByText(tasks[1].title)).not.toBeNull();
