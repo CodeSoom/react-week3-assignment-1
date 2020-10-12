bb2ee36448766dcaf605b5704143fe8f0f646b9d
koremp <im.koremp@gmail.com>
2020-10-12 20:05:53 +0900

    Update App test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,13 +1,51 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 import App from './App';
 
-test('App', () => {
-  const { getByText } = render((
-    <App />
-  ));
+describe('App', () => {
+  context('do not use tasks', () => {
+    it('renders "추가" button', () => {
+      const { getByText } = render((
+        <App />
+      ));
 
-  expect(getByText(/추가/)).not.toBeNull();
+      expect(getByText(/추가/)).not.toBeNull();
+    });
+  });
+
+  context('use tasks', () => {
+    it('add task after click "추가" button', () => {
+      const { getByLabelText, getByText } = render((
+        <App />
+      ));
+
+      fireEvent.change(getByLabelText('할 일'), {
+        target: { value: '무언가 하기' },
+      });
+
+      fireEvent.click(getByText('추가'));
+
+      expect(getByText(/무언가 하기/)).not.toBeNull();
+    });
+
+    it('remove task after click "완료" button', () => {
+      const { getByLabelText, getByText } = render((
+        <App />
+      ));
+
+      fireEvent.change(getByLabelText('할 일'), {
+        target: { value: '무언가 하기' },
+      });
+
+      fireEvent.click(getByText('추가'));
+
+      expect(getByText(/무언가 하기/)).not.toBeNull();
+
+      fireEvent.click(getByText('완료'));
+
+      expect(getByText(/할 일이 없어요!/)).not.toBeNull();
+    });
+  });
 });
