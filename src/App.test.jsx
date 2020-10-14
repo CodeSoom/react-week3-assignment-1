815a573044b3ff77d87749add6aaabe32e9fb72d
thkim <thkim2017@gmail.com>
2020-10-14 12:56:39 +0900

    modify the App.test.jsx ----------------------- accomplished to make coverage 100%

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,13 +1,60 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 import App from './App';
 
-test('App', () => {
-  const { getByText } = render((
-    <App />
-  ));
+describe('App', () => {
+  context('when being initialized', () => {
+    it('shows no-tasks-message', () => {
+      const { getByText } = render((
+        <App />
+      ));
+      expect(getByText('할 일이 없어요!')).not.toBeNull();
+    });
+  });
 
-  expect(getByText(/추가/)).not.toBeNull();
+  context('when recieving input value', () => {
+    it('shows that in inputbox', () => {
+      const { getByPlaceholderText } = render((
+        <App />
+      ));
+      const inputBox = getByPlaceholderText('할 일을 입력해 주세요');
+      expect(inputBox).toHaveDisplayValue('');
+
+      fireEvent.change(inputBox, { target: { value: '뭐라도 하기' } });
+      expect(inputBox).toHaveDisplayValue('뭐라도 하기');
+    });
+  });
+
+  context('when clicking "추가" button', () => {
+    it('adds a task', () => {
+      const { getByText, getByPlaceholderText, container } = render((
+        <App />
+      ));
+
+      const inputBox = getByPlaceholderText('할 일을 입력해 주세요');
+
+      fireEvent.change(inputBox, { target: { value: '뭐라도 하기' } });
+      fireEvent.click(getByText('추가'));
+
+      expect(container).toHaveTextContent('뭐라도 하기');
+    });
+  });
+
+  context('when clicking "완료" button', () => {
+    it('removes the task', () => {
+      const { getByText, getByPlaceholderText } = render((
+        <App />
+      ));
+
+      const inputBox = getByPlaceholderText('할 일을 입력해 주세요');
+
+      fireEvent.change(inputBox, { target: { value: '뭐라도 하기' } });
+      fireEvent.click(getByText('추가'));
+      fireEvent.click(getByText('완료'));
+
+      expect(getByText('할 일이 없어요!')).not.toBeNull();
+    });
+  });
 });
