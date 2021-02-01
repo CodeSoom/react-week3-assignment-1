44ed118bb520990c6a30af1c0658dfe14aa1a190
Blueberry <54464320+lee01097@users.noreply.github.com>
2021-02-02 05:41:18 +0900

    List.test.jsx 테스트 케이스 추가: tasks가 있는 경우의 테스트 케이스 추가(filledList)

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -4,12 +4,18 @@ import { render } from '@testing-library/react';
 
 import List from './List';
 
-test('List', () => {
+test('emptyList', () => {
   const tasks = [];
-
   const { container } = render((
     <List tasks={tasks} />
   ));
-
   expect(container).toHaveTextContent('할 일이 없어요!');
+});
+
+test('filledList', () => {
+  const tasks = [{ id: 1, title: '코드짜기' }];
+  const { container } = render((
+    <List tasks={tasks} />
+  ));
+  expect(container).toHaveTextContent('코드짜기');
 });
