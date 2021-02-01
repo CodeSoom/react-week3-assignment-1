c7a226d51e0748cbe1448f1db9eb52d77f1bd91b
shinhyogeun <tls1gy2rms3@naver.com>
2021-02-02 01:33:32 +0900

    refactor : InputComponent Test작성

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -4,24 +4,33 @@ import { render, fireEvent } from '@testing-library/react';
 
 import Input from './Input';
 
-describe('<Input />', () => {
-  const value = '';
-  const onChange = jest.fn();
-  const onClick = jest.fn();
+const value = '';
+const onChange = jest.fn();
+const onClick = jest.fn();
 
+test('인풋에 변경이 일어나면 onChange함수가 실행된다.', () => {
   const { getByPlaceholderText } = render((<Input
     value={value}
     onChange={onChange}
     onClick={onClick}
   />));
 
-  it('If Input change then onChange function occur?', () => {
-    const input = getByPlaceholderText('할 일을 입력해 주세요');
-    fireEvent.change(input, {
-      target: {
-        value: '누워있기',
-      },
-    });
-    expect(input).toHaveAttribute('value', '누워있기');
+  fireEvent.change(getByPlaceholderText('할 일을 입력해 주세요'), {
+    target: {
+      value: '누워있기',
+    },
   });
+  expect(onChange).toBeCalled();
+});
+
+test('추가버튼을 클릭하면 onClick함수가 실행된다.', () => {
+  const { getByText } = render((<Input
+    value={value}
+    onChange={onChange}
+    onClick={onClick}
+  />));
+
+  expect(onClick).not.toBeCalled();
+  fireEvent.click(getByText('추가'));
+  expect(onClick).toBeCalled();
 });
