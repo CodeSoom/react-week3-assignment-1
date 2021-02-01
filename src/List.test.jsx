7a7a95a3805b2e9ff4d0468bbfe92585ba1a72ad
EL <ickyicky@gmail.com>
2021-02-01 21:10:39 +0900

    TST: 리스트- 테스트 실패하기

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -4,8 +4,20 @@ import { render } from '@testing-library/react';
 
 import List from './List';
 
-test('List', () => {
+test('tasks 없을 때', () => {
   const tasks = [];
+
+  const { container } = render((
+    <List
+      tasks={tasks}
+    />
+  ));
+
+  expect(container).toHaveTextContent('할 일이 없어요!');
+});
+
+test('tasks exist', () => {
+  const tasks = [{ id: Date.now(), title: '할일1' }];
 
   const { container } = render((
     <List
