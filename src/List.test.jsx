39dfc99d091eb3b8a50cdd521b6ab16c62a37c2f
dodoongtak <themighty.tak@gmail.com>
2021-02-01 18:32:57 +0900

    🔵 Refactor - Prompts a message that tasks are empty
    Assign empty array to variable 'tasks'.
    
    Replace the value of prop 'tasks' by referencing variable tasks.

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -5,10 +5,10 @@ import { render } from '@testing-library/react';
 import List from './List';
 
 test('List', () => {
-  const tasks = undefined;
+  const tasks = [];
 
   const { container } = render((
-    <List tasks={[]} />
+    <List tasks={tasks} />
   ));
 
   expect(container).toHaveTextContent('할 일이 없어요!');
