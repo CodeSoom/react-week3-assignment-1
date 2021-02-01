570b8f7dd78ffd8ab6884b2ec913b08d8375ced9
Blueberry <54464320+lee01097@users.noreply.github.com>
2021-02-02 03:15:25 +0900

    List.test.jsx 추가 및 테스트 케이스(리스트가 비어있을 시의 경우) 작동 확인

---
+++ src/List.test.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import List from './List';
+
+test('List', () => {
+  const tasks = [];
+
+  const { container } = render((
+    <List tasks={tasks} />
+  ));
+
+  expect(container).toHaveTextContent('할 일이 없어요!');
+});
