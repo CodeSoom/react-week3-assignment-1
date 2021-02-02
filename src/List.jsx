3b1483e8067965b36c38eaf6b048953833d54b8b
dodoongtak <themighty.tak@gmail.com>
2021-02-02 15:18:30 +0900

    🟢 Pass - List with null type

--- src/List.jsx
+++ src/List.jsx
@@ -3,6 +3,11 @@ import React from 'react';
 import Item from './Item';
 
 export default function List({ tasks, onClickDelete }) {
+  if (tasks === null) {
+    return (
+      <p>할 일이 없어요!</p>
+    );
+  }
   if (tasks.length === 0) {
     return (
       <p>할 일이 없어요!</p>
