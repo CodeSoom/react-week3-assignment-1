6d86d6eeed240e0339ef8db854d3109a21e7c6eb
dodoongtak <themighty.tak@gmail.com>
2021-02-02 16:29:26 +0900

    Refactor #9 - Transform to describe-context-it format

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -15,42 +15,50 @@ const renderListTemplate = (items = []) => render((
   <List tasks={items} onClickDelete={onClickDelete} />
 ));
 
-test('Empty List', () => {
-  const { container } = renderListTemplate();
+describe('List Component', () => {
+  context('when null type', () => {
+    it('prompts empty message', () => {
+      const { container } = renderListTemplate(null);
 
-  expect(container).toHaveTextContent('할 일이 없어요!');
-});
-
-test('List with tasks', () => {
-  const { container } = renderListTemplate(tasks);
-
-  tasks.forEach(({ title }) => {
-    expect(container).toHaveTextContent(title);
-  });
-});
-
-test('List with 완료 buttons', () => {
-  const { getAllByText } = renderListTemplate(tasks);
-
-  const completeButtonLength = getAllByText('완료').length;
-
-  expect(completeButtonLength).toBe(tasks.length);
-});
-
-test('Buttons with event', () => {
-  const { getAllByText } = renderListTemplate(tasks);
-
-  const completeButton = getAllByText('완료');
-
-  completeButton.forEach((button) => {
-    fireEvent.click(button);
+      expect(container).toHaveTextContent('할 일이 없어요!');
+    });
   });
 
-  expect(onClickDelete).toBeCalledWith(tasks.length);
-});
+  context('without a task', () => {
+    it('prompts empty message', () => {
+      const { container } = renderListTemplate();
 
-test('List with null type', () => {
-  const { container } = renderListTemplate(null);
+      expect(container).toHaveTextContent('할 일이 없어요!');
+    });
+  });
 
-  expect(container).toHaveTextContent('할 일이 없어요!');
+  context('with tasks', () => {
+    it('render title of the tasks', () => {
+      const { container } = renderListTemplate(tasks);
+
+      tasks.forEach(({ title }) => {
+        expect(container).toHaveTextContent(title);
+      });
+    });
+
+    it('render 완료 button', () => {
+      const { getAllByText } = renderListTemplate(tasks);
+
+      const completeButtonLength = getAllByText('완료').length;
+
+      expect(completeButtonLength).toBe(tasks.length);
+    });
+
+    it('buttons have event', () => {
+      const { getAllByText } = renderListTemplate(tasks);
+
+      const completeButton = getAllByText('완료');
+
+      completeButton.forEach((button) => {
+        fireEvent.click(button);
+      });
+
+      expect(onClickDelete).toBeCalledWith(tasks.length);
+    });
+  });
 });
