0918a13efa2ca608e24da1fcd0740c000a19314b
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-02-02 14:09:18 +0900

    :white_check_mark: Test: Input.jsx

---
+++ src/Input.test.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import {
+  fireEvent, render, screen,
+} from '@testing-library/react';
+
+import Input from './Input';
+
+test('1. input 컴포넌트 출력확인 ', () => {
+  const onChange = jest.fn();
+  render(<Input value="할일!" onChange={onChange} />);
+
+  const label = screen.getByText('할 일');
+  const input = screen.getByPlaceholderText('할 일을 입력해 주세요');
+  const button = screen.getByRole('button');
+
+  expect(label).toHaveTextContent('할 일');
+  expect(input.value).toBe('할일!');
+  expect(button).toHaveTextContent('추가');
+});
+
+test('2. input 입력 확인', () => {
+  const onChange = jest.fn();
+  render(<Input onChange={onChange} />);
+
+  const input = screen.getByPlaceholderText('할 일을 입력해 주세요');
+  fireEvent.change(input, { target: { value: 'test code 작성하기' } });
+
+  expect(onChange).toHaveBeenCalledTimes(1);
+  expect(input.value).toBe('test code 작성하기');
+});
+
+test('3. 버튼클릭 확인', () => {
+  const onClick = jest.fn();
+  render(<Input onClick={onClick} />);
+  fireEvent.click(screen.getByRole('button'));
+
+  expect(onClick).toHaveBeenCalledTimes(1);
+});
