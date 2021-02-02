11ba153b38862e51e371b1edae65811562908aba
dodoongtak <themighty.tak@gmail.com>
2021-02-02 19:33:17 +0900

    🟢 Pass - Add button appears
    Could not figure out fail process before pass and refactor

---
+++ src/Input.test.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import Input from './Input';
+
+const onChange = jest.fn();
+const onClick = jest.fn();
+
+const renderInputTemplate = (value = '') => render((
+  <Input value={value} onChange={onChange} onClick={onClick} />
+));
+
+describe('Input Component', () => {
+  context('when render', () => {
+    it('add button appears', () => {
+      const { getByText } = renderInputTemplate();
+
+      const addButton = getByText('추가');
+
+      expect(addButton).toHaveTextContent('추가');
+    });
+  });
+});
