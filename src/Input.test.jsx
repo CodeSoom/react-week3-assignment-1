ccb602bb99cd3b1fdd981db075c067fd9d104a0c
Yoon <yoonhe@gmail.com>
2021-02-02 21:03:00 +0900

    create Input Test Case

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -4,33 +4,60 @@ import { render, fireEvent } from '@testing-library/react';
 
 import Input from './Input';
 
-test('Item', () => {
-  const value = '';
-  const handleChange = jest.fn();
-  const handleClickAddTask = jest.fn();
+/**
+ * 테스트 목록
+ *
+ * Input에
+ *  value가 없을때
+ *    placehoder에 "할 일을 입력해 주세요" 가 노출되는지
+ *  value가 변경되었을때
+ *    onChange 함수가 실행되는지
+ *  button을 클릭했을때
+ *   onClick 함수가 실행되는지
+ */
+const handleChangeInput = jest.fn();
+const handleClickAddTask = jest.fn();
+const value = '';
 
-  const { getByPlaceholderText, getByText } = render(<Input
+const renderInput = () => render(
+  <Input
     value={value}
-    onChange={handleChange}
+    onChange={handleChangeInput}
     onClick={handleClickAddTask}
-  />);
+  />,
+);
 
-  const input = getByPlaceholderText('할 일을 입력해 주세요');
-  const button = getByText('추가');
+describe('Input', () => {
+  context('without value', () => {
+    it('placeholder is displayed', () => {
+      const { getByPlaceholderText } = renderInput();
 
-  expect(input).toBeInTheDocument();
+      expect(getByPlaceholderText('할 일을 입력해 주세요')).toBeInTheDocument();
+    });
+  });
 
-  fireEvent.change(input, { target: { value: '홈트하기' } });
+  context('with value change', () => {
+    it('onChange function run', () => {
+      const { getByPlaceholderText } = renderInput();
 
-  expect(input).toBeInTheDocument('홈트하기');
+      fireEvent.change(getByPlaceholderText('할 일을 입력해 주세요'), { target: { value: '홈트하기' } });
 
-  // 공식문서를 보고 아래와같이 따라해보았는데.. 왜 안되는지 모르겠습니다..
-  // https://testing-library.com/docs/example-input-event/
-  // expect(input.value).toBe('홈트하기');
+      expect(handleChangeInput).toBeCalled();
 
-  fireEvent.click(button);
+      /**
+       * 질문) 이부분에서 handleChangeInput가 실행되었을때 `input`의 `value`로 `홈트하기`가
+       * 들어오는것은 `state` 관련된 것이므로 `Input`에서 테스트할 필요가 없나요?
+       *
+      */
+    });
+  });
 
-  expect(handleClickAddTask).toBeCalled();
-  // handleClickAddTask가 실행되면 tasks에 task가 추가되는 것까지 테스트를 Input에서 하나요?
-  // 그 테스트를 하려면 어떻게 해야 하는지 감이 잡히지 않습니다... ㅜㅜ
+  context('When the button is clicked', () => {
+    it('onClick function run', () => {
+      const { getByText } = renderInput();
+
+      fireEvent.click(getByText('추가'));
+      expect(handleClickAddTask).toBeCalled();
+    });
+  });
 });
