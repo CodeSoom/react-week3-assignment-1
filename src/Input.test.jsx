3aa2079e0f3d672d1e95d208da023be914b8931b
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-02-02 22:51:32 +0900

    Pass : Test button '추가' with handler function
    통과 : '추가'버튼이 클릭되면 handler함수가 실행되는지 테스트합니다

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -4,12 +4,16 @@ import { fireEvent, render } from '@testing-library/react';
 import Input from './Input';
 
 test('Input', () => {
-  const { container } = render(
-    <Input />,
+  const handleClick = jest.fn();
+
+  const { container, getByText } = render(
+    <Input
+      onClick={handleClick}
+    />,
   );
   expect(container).toHaveTextContent('할 일');
   expect(container).toHaveTextContent('추가');
 
-  fireEvent.click('완료');
+  fireEvent.click(getByText('추가'));
   expect(handleClick).toBeCalled();
 });
