34f951b2dcc3a04c55cb51b0b1366b14f63b7ef4
datalater <the7mincheol@gmail.com>
2021-02-02 15:26:13 +0900

    Input 컴포넌트의 추가 버튼 클릭을 테스트하라

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -5,16 +5,22 @@ import { render, fireEvent } from '@testing-library/react';
 import Input from './Input';
 
 test('Input', () => {
-  const handleChangeTitle = jest.fn();
+  const handleChange = jest.fn();
+  const handleClick = jest.fn();
 
-  const { getByLabelText } = render((
+  const { getByLabelText, getByText } = render((
     <Input
-      onChange={handleChangeTitle}
+      onChange={handleChange}
+      onClick={handleClick}
     />
   ));
 
   const input = getByLabelText('할 일');
-
   fireEvent.change(input, { target: { value: '뭐라도 하기' } });
   expect(input.value).toBe('뭐라도 하기');
+  expect(handleClick).not.toBeCalled();
+
+  const button = getByText('추가');
+  fireEvent.click(button);
+  expect(handleClick).toBeCalled();
 });
