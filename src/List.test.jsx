5363b3f4f6bbcbf1b89de0a625bf5f12a1e51a75
최창현 <chchoi_einz@einz.co.kr>
2021-02-02 13:39:56 +0900

    TST: Add describe context it

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -4,26 +4,45 @@ import { render } from '@testing-library/react';
 
 import List from './List';
 
-test('tasks 없을 때', () => {
-  const tasks = [];
 
-  const { container } = render((
-    <List
-      tasks={tasks}
-    />
-  ));
+describe('List', () => {
+  context('tasks의 length가 0인 경우', () => {
+    const tasks = [];
 
-  expect(container).toHaveTextContent('할 일이 없어요!');
-});
+    it('"할 일이 없어요!"를 표시한다.', () => {
+      const { container } = render((
+        <List
+          tasks={tasks}
+        />
+      ));
 
-test('tasks exist', () => {
-  const tasks = [{ id: Date.now(), title: '할일1' }];
+      expect(container).toHaveTextContent('할 일이 없어요!');
+    });
+  });
 
-  const { container } = render((
-    <List
-      tasks={tasks}
-    />
-  ));
+  context('tasks length가 1 이상인 경우', () => {
+    const tasks = [
+      {
+        id: 1,
+        title: '공부하기',
+      },
+      {
+        id: 2,
+        title: '운동하기',
+      }
+    ];
 
-  expect(container).toHaveTextContent('할일1');
+    it('"공부하기!"를 표시한다.', () => {
+      const { container } = render((
+        <List
+          tasks={tasks}
+        />
+      ));
+
+      expect(container).toHaveTextContent('공부하기');
+      expect(container).toHaveTextContent('운동하기');
+    });
+
+  });
+
 });
