e83f09dd536641cd5a89d93e5bc0868166446e31
shinhyogeun <tls1gy2rms3@naver.com>
2021-02-02 17:02:45 +0900

    refactor : Input.test.jsx에 describe-it 적용

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -4,33 +4,35 @@ import { render, fireEvent } from '@testing-library/react';
 
 import Input from './Input';
 
-const value = '';
 const onChange = jest.fn();
 const onClick = jest.fn();
 
-test('인풋에 변경이 일어나면 onChange함수가 실행된다.', () => {
-  const { getByPlaceholderText } = render((<Input
+function showInputWith(value, handleChange = onChange, handleClick = onClick) {
+  return render((<Input
     value={value}
-    onChange={onChange}
-    onClick={onClick}
+    onChange={handleChange}
+    onClick={handleClick}
   />));
+}
 
-  fireEvent.change(getByPlaceholderText('할 일을 입력해 주세요'), {
-    target: {
-      value: '누워있기',
-    },
+describe('Input에서', () => {
+  it('인풋에 변경이 일어나면 onChange함수가 실행된다.', () => {
+    const { getByPlaceholderText } = showInputWith('밥먹기');
+
+    expect(onChange).not.toBeCalled();
+    fireEvent.change(getByPlaceholderText('할 일을 입력해 주세요'), {
+      target: {
+        value: '누워있기',
+      },
+    });
+    expect(onChange).toBeCalled();
   });
-  expect(onChange).toBeCalled();
-});
 
-test('추가버튼을 클릭하면 onClick함수가 실행된다.', () => {
-  const { getByText } = render((<Input
-    value={value}
-    onChange={onChange}
-    onClick={onClick}
-  />));
+  it('추가버튼을 클릭하면 onClick함수가 실행된다.', () => {
+    const { getByText } = showInputWith('밥먹기');
 
-  expect(onClick).not.toBeCalled();
-  fireEvent.click(getByText('추가'));
-  expect(onClick).toBeCalled();
+    expect(onClick).not.toBeCalled();
+    fireEvent.click(getByText('추가'));
+    expect(onClick).toBeCalled();
+  });
 });
