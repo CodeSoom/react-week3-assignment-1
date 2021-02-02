a90786d6196d24f56773b2a25c2d54e1b3f9866e
dodoongtak <themighty.tak@gmail.com>
2021-02-02 13:34:05 +0900

    Remove #2 - Clicking 완료 button invokes function case
    This case does not involve with conerns that List component puts on.
    
    The implementation of function must be understood by List Component.

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -27,19 +27,3 @@ test('List with tasks', () => {
   expect(container).toHaveTextContent('어서와 TDD는 처음이지?');
   expect(container).toHaveTextContent('아직 시작도 안했어 ^^');
 });
-
-test('Clicking 완료 button invokes onClickDelete function', () => {
-  const tasks = [
-    { id: 1, title: '어서와 TDD는 처음이지?' },
-  ];
-
-  const { container, getByText } = renderTemplate(tasks);
-
-  expect(onClickDelete).not.toBeCalled();
-
-  expect(container).toHaveTextContent('어서와 TDD는 처음이지?');
-
-  fireEvent.click(getByText('완료'));
-
-  expect(onClickDelete).toBeCalledWith(1);
-});
