69cec6a3d2a5ecea70c7e059355e7a8e3fd0febf
datalater <the7mincheol@gmail.com>
2021-02-03 14:24:35 +0900

    List 컴포넌트 테스트를 describe-context-it으로 작성하라
    RSpect에서 유래한 스타일 가이드인 dsecribe, context, it을
    사용하여 테스트 코드에 계층성을 부여하여 작성한다.

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -4,44 +4,55 @@ import { fireEvent, render } from '@testing-library/react';
 
 import List from './List';
 
-test('List shows message when tasks is empty', () => {
-  const emptyTasks = [];
-
-  const { container } = render((
-    <List
-      tasks={emptyTasks}
-    />
-  ));
-  expect(container).toHaveTextContent('할 일이 없어요!');
-});
-
-test('List shows tasks', () => {
-  const tasks = [
-    {
-      id: 1,
-      title: '아무것도 하지 않기 #1',
-    },
-    {
-      id: 2,
-      title: '아무것도 하지 않기 #2',
-    },
-  ];
-
+describe('List', () => {
   const handleClickDelete = jest.fn();
 
-  const { container, getAllByText } = render((
-    <List
-      tasks={tasks}
-      onClickDelete={handleClickDelete}
-    />
-  ));
+  function renderList(tasks) {
+    return render((
+      <List
+        tasks={tasks}
+        onClickDelete={handleClickDelete}
+      />
+    ));
+  }
 
-  expect(container).toHaveTextContent('아무것도 하지 않기 #1');
-  expect(container).toHaveTextContent('아무것도 하지 않기 #2');
+  context('tasks가 없는 경우', () => {
+    const tasks = [];
 
-  const buttons = getAllByText('완료');
-  buttons.forEach((button) => {
-    fireEvent.click(button);
-    expect(handleClickDelete).toBeCalled();
+    it('"할 일이 없어요!"를 표시한다.', () => {
+      const { container } = renderList(tasks);
+      expect(container).toHaveTextContent('할 일이 없어요!');
+    });
+  });
+
+  context('tasks가 있는 경우', () => {
+    const tasks = [
+      {
+        id: 1,
+        title: '아무것도 하지 않기 #1',
+      },
+      {
+        id: 2,
+        title: '아무것도 하지 않기 #2',
+      },
+    ];
+
+    it('tasks를 보여준다.', () => {
+      const { container } = renderList(tasks);
+
+      expect(container).toHaveTextContent('아무것도 하지 않기 #1');
+      expect(container).toHaveTextContent('아무것도 하지 않기 #2');
+    });
+
+    it('"완료" 버튼을 누르면 id 파라미터를 가진 핸들러가 실행된다.', () => {
+      const { getAllByText } = renderList(tasks);
+
+      const buttons = getAllByText('완료');
+
+      buttons.forEach((button) => {
+        fireEvent.click(button);
+        expect(handleClickDelete).toBeCalledWith(1);
+      });
+    });
   });
 });
