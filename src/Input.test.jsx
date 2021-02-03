7875b6718d0424e2b1b19b73f54dec51523c03df
shinhyogeun <tls1gy2rms3@naver.com>
2021-02-03 19:45:39 +0900

    refactor : Lint오류 수정

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { render, fireEvent, getAllByPlaceholderText } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import Input from './Input';
 
