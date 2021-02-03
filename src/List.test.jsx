13667b3aa1831340710bb4dbc5b5b14be1fc45ed
jjkim01 <jaen6563@gmail.com>
2021-02-04 00:05:22 +0900

    List 커버리지 100%

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
 
-import { render, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 import List from './List';
 
-test('List', () => {
+test('List has more than one item', () => {
+  const tasks = [
+    { id: 1, title: 'testTitle' },
+  ];
+
+  const { container } = render((
+    <List
+      tasks={tasks}
+    />
+  ));
+
+  expect(container).toHaveTextContent('testTitle');
 });
 
+test('List has no items', () => {
+  const tasks = [];
+
+  const { container } = render((
+    <List
+      tasks={tasks}
+    />
+  ));
+
+  expect(container).toHaveTextContent('할 일이 없어요!');
+});
