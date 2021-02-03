8dd2b55c2d5775c4189025866039e5fc20c23b8f
Blueberry <54464320+lee01097@users.noreply.github.com>
2021-02-03 20:55:36 +0900

    Page.test.jsx 문제 해결: rerender 추가하여, 할 일이 추가되었을 시 화면에 보이는지 여부를 테스트하는 케이스 추가

--- src/Page.test.jsx
+++ src/Page.test.jsx
@@ -9,7 +9,7 @@ test('addedTaskViewed', () => {
   const handleClickAdd = jest.fn();
   const handleClickDelete = jest.fn();
   const tasks = [{ id: 1, title: '안녕하세요' }];
-  const { container, getByText } = render((
+  const { container, getByText, rerender } = render((
     <Page
       taskTitle="이게 외않되?"
       onChangeTitle={handleChangeTitle}
@@ -24,6 +24,15 @@ test('addedTaskViewed', () => {
 
   expect(handleClickAdd).toBeCalled();
 
+  rerender((
+    <Page
+      taskTitle=""
+      onChangeTitle={handleChangeTitle}
+      onClickAddTask={handleClickAdd}
+      tasks={[...tasks, { id: 2, title: '이게 외않되?' }]}
+      onClickDeleteTask={handleClickDelete}
+    />
+  ));
   expect(container).toHaveTextContent('안녕하세요');
   expect(container).toHaveTextContent('이게 외않되?');
 });
