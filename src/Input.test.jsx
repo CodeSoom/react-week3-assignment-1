f4bbcc3f96bcc319284995679986c904671761b9
최창현 <chchoi_einz@einz.co.kr>
2021-02-03 16:34:35 +0900

    TST: Add test cases

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -1,24 +1,49 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import Input from './Input';
 
 describe('Input', () => {
-  const taskTitle = ''
-  const onChangeTitle = jest.fn();
-  const onClickAddTask = jest.fn();
-
-  render((
-    <Input
-      value={taskTitle}
-      onChange={onChangeTitle}
-      onClick={onClickAddTask}
-    />
-  ));
+  function myRender({ taskTitle, onChangeTitle, onClickAddTask }) {
+    return render((
+      <Input
+        value={taskTitle}
+        onChange={onChangeTitle}
+        onClick={onClickAddTask}
+      />
+    ));
+  }
 
   it('UI 정상노출 확인', () => {
+    const onChangeTitle = jest.fn();
+    const onClickAddTask = jest.fn();
 
+    const {
+      getByLabelText,
+      getByPlaceholderText,
+      getByText,
+    } = myRender({ onChangeTitle, onClickAddTask });
+
+    expect(getByLabelText('할 일')).toBeInTheDocument();
+    expect(getByPlaceholderText('할 일을 입력해 주세요')).toBeInTheDocument();
+    expect(getByText('추가')).toBeInTheDocument();
   });
 
+  it('input 값 입력', () => {
+    const onChangeTitle = jest.fn();
+    const onClickAddTask = jest.fn();
+
+    const { getByPlaceholderText } = myRender({ onChangeTitle, onClickAddTask });
+
+    const input = getByPlaceholderText('할 일을 입력해 주세요');
+
+    fireEvent.change(input, {
+      target: {
+        value: '인풋 작성',
+      },
+    });
+
+    expect(input.value).toBe('인풋 작성');
+  });
 });
--- src/List.test.jsx
+++ src/List.test.jsx
@@ -5,15 +5,17 @@ import { render } from '@testing-library/react';
 import List from './List';
 
 describe('List', () => {
-  context('tasks의 length가 0인 경우', () => {
-    const tasks = [];
+  function myRender({ tasks = [] }) {
+    return render((
+      <List
+        tasks={tasks}
+      />
+    ));
+  }
 
+  context('tasks의 length가 0인 경우', () => {
     it('"할 일이 없어요!"를 표시한다.', () => {
-      const { container } = render((
-        <List
-          tasks={tasks}
-        />
-      ));
+      const { container } = myRender({});
 
       expect(container).toHaveTextContent('할 일이 없어요!');
     });
@@ -32,14 +34,10 @@ describe('List', () => {
     ];
 
     it('tasks의 항목들을 모두 표시한다.', () => {
-      const { container, getAllByText, getAllByTestId } = render((
-        <List
-          tasks={tasks}
-        />
-      ));
+      const { container, getAllByText, getAllByTestId } = myRender({ tasks });
 
-      expect(getAllByTestId('todo-item').length).toEqual(2);
-      expect(getAllByText('완료').length).toEqual(2);
+      expect(getAllByTestId('todo-item')).toHaveLength(2);
+      expect(getAllByText('완료')).toHaveLength(2);
       expect(container).toHaveTextContent('공부하기');
       expect(container).toHaveTextContent('운동하기');
     });
