b1c58d37bcda8332f0c7b1a387319f936c5990ed
jjkim01 <jaen6563@gmail.com>
2021-02-04 00:51:30 +0900

    Input 테스트 작성 - 함수 동작 테스트 추가 필요

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -1,8 +1,37 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import Input from './Input';
 
-test('x', () => {
+test('To do text typing test ', () => {
+  const taskTitle = '커피 마시기';
+
+  const onChangeTitle = jest.fn();
+  const onClickAddTask = jest.fn();
+
+  const { container, getByText, getByPlaceholderText } = render((
+    <Input
+      onChange={onChangeTitle}
+      onClick={onClickAddTask}
+    />
+  ));
+
+  expect(container).toHaveTextContent('할 일');
+  expect(container).toHaveTextContent('추가');
+
+  const todoTitleField = getByPlaceholderText('할 일을 입력해 주세요');
+  fireEvent.change(todoTitleField, {
+    target: {
+      value: taskTitle,
+    },
+  });
+  expect(todoTitleField.value).toBe(taskTitle);
+  // 23~29: onChangeTitle()가 해야할 일을 수동으로 구현한 것 같은데.. 더 좋은 방향이 있을까요?
+
+  const addButton = getByText('추가');
+  fireEvent.click(addButton);
+
+  // 32~33: 추가 버튼을 클릭하면 edit text의 내용이 사라진 것을 체크하려고 해요.
+  // expect(todoTitleField.value).toBe('');
 });
