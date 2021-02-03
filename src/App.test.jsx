4b05883f981767ab58bb68d400de4341e3447317
jjkim01 <jaen6563@gmail.com>
2021-02-04 01:08:51 +0900

    App 테스트 추가

---
+++ src/App.test.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { render, fireEvent } from '@testing-library/react';
+
+import App from './App';
+
+test('x', () => {
+  // state가 존재하는지 검사?
+  // 3개 함수가 동작하는지 검사?
+  // Page 컴포넌트가 존재하는지 검사?
+});
