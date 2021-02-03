1ab6f32c5536ef80312fd8e6656cc244c76a2131
Blueberry <54464320+lee01097@users.noreply.github.com>
2021-02-03 21:48:15 +0900

    App.test.jsx수정: 처음 input의 값이 비었는지 확인

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -5,10 +5,13 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 test('AppRendering', () => {
-  const { container } = render((
+  const { container, getByLabelText } = render((
     <App />
   ));
 
+  const input = getByLabelText('할 일');
+
+  expect(input).toHaveValue('');
   expect(container).toHaveTextContent('To-do');
   expect(container).toHaveTextContent('추가');
   expect(container).toHaveTextContent('할 일이 없어요!');
