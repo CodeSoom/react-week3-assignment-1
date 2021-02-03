5a73433256c73d37777c5a05270c8dd0c9a17f4f
Yoon <yoonhe@gmail.com>
2021-02-03 20:56:57 +0900

    Edit Page component test case

--- src/Page.test.jsx
+++ src/Page.test.jsx
@@ -14,46 +14,43 @@ import Page from './Page';
  *  tasks가 화면에 보여지는지
  */
 
-const taskTitle = '홈트 하기';
-const handleChageTitle = jest.fn();
-
-const tasks = [
-  { id: 1, title: '삼겹살 먹기' },
-  { id: 2, title: '코딩하기' },
-];
-const handleClickAddTask = jest.fn();
-const handleClickDeleteTask = jest.fn();
-
-const rederPage = () => render(
-  <Page
-    taskTitle={taskTitle}
-    handleChageTitle={handleChageTitle}
-    handleClickAddTask={handleClickAddTask}
-    tasks={tasks}
-    handleClickDeleteTask={handleClickDeleteTask}
-  />,
-);
-
 describe('Page', () => {
+  const taskTitle = '홈트 하기';
+  const handleChageTitle = jest.fn();
+
+  const tasks = [
+    { id: 1, title: '삼겹살 먹기' },
+    { id: 2, title: '코딩하기' },
+  ];
+
+  const handleClickAddTask = jest.fn();
+  const handleClickDeleteTask = jest.fn();
+
+  const rederPage = () => render(
+    <Page
+      taskTitle={taskTitle}
+      handleChageTitle={handleChageTitle}
+      handleClickAddTask={handleClickAddTask}
+      tasks={tasks}
+      handleClickDeleteTask={handleClickDeleteTask}
+    />,
+  );
+
   it('title is rendered', () => {
     const { container } = rederPage();
 
     expect(container).toHaveTextContent('To-do');
   });
 
-  context('When Input component is rendered', () => {
-    it('task title is displayed', () => {
-      const { getByDisplayValue } = rederPage();
-      expect(getByDisplayValue(taskTitle)).toBeInTheDocument();
-    });
+  it('task title is displayed', () => {
+    const { getByDisplayValue } = rederPage();
+    expect(getByDisplayValue(taskTitle)).toBeInTheDocument();
   });
 
-  context('When List component is rendered', () => {
-    it('tasks is displayed', () => {
-      const { container } = rederPage();
+  it('tasks is displayed', () => {
+    const { container } = rederPage();
 
-      expect(container).toHaveTextContent(tasks[0].title);
-      expect(container).toHaveTextContent(tasks[1].title);
-    });
+    expect(container).toHaveTextContent(tasks[0].title);
+    expect(container).toHaveTextContent(tasks[1].title);
   });
 });
