e073403a1dd807ea32828c18a57935abd3aa0c52
Yoon <yoonhe@gmail.com>
2021-02-03 20:53:57 +0900

    Edit App component test case

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 
-import {
-  render, fireEvent, screen,
-} from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import App from './App';
 
@@ -19,31 +17,36 @@ import App from './App';
 const renderApp = () => render(<App />);
 
 describe('App', () => {
-  it('Entering a value in input changes the value', () => {
-    const { getByDisplayValue, getByPlaceholderText } = renderApp();
+  it('listens change event', () => {
+    const { getByLabelText } = renderApp();
 
-    expect(getByPlaceholderText('할 일을 입력해 주세요')).toBeInTheDocument();
+    expect(getByLabelText('할 일')).toBeInTheDocument();
 
-    fireEvent.change(getByPlaceholderText('할 일을 입력해 주세요'), { target: { value: '홈트 하기' } });
+    fireEvent.change(getByLabelText('할 일'), { target: { value: '홈트 하기' } });
 
-    expect(getByDisplayValue('홈트 하기')).toBeInTheDocument();
+    expect(getByLabelText('할 일').value).toBe('홈트 하기');
   });
 
-  it('When you click the add button, the input value is added to tasks', () => {
-    const { getByDisplayValue, getByPlaceholderText, getByText } = renderApp();
+  it('listens click event', () => {
+    const {
+      getByLabelText, getByText, getAllByText,
+    } = renderApp();
 
-    expect(getByPlaceholderText('할 일을 입력해 주세요')).toBeInTheDocument();
-
-    fireEvent.change(getByPlaceholderText('할 일을 입력해 주세요'), { target: { value: '홈트 하기' } });
-    expect(getByDisplayValue('홈트 하기')).toBeInTheDocument();
-
+    fireEvent.change(getByLabelText('할 일'), { target: { value: '홈트하기' } });
+    expect(getByLabelText('할 일').value).toBe('홈트하기');
     fireEvent.click(getByText('추가'));
 
-    // screen.debug();
-    // 디버거를 통해 input value가 빈문자열이 된것을 확인했는데
-    // 왜 아래의 테스트에서는 여전히 value가 "홈트하기"로 나오는지 모르겠습니다 ...
-    expect(getByPlaceholderText('할 일을 입력해 주세요').value).toBe('');
+    fireEvent.change(getByLabelText('할 일'), { target: { value: '코딩하기' } });
+    fireEvent.click(getByText('추가'));
 
-    expect(getByText('홈트 하기')).toBeInTheDocument();
+    expect(getByLabelText('할 일').value).toBe('');
+
+    expect(getAllByText('완료')).toHaveLength(2);
+
+    fireEvent.click(getAllByText('완료')[0]);
+    expect(getAllByText('완료')).toHaveLength(1);
+
+    fireEvent.click(getByText('완료'));
+    expect(getByText('할 일이 없어요!')).toBeInTheDocument();
   });
 });
