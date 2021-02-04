9bb59582cbb98220018e8b37fcd96f73f0226659
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-02-04 21:22:54 +0900

    Fail : Try to test if it update input change
    실패 : 사용자 입력이 변하는 것을 반영하는지 테스트하는것을 시도했습니다.

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { fireEvent, render } from '@testing-library/react';
-import ReactTestUtils from 'react-dom/test-utils';
 
 import Input from './Input';
 
@@ -14,13 +13,13 @@ describe('Input', () => {
   );
   expect(container).toHaveTextContent('할 일');
   expect(container).toHaveTextContent('추가');
-  expect(getByPlaceholderText('할 일을 입력해주세요')).toHaveTextContent('할 일을 입력해 주세요');
+  expect(getByPlaceholderText('할 일을 입력해 주세요')).toHaveTextContent('할 일을 입력해 주세요');
 
   expect(handleClick).not.toBeCalled();
   fireEvent.click(getByText('추가'));
   expect(handleClick).toBeCalled();
 
   it('입력이 변경됨을 표시합니다.', () => {
-    ReactTestUtils.Simulate.change(React.createRef);
+    fireEvent.change(getByPlaceholderText('할 일을 입력해 주세요'), 'TDD 과제하기');
   });
 });
