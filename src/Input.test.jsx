939ba060ee6db2069c0bb19a16309692d9002a8d
Najeong-Kim <73640737+Najeong-Kim@users.noreply.github.com>
2021-02-04 17:45:50 +0900

    Input과 List 테스트 코드 작성

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -6,11 +6,24 @@ import Input from './Input';
 
 test('Input', () => {
   const value = '테스트';
+  const handleChange = jest.fn();
+  const handleClick = jest.fn();
 
-  const { container } = render ((
-    <Input value={value} />
+  const { container, getByText } = render ((
+    <Input
+      value={value}
+      onChange={handleChange}
+      onClick={handleClick}
+    />
   ));
 
-  expect(container).toHaveTextContent('테스트');
+  expect(container).toHaveTextContent('추가');
+  // expect(container).toHaveTextContent('테스트');
 
-})
+  expect(handleChange).toBeCalled();
+
+  fireEvent.click(getByText('추가'));
+
+  expect(container).not.toHaveTextContent('테스트');
+
+});
--- src/List.test.jsx
+++ src/List.test.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
 
+import { render, fireEvent } from '@testing-library/react';
+
 import List from './List';
 
 test('List', () => {
-  const tasks = '어쩌구저쩌구',
+  const task = {
+    id: 1,
+    title: '어쩌구저쩌구',
+  };  
 
-  const onClickDelete = jest.fn();
+  const handleClick = jest.fn();
 
-})
+  const { container, getByText } = render((
+    <List
+      tasks={task}
+      onClickDelete={handleClick}
+    />
+  ));
+
+  expect(container).toHaveTextContent('어쩌구저쩌구');
+
+  expect(handleClick).not.toBeCalled();
+
+  fireEvent.click(getByText('완료'));
+
+  expect(handleClick).toBeCalledWith(1);
+});
