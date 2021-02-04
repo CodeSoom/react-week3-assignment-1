56858dcd668f350a22b3454795db51d6e6e5285b
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-02-04 22:23:11 +0900

    Add context-it pattern at add button
    상황과 행동을 구체적으로 설명해서 이해하시기 쉽도록 추가버튼 누른 경우를
    context-it 패턴으로 수정했습니다.

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -11,17 +11,22 @@ describe('Input', () => {
       onClick={handleClick}
     />,
   );
+
   expect(container).toHaveTextContent('할 일');
   expect(container).toHaveTextContent('추가');
   expect(getByPlaceholderText('할 일을 입력해 주세요')).toBe('할 일을 입력해 주세요');
 
-  expect(handleClick).not.toBeCalled();
-  fireEvent.click(getByText('추가'));
-  expect(handleClick).toBeCalled();
-
   context('Todo를 입력하는 경우', () => {
     it('입력이 변경됨을 표시합니다.', () => {
       fireEvent.change(getByPlaceholderText('할 일을 입력해 주세요'), { target: { value: 'TDD 과제하기' } });
+    });
+  });
+
+  context('추가 버튼을 누른 경우', () => {
+    it('onClick함수를 실행합니다.', () => {
+      expect(handleClick).not.toBeCalled();
+      fireEvent.click(getByText('추가'));
+      expect(handleClick).toBeCalled();
     });
   });
 });
