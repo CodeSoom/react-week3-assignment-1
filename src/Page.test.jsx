9d1f49d2cd33aabbefd1d74d4728fe4840a55a2e
datalater <the7mincheol@gmail.com>
2021-02-04 14:34:17 +0900

    Page 컴포넌트 테스트를 describe-context로 계층화하라

--- src/Page.test.jsx
+++ src/Page.test.jsx
@@ -4,40 +4,38 @@ import { render } from '@testing-library/react';
 
 import Page from './Page';
 
-test('Page shows tasks and input form', () => {
-  const tasks = [
-    {
-      id: 1,
-      title: '아무것도 하지 않기 #1',
-    },
-    {
-      id: 2,
-      title: '아무것도 하지 않기 #2',
-    },
-  ];
-
-  const { container } = render(
-    <Page
+describe('Page', () => {
+  function renderPage(tasks) {
+    return render(<Page
       tasks={tasks}
-    />,
-  );
+    />);
+  }
 
-  expect(container).toHaveTextContent('할 일');
+  context('With tasks', () => {
+    const tasks = [
+      {
+        id: 1,
+        title: '아무것도 하지 않기 #1',
+      },
+      {
+        id: 2,
+        title: '아무것도 하지 않기 #2',
+      },
+    ];
 
-  expect(container).toHaveTextContent('아무것도 하지 않기 #1');
-  expect(container).toHaveTextContent('아무것도 하지 않기 #2');
-});
+    it('shows tasks', () => {
+      const { container } = renderPage(tasks);
+      expect(container).toHaveTextContent('아무것도 하지 않기 #1');
+      expect(container).toHaveTextContent('아무것도 하지 않기 #2');
+    });
+  });
 
-test('Page shows no task message and input form', () => {
-  const tasks = [];
+  context('Without tasks', () => {
+    const tasks = [];
 
-  const { container } = render(
-    <Page
-      tasks={tasks}
-    />,
-  );
-
-  expect(container).toHaveTextContent('할 일');
-
-  expect(container).toHaveTextContent('할 일이 없어요!');
+    it('shows "no tasks message"', () => {
+      const { container } = renderPage(tasks);
+      expect(container).toHaveTextContent('할 일이 없어요!');
+    });
+  });
 });
