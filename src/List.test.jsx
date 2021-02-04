1915fad0b9ecdcbecd3fa446ecb0be027cd5ebfd
HongJungKim <58525009+HongJungKim-dev@users.noreply.github.com>
2021-02-04 23:49:47 +0900

    Remove comment'//'
    주석처리했던것을 제거합니다.

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -1,50 +1,50 @@
-// import React from 'react';
-// import { render, fireEvent } from '@testing-library/react';
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
 
-// import List from './List';
+import List from './List';
 
-// describe('List', () => {
-//   const handleClick = jest.fn();
+describe('List', () => {
+  const handleClick = jest.fn();
 
-//   function renderList({ tasks, handleClickDelete }) {
-//     return render((
-//       <List
-//         tasks={tasks}
-//         onClickDelete={handleClickDelete}
-//       />
-//     ));
-//   }
+  function renderList({ tasks, handleClickDelete }) {
+    return render((
+      <List
+        tasks={tasks}
+        onClickDelete={handleClickDelete}
+      />
+    ));
+  }
 
-//   context('task가 없을 경우', () => {
-//     const tasks = [];
+  context('task가 없을 경우', () => {
+    const tasks = [];
 
-//     it('"할 일이 없어요!"를 표시한다.', () => {
-//       const { container } = renderList(tasks, handleClick);
+    it('"할 일이 없어요!"를 표시한다.', () => {
+      const { container } = renderList(tasks, handleClick);
 
-//       expect(container).toHaveTextContent('할 일이 없어요!');
-//     });
-//   });
+      expect(container).toHaveTextContent('할 일이 없어요!');
+    });
+  });
 
-//   context('task가 있을 경우', () => {
-//     const tasks = [
-//       {
-//         id: 1,
-//         title: '뭐라도 하기',
-//       },
-//     ];
+  context('task가 있을 경우', () => {
+    const tasks = [
+      {
+        id: 1,
+        title: '뭐라도 하기',
+      },
+    ];
 
-//     it('입력된 할 일을 표시한다.', () => {
-//       const { container } = renderList(tasks, handleClick);
+    it('입력된 할 일을 표시한다.', () => {
+      const { container } = renderList(tasks, handleClick);
 
-//       expect(container).toHaveTextContent('뭐라도 하기');
-//     });
+      expect(container).toHaveTextContent('뭐라도 하기');
+    });
 
-//     it('완료버튼을 누를 경우 Todo를 삭제한다.', () => {
-//       const { getByText } = renderList(tasks, handleClick);
+    it('완료버튼을 누를 경우 Todo를 삭제한다.', () => {
+      const { getByText } = renderList(tasks, handleClick);
 
-//       expect(handleClick).not.toBeCalled();
-//       fireEvent.click(getByText('완료'));
-//       expect(handleClick).toBeCalled();
-//     });
-//   });
-// });
+      expect(handleClick).not.toBeCalled();
+      fireEvent.click(getByText('완료'));
+      expect(handleClick).toBeCalled();
+    });
+  });
+});
