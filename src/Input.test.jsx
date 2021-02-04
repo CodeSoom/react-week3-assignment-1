f62fe8b29110be6a49de98c23499c5060c756cf4
datalater <the7mincheol@gmail.com>
2021-02-04 14:42:41 +0900

    Input 컴포넌트 테스트를 describe-it으로 계층화하라

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -4,23 +4,42 @@ import { render, fireEvent } from '@testing-library/react';
 
 import Input from './Input';
 
-test('Input', () => {
-  const handleChange = jest.fn();
-  const handleClick = jest.fn();
+describe('Input', () => {
+  function renderInput() {
+    const handleChange = jest.fn();
+    const handleClick = jest.fn();
 
-  const { getByLabelText, getByText } = render((
-    <Input
-      onChange={handleChange}
-      onClick={handleClick}
-    />
-  ));
+    const { getByLabelText, getByText } = render((
+      <Input
+        onChange={handleChange}
+        onClick={handleClick}
+      />
+    ));
 
-  const input = getByLabelText('할 일');
-  fireEvent.change(input, { target: { value: '뭐라도 하기' } });
-  expect(input.value).toBe('뭐라도 하기');
-  expect(handleClick).not.toBeCalled();
+    return {
+      getByLabelText,
+      getByText,
+      handleChange,
+      handleClick,
+    };
+  }
 
-  const button = getByText('추가');
-  fireEvent.click(button);
-  expect(handleClick).toBeCalled();
+  it('updates input value', () => {
+    const { getByLabelText, handleClick } = renderInput();
+
+    const input = getByLabelText('할 일');
+    fireEvent.change(input, { target: { value: '뭐라도 하기' } });
+
+    expect(input.value).toBe('뭐라도 하기');
+    expect(handleClick).not.toBeCalled();
+  });
+
+  it('fires handleClick when the button is clicked', () => {
+    const { getByText, handleClick } = renderInput();
+
+    const button = getByText('추가');
+    fireEvent.click(button);
+
+    expect(handleClick).toBeCalled();
+  });
 });
