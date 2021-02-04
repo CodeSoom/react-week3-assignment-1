9dd79b35e0d73ea4f9b41d9c6f405cd42db67f3e
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-02-04 23:54:26 +0900

    Fix to previous version on add button
    오류로 인해 이전처럼 context-it패턴을 지웠습니다. 다시 해결하겠습니다.

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -6,20 +6,16 @@ import List from './List';
 describe('List', () => {
   const handleClick = jest.fn();
 
-  function renderList({ tasks, handleClickDelete }) {
-    return render((
-      <List
-        tasks={tasks}
-        onClickDelete={handleClickDelete}
-      />
-    ));
-  }
-
   context('task가 없을 경우', () => {
     const tasks = [];
 
     it('"할 일이 없어요!"를 표시한다.', () => {
-      const { container } = renderList(tasks, handleClick);
+      const { container } = render((
+        <List
+          tasks={tasks}
+          onClickDelete={handleClick}
+        />
+      ));
 
       expect(container).toHaveTextContent('할 일이 없어요!');
     });
@@ -34,13 +30,23 @@ describe('List', () => {
     ];
 
     it('입력된 할 일을 표시한다.', () => {
-      const { container } = renderList(tasks, handleClick);
+      const { container } = render((
+        <List
+          tasks={tasks}
+          onClickDelete={handleClick}
+        />
+      ));
 
       expect(container).toHaveTextContent('뭐라도 하기');
     });
 
     it('완료버튼을 누를 경우 Todo를 삭제한다.', () => {
-      const { getByText } = renderList(tasks, handleClick);
+      const { getByText } = render((
+        <List
+          tasks={tasks}
+          onClickDelete={handleClick}
+        />
+      ));
 
       expect(handleClick).not.toBeCalled();
       fireEvent.click(getByText('완료'));
--- src/Page.test.jsx
+++ src/Page.test.jsx
@@ -1,4 +1,14 @@
-describe('List', () => {
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import Page from './Page';
+
+describe('Page', () => {
+  const { container } = render(
+    <Page />,
+  );
+  expect(container).toHaveTextContent('To-do');
+
   context('task가 없을 경우', () => {
 
   });
