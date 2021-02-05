714b0a8a4473cbd7b98a0cc5ea91e017eacaba8b
Magrfs <magnoliarfsit@gmail.com>
2021-02-05 15:21:15 +0900

    List 테스트에서 할 일이 없을 때 작성

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -5,6 +5,22 @@ import { render } from '@testing-library/react';
 import List from './List';
 
 
-test('Lisk', () => {
+describe('List', () => {
+  const onClickDelete = jest.fn();
 
+  context("할 일이 없을 때", () => {
+    const tasks = [];
+
+    it('"할 일이 없어요!"를 표시', () => {
+      const { container } = render((
+        <List tasks={tasks} onClickDelete={onClickDelete} />
+      ));
+
+      expect(container).toHaveTextContent('할 일이 없어요!');
+    });
+  });
+
+  context("할 일이 있을 때", () => {
+    // ??
+  });
 });
--- src/Page.test.jsx
+++ src/Page.test.jsx
@@ -6,5 +6,19 @@ import Page from './Page';
 
 
 test('Page', () => {
+  const onChangeTitle = jest.fn();
+  const onClickAddTask = jest.fn();
+  const onClickDeleteTask = jest.fn();
+
+  const { container } = render((
+    <Page 
+      tasks = {[]}
+    />
+  ));
+
+  expect(container).toHaveTextContent('To-do');
+  expect(container).toHaveTextContent('할 일');
+  expect(container).toHaveTextContent('추가');
+  expect(container).toHaveTextContent('할 일이 없어요!');
 
 });
