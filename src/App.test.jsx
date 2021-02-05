52c4be7f450b729cacbeafbf261967aeb8ae873a
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-02-05 14:18:49 +0900

    :white_check_mark: TestRefactor: App.test.jsx
    기존 화면 출력만 test 하는 것을 상태변화 test 로 수정하였음

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,33 +1,77 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
-import App from './Page';
+import App from './App';
 
-test('Page 컴포넌트 출력확인', () => {
-  const state = {
-    taskTitle: '',
-    tasks: [],
+describe('App', () => {
+  const renderApp = () => {
+    const queries = render(
+      (<App />),
+    );
+    const input = queries.getByPlaceholderText('할 일을 입력해 주세요');
+    const button = queries.getByRole('button');
+    return {
+      ...queries, input, button,
+    };
   };
-  const { taskTitle, tasks } = state;
-  const handleChangeTitle = jest.fn();
-  const handleClickAddTask = jest.fn();
-  const handleClickDeleteTask = jest.fn();
+  context('1. input에 글자를 입력할 때', () => {
+    it('input value 가 변화되어야 함', () => {
+      const { input } = renderApp();
 
-  const { getByText, getByPlaceholderText } = render(
-    <App
-      taskTitle={taskTitle}
-      tasks={tasks}
-      onChangeTitle={handleChangeTitle}
-      onClickAddTask={handleClickAddTask}
-      onClickDeleteTask={handleClickDeleteTask}
-    />,
-  );
+      expect(input).toBeInTheDocument();
 
-  const header = getByText('To-do');
-  const input = getByPlaceholderText('할 일을 입력해 주세요');
-  const paragraph = getByText('할 일이 없어요!');
+      fireEvent.change(input, { target: { value: 'test code 작성하기' } });
 
-  expect(header).toBeInTheDocument();
-  expect(input).toBeInTheDocument();
-  expect(paragraph).toBeInTheDocument();
+      expect(input.value).toBe('test code 작성하기');
+    });
+  });
+
+  context('2. 추가버튼을 클릭했을 때', () => {
+    it('할일 목록에 새로운 할일이 추가되어야 함', () => {
+      const {
+        input, button, getByText, getAllByRole, queryByText,
+      } = renderApp();
+
+      expect(getByText('할 일이 없어요!')).toBeInTheDocument();
+      expect(button).toBeInTheDocument();
+
+      fireEvent.change(input, { target: { value: 'test code 작성하기' } });
+      fireEvent.click(button);
+      const todoList = getAllByRole('listitem');
+
+      expect(queryByText('할 일이 없어요!')).not.toBeInTheDocument();
+      expect(todoList[0]).toHaveTextContent('test code 작성하기');
+    });
+  });
+
+  context('3. 완료버튼을 클릭했을 때', () => {
+    it('할일목록에서 해당 할일이 제거되어야 함', () => {
+      const {
+        input, button, getByText, getAllByRole, queryByText, getAllByText,
+      } = renderApp();
+
+      expect(getByText('할 일이 없어요!')).toBeInTheDocument();
+      expect(button).toBeInTheDocument();
+
+      fireEvent.change(input, { target: { value: '출근하기' } });
+      fireEvent.click(button);
+      fireEvent.change(input, { target: { value: '퇴근하기' } });
+      fireEvent.click(button);
+      const beforTodoList = getAllByRole('listitem');
+
+      expect(queryByText('할 일이 없어요!')).not.toBeInTheDocument();
+      expect(beforTodoList[0]).toHaveTextContent('출근하기');
+      expect(beforTodoList[1]).toHaveTextContent('퇴근하기');
+
+      const completeButton = getAllByText('완료', { selector: 'button' });
+
+      expect(completeButton[0]).toBeInTheDocument();
+
+      fireEvent.click(completeButton[0]);
+      const afterTodoList = getAllByRole('listitem');
+
+      expect(afterTodoList.length).toBe(1);
+      expect(queryByText('출근하기')).not.toBeInTheDocument();
+    });
+  });
 });
