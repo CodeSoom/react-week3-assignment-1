ffef91ec9ea3adedef9e2d89f19291c99a03632a
hxunz <guswl2843@gmail.com>
2021-02-06 01:48:14 +0900

    - beforeEach 사용
     -  할일이 없을때 구현

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -1,31 +1,21 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, waitForElementToBeRemoved } from '@testing-library/react';
 
 import List from './List';
 
 describe('List', () => {
+  beforeEach(() => {
+    const onClickDelete = jest.fn();
+  });
 
   it('할 일이 없어요!', () => {
-    const tasks = [];
-  }
-  
+    const { getByText } = render(<List />);
+    expect(getByText('할 일이 없어요!')).toBeInTheDocument();
+  });
+
   it('놀기', () => {
-    const tasks = {
-      task: task,
-      key: task.id,
-    };
-  }
-
-  const onClickDelete = jest.fn();
-
-  const { container } = render((
-    <List
-      tasks={tasks}
-      onClickDelete={onClickDelete}
-    />
-  ));
-
-  expect(container).toHaveTextContent('할 일이 없어요!');
-  expect(container).toHaveTextContent('');
+    const tasks = {};
+    expect(map(task));
+  });
 });
