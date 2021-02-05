c9d7cd86552e5ed2398403782478133da27a147c
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-02-05 11:19:40 +0900

    :white_check_mark: TestRefactor: input.test.jsx
    - 반복되는 구문을 분리
    render 와 mock 함수 선언이 반복되어 분리함
    
    - input change event 발생하는지만 확인
    상태변경은 App.test.jsx 에서 시행예정
    
    - DCI pattern 사용 안함
    test 맥락에 맞지 않음

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -5,54 +5,45 @@ import {
 
 import Input from './Input';
 
-describe('Input component Test', () => {
-  describe('1. input 컴포넌트 render 할 때', () => {
-    test('label,input,button 출력되어야 함 ', () => {
-      const onChange = jest.fn();
-      const { getByText, getByPlaceholderText, getByRole } = render(
-        <Input
-          value="할일!"
-          onChange={onChange}
-        />,
-      );
+describe('Input', () => {
+  const onChange = jest.fn();
+  const onClick = jest.fn();
+  const inputRender = (value = '') => render(
+    (<Input
+      value={value}
+      onChange={onChange}
+      onClick={onClick}
+    />),
+  );
+  test('1. label,input,button 출력확인', () => {
+    const { getByText, getByPlaceholderText, getByRole } = inputRender('할일!');
+    const label = getByText('할 일');
+    const input = getByPlaceholderText('할 일을 입력해 주세요');
+    const button = getByRole('button');
 
-      const label = getByText('할 일');
-      const input = getByPlaceholderText('할 일을 입력해 주세요');
-      const button = getByRole('button');
-
-      expect(label).toHaveTextContent('할 일');
-      expect(input.value).toBe('할일!');
-      expect(button).toHaveTextContent('추가');
-    });
+    expect(label).toHaveTextContent('할 일');
+    expect(input.value).toBe('할일!');
+    expect(button).toHaveTextContent('추가');
   });
 
-  describe('2. input 에 글자를 입력할 때', () => {
-    test('onChange 후 글자가 변경되어야 함 ', () => {
-      const onChange = jest.fn();
-      const { getByPlaceholderText } = render(<Input onChange={onChange} />);
+  test('2. input change event 확인', () => {
+    const { getByPlaceholderText } = inputRender();
+    const input = getByPlaceholderText('할 일을 입력해 주세요');
 
-      const input = getByPlaceholderText('할 일을 입력해 주세요');
+    expect(onChange).not.toHaveBeenCalled();
 
-      expect(onChange).not.toHaveBeenCalled();
-      expect(input.value).toBe('');
+    fireEvent.change(input, { target: { value: 'test code 작성하기' } });
 
-      fireEvent.change(input, { target: { value: 'test code 작성하기' } });
-
-      expect(onChange).toHaveBeenCalledTimes(1);
-      expect(input.value).toBe('test code 작성하기');
-    });
+    expect(onChange).toHaveBeenCalled();
   });
 
-  describe('3. 버튼을 클릭했을 때', () => {
-    test('onClick 에 할당된 함수가 실행되어야 함', () => {
-      const onClick = jest.fn();
-      const { getByRole } = render(<Input onClick={onClick} />);
+  test('3. button click event 확인', () => {
+    const { getByRole } = inputRender();
 
-      expect(onClick).not.toHaveBeenCalled();
+    expect(onClick).not.toHaveBeenCalled();
 
-      fireEvent.click(getByRole('button'));
+    fireEvent.click(getByRole('button'));
 
-      expect(onClick).toHaveBeenCalledTimes(1);
-    });
+    expect(onClick).toHaveBeenCalled();
   });
 });
