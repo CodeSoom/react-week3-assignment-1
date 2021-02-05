b634fce168f0e7a7289d7897119ab009f1f01032
jjkim01 <jaen6563@gmail.com>
2021-02-05 21:47:47 +0900

    extract getListComponentContainer()

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -5,16 +5,20 @@ import { render } from '@testing-library/react';
 import List from './List';
 
 describe('List', () => {
+  function getListComponentContainer(tasks) {
+    const { container } = render((
+      <List tasks={tasks} />
+    ));
+    return container;
+  }
+
   context('할일이 있는 경우', () => {
     const tasks = [
       { id: 1, title: 'testTitle' },
     ];
 
     it('할 일 목록을 표시한다.', () => {
-      const { container } = render((
-        <List tasks={tasks} />
-      ));
-
+      const container = getListComponentContainer(tasks);
       expect(container).toHaveTextContent('testTitle');
     });
   });
@@ -23,9 +27,7 @@ describe('List', () => {
     const tasks = [];
 
     it('"할 일이 없어요!"를 표시한다.', () => {
-      const { container } = render((
-        <List tasks={tasks} />
-      ));
+      const container = getListComponentContainer(tasks);
 
       expect(container).toHaveTextContent('할 일이 없어요!');
     });
