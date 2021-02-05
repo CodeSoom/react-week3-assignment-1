d2c4e937b05394a6f9a42a208cb8a2568c033f52
jjkim01 <jaen6563@gmail.com>
2021-02-05 22:35:13 +0900

    주석 삭제, 각 버튼 동작 테스트 추가

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -4,34 +4,34 @@ import { render, fireEvent } from '@testing-library/react';
 
 import Input from './Input';
 
-test('To-do text typing test', () => {
-  const taskTitle = '커피 마시기';
+describe('Input', () => {
+  context('할일 입력할 때', () => {
+    const taskTitle = '커피 마시기';
+    const onChangeTitle = jest.fn();
+    const onClickAddTask = jest.fn();
 
-  const onChangeTitle = jest.fn();
-  const onClickAddTask = jest.fn();
+    it('값이 입력됐을 때 onChange()가 호출된다.', () => {
+      const { getByPlaceholderText } = render((
+        <Input onChange={onChangeTitle} />
+      ));
 
-  const { container, getByText, getByPlaceholderText } = render((
-    <Input
-      onChange={onChangeTitle}
-      onClick={onClickAddTask}
-    />
-  ));
+      const todoTitleField = getByPlaceholderText('할 일을 입력해 주세요');
+      fireEvent.change(todoTitleField, {
+        target: {
+          value: taskTitle,
+        },
+      });
+      expect(todoTitleField.value).toBe(taskTitle);
+      expect(onChangeTitle).toBeCalled();
+    });
 
-  expect(container).toHaveTextContent('할 일');
-  expect(container).toHaveTextContent('추가');
+    it('추가버튼을 누르면 입력창에 글자들이 사라진다.', () => {
+      const { getByText } = render((
+        <Input onClick={onClickAddTask} />
+      ));
 
-  const todoTitleField = getByPlaceholderText('할 일을 입력해 주세요');
-  fireEvent.change(todoTitleField, {
-    target: {
-      value: taskTitle,
-    },
+      fireEvent.click(getByText('추가'));
+      expect(onClickAddTask).toBeCalled();
+    });
   });
-  expect(todoTitleField.value).toBe(taskTitle);
-  // 23~29: onChangeTitle()가 해야할 일을 수동으로 구현한 것 같은데.. 더 좋은 방향이 있을까요?
-
-  const addButton = getByText('추가');
-  fireEvent.click(addButton);
-
-  // 32~33: 추가 버튼을 클릭하면 edit text의 내용이 사라진 것을 체크하려고 해요.
-  // expect(todoTitleField.value).toBe('');
 });
