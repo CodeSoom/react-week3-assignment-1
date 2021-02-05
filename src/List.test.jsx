1d05c153dfa07d306aebfa2a9ad573ffa580d7bb
Yoon <yoonhe@gmail.com>
2021-02-05 20:49:27 +0900

    Edit List Component test case

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -4,22 +4,19 @@ import { render, fireEvent } from '@testing-library/react';
 
 import List from './List';
 
-/**
- * 테스트 목록
- *
- * list가 아무것도 없을때 "할 일이 없어요!"가 보이는지
- * List가 있을때 task가 화면에 보이는지
- */
+describe('List', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
 
-describe('List', () => { // 테스트 하려는 대상
   const handleClickDelete = jest.fn();
 
-  const renderList = ({ tasks }) => render(
-    <List tasks={tasks} onClickDelete={handleClickDelete} />,
-  );
+  const renderList = ({ tasks }) => render((
+    <List tasks={tasks} onClickDelete={handleClickDelete} />
+  ));
 
-  context('without tasks', () => { // when, with, without을 사용해 상황(맥락) 서술
-    it('renders no task message', () => { // 테스트 내용 서술
+  context('without tasks', () => {
+    it('renders no task message', () => {
       const { container } = renderList({ tasks: [] });
 
       expect(container).toHaveTextContent('할 일이 없어요!');
@@ -34,6 +31,7 @@ describe('List', () => { // 테스트 하려는 대상
       ];
 
       const { container, getAllByText } = renderList({ tasks });
+
       expect(container).toHaveTextContent(tasks[0].title);
       expect(container).toHaveTextContent(tasks[1].title);
 
