8ca119be2e15fc71132b054ce060ccac08f667c2
Yoon <yoonhe@gmail.com>
2021-02-05 20:49:07 +0900

    Edit Input Component test case

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -4,27 +4,15 @@ import { render, fireEvent } from '@testing-library/react';
 
 import Input from './Input';
 
-/**
- * 테스트 목록
- *
- * Input에
- *  value가 없을때
- *    placehoder에 "할 일을 입력해 주세요" 가 노출되는지
- *  value가 변경되었을때
- *    onChange 함수가 실행되는지
- *  button을 클릭했을때
- *   onClick 함수가 실행되는지
- */
-let handleChangeInput;
-let handleClickAddTask;
-
-beforeEach(() => {
-  handleChangeInput = jest.fn();
-  handleClickAddTask = jest.fn();
-});
-
 describe('Input', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
   const value = '';
+
+  const handleChangeInput = jest.fn();
+  const handleClickAddTask = jest.fn();
 
   const renderInput = () => render((
     <Input
@@ -44,20 +32,18 @@ describe('Input', () => {
 
   context('with value', () => {
     it('listens change event', () => {
-      const { getByLabelText, getByText } = renderInput();
+      const { getByLabelText } = renderInput();
 
       fireEvent.change(getByLabelText('할 일'), { target: { value: '홈트하기' } });
 
       expect(handleChangeInput).toBeCalled();
-
-      fireEvent.click(getByText('추가'));
-      expect(handleClickAddTask).toBeCalled();
     });
 
     it('listens "추가" button click event', () => {
       const { getByText } = renderInput();
 
       fireEvent.click(getByText('추가'));
+
       expect(handleClickAddTask).toBeCalled();
     });
   });
