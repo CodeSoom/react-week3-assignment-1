a91f8e80477dce19b585d7d3fa3d77f6c1a3cd66
hhjin015 <heo015414@gmail.com>
2021-02-05 20:03:03 +0900

    Add Input test

---
+++ src/Input.test.jsx
@@ -0,0 +1,26 @@
+﻿import React from 'react';
+
+import { render, fireEvent } from '@testing-library/react';
+
+import Input from './Input';
+
+test('Input', () => {
+  const onClick = jest.fn();
+
+  const { container, getByText, getByPlaceholderText } = render((
+    <Input onClick={onClick} />
+  ));
+  const input = getByPlaceholderText('할 일을 입력해 주세요');
+
+  expect(container).toHaveTextContent('할 일');
+
+  fireEvent.change(input, { target: { value: '코드숨 과제' } });
+
+  expect(input.value).toBe('코드숨 과제');
+
+  expect(onClick).not.toBeCalled();
+
+  fireEvent.click(getByText('추가'));
+
+  expect(onClick).toBeCalled();
+});
