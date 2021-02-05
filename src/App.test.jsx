7bc967ccc06afec0359c87d6a1e25c424d14c210
Yoon <yoonhe@gmail.com>
2021-02-05 20:48:37 +0900

    Edit App Component test case

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -4,40 +4,40 @@ import { render, fireEvent } from '@testing-library/react';
 
 import App from './App';
 
-/**
- * 테스트 목록
- *
- * Page 컴포넌트가 렌더링되었을때
- *  input에 값이 입력하면 input value가 바뀐다
- *  추가버튼을 누르면 input value가 tasks에 추가된다
- *
- *  task에서 완료 버튼을 누르면 해당 task가 사라진다
- */
-
-const renderApp = () => render(<App />);
-
 describe('App', () => {
+  const renderApp = () => render(<App />);
+
   it('listens change event', () => {
     const { getByLabelText } = renderApp();
-
-    expect(getByLabelText('할 일')).toBeInTheDocument();
 
     fireEvent.change(getByLabelText('할 일'), { target: { value: '홈트 하기' } });
 
     expect(getByLabelText('할 일').value).toBe('홈트 하기');
   });
 
-  it('listens click event', () => {
+  it('listens "추가" button click event', () => {
+    const {
+      getByLabelText, getByText,
+    } = renderApp();
+
+    fireEvent.change(getByLabelText('할 일'), { target: { value: '홈트하기' } });
+    fireEvent.click(getByText('추가'));
+
+    expect(getByLabelText('할 일').value).toBe('');
+    expect(getByText('홈트하기')).toBeInTheDocument();
+  });
+
+  it('listens "완료" button click event', () => {
     const {
       getByLabelText, getByText, getAllByText,
     } = renderApp();
 
     fireEvent.change(getByLabelText('할 일'), { target: { value: '홈트하기' } });
-    expect(getByLabelText('할 일').value).toBe('홈트하기');
+
     fireEvent.click(getByText('추가'));
-    expect(getByLabelText('할 일').value).toBe('');
 
     fireEvent.click(getAllByText('완료')[0]);
+
     expect(getByText('할 일이 없어요!')).toBeInTheDocument();
   });
 });
