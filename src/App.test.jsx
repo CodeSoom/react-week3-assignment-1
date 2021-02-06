29eaba049082a24f28567001e34ca13f45a8c77f
jjkim01 <jaen6563@gmail.com>
2021-02-06 23:44:20 +0900

    Context 삭제, describe-it 구조로 변경

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -4,46 +4,44 @@ import { render, fireEvent } from '@testing-library/react';
 
 import App from './App';
 
-describe('App', () => {
-  context('화면이 띄워질 때', () => {
-    const taskTitle = '커피 마시기';
+describe('할일 추가, 완료 테스트', () => {
+  const taskTitle = '커피 마시기';
 
-    it('추가버튼을 누르면 입력창에 글자들이 사라진다.', () => {
-      const { container, getByText, getByPlaceholderText } = render((
-        <App />
-      ));
+  it('추가버튼을 누르면 입력창에 글자들이 사라진다.', () => {
+    const { container, getByText, getByPlaceholderText } = render((
+      <App />
+    ));
 
-      const todoTitleField = getByPlaceholderText('할 일을 입력해 주세요');
-      fireEvent.change(todoTitleField, {
-        target: {
-          value: taskTitle,
-        },
-      });
-      expect(todoTitleField.value).toBe(taskTitle);
+    const todoTitleField = getByPlaceholderText('할 일을 입력해 주세요');
+    fireEvent.change(todoTitleField, {
+      target: {
+        value: taskTitle,
+      },
+    });
+    expect(todoTitleField.value).toBe(taskTitle);
 
-      const addButton = getByText('추가');
-      fireEvent.click(addButton);
-      expect(todoTitleField.value).toBe('');
-      expect(container).toHaveTextContent(taskTitle);
+    const addButton = getByText('추가');
+    fireEvent.click(addButton);
+    expect(todoTitleField.value).toBe('');
+    expect(container).toHaveTextContent(taskTitle);
+  });
+
+  it('완료버튼을 누르면 할일이 사라진다.', () => {
+    const { container, getByText, getByPlaceholderText } = render((
+      <App />
+    ));
+
+    const todoTitleField = getByPlaceholderText('할 일을 입력해 주세요');
+    fireEvent.change(todoTitleField, {
+      target: {
+        value: taskTitle,
+      },
     });
 
-    it('완료버튼을 누르면 할일이 사라진다.', () => {
-      const { container, getByText, getByPlaceholderText } = render((
-        <App />
-      ));
+    fireEvent.click(getByText('추가'));
 
-      const todoTitleField = getByPlaceholderText('할 일을 입력해 주세요');
-      fireEvent.change(todoTitleField, {
-        target: {
-          value: taskTitle,
-        },
-      });
-
-      fireEvent.click(getByText('추가'));
-
-      const doneButton = getByText('완료');
-      fireEvent.click(doneButton);
-      expect(container).toHaveTextContent('할 일이 없어요!');
-    });
+    const doneButton = getByText('완료');
+    fireEvent.click(doneButton);
+    expect(container).toHaveTextContent('할 일이 없어요!');
   });
 });
--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -4,35 +4,33 @@ import { render, fireEvent } from '@testing-library/react';
 
 import Input from './Input';
 
-describe('Input', () => {
-  context('할일 입력할 때', () => {
-    const taskTitle = '커피 마시기';
-    const onChangeTitle = jest.fn();
-    const onClickAddTask = jest.fn();
+describe('입력값 변화, 할일 추가 함수 테스트', () => {
+  const taskTitle = '커피 마시기';
+  const onChangeTitle = jest.fn();
+  const onClickAddTask = jest.fn();
 
-    it('값이 입력됐을 때 onChange()가 호출된다.', () => {
-      const { getByPlaceholderText } = render((
-        <Input onChange={onChangeTitle} />
-      ));
+  it('값이 입력됐을 때 onChange()가 호출된다.', () => {
+    const { getByPlaceholderText } = render((
+      <Input onChange={onChangeTitle} />
+    ));
 
-      const todoTitleField = getByPlaceholderText('할 일을 입력해 주세요');
-      fireEvent.change(todoTitleField, {
-        target: {
-          value: taskTitle,
-        },
-      });
-      expect(todoTitleField.value).toBe(taskTitle);
-      expect(onChangeTitle).toBeCalled();
+    const todoTitleField = getByPlaceholderText('할 일을 입력해 주세요');
+    fireEvent.change(todoTitleField, {
+      target: {
+        value: taskTitle,
+      },
     });
+    expect(todoTitleField.value).toBe(taskTitle);
+    expect(onChangeTitle).toBeCalled();
+  });
 
-    it('추가버튼을 누르면 입력창에 글자들이 사라진다.', () => {
-      const { getByText, getByPlaceholderText } = render((
-        <Input onClick={onClickAddTask} />
-      ));
+  it('추가버튼을 누르면 입력창에 글자들이 사라진다.', () => {
+    const { getByText, getByPlaceholderText } = render((
+      <Input onClick={onClickAddTask} />
+    ));
 
-      fireEvent.click(getByText('추가'));
-      expect(onClickAddTask).toBeCalled();
-      expect(getByPlaceholderText('할 일을 입력해 주세요').value).toBe('');
-    });
+    fireEvent.click(getByText('추가'));
+    expect(onClickAddTask).toBeCalled();
+    expect(getByPlaceholderText('할 일을 입력해 주세요').value).toBe('');
   });
 });
