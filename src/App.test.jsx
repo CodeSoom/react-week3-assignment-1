6b3f82949bb15a7ad48c9c9f9022e0a1431995da
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-02-06 23:12:39 +0900

    Remove onChange function
    App에서는 onChange가 컴포넌트에 없어서 onChange함수가 실행되는지
    보는것이 관심사가 아니므로 삭제합니다.

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -4,7 +4,6 @@ import { render, fireEvent } from '@testing-library/react';
 import App from './App';
 
 describe('App', () => {
-  const onChange = jest.fn();
   const onClickAddTask = jest.fn();
   const onClickDeleteTask = jest.fn();
 
@@ -12,9 +11,8 @@ describe('App', () => {
     <App />
   ));
   it('input 입력시 input이 업데이트 된다.', () => {
-    expect(onChange).not.toBeCalled();
     fireEvent.change(getByPlaceholderText('할 일을 입력해 주세요'), { target: { value: 'TDD 과제하기' } });
-    expect(onChange).toBeCalled();
+    expect(getByPlaceholderText('할 일을 입력해 주세요')).toHaveValue('TDD 과제하기');
   });
 
   it('추가버튼을 누르면 onClickAddTask가 실행된다.', () => {
