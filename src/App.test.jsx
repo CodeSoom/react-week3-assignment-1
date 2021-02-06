bd521ab80f0d20af3ab35590f4af7f34dcba7e30
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-02-06 23:24:10 +0900

    Remove redundant fireEvent
    fireEvent.change부분이 중복되어서 상단으로 이동하여서 input입력시,
    추가버튼시 둘다 이용할 수 있도록 했습니다.

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -7,13 +7,13 @@ describe('App', () => {
   const { container, getByText, getByPlaceholderText } = render((
     <App />
   ));
+
+  fireEvent.change(getByPlaceholderText('할 일을 입력해 주세요'), { target: { value: 'TDD 과제하기' } });
   it('input 입력시 input이 업데이트 된다.', () => {
-    fireEvent.change(getByPlaceholderText('할 일을 입력해 주세요'), { target: { value: 'TDD 과제하기' } });
     expect(getByPlaceholderText('할 일을 입력해 주세요')).toHaveValue('TDD 과제하기');
   });
 
   it('추가버튼을 누르면 input내용이 비워집니다.', () => {
-    fireEvent.change(getByPlaceholderText('할 일을 입력해 주세요'), { target: { value: 'TDD 과제하기' } });
     fireEvent.click(getByText('추가'));
     expect(getByPlaceholderText('할 일을 입력해 주세요')).toHaveValue('');
     expect(container).toHaveTextContent('TDD 과제하기');
