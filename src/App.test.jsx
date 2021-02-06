2d48e0d16fbc1aca0630f6ca63f8dd1d8f61d81a
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-02-06 23:19:38 +0900

    Add testing if input is empty when click add
    add클릭시 input내용이 비워지는지 확인합니다.

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -4,7 +4,7 @@ import { render, fireEvent } from '@testing-library/react';
 import App from './App';
 
 describe('App', () => {
-  const { getByText, getByPlaceholderText } = render((
+  const { container, getByText, getByPlaceholderText } = render((
     <App />
   ));
   it('input 입력시 input이 업데이트 된다.', () => {
@@ -12,8 +12,11 @@ describe('App', () => {
     expect(getByPlaceholderText('할 일을 입력해 주세요')).toHaveValue('TDD 과제하기');
   });
 
-  it('추가버튼을 누르면 onClickAddTask가 실행된다.', () => {
+  it('추가버튼을 누르면 input내용이 비워집니다.', () => {
+    fireEvent.change(getByPlaceholderText('할 일을 입력해 주세요'), { target: { value: 'TDD 과제하기' } });
     fireEvent.click(getByText('추가'));
+    expect(getByPlaceholderText('할 일을 입력해 주세요')).toHaveValue('');
+    expect(container).toHaveTextContent('TDD 과제하기');
   });
 
   it('완료버튼을 누르면 onClickDeleteTask가 실행된다.', () => {
