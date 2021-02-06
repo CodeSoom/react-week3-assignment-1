faa8ca94a7c28945bae2a5f3469b40decc7434be
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-02-06 22:41:52 +0900

    Add 'it' to test App
    input입력시, 추가버튼 클릭시, 완료버튼 클릭시에 it패턴으로 test를
    하는것을 추가하였습니다.

---
+++ src/App.test.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+
+import App from './App';
+
+describe('App', () => {
+  const onChange = jest.fn();
+  const onClickAddTask = jest.fn();
+  const onClickDeleteTask = jest.fn();
+
+  const { getByText, getByPlaceholderText } = render((
+    <App />
+  ));
+  it('input 입력시 input이 업데이트 된다.', () => {
+    expect(onChange).not.toBeCalled();
+    fireEvent.change(getByPlaceholderText('할 일을 입력해 주세요'), { target: { value: 'TDD 과제하기' } });
+    expect(onChange).toBeCalled();
+  });
+
+  it('추가버튼을 누르면 onClickAddTask가 실행된다.', () => {
+    expect(onClickAddTask).not.toBeCalled();
+    fireEvent.click(getByText('추가'));
+    expect(onClickAddTask).toBeCalled();
+  });
+
+  it('완료버튼을 누르면 onClickDeleteTask가 실행된다.', () => {
+    expect(onClickDeleteTask).not.toBeCalled();
+    fireEvent.click(getByText('완료'));
+    expect(onClickDeleteTask).toBeCalled();
+  });
+});
