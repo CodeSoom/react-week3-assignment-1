63a8e10b7d18298a0aa00771f8b90fab50b0d702
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-02-07 14:08:17 +0900

    Avoid Failed prop type warning
    render함수에 value를 쓰려하는데 onChange없이 value를 쓰다보니Failed prop
    type warning에서 read only로 된다고 하기에'입력이 변경됨을 표시한다.'의
    경우를 describe내에서 처리했습니다.

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -1,39 +1,43 @@
 import React from 'react';
+
 import { fireEvent, render } from '@testing-library/react';
 
 import Input from './Input';
 
 describe('Input', () => {
   const handleClick = jest.fn();
+  const onChange = jest.fn();
 
-  const { container, getByText, getByPlaceholderText } = render((
-    <Input
+  function renderInput({ value }) {
+    return render((<Input
+      value={value}
       handleClick={handleClick}
-    />
-  ));
+      onChange={onChange}
+    />));
+  }
 
   it('화면에 나타나는 content들을 표시합니다.', () => {
+    const { container, getByPlaceholderText } = renderInput({ value: '' });
+
     expect(container).toHaveTextContent('할 일');
     expect(container).toHaveTextContent('추가');
     expect(getByPlaceholderText('할 일을 입력해 주세요')).toBeInTheDocument();
+    expect(getByPlaceholderText('할 일을 입력해 주세요')).toHaveValue('');
   });
 
   it('추가버튼을 누르면 handleClick을 호출합니다.', () => {
+    const { getByText } = renderInput({ value: '' });
+
     expect(handleClick).not.toBeCalled();
     fireEvent.click(getByText('추가'));
     expect(handleClick).toBeCalled();
   });
-});
 
-describe('사용자가 Todo를 입력하는 경우', () => {
-  const onChange = jest.fn();
-
-  const { getByPlaceholderText } = render((
-    <Input
-      onChange={onChange}
-    />
-  ));
   it('입력이 변경됨을 표시합니다.', () => {
+    const { getByPlaceholderText } = renderInput({ value: 'TDD 과제하기' });
+
+    expect(getByPlaceholderText('할 일을 입력해 주세요')).toHaveValue('TDD 과제하기');
+
     expect(onChange).not.toBeCalled();
     fireEvent.change(getByPlaceholderText('할 일을 입력해 주세요'), { target: { value: 'TDD 과제하기' } });
     expect(onChange).toBeCalled();
--- src/List.test.jsx
+++ src/List.test.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import { render, fireEvent } from '@testing-library/react';
 
 import List from './List';
