2eaeed1a7ede35404d8bc2e08194ac1fcc0be6a1
gimsesu <tkfrn4799@gmail.com>
2021-02-09 15:32:08 +0900

    App 테스트 작성
    - 페이지에 떠야할 텍스트가 제대로 뜨는지 확인
     - To-do
     - 할 일
     - 할 일이 없어요!
    
    - '할 일'의 경우 테스트 실패
     - "TypeError: Expected container to be an Element,
     a Document or a DocumentFragment but got RegExp."

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -4,6 +4,12 @@ import { render } from '@testing-library/react';
 
 import App from './App';
 
-const a = render(
-  <App />,
-);
+test('App', () => {
+  const { getByText, getAllByText } = render((
+    <App />
+  ));
+
+  expect(getByText(/To-do/)).not.toBeNull();
+  expect(getAllByText(/할 일/)).not.toBeNull();
+  expect(getByText(/할 일이 없어요!/));
+});
