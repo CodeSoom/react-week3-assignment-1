3a7d56a830ec7d79108aa32c399795d123620afd
gimsesu <tkfrn4799@gmail.com>
2021-02-11 08:15:51 +0900

    List.test.jsx 4
    describe - it으로 할 일을 그리는 것와 '완료' 버튼을 그리는 테스트를 분리.

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -5,15 +5,27 @@ import { fireEvent, render } from '@testing-library/react';
 import List from './List';
 
 describe('List', () => {
-  test('Task exist', () => {
-    const tasks = [
-      { id: 1, title: 'Task-1' },
-      { id: 2, title: 'Task-2' },
-    ];
+  const onClickDelete = jest.fn();
 
-    const onClickDelete = jest.fn();
+  const tasks = [
+    { id: 1, title: 'Task-1' },
+    { id: 2, title: 'Task-2' },
+  ];
 
-    const { getByText, getAllByText } = render((
+  it('renders task', () => {
+    const { getByText } = render((
+      <List
+        tasks={tasks}
+        onClickDelete={onClickDelete}
+      />
+    ));
+
+    expect(getByText(/Task-1/)).not.toBeNull();
+    expect(getByText(/Task-2/)).not.toBeNull();
+  });
+
+  it('renders "완료" buttons with tasks', () => {
+    const { getAllByText } = render((
       <List
         tasks={tasks}
         onClickDelete={onClickDelete}
@@ -21,9 +33,6 @@ describe('List', () => {
     ));
 
     const buttons = getAllByText(/완료/);
-
-    expect(getByText(/Task-1/)).not.toBeNull();
-    expect(getByText(/Task-2/)).not.toBeNull();
 
     fireEvent.click(buttons[1]);
 
