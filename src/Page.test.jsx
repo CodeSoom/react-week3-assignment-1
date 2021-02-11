7fc54f066b24207d65d3f51ecd1385d2bb767c44
gimsesu <tkfrn4799@gmail.com>
2021-02-11 18:10:29 +0900

    Page.test.jsx 3
    '추가' 버튼 render 테스트 작성하기

--- src/Page.test.jsx
+++ src/Page.test.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import Page from './Page';
 
 // TODO: Page 컴포넌트 테스트 작성
 // 1. Page render 확인
+// 2. 각 상태 기능 생성
 
 describe('Page', () => {
+  const taskTitle = '';
+  const handleChangeTitle = jest.fn();
+  const handleClickAddTask = jest.fn();
+  const handleClickDeleteTask = jest.fn();
+
   const tasks = [
     { id: 1, title: '오늘은 무엇을 해볼까?' },
   ];
@@ -15,11 +21,35 @@ describe('Page', () => {
   it('render Head Title', () => {
     const { getByText } = render((
       <Page
+        taskTitle={taskTitle}
+        onChangeTitle={handleChangeTitle}
+        onClickAddTask={handleClickAddTask}
         tasks={tasks}
+        onClickDeleteTask={handleClickDeleteTask}
       />
     ));
 
     expect(getByText(/To-do/)).not.toBeNull();
     expect(getByText(/오늘은 무엇을 해볼까/)).not.toBeNull();
+  });
+
+  it('render button "추가" to add task', () => {
+    const { getByText } = render((
+      <Page
+        taskTitle={taskTitle}
+        onChangeTitle={handleChangeTitle}
+        onClickAddTask={handleClickAddTask}
+        tasks={tasks}
+        onClickDeleteTask={handleClickDeleteTask}
+      />
+    ));
+
+    const addButton = getByText(/추가/);
+
+    expect(handleClickAddTask).not.toBeCalled();
+
+    fireEvent.click(addButton);
+
+    expect(handleClickAddTask).toBeCalled();
   });
 });
