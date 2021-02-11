dcca69bf963d153cd383499ec8b983263026c4b4
gimsesu <tkfrn4799@gmail.com>
2021-02-11 10:07:26 +0900

    Page.test.jsx 2
    - Page를 render 할 때 'To-do' 텍스트가 출력되는지 확인
    - 할 일이 출력되는지 확인

--- src/Page.test.jsx
+++ src/Page.test.jsx
@@ -1,14 +1,25 @@
-// TODO: Page 컴포넌트 테스트 작성
-// 1. Page render 확인
+import React from 'react';
 
 import { render } from '@testing-library/react';
 
 import Page from './Page';
 
+// TODO: Page 컴포넌트 테스트 작성
+// 1. Page render 확인
+
 describe('Page', () => {
-  test('render Page', () => {
-    render((
-      <Page />
+  const tasks = [
+    { id: 1, title: '오늘은 무엇을 해볼까?' },
+  ];
+
+  it('render Head Title', () => {
+    const { getByText } = render((
+      <Page
+        tasks={tasks}
+      />
     ));
+
+    expect(getByText(/To-do/)).not.toBeNull();
+    expect(getByText(/오늘은 무엇을 해볼까/)).not.toBeNull();
   });
 });
