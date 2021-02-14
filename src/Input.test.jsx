d2f34b2bb14ed17be8cceabf03f8ea4cc5c05499
gimsesu <tkfrn4799@gmail.com>
2021-02-14 14:00:59 +0900

    Input.test.jsx - input value 확인하기

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -5,17 +5,19 @@ import { render } from '@testing-library/react';
 import Input from './Input';
 
 describe('Input', () => {
-  const taskTitle = '';
+  const taskTitle = '안녕';
   const handleChangeTitle = jest.fn();
   const handleClickAddTask = jest.fn();
 
   it('render input', () => {
-    render((
+    const { getByDisplayValue } = render((
       <Input
         value={taskTitle}
         onChange={handleChangeTitle}
         onClick={handleClickAddTask}
       />
     ));
+
+    expect(getByDisplayValue('안녕')).not.toBeNull();
   });
 });
