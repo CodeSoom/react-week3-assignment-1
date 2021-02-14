a173e7078371c23769515bc744ae24973f72a568
gimsesu <tkfrn4799@gmail.com>
2021-02-14 14:09:33 +0900

    Input.test.jsx - onChange 테스트 작성하기

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import Input from './Input';
 
@@ -9,7 +9,7 @@ describe('Input', () => {
   const handleChangeTitle = jest.fn();
   const handleClickAddTask = jest.fn();
 
-  it('render input', () => {
+  it('render the current input value', () => {
     const { getByDisplayValue } = render((
       <Input
         value={taskTitle}
@@ -19,5 +19,23 @@ describe('Input', () => {
     ));
 
     expect(getByDisplayValue('안녕')).not.toBeNull();
+  });
+
+  it('render input to write task', () => {
+    const { getByLabelText } = render((
+      <Input
+        value={taskTitle}
+        onChange={handleChangeTitle}
+        onClick={handleClickAddTask}
+      />
+    ));
+
+    const input = getByLabelText('할 일');
+
+    expect(handleChangeTitle).not.toBeCalled();
+
+    fireEvent.change(input, { target: { value: '커피를 마시자' } });
+
+    expect(handleChangeTitle).toBeCalled();
   });
 });
