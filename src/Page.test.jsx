0f992f5dae1fbf3a24ef6b2903b6b7c275c0d158
yujonglee <yujonglee@g.skku.edu>
2021-05-17 11:56:30 +0900

    Page.test.jsx를 작성하라

---
+++ src/Page.test.jsx
@@ -0,0 +1,26 @@
+import { render, screen } from '@testing-library/react';
+import Page from './Page';
+
+describe('Page.jsx', () => {
+  beforeEach(() => {
+    render(
+      <Page
+        taskTitle=""
+        tasks={[{ id: 1 }]}
+        onChangeTitle={() => null}
+        onClickAddTask={() => null}
+        onClickDeleteTask={() => null}
+      />,
+    );
+  });
+
+  it('header renders', () => {
+    expect(screen.getByText('To-do')).toBeInTheDocument();
+  });
+  it('Input component renders', () => {
+    expect(screen.getByRole('textbox')).toBeInTheDocument();
+  });
+  it('List component renders', () => {
+    expect(screen.getByRole('list')).toBeInTheDocument();
+  });
+});
