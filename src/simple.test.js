5c70e5715bea98258ae87c89bb9e1bfd74f3b1f4
seong-dodo <dyrnxm52@hanmail.net>
2021-05-17 14:00:34 +0900

    3강 강의 듣고 따라하기2
    - Signature

--- src/simple.test.js
+++ src/simple.test.js
@@ -1,4 +1,10 @@
-test('1+1의 결과값은 2이다', () => {
-  // assertion => A(actual)가 B(expect)여야 한다.
-  expect(1+1).toBe(5);
+function add(x, y) {
+  //ToDo :
+ return x + y;
+}
+
+test('add', () => {
+  expect(add(1, 3)).toBe(4);
 });
+
+//signature - name(add), parmeters(x, y), return(result)
