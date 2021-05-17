6300c5b93d4bf52ebc91c35133c6f53b7756f1ce
최한울 <hanul.choi95@nts-corp.com>
2021-05-17 22:48:37 +0900

    App 컴포넌트 테스트 작성
    - 할 일 추가, 삭제, 변경에 대한 테스트 작성

---
+++ src/App.test.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import { render, fireEvent } from '@testing-library/react';
+import App from "./App";
+
+test('App', () => {
+  const { container, getByText, getByLabelText } = render(
+    <App />
+  );
+
+  const todo = getByLabelText('할 일');
+
+  expect(container).toHaveTextContent('To-do');
+  expect(container).toHaveTextContent('할 일');
+  expect(container).toHaveTextContent('추가');
+  expect(container).toHaveTextContent('할 일이 없어요!');
+
+  fireEvent.change(todo, { target: { value: '멋대로 살기' }});
+  fireEvent.click(getByText('추가'));
+
+  expect(container).toHaveTextContent('To-do');
+  expect(container).toHaveTextContent('할 일');
+  expect(container).toHaveTextContent('추가');
+  expect(container).toHaveTextContent('멋대로 살기');
+  expect(container).toHaveTextContent('완료');
+
+  fireEvent.click(getByText('완료'));
+
+  expect(container).toHaveTextContent('To-do');
+  expect(container).toHaveTextContent('할 일');
+  expect(container).toHaveTextContent('추가');
+  expect(container).toHaveTextContent('할 일이 없어요!');
+
+});
