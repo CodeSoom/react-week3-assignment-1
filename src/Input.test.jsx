9a1846ed552a7070e618cb15221358bd1979eed0
yujonglee <yujonglee@g.skku.edu>
2021-05-17 11:22:49 +0900

    Input.test.jsx를 작성하라

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -2,28 +2,58 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Input from './Input';
 
-it('', () => {
-  const taskTitle = 'ab';
+describe('Input.jsx', () => {
+  const taskTitle = '';
   const onChangeTitle = jest.fn();
   const onClickAddTask = jest.fn();
 
-  render(
-    <Input
-      value={taskTitle}
-      onChange={onChangeTitle}
-      onClick={onClickAddTask}
-    />,
-  );
+  beforeEach(() => {
+    render(
+      <Input
+        value={taskTitle}
+        onChange={onChangeTitle}
+        onClick={onClickAddTask}
+      />,
+    );
+  });
 
-  expect(onClickAddTask).not.toBeCalled();
+  describe('test add button', () => {
+    it('button renders', () => {
+      expect(screen.getByRole('button', { name: '추가' })).toBeInTheDocument();
+    });
 
-  userEvent.click(screen.getByRole('button', { name: '추가' }));
+    it('button works', () => {
+      expect(onClickAddTask).not.toBeCalled();
 
-  expect(onClickAddTask).toBeCalledTimes(1);
+      userEvent.click(screen.getByRole('button', { name: '추가' }));
 
-  const InputBox = screen.getByRole('textbox', { name: '할 일' });
-  expect(InputBox).toHaveValue('ab');
-  userEvent.type(InputBox, 'abcd');
+      expect(onClickAddTask).toBeCalledTimes(1);
+    });
+  });
 
-  expect(onChangeTitle).toBeCalledTimes(4);
+  describe('test inputbox', () => {
+    it('inputbox renders', () => {
+      expect(screen.getByRole('textbox', { name: '할 일' })).toBeInTheDocument();
+    });
+
+    context('before type', () => {
+      it('onChange call X', () => {
+        expect(onChangeTitle).not.toBeCalled();
+      });
+      it('empty input', () => {
+        expect(screen.getByRole('textbox', { name: '할 일' })).toHaveValue('');
+      });
+    });
+
+    context('after type', () => {
+      it('onChange call O', () => {
+        userEvent.type(screen.getByRole('textbox'), 'abcd');
+        expect(onChangeTitle).toBeCalledTimes(4);
+      });
+    //   it('filled input', () => {
+    //     userEvent.type(screen.getByRole('textbox'), 'abcd');
+    //     expect(screen.getByRole('textbox', { name: '할 일' })).toHaveValue('abcd');
+    //   });
+    });
+  });
 });
