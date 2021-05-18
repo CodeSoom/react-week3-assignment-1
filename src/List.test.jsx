a8e66b3ff091b1bd2398a5a1328e8a7a5aa8f25c
salangdung-i <dladmswl92@naver.com>
2021-05-18 15:26:51 +0900

    List.test.jsx 테스트시 완료버튼 클릭안되는 오류

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -1,5 +1,35 @@
+import { render, fireEvent } from '@testing-library/react';
+
 import List from './List';
 
 test('List', () => {
-    //TODO
+     const task = [{
+         id:1,
+         title: '코드숨'},
+     ];
+
+    const handleClick = jest.fn();
+
+    const { container, getByText } = render((
+        <List
+          tasks={task}
+          onClickDelete={handleClick}
+        />
+    ));
+
+    // 1. 코드숨 완료가 생긴다. 
+    expect(container).toHaveTextContent('코드숨');
+    expect(container).toHaveTextContent('완료');
+ 
+    // 2.완료 버튼을 눌렀을 떄 task.title =>'코드숨' 이 사라지고,
+    // '할 일이 없어요!' 가 출력된다.
+    
+    expect(handleClick).not.toBeCalled();
+
+    fireEvent.click(getByText('완료'));
+  
+    expect(handleClick).toBeCalledWith(1);
+  
+    expect(container).toHaveTextContent('할 일이 없어요!');  
+        
 });
