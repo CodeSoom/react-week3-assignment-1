53db79c452d8f3c7d6160ae62b77e31ab1e09441
seong-dodo <dyrnxm52@hanmail.net>
2021-05-18 22:15:24 +0900

    Input 컴포넌트 데스트 코드 추가 및 변경
    - 추가버튼이 눌러진다 추가
    - 할 일을 입력하면 입력갑으로 변경되어야한다 변경

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -2,7 +2,7 @@ import { render, fireEvent } from '@testing-library/react';
 
 import Input from './Input';
 
-test('할 일을 입력할 수 있다', () => {
+test('할 일을 입력하면 입력값으로 변경되어야 한다', () => {
   const value = '뭐라도 하기';
   const handleChange = jest.fn();
   const handleClick = jest.fn();
@@ -17,6 +17,7 @@ test('할 일을 입력할 수 있다', () => {
 
   const input = getByPlaceholderText('할 일을 입력해 주세요');
 
+  expect(handleChange).not.toBeCalled();
   fireEvent.change(input, {
     target: {
       value: '뭐라도 하기',
@@ -24,4 +25,20 @@ test('할 일을 입력할 수 있다', () => {
   });
 
   expect(input).toHaveAttribute('value', '뭐라도 하기');
+});
+
+test('추가버튼이 눌러진다', () => {
+  const handleClick = jest.fn();
+
+  const { container, getByText } = render((
+    <Input
+      onClick={handleClick}
+    />
+  ));
+
+  expect(container).toHaveTextContent('추가');
+
+  expect(handleClick).not.toBeCalled();
+
+  fireEvent.click(getByText('추가'));
 });
