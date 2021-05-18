f91d1686bf1eb10f6681fc964052612dc56ec89d
yujonglee <yujonglee@g.skku.edu>
2021-05-18 13:49:41 +0900

    renderList함수를 추출하라

--- src/tests/List.test.jsx
+++ src/tests/List.test.jsx
@@ -2,35 +2,40 @@ import { render, screen } from '@testing-library/react';
 
 import List from '../components/List';
 
+const renderEmptyList = () => {
+  render(
+    <List
+      tasks={[]}
+      onClickDelete={() => null}
+    />,
+  );
+};
+
+const renderNonEmptyList = () => {
+  render(
+    <List
+      tasks={[{ id: 1 }, { id: 2 }, { id: 3 }]}
+      onClickDelete={() => null}
+    />,
+  );
+};
+
 describe('Test List component', () => {
   context('when list is empty', () => {
-    render(
-      <List
-        tasks={[]}
-        onClickDelete={() => null}
-      />,
-    );
-
+    renderEmptyList();
     it('text renders', () => {
       expect(screen.getByText('할 일이 없어요!')).toBeInTheDocument();
     });
   });
 
   context('when list is not empty', () => {
-    const element = (
-      <List
-        tasks={[{ id: 1 }, { id: 2 }, { id: 3 }]}
-        onClickDelete={() => null}
-      />
-    );
-
     it('items render', () => {
-      render(element);
+      renderNonEmptyList();
       expect(screen.getAllByRole('listitem').length).toBe(3);
     });
 
     it('buttons render', () => {
-      render(element);
+      renderNonEmptyList();
       expect(screen.getAllByRole('button').length).toBe(3);
     });
   });
