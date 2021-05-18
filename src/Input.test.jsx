d9844e40930cda179d8ca03a2aaa816e663d28ea
최한울 <hanul.choi95@nts-corp.com>
2021-05-18 22:24:42 +0900

    Input 컴포넌트 추가 버튼 테스트
    - 추가 버튼을 눌러보고 정말 눌렀는지 확인하는 테스트 작성

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import Input from './Input';
 
 test('add Todo', () => {
-  const { container } = render(
+  const handleClickAddTodo = jest.fn();
+
+  const { container, getByText } = render(
     (
-      <Input />
+      <Input onClick={handleClickAddTodo} />
     ),
   );
 
   expect(container).toHaveTextContent('할 일');
   expect(container).toHaveTextContent('추가');
+
+  expect(handleClickAddTodo).not.toBeCalled();
+  fireEvent.click(getByText('추가'));
+  expect(handleClickAddTodo).toBeCalled();
 });
