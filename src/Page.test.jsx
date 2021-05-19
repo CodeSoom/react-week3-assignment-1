44500472c23e80f8f66a26140e40075f5781a13c
최한울 <hanul.choi95@nts-corp.com>
2021-05-19 20:43:49 +0900

    Page 컴포넌트 describe - it 사용하기

--- src/Page.test.jsx
+++ src/Page.test.jsx
@@ -4,20 +4,18 @@ import { render } from '@testing-library/react';
 
 import Page from './Page';
 
-test('Page', () => {
-  const tasks = [
-    { id: 1, title: '멋대로 살기' },
-  ];
+describe('Page 컴포넌트', () => {
+  it('렌더링 된 화면 확인', () => {
+    const tasks = [
+      { id: 1, title: '멋대로 살기' },
+    ];
 
-  const { container } = render(
-    (
-      <Page tasks={tasks} />
-    ),
-  );
+    const { container } = render((<Page tasks={tasks} />));
 
-  expect(container).toHaveTextContent('To-do');
-  expect(container).toHaveTextContent('할 일');
-  expect(container).toHaveTextContent('추가');
-  expect(container).toHaveTextContent('멋대로 살기');
-  expect(container).toHaveTextContent('완료');
+    expect(container).toHaveTextContent('To-do');
+    expect(container).toHaveTextContent('할 일');
+    expect(container).toHaveTextContent('추가');
+    expect(container).toHaveTextContent('멋대로 살기');
+    expect(container).toHaveTextContent('완료');
+  });
 });
