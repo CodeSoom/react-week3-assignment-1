e7fdeadc37509944bb71aa1bbcc9a4b94e134634
최한울 <hanul.choi95@nts-corp.com>
2021-05-19 21:09:20 +0900

    App 컴포넌트 테스트 분리
    - 할일 추가와 할일 완료 테스트가 겹쳐있어서 분리
    - 그치만 할 일 추가 후 완료 버튼이 생기기 때문에 제대로 분리하지 못했음. 추후 완전 분리 필요.

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -5,30 +5,28 @@ import { render, fireEvent } from '@testing-library/react';
 import App from './App';
 
 describe('App 컴포넌트', () => {
-  it('할 일 추가하고 할 일 완료하기', () => {
-    const { container, getByText, getByLabelText } = render(
+  it('할 일 추가하기', () => {
+    const { getByText, getByLabelText } = render(
       (
         <App />
       ),
     );
 
-    const todo = getByLabelText('할 일');
-
-    fireEvent.change(todo, { target: { value: '멋대로 살기' } });
+    fireEvent.change(getByLabelText('할 일'), { target: { value: '멋대로 살기' } });
     fireEvent.click(getByText('추가'));
+    expect(getByText('멋대로 살기')).toHaveTextContent('멋대로 살기');
+  });
 
-    expect(container).toHaveTextContent('To-do');
-    expect(container).toHaveTextContent('할 일');
-    expect(container).toHaveTextContent('추가');
-    expect(container).toHaveTextContent('멋대로 살기');
-    expect(container).toHaveTextContent('완료');
+  it('할 일 추가 후 완료하여 할 일 제거하기', () => {
+    const { getByText, getByLabelText } = render(
+      (
+        <App />
+      ),
+    );
 
+    fireEvent.change(getByLabelText('할 일'), { target: { value: '멋대로 살기' } });
+    fireEvent.click(getByText('추가'));
     fireEvent.click(getByText('완료'));
-
-    expect(container).toHaveTextContent('To-do');
-    expect(container).toHaveTextContent('할 일');
-    expect(container).toHaveTextContent('추가');
-    expect(container).toHaveTextContent('할 일이 없어요!');
-  })
+    expect(getByText('할 일이 없어요!')).toHaveTextContent('할 일이 없어요!');
+  });
 });
-
