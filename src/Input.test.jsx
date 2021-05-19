b3ee4b00eb534bdffdf0c0be2563226f15c8c0aa
dngwoodo <dngwoodo@gmail.com>
2021-05-19 16:55:26 +0900

    test: Add Input test case code for UI
    Add test case code for 'it renders label, input, button'

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -1,5 +1,14 @@
+import { screen, render } from '@testing-library/react';
+
+import Input from './Input';
+
 describe('<Input />', () => {
-  it('renders label, input, button', () => {});
+  it('renders label, input, button', () => {
+    render(<Input />);
+
+    screen.getByRole('textbox', { name: /할 일/ });
+    screen.getByRole('button', { name: /추가/ });
+  });
 
   it('changes input', () => {});
 
