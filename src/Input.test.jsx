ba58c65a269b1c704445ad042fc4b53bdaf1cd25
최한울 <hanul.choi95@nts-corp.com>
2021-05-19 22:08:52 +0900

    Input 컴포넌트 given2 적용 및 placeholder 확인 테스트 추가
    - given2를 적용하여 중복 제거
    - placeholder 내용을 확인하기 위해 테스트 작성

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -1,3 +1,4 @@
+/* global given */
 import React from 'react';
 
 import { render, fireEvent } from '@testing-library/react';
@@ -5,9 +6,24 @@ import { render, fireEvent } from '@testing-library/react';
 import Input from './Input';
 
 describe('Input 컴포넌트', () => {
+  given('render', () => render(<Input onClick={given.clickEvent} />));
+
+  it('렌더링 페이지 텍스트 확인', () => {
+    const { container } = given.render;
+    expect(container).toHaveTextContent('할 일추가');
+  });
+
+  it('placeholder 텍스트 확인', () => {
+    const { getByLabelText } = given.render;
+    const placeholder = getByLabelText('할 일').placeholder.toString();
+    expect(placeholder).toBe('할 일을 입력해 주세요');
+  });
+
   it('add Todo', () => {
     const handleClickAddTodo = jest.fn();
-    const { getByText } = render((<Input onClick={handleClickAddTodo} />));
+    given('clickEvent', () => handleClickAddTodo);
+
+    const { getByText } = given.render;
 
     expect(handleClickAddTodo).not.toBeCalled();
     fireEvent.click(getByText('추가'));
