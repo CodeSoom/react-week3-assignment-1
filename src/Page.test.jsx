e3717837373a64b519b9ac044708c479af34e7b7
dngwoodo <dngwoodo@gmail.com>
2021-05-20 00:11:22 +0900

    test: Add Page test case code for UI
    Add Page test case code for 'it renders title, Input, List'

--- src/Page.test.jsx
+++ src/Page.test.jsx
@@ -1,5 +1,18 @@
+import { render, screen } from '@testing-library/react';
+import Page from './Page';
+
 describe('<Page />', () => {
-  it('renders title, Input, List', () => {});
+  const tasks = [];
+  it('renders title, Input, List', () => {
+    render(<Page tasks={tasks} />);
+
+    screen.getByRole('heading', { name: /To-do/i });
+
+    screen.getByRole('textbox', { name: /할 일/ });
+    screen.getByRole('button', { name: /추가/ });
+
+    screen.getByText(/할 일이 없어요!/);
+  });
 
   it('renders text when tasks does not exist ', () => {});
 
