d3c5b231bc1daf277d0e70f80c2d1ebdae6676fe
최한울 <hanul.choi95@nts-corp.com>
2021-05-19 20:48:43 +0900

    App 컴포넌트 describe - it 사용하기

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -4,28 +4,31 @@ import { render, fireEvent } from '@testing-library/react';
 
 import App from './App';
 
-test('App', () => {
-  const { container, getByText, getByLabelText } = render(
-    (
-      <App />
-    ),
-  );
+describe('App 컴포넌트', () => {
+  it('할 일 추가하고 할 일 완료하기', () => {
+    const { container, getByText, getByLabelText } = render(
+      (
+        <App />
+      ),
+    );
 
-  const todo = getByLabelText('할 일');
+    const todo = getByLabelText('할 일');
 
-  fireEvent.change(todo, { target: { value: '멋대로 살기' } });
-  fireEvent.click(getByText('추가'));
+    fireEvent.change(todo, { target: { value: '멋대로 살기' } });
+    fireEvent.click(getByText('추가'));
 
-  expect(container).toHaveTextContent('To-do');
-  expect(container).toHaveTextContent('할 일');
-  expect(container).toHaveTextContent('추가');
-  expect(container).toHaveTextContent('멋대로 살기');
-  expect(container).toHaveTextContent('완료');
+    expect(container).toHaveTextContent('To-do');
+    expect(container).toHaveTextContent('할 일');
+    expect(container).toHaveTextContent('추가');
+    expect(container).toHaveTextContent('멋대로 살기');
+    expect(container).toHaveTextContent('완료');
 
-  fireEvent.click(getByText('완료'));
+    fireEvent.click(getByText('완료'));
 
-  expect(container).toHaveTextContent('To-do');
-  expect(container).toHaveTextContent('할 일');
-  expect(container).toHaveTextContent('추가');
-  expect(container).toHaveTextContent('할 일이 없어요!');
+    expect(container).toHaveTextContent('To-do');
+    expect(container).toHaveTextContent('할 일');
+    expect(container).toHaveTextContent('추가');
+    expect(container).toHaveTextContent('할 일이 없어요!');
+  })
 });
+
