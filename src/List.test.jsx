61d388a0fb1c1b91953f8e1dd71e6f6f71fb01c8
dngwoodo <dngwoodo@gmail.com>
2021-05-19 15:24:25 +0900

    test: Add List test case

---
+++ src/List.test.jsx
@@ -0,0 +1,23 @@
+describe('<List />', () => {
+  const tasks = [
+    {
+      id: 1,
+      title: '뭐라도 하기',
+    },
+    {
+      id: 1,
+      title: '뭐라도 하기 2',
+    },
+  ];
+
+  const emptyTasks = [];
+  it('renders text when tasks does not exist', () => {
+
+  });
+
+  it('renders tasks', () => {
+
+  });
+
+  it('calls onClickDelete', () => {});
+});
