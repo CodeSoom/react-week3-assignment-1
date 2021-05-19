d8892f061aa07a234105a7461b6ad1c9c13414c5
dngwoodo <dngwoodo@gmail.com>
2021-05-20 01:32:23 +0900

    test: Add test case code for taskTitle

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,5 +1,15 @@
+import { fireEvent, screen, render } from '@testing-library/react';
+
+import App from './App';
+
 describe('<App />', () => {
-  it('changes taskTitle', () => {});
+  it('changes taskTitle', () => {
+    render(<App />);
+
+    fireEvent.change(screen.getByRole('textbox', { name: '할 일' }), { target: { value: '뭐라도 하기' } });
+
+    expect(screen.getByRole('textbox', { name: '할 일' })).toHaveAttribute('value', '뭐라도 하기');
+  });
 
   it('creates new task', () => {});
 
