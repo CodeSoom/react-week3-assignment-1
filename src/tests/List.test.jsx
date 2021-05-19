57d7e41d418f0dcdabc30f20b3f2f8574a831fa8
yujonglee <yujonglee@g.skku.edu>
2021-05-19 11:28:42 +0900

    List.test.jsx를 given2를 활용하여 리팩터링하라

--- src/tests/List.test.jsx
+++ src/tests/List.test.jsx
@@ -1,42 +1,37 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 import List from '../components/List';
 
-const renderEmptyList = () => {
-  render((
-    <List
-      tasks={[]}
-      onClickDelete={jest.fn()}
-    />
-  ));
-};
-
-const renderNonEmptyList = () => {
-  render((
-    <List
-      tasks={[{ id: 1 }, { id: 2 }, { id: 3 }]}
-      onClickDelete={jest.fn()}
-    />
-  ));
-};
-
 describe('List component', () => {
+  given('container', () => render((
+    <List
+      tasks={given.tasks}
+      onClickDelete={jest.fn()}
+    />
+  )));
+
   context('without tasks', () => {
-    renderEmptyList();
     it('renders message', () => {
-      expect(screen.getByText('할 일이 없어요!')).toBeInTheDocument();
+      given('tasks', () => []);
+      const { getByText } = given.container;
+
+      expect(getByText('할 일이 없어요!')).toBeInTheDocument();
     });
   });
 
   context('with tasks', () => {
     it('renders tasks', () => {
-      renderNonEmptyList();
-      expect(screen.getAllByRole('listitem').length).toBe(3);
+      given('tasks', () => [{ id: 1 }, { id: 2 }, { id: 3 }]);
+      const { getAllByRole } = given.container;
+
+      expect(getAllByRole('listitem').length).toBe(3);
     });
 
     it('renders finish button for each task', () => {
-      renderNonEmptyList();
-      expect(screen.getAllByRole('button').length).toBe(3);
+      given('tasks', () => [{ id: 1 }, { id: 2 }, { id: 3 }]);
+      const { getAllByRole } = given.container;
+
+      expect(getAllByRole('button').length).toBe(3);
     });
   });
 });
