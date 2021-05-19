942ec9df9117d8dd05aa9e1470c6a6a4a693f1d5
dngwoodo <dngwoodo@gmail.com>
2021-05-19 18:53:47 +0900

    test: Add Input test case code for change value
    Add Input test case code for 'it changes input'

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -1,8 +1,16 @@
-import { screen, render } from '@testing-library/react';
+import {
+  screen, render, fireEvent,
+} from '@testing-library/react';
 
 import Input from './Input';
 
 describe('<Input />', () => {
+  let value = '';
+
+  const onChange = jest.fn((e) => {
+    value = e.target.value;
+  });
+
   it('renders label, input, button', () => {
     render(<Input />);
 
@@ -10,7 +18,19 @@ describe('<Input />', () => {
     screen.getByRole('button', { name: /추가/ });
   });
 
-  it('changes input', () => {});
+  it('changes input', () => {
+    const { rerender } = render(<Input value={value} onChange={onChange} />);
 
-  it('calls onClick when click button', () => {});
+    const input = screen.getByRole('textbox', { name: /할 일/ });
+
+    fireEvent.change(input, { target: { value: '뭐라도 하기' } });
+
+    rerender(<Input value={value} onChange={onChange} />);
+
+    expect(onChange).toBeCalled();
+    expect(input).toHaveAttribute('value', '뭐라도 하기');
+  });
+
+  it('calls onClick when click button', () => {
+  });
 });
