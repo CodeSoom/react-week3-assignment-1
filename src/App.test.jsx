8f2a8565391d46628edc1daaac0f04cac620fc17
dngwoodo <dngwoodo@gmail.com>
2021-05-20 01:32:57 +0900

    test: Add App test case code to create new task

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -11,7 +11,14 @@ describe('<App />', () => {
     expect(screen.getByRole('textbox', { name: '할 일' })).toHaveAttribute('value', '뭐라도 하기');
   });
 
-  it('creates new task', () => {});
+  it('creates new task', () => {
+    render(<App />);
+
+    fireEvent.change(screen.getByRole('textbox', { name: '할 일' }), { target: { value: '뭐라도 하기' } });
+    fireEvent.click(screen.getByRole('button', { name: '추가' }));
+
+    screen.getByText('뭐라도 하기');
+  });
 
   it('deletes a task', () => {});
 });
