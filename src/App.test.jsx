a80e169c4a096bfa538dbab50ed10d3f2dd87b81
seong-dodo <dyrnxm52@hanmail.net>
2021-05-19 20:53:37 +0900

    App컴포넌트 테스트 코드 만들기2
    - input에 할 일을 입력하면 입력값으로 바뀐다

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -18,3 +18,22 @@ test('App컴포넌트 초기화 상태', () => {
   expect(container.hasChildNodes('list')).toBe(true);
   expect(container).toHaveTextContent('할 일이 없어요!');
 });
+
+describe('input', () => {
+  it('input에 할 일을 입력하면 입력값으로 바뀐다', () => {
+    const { getByPlaceholderText } = render((
+      <App />
+    ));
+
+    const input = getByPlaceholderText('할 일을 입력해 주세요');
+    expect(input).toHaveAttribute('value', '');
+
+    fireEvent.change(input, {
+      target: {
+        value: '뭐라도 하기',
+      },
+    });
+
+    expect(input).toHaveAttribute('value', '뭐라도 하기');
+  });
+});
