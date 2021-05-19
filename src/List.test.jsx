880c29350393656edf158be0c570d05a5b24f327
최한울 <hanul.choi95@nts-corp.com>
2021-05-19 21:48:09 +0900

    List 컴포넌트 given2 사용하여 중복 제거

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -1,3 +1,4 @@
+/* global given */
 import React from 'react';
 
 import { render } from '@testing-library/react';
@@ -5,18 +6,20 @@ import { render } from '@testing-library/react';
 import List from './List';
 
 describe('TodoList 컴포넌트', () => {
+  given('render', () => render(<List tasks={given.tasks} />));
+
   it('할 일이 없을 때', () => {
-    const { container } = render((<List tasks={[]} />));
+    given('tasks', () => []);
+    const { container } = given.render;
     expect(container).toHaveTextContent('할 일이 없어요!');
   });
 
   it('할 일이 있을 때', () => {
-    const tasks = [
+    given('tasks', () => [
       { id: 1, title: '멋대로 살기' },
       { id: 2, title: '아무렇게나 살기' },
-    ];
-
-    const { container } = render((<List tasks={tasks} />));
+    ]);
+    const { container } = given.render;
     expect(container).toHaveTextContent('멋대로 살기완료');
     expect(container).toHaveTextContent('아무렇게나 살기완료');
   });
