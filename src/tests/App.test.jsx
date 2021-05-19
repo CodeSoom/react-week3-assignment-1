bba2c4e0611f92eeea5472780190e4355b6b4e55
yujonglee <yujonglee@g.skku.edu>
2021-05-19 13:31:58 +0900

    App.jsx를 given2를 활용하여 리팩터링하라

--- src/tests/App.test.jsx
+++ src/tests/App.test.jsx
@@ -1,51 +1,58 @@
-import { render, screen, within } from '@testing-library/react';
+import { render, within } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import App from '../App';
 
-const addTask = (title) => {
-  userEvent.type(
-    screen.getByRole('textbox', { name: '할 일' }),
-    title,
-  );
-  userEvent.click(screen.getByRole('button', { name: '추가' }));
-};
-
-const deleteTask = (title) => {
-  const targetTask = screen.getByText(title);
-  userEvent.click(within(targetTask).getByRole('button', { name: '완료' }));
-};
-
 describe('App component', () => {
-  beforeEach(() => {
-    render(<App />);
+  given('container', () => (
+    render(<App />)
+  ));
+
+  it('adds current input to list', () => {
+    const { getByText, queryByText, getByRole } = given.container;
+
+    expect(queryByText('건물 매입')).not.toBeInTheDocument();
+
+    userEvent.type(
+      getByRole('textbox', { name: '할 일' }),
+      '건물 매입',
+    );
+    userEvent.click(getByRole('button', { name: '추가' }));
+
+    expect(getByText('건물 매입')).toBeInTheDocument();
   });
 
-  context('when user wants to add task', () => {
-    it('adds current input to list', () => {
-      expect(screen.queryByText('건물 매입')).not.toBeInTheDocument();
+  it('only removes target task from list', () => {
+    const { getByText, queryByText, getByRole } = given.container;
 
-      addTask('건물 매입');
+    userEvent.type(
+      getByRole('textbox', { name: '할 일' }),
+      '건물1 매입',
+    );
+    userEvent.click(getByRole('button', { name: '추가' }));
 
-      expect(screen.getByText('건물 매입')).toBeInTheDocument();
-    });
-  });
+    userEvent.type(
+      getByRole('textbox', { name: '할 일' }),
+      '건물2 매입',
+    );
+    userEvent.click(getByRole('button', { name: '추가' }));
 
-  context('when user wants to delete task', () => {
-    it('only removes target task from list', () => {
-      addTask('건물1 매입');
-      addTask('건물2 매입');
-      expect(screen.getByText('건물1 매입')).toBeInTheDocument();
-      expect(screen.getByText('건물2 매입')).toBeInTheDocument();
+    expect(getByText('건물1 매입')).toBeInTheDocument();
+    expect(getByText('건물2 매입')).toBeInTheDocument();
 
-      deleteTask('건물1 매입');
-      expect(screen.queryByText('건물1 매입')).not.toBeInTheDocument();
-      expect(screen.getByText('건물2 매입')).toBeInTheDocument();
+    userEvent.click(
+      within(getByText('건물1 매입'))
+        .getByRole('button', { name: '완료' }),
+    );
+    expect(queryByText('건물1 매입')).not.toBeInTheDocument();
+    expect(getByText('건물2 매입')).toBeInTheDocument();
 
-      deleteTask('건물2 매입');
-      expect(screen.queryByText('건물1 매입')).not.toBeInTheDocument();
-      expect(screen.queryByText('건물2 매입')).not.toBeInTheDocument();
-      expect(screen.getByText('할 일이 없어요!')).toBeInTheDocument();
-    });
+    userEvent.click(
+      within(getByText('건물2 매입'))
+        .getByRole('button', { name: '완료' }),
+    );
+    expect(queryByText('건물1 매입')).not.toBeInTheDocument();
+    expect(queryByText('건물2 매입')).not.toBeInTheDocument();
+    expect(getByText('할 일이 없어요!')).toBeInTheDocument();
   });
 });
