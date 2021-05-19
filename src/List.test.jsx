49c181de7788e25f76623653a5308d4890dbd35f
최한울 <hanul.choi95@nts-corp.com>
2021-05-19 19:33:31 +0900

    List 컴포넌트 describe - it 사용하기
    - context는 추후 구현 예정

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -4,28 +4,20 @@ import { render } from '@testing-library/react';
 
 import List from './List';
 
-test('TodoList', () => {
-  const notExistTodoContainer = render(
-    (
-      <List tasks={[]} />
-    ),
-  ).container;
+describe('TodoList', () => {
+  it('Nothing Todo', () => {
+    const { container } = render((<List tasks={[]} />));
+    expect(container).toHaveTextContent('할 일이 없어요!');
+  });
 
-  expect(notExistTodoContainer).toHaveTextContent('할 일이 없어요!');
+  it('Exist Todo', () => {
+    const tasks = [
+      { id: 1, title: '멋대로 살기' },
+      { id: 2, title: '아무렇게나 살기' },
+    ];
 
-  const tasks = [
-    { id: 1, title: '멋대로 살기' },
-    { id: 2, title: '아무렇게나 살기' },
-  ];
-
-  const todoListContainer = render(
-    (
-      <List tasks={tasks} />
-    ),
-  ).container;
-
-  expect(todoListContainer).toHaveTextContent('멋대로 살기');
-  expect(todoListContainer).toHaveTextContent('완료');
-  expect(todoListContainer).toHaveTextContent('아무렇게나 살기');
-  expect(todoListContainer).toHaveTextContent('완료');
+    const { container } = render((<List tasks={tasks} />));
+    expect(container).toHaveTextContent('멋대로 살기완료');
+    expect(container).toHaveTextContent('아무렇게나 살기완료');
+  });
 });
