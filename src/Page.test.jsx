e24f63cf905a2741b9a9be82f03cde84162c8cd3
dngwoodo <dngwoodo@gmail.com>
2021-05-19 23:53:04 +0900

    test: Add Page test case

---
+++ src/Page.test.jsx
@@ -0,0 +1,9 @@
+describe('<Page />', () => {
+  it('renders title, Input, List', () => {});
+
+  it('renders text when tasks does not exist ', () => {});
+
+  it('creates new task', () => {});
+
+  it('removes task', () => {});
+});
