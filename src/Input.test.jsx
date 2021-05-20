4077ed8b65326ba71964a9ead0e79fe8e067ae6d
hanvit <hanvit7@gmail.com>
2021-05-20 14:57:01 +0900

    List 컴포넌트의 has tasks 경우 및 Input 컴포넌트 테스트 추가
    List 컴포넌트에 3개의 tasks를 전달 후 리스트로 표시됨을 확인
    Input 컴포넌트의 기본 동작 테스트 추가

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -1,11 +1,22 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 import Input from './Input';
 
 test('Input', () => {
-  const { container } = render((
-    <Input />
+  const handleChange = jest.fn();
+  const handleClick = jest.fn();
+
+  const {
+    container, getByPlaceholderText, getByRole, getByText,
+  } = render((
+    <Input value="새로운 할일" onChange={handleChange} onClick={handleClick} />
   ));
 
   expect(container).toHaveTextContent('할 일');
+  expect(getByRole('textbox').value).toMatch('새로운 할일');
+  expect(getByPlaceholderText('할 일을 입력해 주세요')).toBeValid();
+  expect(getByRole('button', { name: '추가' })).toBeValid();
+
+  fireEvent.click(getByText('추가'));
+  expect(getByRole('textbox').value).toMatch('');
 });
--- src/List.test.jsx
+++ src/List.test.jsx
@@ -1,19 +1,30 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 import List from './List';
 
 describe('List', () => {
-  test('empty task', () => {
+  const onClickDelete = jest.fn();
+
+  it('empty task', () => {
     const { container } = render((
-      <List tasks={[]} />
+      <List tasks={[]} onClickDelete={onClickDelete} />
     ));
     expect(container).toHaveTextContent('할 일이 없어요!');
   });
 
-  test('task', () => {
-    const { container } = render((
-      <List tasks={[{ id: 1, task: '새로운할일' }]} />
+  it('has tasks', () => {
+    const tasks = [
+      { id: 1, title: '새로운 할일 #1' },
+      { id: 2, title: '새로운 할일 #2' },
+      { id: 3, title: '새로운 할일 #3' },
+    ];
+    const {
+      container, getAllByRole, getAllByText,
+    } = render((
+      <List tasks={tasks} onClickDelete={onClickDelete} />
     ));
     expect(container).toHaveTextContent('완료');
+    expect(getAllByText(/새로운 할일 #\d+$/)).toHaveLength(tasks.length);
+    expect(getAllByRole('listitem')).toHaveLength(tasks.length);
   });
 });
