7aca533c1c04cde56d7e1c7b4fe6fcea0213612c
hanvit <hanvit7@gmail.com>
2021-05-21 00:22:22 +0900

    Page 컴포넌트 테스트 구현
    할 일이 있는 경우
    - "To-do" 타이틀을 표시한다.
    - "할 일 없어요!"를 표시한다.
    
    할 일이 없는 경우
    - "To-do" 타이틀을 표시한다.
    - list를 표시한다.

--- src/Page.test.jsx
+++ src/Page.test.jsx
@@ -1,12 +1,76 @@
 import { render } from '@testing-library/react';
 
-import App from './App';
+import Page from './Page';
 
-describe('App', () => {
-  test('empty App', () => {
-    const { container } = render((
-      <App />
-    ));
-    expect(container).toHaveTextContent('할 일이 없어요!');
+describe('Page', () => {
+  const onChangeTitle = jest.fn();
+  const onClickAddTask = jest.fn();
+  const onClickDeleteTask = jest.fn();
+
+  context('할 일이 없는 경우', () => {
+    const tasks = [];
+    it('"To-do" 타이틀을 표시한다.', () => {
+      const { container } = render((
+        <Page
+          taskTitle=""
+          onChangeTitle={onChangeTitle}
+          onClickAddTask={onClickAddTask}
+          tasks={tasks}
+          onClickDeleteTask={onClickDeleteTask}
+        />
+      ));
+
+      expect(container).toHaveTextContent('To-do');
+    });
+
+    it('"할 일 없어요!"를 표시한다.', () => {
+      const { getByText } = render((
+        <Page
+          taskTitle=""
+          onChangeTitle={onChangeTitle}
+          onClickAddTask={onClickAddTask}
+          tasks={tasks}
+          onClickDeleteTask={onClickDeleteTask}
+        />
+      ));
+
+      expect(getByText('할 일이 없어요!')).toBeInTheDocument();
+    });
+  });
+
+  context('할 일이 있는 경우', () => {
+    const tasks = [
+      { id: 1, title: '새로운 할일 #1' },
+      { id: 2, title: '새로운 할일 #2' },
+      { id: 3, title: '새로운 할일 #3' },
+    ];
+
+    it('"To-do" 타이틀을 표시한다.', () => {
+      const { container } = render((
+        <Page
+          taskTitle=""
+          onChangeTitle={onChangeTitle}
+          onClickAddTask={onClickAddTask}
+          tasks={tasks}
+          onClickDeleteTask={onClickDeleteTask}
+        />
+      ));
+
+      expect(container).toHaveTextContent('To-do');
+    });
+
+    it('list를 표시한다.', () => {
+      const { getByRole } = render((
+        <Page
+          taskTitle=""
+          onChangeTitle={onChangeTitle}
+          onClickAddTask={onClickAddTask}
+          tasks={tasks}
+          onClickDeleteTask={onClickDeleteTask}
+        />
+      ));
+
+      expect(getByRole('list')).toBeInTheDocument();
+    });
   });
 });
