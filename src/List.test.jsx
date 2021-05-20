e8c560bc702926537c0510b71784c23317e76cf3
seong-dodo <dyrnxm52@hanmail.net>
2021-05-20 22:09:05 +0900

    피드백 반영 4
    - List 컴포넌트 테스트 피드백 반영 수정

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -2,25 +2,27 @@ import { render } from '@testing-library/react';
 
 import List from './List';
 
-test('할 일이 입력되면 할 일 목록을 볼 수 있다', () => {
-  const tasks = [{
-    id: 1,
-    title: '뭐라도 하기',
-  }];
+describe('List 컴포넌트 관련 테스트', () => {
+  it('할 일이 입력되면 할 일 목록을 볼 수 있다', () => {
+    const tasks = [{
+      id: 1,
+      title: '뭐라도 하기',
+    }];
 
-  const { container } = render((
-    <List tasks={tasks} />
-  ));
+    const { container } = render((
+      <List tasks={tasks} />
+    ));
 
-  expect(container).toHaveTextContent('뭐라도 하기');
-});
+    expect(container).toHaveTextContent('뭐라도 하기');
+  });
 
-test('할 일 목록이 없으면 할 일이 없어요', () => {
-  const tasks = [];
+  it('할 일 목록이 없으면 할 일이 없어요', () => {
+    const tasks = [];
 
-  const { container } = render((
-    <List tasks={tasks} />
-  ));
+    const { container } = render((
+      <List tasks={tasks} />
+    ));
 
-  expect(container).toHaveTextContent('할 일이 없어요!');
+    expect(container).toHaveTextContent('할 일이 없어요!');
+  });
 });
