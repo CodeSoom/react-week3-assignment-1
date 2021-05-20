3c9198f395493e0ddaa1af59b086c250a659de85
salangdung-i <dladmswl92@naver.com>
2021-05-20 13:56:06 +0900

    App.test.jsx 추가

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,5 +1,38 @@
+import { render, fireEvent } from '@testing-library/react';
+
 import App from './App';
 
 test('App', () => {
-    //TODO
+    const taskTitle = '테스트코드만들기';
+    const tasks =  {
+            id: 1,
+            title: '코드숨 공부하기',
+    };
+
+    const handleChangeTitle = jest.fn();
+    const handleClickAddTask = jest.fn();
+    const handleClickDeleteTask = jest.fn();
+
+    const { container, getByText } = render((
+        <App
+            taskTitle={taskTitle}
+            onChangeTitle={handleChangeTitle}
+            onClickAddTask={handleClickAddTask}
+            tasks={tasks}
+            onClickDeleteTask={handleClickDeleteTask}
+        />
+    ));
+    
+    expect(container).toHaveTextContent('할 일이 없어요!');
+    
+    expect(handleChangeTitle).not.toBeCalled();  
+    
+    expect(handleClickAddTask).not.toBeCalled();
+    fireEvent.click(getByText('추가'));
+
+    expect(handleClickDeleteTask).not.toBeCalled();
+    fireEvent.click(getByText('완료'));
+
+
+  
 });
