febd4ae1f11427868c9b5b10ba35402ba87b2749
hxunz <guswl2843@gmail.com>
2021-05-20 17:22:09 +0900

    Page 테스트 작성

--- src/Page.test.jsx
+++ src/Page.test.jsx
@@ -2,7 +2,22 @@ import React from 'react';
 
 import { render, fireEvent } from '@testing-library/react';
 
+import Input from './Input';
 import List from './List';
+
+test('addTodo', () => {
+  const handleClick = jest.fn();
+
+  const { container, getByText } = render((
+    <Input />
+  ));
+
+  expect(container).toHaveTextContent('추가');
+
+  expect(handleClick).not.toBeCalled();
+
+  fireEvent.click(getByText('추가'));
+});
 
 test('List', () => {
   const task = {
