1833cbf673ac60ce92c4fe8f1b123f1dd141bcbf
hanvit <hanvit7@gmail.com>
2021-05-20 23:25:16 +0900

    List 컴포넌트 테스트 리팩토링
    describe - context - it 구조로 변경

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -1,30 +1,35 @@
-import { fireEvent, render } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 import List from './List';
 
 describe('List', () => {
   const onClickDelete = jest.fn();
 
-  it('empty task', () => {
-    const { container } = render((
-      <List tasks={[]} onClickDelete={onClickDelete} />
-    ));
-    expect(container).toHaveTextContent('할 일이 없어요!');
+  context('할 일이 없는 경우', () => {
+    it('"할 일 없어요!"를 표시한다.', () => {
+      const { container } = render((
+        <List tasks={[]} onClickDelete={onClickDelete} />
+      ));
+
+      expect(container).toHaveTextContent('할 일이 없어요!');
+    });
   });
 
-  it('has tasks', () => {
-    const tasks = [
-      { id: 1, title: '새로운 할일 #1' },
-      { id: 2, title: '새로운 할일 #2' },
-      { id: 3, title: '새로운 할일 #3' },
-    ];
-    const {
-      container, getAllByRole, getAllByText,
-    } = render((
-      <List tasks={tasks} onClickDelete={onClickDelete} />
-    ));
-    expect(container).toHaveTextContent('완료');
-    expect(getAllByText(/새로운 할일 #\d+$/)).toHaveLength(tasks.length);
-    expect(getAllByRole('listitem')).toHaveLength(tasks.length);
+  context('할 일이 있는 경우', () => {
+    it('할 일의 title과 완료 버튼을 리스트로 보여준다.', () => {
+      const tasks = [
+        { id: 1, title: '새로운 할일 #1' },
+        { id: 2, title: '새로운 할일 #2' },
+        { id: 3, title: '새로운 할일 #3' },
+      ];
+
+      const { container, getAllByRole, getAllByText } = render((
+        <List tasks={tasks} onClickDelete={onClickDelete} />
+      ));
+
+      expect(container).toHaveTextContent('완료');
+      expect(getAllByText(/새로운 할일 #\d+$/)).toHaveLength(tasks.length);
+      expect(getAllByRole('listitem')).toHaveLength(tasks.length);
+    });
   });
 });
