37c74069cc30d08f061bb257e0311ce32b219364
seong-dodo <dyrnxm52@hanmail.net>
2021-05-20 16:01:50 +0900

    App컴포넌트 테스트 코드 만들기3(수정)

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -21,19 +21,36 @@ test('App컴포넌트 초기화 상태', () => {
 
 describe('input', () => {
   it('input에 할 일을 입력하면 입력값으로 바뀐다', () => {
+    // given : 이 테스트를 수행하기 위해 주어저야하는 상황 (이거일때)
     const { getByPlaceholderText } = render((
       <App />
     ));
-
     const input = getByPlaceholderText('할 일을 입력해 주세요');
-    expect(input).toHaveAttribute('value', '');
-
+    // when : 테스트를 수행 (이렇게 하면)
     fireEvent.change(input, {
       target: {
         value: '뭐라도 하기',
       },
     });
-
+    // then : 테스트를 수행했을때 예상하는 결과값 (이렇게 되어야한다)
     expect(input).toHaveAttribute('value', '뭐라도 하기');
+  });
+});
+
+describe('button', () => {
+  it('button을 클릭하면 순서를 가진 list가 생성된다', () => {
+    // given
+    const { getByText, getByPlaceholderText, container } = render(<App />);
+    const button = getByText('추가');
+    const input = getByPlaceholderText('할 일을 입력해 주세요');
+    fireEvent.change(input, {
+      target: {
+        value: '뭐라도 하기',
+      },
+    });
+    // when
+    fireEvent.click(button);
+    // then
+    expect(container).toHaveTextContent('뭐라도 하기');
   });
 });
