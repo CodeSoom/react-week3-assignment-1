63449cf4017c087d0a830f2bada3ccd0c700f135
seong-dodo <dyrnxm52@hanmail.net>
2021-05-20 22:13:33 +0900

    피드백 반영 5
    - Item 컴포넌트 테스트 피드백 반영 수정

--- src/Item.test.jsx
+++ src/Item.test.jsx
@@ -2,27 +2,29 @@ import { render, fireEvent } from '@testing-library/react';
 
 import Item from './Item';
 
-test('Item', () => {
-  const task = {
-    id: 1,
-    title: '뭐라도 하기',
-  };
+describe('Item 컴포넌트 관련 테스트', () => {
+  it('Item', () => {
+    const task = {
+      id: 1,
+      title: '뭐라도 하기',
+    };
 
-  const handleClick = jest.fn();
+    const handleClick = jest.fn();
 
-  const { container, getByText } = render((
-    <Item
-      task={task}
-      onClickDelete={handleClick}
-    />
-  ));
+    const { container, getByText } = render((
+      <Item
+        task={task}
+        onClickDelete={handleClick}
+      />
+    ));
 
-  expect(container).toHaveTextContent('뭐라도 하기');
-  expect(container).toHaveTextContent('완료');
+    expect(container).toHaveTextContent('뭐라도 하기');
+    expect(container).toHaveTextContent('완료');
 
-  expect(handleClick).not.toBeCalled();
+    expect(handleClick).not.toBeCalled();
 
-  fireEvent.click(getByText('완료'));
+    fireEvent.click(getByText('완료'));
 
-  expect(handleClick).toBeCalledWith(1);
+    expect(handleClick).toBeCalledWith(1);
+  });
 });
