566ac817f9dbac1c3fc8a23253d381bfabb2e1bf
seong-dodo <dyrnxm52@hanmail.net>
2021-05-20 22:04:28 +0900

    피드백 반영 3
    - Input 컴포넌트 테스트 피드백 반영 수정

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -2,43 +2,40 @@ import { render, fireEvent } from '@testing-library/react';
 
 import Input from './Input';
 
-test('할 일을 입력하면 입력값으로 변경되어야 한다', () => {
-  const value = '뭐라도 하기';
-  const handleChange = jest.fn();
-  const handleClick = jest.fn();
+describe('Input 컴포넌트 관련 테스트', () => {
+  it('할 일을 입력하면 입력값으로 변경되어야 한다', () => {
+    const value = '뭐라도 하기';
+    const handleChange = jest.fn();
+    const handleClick = jest.fn();
+    const { getByPlaceholderText } = render((
+      <Input
+        value={value}
+        onChange={handleChange}
+        onClick={handleClick}
+      />
+    ));
+    const input = getByPlaceholderText('할 일을 입력해 주세요');
 
-  const { getByPlaceholderText } = render((
-    <Input
-      value={value}
-      onChange={handleChange}
-      onClick={handleClick}
-    />
-  ));
+    expect(handleChange).not.toBeCalled();
+    fireEvent.change(input, {
+      target: {
+        value: '뭐라도 하기',
+      },
+    });
 
-  const input = getByPlaceholderText('할 일을 입력해 주세요');
-
-  expect(handleChange).not.toBeCalled();
-  fireEvent.change(input, {
-    target: {
-      value: '뭐라도 하기',
-    },
+    expect(input).toHaveAttribute('value', '뭐라도 하기');
   });
 
-  expect(input).toHaveAttribute('value', '뭐라도 하기');
-});
+  it('추가버튼을 클릭할 수 있다', () => {
+    const handleClick = jest.fn();
 
-test('추가버튼이 눌러진다', () => {
-  const handleClick = jest.fn();
+    const { container, getByText } = render((
+      <Input onClick={handleClick} />
+    ));
 
-  const { container, getByText } = render((
-    <Input
-      onClick={handleClick}
-    />
-  ));
+    expect(container).toHaveTextContent('추가');
 
-  expect(container).toHaveTextContent('추가');
-
-  expect(handleClick).not.toBeCalled();
-
-  fireEvent.click(getByText('추가'));
+    expect(handleClick).not.toBeCalled();
+    fireEvent.click(getByText('추가'));
+  });
 });
