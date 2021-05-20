65619ba85d1155b6ef94826d5c75210f0aa5dd09
seong-dodo <dyrnxm52@hanmail.net>
2021-05-20 22:20:21 +0900

    피드백반영 및 인수테스트 통과
    - App컴포넌트 테스드 코드 피드백 반영 수정
    - 인수 테스트 통과 & 테스트 커버리지 100% 달성
    - ESLint 통과

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,7 +2,7 @@ import { render, fireEvent } from '@testing-library/react';
 
 import App from './App';
 
-describe('App 화면', () => {
+describe('App 컴포넌트 관련 테스트', () => {
   it('App컴포넌트 초기화 상태모습 확인하기', () => {
     const { container, getByPlaceholderText } = render(<App />);
 
@@ -17,9 +17,7 @@ describe('App 화면', () => {
     expect(container.hasChildNodes('list')).toBe(true);
     expect(container).toHaveTextContent('할 일이 없어요!');
   });
-});
 
-describe('input', () => {
   it('input에 할 일을 입력하면 입력값으로 바뀐다', () => {
     const { getByPlaceholderText } = render(<App />);
     const input = getByPlaceholderText('할 일을 입력해 주세요');
@@ -32,9 +30,7 @@ describe('input', () => {
 
     expect(input).toHaveAttribute('value', '뭐라도 하기');
   });
-});
 
-describe('button', () => {
   it('button을 클릭하면 입력값이 추가된다', () => {
     const { getByText, getByPlaceholderText, container } = render(<App />);
     const button = getByText('추가');
@@ -49,9 +45,7 @@ describe('button', () => {
 
     expect(container).toHaveTextContent('뭐라도 하기');
   });
-});
 
-describe('완료버튼', () => {
   it('완료버튼을 누르면 할 일이 삭제되어야 한다', () => {
     const { getByText, getByPlaceholderText, container } = render(<App />);
     const button = getByText('추가');
