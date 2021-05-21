a79fe7d08dd25b9a7bd51929a60d2ecbcbd85507
hxunz <guswl2843@gmail.com>
2021-05-21 19:12:05 +0900

    describe 사용:  List 테스트 내에서 List 목록이 있을때와 없을때

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -4,42 +4,44 @@ import { render, fireEvent } from '@testing-library/react';
 
 import List from './List';
 
-test('List', () => {
-  const task = {
-    id: 1,
-    title: '뭐라도 하기',
-  };
+describe('List', () => {
+  test('exist List', () => {
+    const task = {
+      id: 1,
+      title: '뭐라도 하기',
+    };
 
-  const handleClick = jest.fn();
+    const handleClick = jest.fn();
 
-  const { container, getByText } = render((
-    <List
-      task={task}
-      onClickDelete={handleClick}
-    />
-  ));
+    const { container, getByText } = render((
+      <List
+        task={task}
+        onClickDelete={handleClick}
+      />
+    ));
 
-  expect(container).toHaveTextContent('뭐라도 하기');
-  expect(container).toHaveTextContent('완료');
+    expect(container).toHaveTextContent('뭐라도 하기');
+    expect(container).toHaveTextContent('완료');
 
-  expect(handleClick).not.toBeCalled();
+    expect(handleClick).not.toBeCalled();
 
-  fireEvent.click(getByText('완료'));
+    fireEvent.click(getByText('완료'));
 
-  expect(handleClick).toBeCalledWith(1);
-});
+    expect(handleClick).toBeCalledWith(1);
+  });
 
-test('emptyList', () => {
-  const task = '';
+  test('empty List', () => {
+    const task = '';
 
-  const handleClick = jest.fn();
+    const handleClick = jest.fn();
 
-  const { container } = render((
-    <List
-      task={task}
-      onClickDelete={handleClick}
-    />
-  ));
+    const { container } = render((
+      <List
+        task={task}
+        onClickDelete={handleClick}
+      />
+    ));
 
-  expect(container).toHaveTextContent('할 일이 없어요!');
+    expect(container).toHaveTextContent('할 일이 없어요!');
+  });
 });
