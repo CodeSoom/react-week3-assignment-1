8069232e3268c3be91f90c361f7c224cb6b239e3
박세준 <sjpark@selab.co.kr>
2021-05-21 09:18:41 +0900

    제일 쉬워보이는 List의 Test파일을 작성하였습니다. 이를 토대로 다른부분도 도전해보겠습니다.

---
+++ src/List.test.jsx
@@ -0,0 +1,49 @@
+import { render, fireEvent } from '@testing-library/react';
+
+import List from './List';
+
+test('List', () => {
+//리스트가 제대로 왔는지 확인?
+  const tasks = [
+    {id : 2,
+    title: '뭐라도 해보기.'}
+  ];
+
+  const handleClick = jest.fn();
+
+  const { container, getByText } = render((
+    <List
+      tasks={tasks}
+      onClickDelete={handleClick}
+    />
+  ));
+
+  expect(container).toHaveTextContent('뭐라도 해보기.완료');
+
+  expect(handleClick).not.toBeCalled();
+
+  fireEvent.click(getByText('완료'));
+
+  expect(handleClick).toBeCalledWith(2);
+
+});
+
+
+test('List', () => {
+    //리스트가 제대로 왔는지 확인?
+      let tasks = [
+      ];
+    
+      const handleClick = jest.fn();
+    
+      let { container, getByText } = render((
+        <List
+          tasks={tasks}
+          onClickDelete={handleClick}
+        />
+      ));
+    
+      expect(container).toHaveTextContent('할 일이 없어요!');
+    
+    });
+    
