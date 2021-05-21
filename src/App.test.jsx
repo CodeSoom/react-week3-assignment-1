a6f52656e7d1404f55998f02075fde57e6818396
seong-dodo <dyrnxm52@hanmail.net>
2021-05-21 15:45:01 +0900

    피드백 반영6
    - App컴포넌틑 테스트: queryByText, queryByPlaceholderText로 수정

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -4,17 +4,16 @@ import App from './App';
 
 describe('App 컴포넌트 관련 테스트', () => {
   it('App컴포넌트 초기화 상태모습 확인하기', () => {
-    const { container, getByPlaceholderText } = render(<App />);
+    const { container, queryByText, queryByPlaceholderText } = render(<App />);
+
+    const input = queryByPlaceholderText('할 일을 입력해 주세요');
+    const button = queryByText('추가');
+    const list = queryByText('할 일이 없어요!');
 
     expect(container).toHaveTextContent('To-do');
-    expect(container.hasChildNodes('input')).toBe(true);
-    expect(container.hasChildNodes('button')).toBe(true);
-    expect(container).toHaveTextContent('추가');
-
-    const input = getByPlaceholderText('할 일을 입력해 주세요');
     expect(input).toHaveAttribute('value', '');
-
-    expect(container.hasChildNodes('list')).toBe(true);
+    expect(button).not.toBeNull();
+    expect(list).not.toBeNull();
     expect(container).toHaveTextContent('할 일이 없어요!');
   });
 
@@ -35,6 +34,7 @@ describe('App 컴포넌트 관련 테스트', () => {
     const { getByText, getByPlaceholderText, container } = render(<App />);
     const button = getByText('추가');
     const input = getByPlaceholderText('할 일을 입력해 주세요');
+
     fireEvent.change(input, {
       target: {
         value: '뭐라도 하기',
@@ -50,11 +50,13 @@ describe('App 컴포넌트 관련 테스트', () => {
     const { getByText, getByPlaceholderText, container } = render(<App />);
     const button = getByText('추가');
     const input = getByPlaceholderText('할 일을 입력해 주세요');
+
     fireEvent.change(input, {
       target: {
         value: '뭐라도 하기',
       },
     });
+
     fireEvent.click(button);
 
     fireEvent.click(getByText('완료'));
