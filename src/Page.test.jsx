fe4cc01337124b3047a59da51cd93469cc064d5f
seong-dodo <dyrnxm52@hanmail.net>
2021-05-21 20:11:45 +0900

    피드백 반영 8
    - Page 컴포넌트 테스트 줄 수정
    - queryByPlaceholderText, queryByText로 수정

--- src/Page.test.jsx
+++ src/Page.test.jsx
@@ -6,23 +6,33 @@ describe('Page 컴포넌트 관련 테스트', () => {
   it('input, list 태그 존재 여부 확인하기', () => {
     const tasks = [];
 
-    const { container } = render((
+    const { container, queryByPlaceholderText, queryByText } = render((
       <Page
         tasks={tasks}
       />
     ));
 
-    expect(container.hasChildNodes('input')).toBe(true);
-    expect(container.hasChildNodes('list')).toBe(true);
+    const input = queryByPlaceholderText('할 일을 입력해 주세요');
+    const button = queryByText('추가');
+    const list = queryByText('할 일이 없어요!');
+
+    expect(container).toHaveTextContent('To-do');
+    expect(input).toHaveAttribute('value', '');
+    expect(button).not.toBeNull();
+    expect(list).not.toBeNull();
   });
 
   it('추가버튼 클릭하면 입력값이 보여진다', () => {
     const tasks = [{
       title: '뭐라도 하기',
     }];
+
     const { container, getByText } = render((
-      <Page tasks={tasks} />
+      <Page
+        tasks={tasks}
+      />
     ));
+
     const button = getByText('추가');
 
     fireEvent.click(button);
