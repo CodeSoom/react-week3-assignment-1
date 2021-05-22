f47bfa4c09addf523def96e3ec6121b0888ca1ff
최한울 <hanul.choi95@nts-corp.com>
2021-05-22 23:13:20 +0900

    App 컴포넌트 과제풀이 반영
    - App 컴포넌트는 화면을 보여주는 컴포넌트가 아니고 데이터를 다루는
    컴포넌트이기 때문에 초기 데이터값이 화면에 잘 렌더링되었는지 확인하는
    과정이 적합하다.

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,27 +1,20 @@
 /* global given */
 import React from 'react';
 
-import { render, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 import App from './App';
 
+// 화면을 보여주는 컴포넌트가 아닌 데이터를 다루는 컴포넌트이기 때문에
+// 초기 데이터값이 렌더링 된 모습을 테스트해보는 것이 좋음
+// 현재 tasks 초기 데이터값이 없음
 describe('App 컴포넌트', () => {
-  given('render', () => render(<App />));
+  it('renders', () => {
+    const { getByText } = render((
+      <App />
+    ));
 
-  it('할 일 추가하기', () => {
-    const { getByText, getByLabelText } = given.render;
-
-    fireEvent.change(getByLabelText('할 일'), { target: { value: '멋대로 살기' } });
-    fireEvent.click(getByText('추가'));
-    expect(getByText('멋대로 살기')).toHaveTextContent('멋대로 살기');
-  });
-
-  it('할 일 추가 후 완료하여 할 일 제거하기', () => {
-    const { getByText, getByLabelText } = given.render;
-
-    fireEvent.change(getByLabelText('할 일'), { target: { value: '멋대로 살기' } });
-    fireEvent.click(getByText('추가'));
-    fireEvent.click(getByText('완료'));
-    expect(getByText('할 일이 없어요!')).toHaveTextContent('할 일이 없어요!');
+    expect(getByText(/추가/)).not.toBeNull();
+    expect(getByText(/할 일이 없어요!/)).not.toBeNull();
   });
 });
