0b4d1688ee16eac420691c0b588038c966d47e97
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2021-05-22 12:36:14 +0900

    test: List 컴포넌트 구성요소 테스트 추가

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -1,9 +1,21 @@
 import { render } from '@testing-library/react';
 import List from './List';
 
+test('List 컴포넌트 구성요소는 ol, Item로 구성되어 있다.', () => {
+  const tasks = [{
+    id: 1,
+    title: '뭐라도 하기',
+  }];
+  const { container } = render(<List tasks={tasks} />);
+
+  expect(container).toContainHTML('ol');
+  expect(container).toContainHTML('li');
+
+  expect(container.firstChild).toMatchSnapshot();
+});
+
 test('목록에 할일이 없음을 알 수 있다.', () => {
   const tasks = [];
-
   const { container } = render(<List tasks={tasks} />);
 
   expect(container).toHaveTextContent('할 일이 없어요!');
