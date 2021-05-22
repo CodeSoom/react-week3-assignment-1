e5527d28ec1ed1b06937cad8e26c5227a28506d6
최한울 <hanul.choi95@nts-corp.com>
2021-05-22 15:03:35 +0900

    List 컴포넌트 context 추가
    - 할 일이 있음 <-> 할일이 없음과 같이 상호 반대되는 상황을 테스트 상에서
    보여줄 때, context를 이용한다.
    - context 테스트 설명은 with <-> without 또는 when log in <-> when log out 과 같은 이름으로
    작성하고 그 안에 it() 함수를 통해 표현한다.

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -17,32 +17,40 @@ describe('TodoList 컴포넌트', () => {
     )
   ));
 
-  it('할 일이 없을 때', () => {
-    given('tasks', () => []);
-    const { container } = given.render;
-    expect(container).toHaveTextContent('할 일이 없어요!');
+  context('with todoList', () => {
+    it('할 일 목록 노출', () => {
+      given('tasks', () => [
+        { id: 1, title: '멋대로 살기' },
+        { id: 2, title: '아무렇게나 살기' },
+      ]);
+      const { container } = given.render;
+      expect(container).toHaveTextContent('멋대로 살기완료');
+      expect(container).toHaveTextContent('아무렇게나 살기완료');
+    });
+
+    it('완료 버튼 누르기', () => {
+      given('tasks', () => [
+        { id: 1, title: '재미있게 살기' },
+      ]);
+      const handleClickDeleteTodo = jest.fn();
+      given('clickDelete', () => handleClickDeleteTodo);
+
+      const { getByText } = given.render;
+      expect(handleClickDeleteTodo).not.toBeCalled();
+      fireEvent.click(getByText('완료'));
+      expect(handleClickDeleteTodo).toBeCalled();
+    });
   });
 
-  it('할 일이 있을 때', () => {
-    given('tasks', () => [
-      { id: 1, title: '멋대로 살기' },
-      { id: 2, title: '아무렇게나 살기' },
-    ]);
-    const { container } = given.render;
-    expect(container).toHaveTextContent('멋대로 살기완료');
-    expect(container).toHaveTextContent('아무렇게나 살기완료');
+  context('without todoList', () => {
+    it('할 일이 없어요! 노출', () => {
+      given('tasks', () => []);
+      const { container } = given.render;
+      expect(container).toHaveTextContent('할 일이 없어요!');
+    });
   });
 
-  it('완료 버튼 누르기', () => {
-    given('tasks', () => [
-      { id: 1, title: '재미있게 살기' },
-    ]);
-    const handleClickDeleteTodo = jest.fn();
-    given('clickDelete', () => handleClickDeleteTodo);
 
-    const { getByText } = given.render;
-    expect(handleClickDeleteTodo).not.toBeCalled();
-    fireEvent.click(getByText('완료'));
-    expect(handleClickDeleteTodo).toBeCalled();
-  });
+
+
 });
