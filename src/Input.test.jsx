a7a3cda30136f3e26670d5c8c736a2561dc097bd
최한울 <hanul.choi95@nts-corp.com>
2021-05-22 23:58:48 +0900

    Iuput 컴포넌트 과제 풀이 적용
    - getByDisplayValue, getByPlaceholderText 쿼리문 활용
    - given2 라이브러리 제거

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -6,40 +6,53 @@ import { render, fireEvent } from '@testing-library/react';
 import Input from './Input';
 
 describe('Input 컴포넌트', () => {
-  given('render', () => (
-    render(
+  const handleClickAddTodo = jest.fn();
+  const handleChange = jest.fn();
+
+  function renderInputComponent() {
+    return render(
       (
-        <Input onClick={given.clickEvent} />
+        <Input
+          value="New Task"
+          onChange={handleChange}
+          onClick={handleClickAddTodo}
+        />
       ),
-    )
-  ));
+    );
+  }
 
   it('렌더링 페이지 텍스트 확인', () => {
-    const { container } = given.render;
-    expect(container).toHaveTextContent('할 일추가');
+    const { getByText } = renderInputComponent();
+
+    expect(getByText(/할 일/)).not.toBeNull();
+    expect(getByText(/추가/)).not.toBeNull();
   });
 
   it('placeholder 텍스트 확인', () => {
-    const { getByLabelText } = given.render;
-    const { placeholder } = getByLabelText('할 일');
-    expect(placeholder).toBe('할 일을 입력해 주세요');
+    const { getByPlaceholderText } = renderInputComponent();
+
+    expect(getByPlaceholderText(/할 일을 입력해 주세요/)).not.toBeNull();
   });
 
   it('input 태그 value 변경하기', () => {
-    const { getByLabelText } = given.render;
-    const inputElement = getByLabelText('할 일');
-    fireEvent.change(inputElement, { target: { value: '멋대로 살기' } });
-    expect(inputElement.value).toBe('멋대로 살기');
+    const { getByDisplayValue, getByLabelText } = renderInputComponent();
+
+    expect(getByDisplayValue('New Task')).not.toBeNull();
+
+    fireEvent.change(getByLabelText('할 일'), {
+      target: { value: '멋대로 살기' }
+    });
+
+    expect(handleChange).toBeCalled();
   });
 
   it('add Todo', () => {
-    const handleClickAddTodo = jest.fn();
-    given('clickEvent', () => handleClickAddTodo);
-
-    const { getByText } = given.render;
+    const { getByText } = renderInputComponent();
 
     expect(handleClickAddTodo).not.toBeCalled();
+
     fireEvent.click(getByText('추가'));
+
     expect(handleClickAddTodo).toBeCalled();
   });
 });
