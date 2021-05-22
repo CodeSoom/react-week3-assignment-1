57149259a9d8127b112280892f9e6b09b1e44564
salangdung-i <dladmswl92@naver.com>
2021-05-22 15:25:05 +0900

    App.test.jsx 수정

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,36 +1,20 @@
+import React from 'react';
+
 import { render, fireEvent } from '@testing-library/react';
 
 import App from './App';
 
 test('App', () => {
-    const taskTitle = '테스트코드만들기';
-    const tasks =  {
-            id: 1,
-            title: '코드숨 공부하기',
-    };
-
-    const handleChangeTitle = jest.fn();
-    const handleClickAddTask = jest.fn();
-    const handleClickDeleteTask = jest.fn();
-
+    
     const { container, getByText } = render((
         <App
-            taskTitle={taskTitle}
-            onChangeTitle={handleChangeTitle}
-            onClickAddTask={handleClickAddTask}
-            tasks={tasks}
-            onClickDeleteTask={handleClickDeleteTask}
         />
     ));
     
-    expect(container).toHaveTextContent('할 일이 없어요!');
+    expect(getByText(/추가/)).not.toBeNull();
+    expect(getByText(/아무 것도 하지 않기 #1/)).not.toBeNull();  
     
-    expect(handleChangeTitle).not.toBeCalled();  
-    
-    expect(handleClickAddTask).not.toBeCalled();
-    fireEvent.click(getByText('추가'));
-
-    expect(handleClickDeleteTask).not.toBeCalled();
-    fireEvent.click(getByText('완료'));
-
+    // TOOD: 통합 테스트 코드 작성
+    // CodeceptJS => 실제 브라우저에서 사용자 테스트 실행 가능.
 });
+
