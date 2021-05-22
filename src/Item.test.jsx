6545d1172b76915c18f938564662e64ffb71ea04
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2021-05-22 12:36:31 +0900

    test: Item 컴포넌트 구성요소 테스트 추가

--- src/Item.test.jsx
+++ src/Item.test.jsx
@@ -19,10 +19,9 @@ test('Item', () => {
 
   expect(container).toHaveTextContent('뭐라도 하기');
   expect(container).toHaveTextContent('완료');
-
   expect(handleClick).not.toBeCalled();
 
   fireEvent.click(getByText('완료'));
-
   expect(handleClick).toBeCalledWith(1);
+  expect(container).toMatchSnapshot();
 });
