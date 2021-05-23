2607fa4f82a0367bfeccdae06d5fc84c1541cd52
hxunz <guswl2843@gmail.com>
2021-05-23 19:09:55 +0900

    Input 테스트 수정

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -1,20 +1,32 @@
 import React from 'react';
 
-import { render, fireEvent } from '@testing-library/react';
+import {
+  render, fireEvent,
+} from '@testing-library/react';
 
 import Input from './Input';
 
-test('addTodo', () => {
-  const { handleClick, handleChange } = jest.fn();
+test('Input', () => {
+  const handleChange = jest.fn();
+  const handleClick = jest.fn();
 
-  const { container, getByText } = render((
-    <Input />
+  const { getByDisplayValue, getByLabelText, getByText } = render((
+    <Input
+      value="기존 할 일"
+      onChange={handleChange}
+      onClick={handleClick}
+    />
   ));
 
-  expect(container).toHaveTextContent('추가');
+  expect(getByDisplayValue('기존 할 일')).not.toBeNull();
 
-  expect(handleClick).not.toBeCalled();
+  fireEvent.change(getByLabelText('할 일'), {
+    target: { value: '무언가 하기' },
+  });
+
   expect(handleChange).toBeCalled();
 
   fireEvent.click(getByText('추가'));
+
+  expect(handleClick).toBeCalled();
 });
