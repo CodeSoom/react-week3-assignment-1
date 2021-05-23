28df59166e877440b072e959c3851abe1b7dd370
hanvit <hanvit7@gmail.com>
2021-05-23 14:11:35 +0900

    Page 컴포넌트 핸들러 테스트 추가
    추가 버튼 클릭 시 handleClickAddTask,
    완료 버튼 클릭 시handleClickDeleteTask 핸들러 호출 테스트를 추가함.

--- src/Page.test.jsx
+++ src/Page.test.jsx
@@ -1,35 +1,43 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 import Page from './Page';
 
 describe('Page', () => {
-  const onChangeTitle = jest.fn();
-  const onClickAddTask = jest.fn();
-  const onClickDeleteTask = jest.fn();
-  const renderPage = (tasks) => render((
+  const handleChangeTitle = jest.fn();
+  const handleClickAddTask = jest.fn();
+  const handleClickDeleteTask = jest.fn();
+  const renderPage = (taskTitle, tasks) => render((
     <Page
-      taskTitle=""
-      onChangeTitle={onChangeTitle}
-      onClickAddTask={onClickAddTask}
+      taskTitle={taskTitle}
+      onChangeTitle={handleChangeTitle}
+      onClickAddTask={handleClickAddTask}
       tasks={tasks}
-      onClickDeleteTask={onClickDeleteTask}
+      onClickDeleteTask={handleClickDeleteTask}
     />
   ));
 
   context('할 일이 없는 경우', () => {
+    const taskTitle = '새로운 할일';
     const tasks = [];
     it('"To-do" 타이틀을 표시한다.', () => {
-      const { container } = renderPage(tasks);
+      const { container } = renderPage(taskTitle, tasks);
       expect(container).toHaveTextContent('To-do');
     });
 
     it('"할 일 없어요!"를 표시한다.', () => {
-      const { getByText } = renderPage(tasks);
+      const { getByText } = renderPage(taskTitle, tasks);
       expect(getByText('할 일이 없어요!')).toBeInTheDocument();
+    });
+
+    it('추가 버튼 클릭 시 함수를 호출한다.', () => {
+      const { getByText } = renderPage(taskTitle, tasks);
+      fireEvent.click(getByText('추가'));
+      expect(handleClickAddTask).toBeCalled();
     });
   });
 
   context('할 일이 있는 경우', () => {
+    const taskTitle = '';
     const tasks = [
       { id: 1, title: '새로운 할일 #1' },
       { id: 2, title: '새로운 할일 #2' },
@@ -37,13 +45,26 @@ describe('Page', () => {
     ];
 
     it('"To-do" 타이틀을 표시한다.', () => {
-      const { container } = renderPage(tasks);
+      const { container } = renderPage(taskTitle, tasks);
       expect(container).toHaveTextContent('To-do');
     });
 
     it('list를 표시한다.', () => {
-      const { getByRole } = renderPage(tasks);
+      const { getByRole } = renderPage(taskTitle, tasks);
       expect(getByRole('list')).toBeInTheDocument();
+    });
+
+    it('추가 버튼 클릭 시 함수를 호출한다.', () => {
+      const { getByText } = renderPage(taskTitle, tasks);
+      fireEvent.click(getByText('추가'));
+      expect(handleClickAddTask).toBeCalled();
+    });
+
+    it('완료 버튼 클릭 시 함수를 호출한다.', () => {
+      const { getAllByText } = renderPage(taskTitle, tasks);
+      const buttons = getAllByText('완료');
+      fireEvent.click(buttons[0]);
+      expect(handleClickDeleteTask).toBeCalled();
     });
   });
 });
