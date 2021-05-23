d48ece0676e1e1e5f8dcac0ad157d5980fa91fb8
hxunz <guswl2843@gmail.com>
2021-05-23 18:12:42 +0900

    App test 수정

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
 
-import { render, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 import App from './App';
 
 test('App', () => {
-  const { container, getByText } = render((
+  const { getByText } = render((
     <App />
   ));
 
-  expect(container).toHaveTextContent('뭐라도 하기');
-  expect(container).toHaveTextContent('완료');
-
-  fireEvent.click(getByText('완료'));
+  expect(getByText('추가')).not.toBeNull();
 });
