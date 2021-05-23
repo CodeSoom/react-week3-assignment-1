5e776a78699e906a8f4e0706c8994f5ae25dfc9a
hanvit <hanvit7@gmail.com>
2021-05-23 14:48:17 +0900

    App 컴포넌트 테스트 추가
    '새로운 할 일 추가 후 완료하기' 테스트 추가

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,12 +1,28 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import App from './App';
 
-test('App', () => {
-  const { container, getByText } = render((
-    <App />
-  ));
+describe('App', () => {
+  const renderApp = () => render((<App />));
 
-  expect(container).toHaveTextContent('할 일이 없어요!');
-  expect(getByText(/추가/)).not.toBeNull();
+  it('initial', () => {
+    const { container, getByText } = renderApp();
+    expect(container).toHaveTextContent('할 일이 없어요!');
+    expect(getByText(/추가/)).not.toBeNull();
+  });
+
+  it('새로운 할 일 추가 후 완료하기.', () => {
+    const { getByLabelText, getAllByText, getByText } = renderApp();
+    fireEvent.change(getByLabelText('할 일'), {
+      target: {
+        value: '새로운 할일',
+      },
+    });
+    fireEvent.click(getByText('추가'));
+    expect(getByText(/새로운 할일/)).not.toBeNull();
+
+    const buttons = getAllByText('완료');
+    fireEvent.click(buttons[0]);
+    expect(getByText(/할 일이 없어요!/)).not.toBeNull();
+  });
 });
