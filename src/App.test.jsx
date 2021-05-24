19962704e1c38f61a838010dfa06568cf3489f64
최한울 <hanul.choi95@nts-corp.com>
2021-05-24 09:52:48 +0900

    App 컴포넌트 코드 커버리지 미달로 이전 코드 복원

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import App from './App';
 
@@ -8,12 +8,33 @@ import App from './App';
 // 초기 데이터값이 렌더링 된 모습을 테스트해보는 것이 좋음
 // 현재 tasks 초기 데이터값이 없음
 describe('App 컴포넌트', () => {
-  it('renders', () => {
-    const { getByText } = render((
+  function renderApp() {
+    return render((
       <App />
     ));
+  }
+
+  it('renders', () => {
+    const { getByText } = renderApp();
 
     expect(getByText(/추가/)).not.toBeNull();
+    expect(getByText(/할 일이 없어요!/)).not.toBeNull();
+  });
+
+  it('할 일 추가하기', () => {
+    const { getByText, getByLabelText } = renderApp();
+
+    fireEvent.change(getByLabelText('할 일'), { target: { value: '멋대로 살기' } });
+    fireEvent.click(getByText('추가'));
+    expect(getByText(/멋대로 살기/)).not.toBeNull();
+  });
+
+  it('할 일 추가 후 완료하여 할 일 제거하기', () => {
+    const { getByText, getByLabelText } = renderApp();
+
+    fireEvent.change(getByLabelText('할 일'), { target: { value: '멋대로 살기' } });
+    fireEvent.click(getByText('추가'));
+    fireEvent.click(getByText('완료'));
     expect(getByText(/할 일이 없어요!/)).not.toBeNull();
   });
 });
