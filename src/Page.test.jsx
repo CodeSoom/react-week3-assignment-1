8a31dde8732fea536ddc6a66227f6b66809cbe9c
Sehyun Chung <sehyun.chung@icloud.com>
2021-08-23 09:38:52 +0000

    feat: add tests for `Page`

---
+++ src/Page.test.jsx
@@ -0,0 +1,29 @@
+import { render } from '@testing-library/react';
+
+import Page from './Page';
+
+describe('Page', () => {
+  const tasks = [
+    { id: 100, title: 'test1' },
+    { id: 101, title: 'test2' },
+  ];
+  let container;
+
+  beforeEach(() => {
+    container = render(<Page tasks={tasks} />).container;
+  });
+
+  it('shows title "To-do"', () => {
+    expect(container).toHaveTextContent('To-do');
+  });
+
+  it('has Input component', () => {
+    expect(container).toHaveTextContent(/할 일/);
+    expect(container).toHaveTextContent(/추가/);
+  });
+
+  it('has List component', () => {
+    expect(container).toHaveTextContent('test1');
+    expect(container).toHaveTextContent('test2');
+  });
+});
