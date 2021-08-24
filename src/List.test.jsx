3a7c7e4f5f672ff26fe072ebf60b364a316f2927
Sehyun Chung <sehyun.chung@icloud.com>
2021-08-24 15:41:47 +0000

    fix: use `context` blocks for tests
    - for split context with/without tasks

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -3,23 +3,27 @@ import { render } from '@testing-library/react';
 import List from './List';
 
 describe('List', () => {
-  it('shows placeholder when it has no tasks', () => {
-    const tasks = [];
+  context('without tasks', () => {
+    it('shows placeholder text', () => {
+      const tasks = [];
 
-    const { container } = render(<List tasks={tasks} />);
+      const { container } = render(<List tasks={tasks} />);
 
-    expect(container).toHaveTextContent('할 일이 없어요!');
+      expect(container).toHaveTextContent('할 일이 없어요!');
+    });
   });
 
-  it('shows tasks', () => {
-    const tasks = [
-      { id: 100, title: 'test1' },
-      { id: 101, title: 'test2' },
-    ];
+  context('with tasks', () => {
+    it('shows task items', () => {
+      const tasks = [
+        { id: 100, title: 'test1' },
+        { id: 101, title: 'test2' },
+      ];
 
-    const { container } = render(<List tasks={tasks} />);
+      const { container } = render(<List tasks={tasks} />);
 
-    expect(container).toHaveTextContent('test1');
-    expect(container).toHaveTextContent('test2');
+      expect(container).toHaveTextContent('test1');
+      expect(container).toHaveTextContent('test2');
+    });
   });
 });
