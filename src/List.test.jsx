07e587efc945f63f1fc889d7525dc272004f459c
passwd10 <parkinseo94@naver.com>
2021-08-25 21:13:52 +0900

    refactor: List 테스트를 작성한다

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -3,33 +3,30 @@ import { render } from '@testing-library/react';
 import List from './List';
 
 describe('List', () => {
-  const renderList = ({ tasks }) => {
-    const handleClick = jest.fn();
-
-    return render((
-      <List
-        tasks={tasks}
-        onClickDelete={handleClick}
-      />
-    ));
-  };
+  const renderList = ({ tasks }) => render((
+    <List
+      tasks={tasks}
+      onClickDelete={jest.fn()}
+    />
+  ));
 
   context('without task', () => {
-    const tasks = [];
-    const { container } = renderList({ tasks });
-
     it('renders 할 일이 없어요!', () => {
+      const tasks = [];
+      const { container } = renderList({ tasks });
+
       expect(container).toHaveTextContent('할 일이 없어요!');
     });
   });
 
   context('with tasks', () => {
-    const tasks = [{ id: 0, title: '뭐라도 하기' }, { id: 1, title: '잠자기' }];
-    const { container } = renderList({ tasks });
-
     it('renders tasks', () => {
+      const tasks = [{ id: 0, title: '뭐라도 하기' }, { id: 1, title: '잠자기' }];
+      const { container } = renderList({ tasks });
+
       expect(container).toHaveTextContent('뭐라도 하기');
       expect(container).toHaveTextContent('잠자기');
+      expect(container).toHaveTextContent('완료');
     });
   });
 });
