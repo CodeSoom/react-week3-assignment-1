24176b50f8bf95940efb760e75b3b7c496f8fbce
hanabyhana <yaong1002@gmail.com>
2021-08-25 20:58:06 +0900

    Feat - click, chage 핸들러 test

--- src/input.test.jsx
+++ src/input.test.jsx
@@ -1,26 +1,37 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import Input from './Input';
 
-test('Input', () => {
-  const state = {
-    newId: 100,
-    taskTitle: '',
-    tasks: [],
+test('Input 추가 button 클릭', () => {
+  const task = {
+    newId: 1,
+    taskTitle: '할 일 1',
   };
 
-  const { taskTitle } = state;
+  const { taskTitle } = task;
 
+  const handleChange = jest.fn();
   const handleClick = jest.fn();
 
-  const { container } = render((
+  const { container, getByText, getByPlaceholderText } = render((
     <Input
       value={taskTitle}
-      onChange={handleClick}
+      onChange={handleChange}
       onClick={handleClick}
     />
   ));
 
   expect(container).toHaveTextContent('할 일');
   expect(container).toHaveTextContent('추가');
+
+  expect(handleClick).not.toBeCalled();
+  fireEvent.click(getByText('추가'));
+  expect(handleClick).toBeCalled();
+
+  const inputField = getByPlaceholderText('할 일을 입력해 주세요');
+  const letter = 'a';
+
+  expect(handleChange).not.toBeCalled();
+  fireEvent.change(inputField, { target: { value: letter } });
+  expect(handleChange).toBeCalledTimes(1);
 });
