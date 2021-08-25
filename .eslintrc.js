8b33a5a4993ac6bbee5226f2829b7d119d207cd7
seilylook <seilylook@naver.com>
2021-08-25 17:18:33 +0900

    refactor: page test code with describe && it

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,11 +2,9 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    jest: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
+  extends: ['plugin:react/recommended', 'airbnb'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -14,9 +12,45 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-  ],
+  plugins: ['react'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    actor: 'readonly',
+    Feature: 'readonly',
+    Scenario: 'readonly',
+    context: 'readonly',
+  },
   rules: {
+    indent: ['error', 2],
+    'no-trailing-spaces': 'error',
+    curly: 'error',
+    'brace-style': 'error',
+    'no-multi-spaces': 'error',
+    'space-infix-ops': 'error',
+    'space-unary-ops': 'error',
+    'no-whitespace-before-property': 'error',
+    'func-call-spacing': 'error',
+    'space-before-blocks': 'error',
+    'keyword-spacing': ['error', { before: true, after: true }],
+    'comma-spacing': ['error', { before: false, after: true }],
+    'comma-style': ['error', 'last'],
+    'comma-dangle': ['error', 'always-multiline'],
+    'space-in-parens': ['error', 'never'],
+    'block-spacing': 'error',
+    'array-bracket-spacing': ['error', 'never'],
+    'object-curly-spacing': ['error', 'always'],
+    'key-spacing': ['error', { mode: 'strict' }],
+    'arrow-spacing': ['error', { before: true, after: true }],
+    'jsx-a11y/label-has-associated-control': ['error', { assert: 'either' }],
+    'linebreak-style': 'off',
+
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [
+      2,
+      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+    ],
+    'jsx-quotes': [2, 'prefer-single'],
   },
 };
--- src/Page.test.js
+++ src/Page.test.js
@@ -1,24 +1,46 @@
-import React from "react";
+import React from 'react';
 
-import { render } from "@testing-library/react";
+import { render } from '@testing-library/react';
 
-import Page from "./Page";
+import Page from './Page';
 
-test("Page", () => {
-  const tasks = [];
-  const taskTitle = "";
+// 공식 문서 https://jestjs.io/docs/tutorial-react
 
-  const handleChangeTitle = jest.fn();
-  const handleClickAddTitle = jest.fn();
-  const handleClickDeleteTask = jest.fn();
+// 아래는 공식문서에서 연결된 사이트. good 여기 위주로 참고
+// https://testing-library.com/docs/react-testing-library/example-intro/
+// https://testing-library.com/docs/dom-testing-library/cheatsheet/
 
-  const { getByText } = render(
-    <Page
-      taskTitle={taskTitle}
-      tasks={tasks}
-      onChangeTitle={handleChangeTitle}
-      onClickAddTask={handleClickAddTitle}
-      onClickDeleteTask={handleClickDeleteTask}
-    />
-  );
+// jest-dom git
+// https://github.com/testing-library/jest-dom
+
+// context, describe , it 문법 사용하기
+// https://rinae.dev/posts/react-testing-tutorial-kr 참고.
+
+// render() = DOM에 컴포넌트 렌더링
+// fireEvent = 특정 이벤트 발생.
+
+describe('Page', () => {
+  it('Page', () => {
+    const tasks = [];
+    const taskTitle = '';
+
+    const handleChangeTitle = jest.fn();
+    const handleClickAddTask = jest.fn();
+    const handleClickDeleteTask = jest.fn();
+
+    // getByText 는 text가 있는지 검사
+    const { getByText } = render(
+      <Page
+        taskTitle={taskTitle}
+        tasks={tasks}
+        onChangeTitle={handleChangeTitle}
+        onClickAddTask={handleClickAddTask}
+        onClickDeleteTask={handleClickDeleteTask}
+      />,
+    );
+
+    // jest-dom 함수인 toBeIntheDocument
+    // = This allows you to assert whether an element is present in the document or not.
+    expect(getByText('To-do')).toBeInTheDocument();
+  });
 });
