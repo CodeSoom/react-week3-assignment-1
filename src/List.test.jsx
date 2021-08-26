d2fbb91fc5f974f6ec38f27d2713bd95aaceb031
passwd10 <parkinseo94@naver.com>
2021-08-26 21:29:17 +0900

    test: List 컴포넌트에 테스트를 추가한다

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -1,18 +1,18 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import List from './List';
 
 describe('List', () => {
-  const renderList = ({ tasks }) => render((
+  const renderList = ({ tasks, onClickDelete }) => render((
     <List
       tasks={tasks}
-      onClickDelete={jest.fn()}
+      onClickDelete={onClickDelete}
     />
   ));
 
   context('without task', () => {
+    const tasks = [];
     it('renders 할 일이 없어요!', () => {
-      const tasks = [];
       const { container } = renderList({ tasks });
 
       expect(container).toHaveTextContent('할 일이 없어요!');
@@ -22,11 +22,27 @@ describe('List', () => {
   context('with tasks', () => {
     it('renders tasks', () => {
       const tasks = [{ id: 0, title: '뭐라도 하기' }, { id: 1, title: '잠자기' }];
+
       const { container } = renderList({ tasks });
 
       expect(container).toHaveTextContent('뭐라도 하기');
       expect(container).toHaveTextContent('잠자기');
       expect(container).toHaveTextContent('완료');
+    });
+
+    it('listens button click event', () => {
+      const tasks = [{ id: 0, title: '뭐라도 하기' }, { id: 1, title: '잠자기' }];
+      const onClickDelete = jest.fn();
+
+      const { getAllByText } = renderList({ tasks, onClickDelete });
+
+      expect(onClickDelete).not.toBeCalled();
+
+      const completeButtons = getAllByText('완료');
+
+      fireEvent.click(completeButtons[0]);
+
+      expect(onClickDelete).toHaveBeenCalledTimes(1);
     });
   });
 });
