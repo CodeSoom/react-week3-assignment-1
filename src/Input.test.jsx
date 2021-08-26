dd5d6c4877e1b2297dcdbc2aff8bc1d262af6ba9
developerOlive <chloehiho@gmail.com>
2021-08-27 02:17:27 +0900

    [수정] Input 컴포넌트 | 중복되는 render 코드 메소드 처리, it 부분에 컴포넌트에 기대하는 반응 기재

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -1,44 +1,41 @@
 import React from 'react';
 
-import { render, fireEvent, getByPlaceholderText } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import Input from './Input';
 
-describe('Input 컴포넌트는', () => {
-  const onClick = jest.fn();
+describe('Input은', () => {
   const onChange = jest.fn();
+  const onClick = jest.fn();
   const value = '';
 
-    it('클릭 버튼 이벤트 함수를 호출한다.', () => {
-      const { container, getByText } = render((
-        <Input
-          id="input-task-title"
-          onClick={onClick}
-          onChange={onChange}
-          value={value}
-        />
-      ));
-      fireEvent.click(getByText('추가'));
+  function renderInput() {
+    return render((
+      <Input
+        value={value}
+        onChange={onChange}
+        onClick={onClick}
+      />
+    ));
+  }
 
-      expect(onClick).toBeCalledTimes(1);
+  it('클릭 버튼 이벤트 함수를 호출한다.', () => {
+    const { container, getByText } = renderInput();
+
+    expect(onClick).not.toBeCalled();
+
+    fireEvent.click(getByText('추가'));
+
+    expect(onClick).toBeCalledTimes(1);
+  });
+
+  it('change 이벤트 함수를 호출한다.', () => {
+    const { container, getByPlaceholderText } = renderInput();
+
+    fireEvent.change(getByPlaceholderText('할 일을 입력해 주세요'), { 
+      target: { value: '밥 먹기' } });
+
+    expect(onChange).toBeCalled();
   });
 });
 
-describe('Input 컴포넌트는', () => {
-  context('텍스트 필드에 값을 입력하면', () => {
-    it('onChange 메소드를 실행한다.', () => {
-      const onChange = jest.fn();
-
-      const { container, getByPlaceholderText } = render((
-        <Input
-          id="input-task-title"
-          onChange={onChange}
-        />
-      ));
-
-      fireEvent.change(getByPlaceholderText('할 일을 입력해 주세요'), { target: { value: '밥 먹기' } });
-
-      expect(onChange).toBeCalled();
-    });
-  });
-});
