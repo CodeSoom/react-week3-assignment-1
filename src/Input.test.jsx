a223a050c1a00158a6bdc56f2cac9131709c4081
developerOlive <chloehiho@gmail.com>
2021-08-26 20:22:39 +0900

    [수정] Input 컴포넌트 테스트 시, 버튼 클릭에 대한 테스트 진행으로 변경

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -5,22 +5,18 @@ import { render, fireEvent, getByPlaceholderText } from '@testing-library/react'
 import Input from './Input';
 
 describe("'Input 컴포넌트는", () => {
-  
-  context("'추가' 버튼을 누르면", () => {
-    const onClick = jest.fn();
+  const onClick = jest.fn();
 
-    it("텍스트 필드에 입력한 글자가 사라진다.", () => {
+    it("클릭 버튼 이벤트 함수를 호출한다.", () => {
       const { container, getByText } = render((
         <Input
           id="input-task-title"
           onClick={onClick}
         />
       ));
-
       fireEvent.click(getByText('추가'));
 
-      expect(container).toBeNull;
-    });
+      expect(onClick).toBeCalledTimes(1);
   });
 });
 
