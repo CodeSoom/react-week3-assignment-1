8aefa90e72ed5c86119d49e65bd59905bd7f5368
seilylook <seilylook@naver.com>
2021-08-27 12:29:27 +0900

    refactor: app add button delete button

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,31 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 import App from './App';
 
 describe('App', () => {
-  it('it shows start page', () => {
+  context('tasks are empty', () => {
+    it('it shows empty page', () => {
+      const { getByText } = render(<App />);
+
+      expect(getByText('할 일이 없어요!')).toBeInTheDocument();
+    });
+  });
+
+  context('tasks are not empty', () => {
+    it('it shows click add', () => {
+      const { getByText } = render(<App />);
+
+      fireEvent.click(getByText('추가'));
+    });
+  });
+
+  it('it deletes tasks', () => {
     const { getByText } = render(<App />);
 
-    expect(getByText('할 일이 없어요!')).toBeInTheDocument();
+    fireEvent.click(getByText('추가'));
+
+    fireEvent.click(getByText('완료'));
   });
 });
