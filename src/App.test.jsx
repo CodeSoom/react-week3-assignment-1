8c3a385eb989ef948fea4f6c92240f79f454fa3b
psy082 <zhappy114@gmail.com>
2021-08-28 01:07:19 +0900

    test: App component test 추가
    - input 입력시 value값에 반영되는 지 test
    - input 입력 후 추가 버튼 클릭 시 list item이 추가되는 지 test
    - 추가한 item의 완료 버튼 클릭 시 해당 item이 삭제되는 지 test

---
+++ src/App.test.jsx
@@ -0,0 +1,45 @@
+import { render, fireEvent } from '@testing-library/react';
+import { not } from 'expect';
+
+import App from './App';
+
+test('App', () => {
+  const { container, getByLabelText, getByText } = render((
+    <App />
+  ));
+  // 첫 화면, 빈 리스트 문구
+  expect(container).toHaveTextContent('할 일이 없어요!');
+
+  // task 입력
+  const input = getByLabelText('할 일');
+  expect(input).toHaveValue('');
+  fireEvent.change(input, { target: { value: '이것' } });
+  expect(input).toHaveValue('이것');
+
+  // 입력한 task 추가
+  fireEvent.click(getByText('추가'));
+  expect(input).toHaveValue('');
+
+  // 추가한 task가 리스트에 존재
+  expect(container).not.toHaveTextContent('할 일이 없어요!');
+  expect(container.querySelector('ol')).toBeInTheDocument();
+  expect(container).toHaveTextContent('이것');
+
+  // 새로운 task 입력 및 추가
+  fireEvent.change(input, { target: { value: '저것' } });
+  fireEvent.click(getByText('추가'));
+
+  // 추가한 task가 리스트에 존재
+  expect(container).toHaveTextContent('저것');
+
+  // task 삭제
+  fireEvent.click(getByText('이것').parentNode.querySelector('button'));
+  expect(container).not.toHaveTextContent('이것');
+  expect(container).toHaveTextContent('저것');
+  expect(container).not.toHaveTextContent('할 일이 없어요!');
+
+  // task 삭제
+  fireEvent.click(getByText('저것').parentNode.querySelector('button'));
+  expect(container).not.toHaveTextContent('저것');
+  expect(container).toHaveTextContent('할 일이 없어요!');
+});
