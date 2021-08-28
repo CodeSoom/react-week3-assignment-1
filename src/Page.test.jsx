0c13cd5a442e2b96f21ad541ff2bce180ed3101b
passwd10 <parkinseo94@naver.com>
2021-08-28 23:29:45 +0900

    test: Page 테스트를 수정한다
    - given을 사용하여 parameter를 lazy하게 받는다

--- src/Page.test.jsx
+++ src/Page.test.jsx
@@ -1,40 +1,45 @@
 import { render } from '@testing-library/react';
 
+import given from 'given2';
+
 import Page from './Page';
 
 describe('Page', () => {
-  const renderPage = ({ tasks }) => render((
+  given('tasks', () => []);
+
+  const renderPage = () => render((
     <Page
       taskTitle=""
       onChangeTitle={jest.fn()}
       onClickAddTask={jest.fn()}
-      tasks={tasks}
+      tasks={given.tasks}
       onClickDeleteTask={jest.fn()}
     />
   ));
+
   it('renders title', () => {
-    const { container } = renderPage({ tasks: [] });
+    const { container } = renderPage();
 
     expect(container).toHaveTextContent('To-do');
   });
 
   it('renders input label', () => {
-    const { container } = renderPage({ tasks: [] });
+    const { container } = renderPage();
 
     expect(container).toHaveTextContent('할 일');
   });
 
   it('renders add button', () => {
-    const { container } = renderPage({ tasks: [] });
+    const { container } = renderPage();
 
     expect(container).toHaveTextContent('추가');
   });
 
   context('with tasks', () => {
     it('renders tasks', () => {
-      const { container } = renderPage({
-        tasks: [{ id: 0, title: '아무것도 안하기' }],
-      });
+      given('tasks', () => [{ id: 0, title: '아무것도 안하기' }]);
+
+      const { container } = renderPage();
 
       expect(container).toHaveTextContent('아무것도 안하기');
     });
@@ -42,7 +47,7 @@ describe('Page', () => {
 
   context('without task', () => {
     it('renders 할 일이 없어요!', () => {
-      const { container } = renderPage({ tasks: [] });
+      const { container } = renderPage();
 
       expect(container).toHaveTextContent('할 일이 없어요!');
     });
