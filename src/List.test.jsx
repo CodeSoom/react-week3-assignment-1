859f592adfecebc17a44ac390c7e7948ffd798b4
hanabyhana <yaong1002@gmail.com>
2021-08-29 17:42:38 +0900

    Refactor - describe context it 사용하기

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -1,25 +1,53 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import List from './List';
 
 describe('List', () => {
-  const tasks = [
-    { id: 1, title: '과제1' },
-    { id: 2, title: '과제2' },
-    { id: 3, title: '과제3' },
-  ];
+  const handleClickDeleteTask = jest.fn();
 
-  const handleClick = jest.fn();
-
-  it('renders items', () => {
-    const { getAllByText } = render((
+  function renderList(tasks) {
+    return render((
       <List
         tasks={tasks}
-        onClickDelete={handleClick}
+        onClickDelete={handleClickDeleteTask}
       />
     ));
+  }
 
-    const renderedItems = getAllByText('완료');
-    expect(renderedItems.length).toBe(tasks.length);
+  context('without tasks', () => {
+    it('renders no task message', () => {
+      const tasks = [];
+
+      const { getByText } = renderList(tasks);
+
+      expect(getByText(/할 일이 없어요!/)).not.toBeNull();
+    });
+  });
+
+  context('with tasks', () => {
+    const tasks = [
+      { id: 1, title: '과제1' },
+      { id: 2, title: '과제2' },
+      { id: 3, title: '과제3' },
+    ];
+
+    it('renders tasks', () => {
+      const { getByText } = renderList(tasks);
+
+      expect(getByText(/과제1/)).not.toBeNull();
+      expect(getByText(/과제2/)).not.toBeNull();
+      expect(getByText(/과제3/)).not.toBeNull();
+    });
+
+    it('renders "완료" button to delete a task', () => {
+      const { getAllByText } = renderList(tasks);
+
+      const renderedItems = getAllByText('완료');
+
+      expect(renderedItems.length).toBe(tasks.length);
+
+      fireEvent.click(renderedItems[0]);
+      expect(handleClickDeleteTask).toBeCalledWith(1);
+    });
   });
 });
