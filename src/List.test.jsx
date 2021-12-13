073687a4ba1eeb09a43f9f0b707827995eb1781e
JangWoohyun <wkddngus5@naver.com>
2021-12-13 23:42:04 +0900

    List Component view, onClick

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -1,7 +1,21 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import List from './List';
 
 test('List empty', () => {
   const { container } = render(<List tasks={[]} />);
   expect(container).toHaveTextContent('할 일이 없어요!');
+});
+
+test('List', () => {
+  const { container } = render(<List tasks={[{ id: 1, title: '일어나기' }]} />);
+  expect(container).toHaveTextContent('일어나기');
+});
+
+const handleClickDelete = jest.fn();
+
+test('List click delete', () => {
+  const { getByText } = render(<List tasks={[{ id: 1, title: '일어나기' }]} onClickDelete={handleClickDelete} />);
+  expect(handleClickDelete).not.toBeCalled();
+  fireEvent.click(getByText('완료'));
+  expect(handleClickDelete).toBeCalledWith(1);
 });
