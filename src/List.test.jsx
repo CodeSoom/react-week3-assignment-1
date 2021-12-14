2826f22cc815854b2bf92cf27d4bbb5815b44cb0
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2021-12-14 21:45:02 +0900

    [test] list test 작성

---
+++ src/List.test.jsx
@@ -0,0 +1,11 @@
+describe('List Component', () => {
+  context('task 가 없을 경우', () => {
+    it("task가 없을 경우, '할 일이 없어요!' 출력", () => {});
+  });
+
+  context('task가 있는 경우', () => {
+    it('task title 과 완료 버튼 렌더링', () => {});
+    it('여러개 있는 경우, 여러 task title 과 완료 버튼 렌더링', () => {});
+    it('완료 버튼을 클릭하면 해당 task 사라짐.', () => {});
+  });
+});
