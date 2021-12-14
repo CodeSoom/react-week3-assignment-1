34896722d70c53c46e5c23a1bfeff85eb337c287
padawanR0k <padawanR0k@gmail.com>
2021-12-14 23:10:22 +0900

    up: add App component test code

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -3,4 +3,23 @@ import { render, fireEvent } from '@testing-library/react';
 import App from './App';
 
 test('App', () => {
+  const { container, getByPlaceholderText, getByText } = render(<App />);
+
+  fireEvent.change(getByPlaceholderText('할 일을 입력해 주세요'), {
+    target: { value: '일기쓰기' },
+  });
+
+  expect(getByPlaceholderText('할 일을 입력해 주세요').value).toBe(
+    '일기쓰기',
+  );
+
+  fireEvent.click(getByText('추가'));
+
+  expect(container).toHaveTextContent('일기쓰기');
+
+  fireEvent.click(getByText('완료'));
+
+  expect(container).not.toHaveTextContent('일기쓰기');
+
+  expect(container).toHaveTextContent('할 일이 없어요!');
 });
