afdbda73f53679c9f6ee73b1a5f4f6959905dea0
padawanR0k <padawanR0k@gmail.com>
2021-12-14 13:03:23 +0900

    up: add Input component event handler test code

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -2,5 +2,23 @@ import { render, fireEvent } from '@testing-library/react';
 
 import Input from './Input';
 
-test('Input', () => {
+test('Input 이벤트 리스너 테스트', () => {
+  const handleClick = jest.fn();
+  const handleChange = jest.fn();
+
+  const { getByText, getByPlaceholderText } = render(
+    <Input onChange={handleChange} onClick={handleClick} value="" />,
+  );
+
+  expect(handleClick).not.toBeCalled();
+
+  fireEvent.click(getByText('추가'));
+
+  expect(handleClick).toBeCalled();
+
+  fireEvent.change(getByPlaceholderText('할 일을 입력해 주세요'), {
+    target: { value: '일기쓰기' },
+  });
+
+  expect(handleChange).toBeCalled();
 });
