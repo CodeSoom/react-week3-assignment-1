9813c2276a2af22d79d81b0a72ee356cefa15068
padawanR0k <padawanR0k@gmail.com>
2021-12-14 22:38:04 +0900

    up: add List component test code

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -1,5 +1,25 @@
-import { render, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 import List from './List';
 
-test("List", () => {});
+test('tasks가 0개일 때, 문구 확인', () => {
+  const tasks = [];
+  const { container } = render(
+    <List tasks={tasks} onClickDelete={jest.fn()} />,
+  );
+
+  expect(container).toHaveTextContent('할 일이 없어요!');
+});
+
+test('tasks가 1개이상일 떄, 렌더링 확인', () => {
+  const tasks = [
+    { id: 1, title: '기상' },
+    { id: 2, title: '출근' },
+  ];
+  const { container } = render(
+    <List tasks={tasks} onClickDelete={jest.fn()} />,
+  );
+
+  expect(container).toHaveTextContent('기상');
+  expect(container).toHaveTextContent('출근');
+});
