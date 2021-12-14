ecb8b83a10666252ae80668be75593d3573b375e
jeongseop.lee <sjl0614@naver.com>
2021-12-14 19:33:57 +0900

    :white_check_mark: <List> task 테스트

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -3,9 +3,16 @@ import { render } from '@testing-library/react';
 import List from './List';
 
 test('List', () => {
+  const tasks = [
+    {
+      title: '아무것도 안하기',
+      id: 100,
+    },
+  ];
   const { container } = render((
-    <List tasks={[]} />
+    <List tasks={tasks} />
   ));
 
-  expect(container).toHaveTextContent('할 일이 없어요!');
+  expect(container).toHaveTextContent('아무것도 안하기');
+  expect(container).toHaveTextContent('완료');
 });
