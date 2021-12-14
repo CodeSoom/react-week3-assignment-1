eb6f0cb200fcd561591265fd9f09cf040f8fe064
jeongseop.lee <sjl0614@naver.com>
2021-12-14 20:40:57 +0900

    :white_check_mark: Input handleClick 테스트

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -1,12 +1,26 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import Input from './Input';
 
 test('Input', () => {
-  const { container } = render((
-    <Input />
+  const handleClick = jest.fn();
+
+  const handleChange = jest.fn();
+
+  const { container, getByText } = render((
+    <Input
+      value="아무거나 하기"
+      onClick={handleClick}
+      onChange={handleChange}
+    />
   ));
 
   expect(container).toHaveTextContent('할 일');
   expect(container).toHaveTextContent('추가');
+
+  expect(handleClick).not.toBeCalled();
+
+  fireEvent.click(getByText('추가'));
+
+  expect(handleClick).toBeCalled();
 });
