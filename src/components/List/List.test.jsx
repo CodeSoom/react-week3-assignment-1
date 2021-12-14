9ce1cc2989cc89320d80b26fe100ba11d42776b4
wallababa <ansrlansrl@naver.com>
2021-12-15 00:01:49 +0900

    List 컴포넌트 테스트 추가

---
+++ src/components/List/List.test.jsx
@@ -0,0 +1,30 @@
+import { render } from '@testing-library/react';
+import List, { EMPTY_MESSAGE } from './index';
+
+describe('List', () => {
+  it(`tasks의 length가 0이면 "${EMPTY_MESSAGE}"를 보여줍니다.`, () => {
+    const sut = render(<List tasks={[]} />);
+    const $emptyElement = sut.queryByText(EMPTY_MESSAGE);
+
+    expect($emptyElement).not.toBeNull();
+  });
+
+  it(`tasks의 length가 0이 아니면 "${EMPTY_MESSAGE}"를 보여주지 않습니다.`, () => {
+    const sut = render(<List tasks={[{ id: 1, title: 'foo' }]} />);
+    const $emptyElement = sut.queryByText(EMPTY_MESSAGE);
+
+    expect($emptyElement).toBeNull();
+  });
+
+  it('비어있지 않은 tasks를 전달하면 tasks를 리스트로 보여줍니다.', () => {
+    const tasks = [
+      { id: 1, title: 'foo' },
+      { id: 2, title: 'bar' },
+    ];
+    const sut = render(<List tasks={tasks} />);
+    const $list = sut.queryByRole('list');
+
+    expect($list).not.toBeNull();
+    expect($list.children).toHaveLength(tasks.length);
+  });
+});
--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,9 +1,10 @@
 import Item from '../Item';
 
+export const EMPTY_MESSAGE = '할 일이 없어요!';
 export default function List({ tasks, onClickDelete }) {
   if (tasks.length === 0) {
     return (
-      <p>할 일이 없어요!</p>
+      <p>{EMPTY_MESSAGE}</p>
     );
   }
 
