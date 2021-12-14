bb45c91ad42ed8faaec95cf9c6f2384aaccd9ea0
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2021-12-14 21:35:50 +0900

    [test] input 케이스 테스트 코드 작성

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -1,23 +1,55 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 import Input from './Input';
 
 describe('Input Component', () => {
-  const { container } = render((
-    <Input />
-  ));
+  const value = '아무것도 하지 않기';
+  const onChange = jest.fn();
+  const onClick = jest.fn();
 
   it('render', () => {
-    screen.getByPlaceholderText('할 일을 입력해 주세요');
+    const { container, getByPlaceholderText } = render((
+      <Input />
+    ));
+
     expect(container).toHaveTextContent('할 일');
+    getByPlaceholderText('할 일을 입력해 주세요');
     expect(container).toHaveTextContent('추가');
   });
 
   it('when typing, input changed value', () => {
+    const { getByPlaceholderText } = render((
+      <Input value={value} onChange={onChange} onClick={onClick} />
+    ));
 
+    const input = getByPlaceholderText('할 일을 입력해 주세요');
+    expect(onChange).not.toBeCalled();
+    fireEvent.change(input, {
+      target: {
+        value: '코드숨 과제하기',
+      },
+    });
+    expect(onChange).toHaveBeenCalled();
   });
 
   it('after typing, click button to init value', () => {
+    const { getByPlaceholderText, getByRole } = render((
+      <Input
+        value={value}
+        onChange={onChange}
+        onClick={onClick}
+      />
+    ));
 
+    const input = getByPlaceholderText('할 일을 입력해 주세요');
+    fireEvent.change(input, {
+      target: {
+        value: '코드숨 과제하기',
+      },
+    });
+
+    expect(onClick).not.toHaveBeenCalled();
+    fireEvent.click(getByRole('button'));
+    expect(onClick).toHaveBeenCalled();
   });
 });
