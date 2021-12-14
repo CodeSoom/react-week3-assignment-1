14ae4c4c8afc7fc5e467b21a17b2eca41559f1f4
padawanR0k <padawanR0k@gmail.com>
2021-12-14 22:51:01 +0900

    up: add Page component test code

--- src/Page.test.jsx
+++ src/Page.test.jsx
@@ -2,6 +2,32 @@ import { render, fireEvent } from '@testing-library/react';
 
 import Page from './Page';
 
-test('Page', () => {
+test('Page 테스트', () => {
+  const taskTitle = '';
+  const handleChange = jest.fn();
+  const handleClickAddTask = jest.fn();
+  const tasks = [{ id: 1, title: '기상' }];
+  const handleClickDeleteTask = jest.fn();
+  const { container, getByText, getByPlaceholderText } = render(
+    <Page
+      taskTitle={taskTitle}
+      onChangeTitle={handleChange}
+      onClickAddTask={handleClickAddTask}
+      tasks={tasks}
+      onClickDeleteTask={handleClickDeleteTask}
+    />,
+  );
 
+  expect(container).toHaveTextContent('기상');
+
+  fireEvent.click(getByText('추가'));
+  expect(handleClickAddTask).toBeCalled();
+
+  fireEvent.change(getByPlaceholderText('할 일을 입력해 주세요'), {
+    target: { value: '일기쓰기' },
+  });
+  expect(handleChange).toBeCalled();
+  fireEvent.click(getByText('완료'));
+
+  expect(handleClickDeleteTask).toBeCalledWith(1);
 });
--- src/index.test.jsx
+++
@@ -1,6 +0,0 @@
-import { render, fireEvent } from '@testing-library/react';
-
-import Index from './Index';
-
-test('Index', () => {
-});
