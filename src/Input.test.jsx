3e38c60336dee28cf1e11c63c6d485b9e6ca180d
Superduper-India <sunyoung1542@gmail.com>
2021-12-15 13:28:58 +0900

    Update: Input, List 테스트 작성

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -3,17 +3,43 @@ import { fireEvent, render } from '@testing-library/react';
 import Input from './Input';
 
 test('Input', () => {
-  const handleClick = jest.fn();
+  // 중복
+  const value = '할 일이 없어용';
   const handleChange = jest.fn();
+  const handleClick = jest.fn();
 
-  const { getByText } = render((
+  // Given
+  const { container, getByLabelText, getByText } = render(
     <Input
-      onClick={handleClick}
+      value={value}
       onChange={handleChange}
-    />
-  ));
+      onClick={handleClick}
+    />,
+  );
 
-  const button = getByText('추가');
+  // Then1
+  expect(container).toHaveTextContent('할 일');
+  expect(container).toHaveTextContent('추가');
 
-  fireEvent.click(button);
+  // When2 -> 인풋의 값이 그대로일때
+  fireEvent.change(getByLabelText('할 일'), {
+    target: { value },
+  });
+
+  // Then2 -> onChange handler는 호출되지 않는다.
+  expect(handleChange).not.toBeCalled();
+
+  // When3 -> 인풋의 값이 바뀔때
+  fireEvent.change(getByLabelText('할 일'), {
+    target: { value: '운동하기' },
+  });
+
+  // Then3 -> onChange handler는 호출된다.
+  expect(handleChange).toBeCalled();
+
+  // When4 -> 버튼 클릭할때
+  fireEvent.click(getByText('추가'));
+
+  // Then4 -> onClick handler는 호출된다.
+  expect(handleClick).toBeCalled();
 });
---
+++ src/List.test.jsx
@@ -0,0 +1,44 @@
+import { fireEvent, render } from '@testing-library/react';
+
+import List from './List';
+
+test('List', () => {
+  const tasks = [
+    { id: 100, title: '운동가기' },
+  ];
+
+  const { container } = render(<List
+    tasks={tasks}
+  />);
+
+  expect(tasks).toHaveLength(1);
+  expect(container).toHaveTextContent('운동가기');
+});
+
+test('List2', () => {
+  const tasks = [];
+
+  const { container } = render(<List
+    tasks={tasks}
+  />);
+
+  expect(tasks).toHaveLength(0);
+  expect(container).toHaveTextContent('할 일이 없어요!');
+});
+
+test('List3', () => {
+  const tasks = [
+    { id: 100, title: '운동가기' },
+  ];
+
+  const handleClick = jest.fn();
+
+  const { container } = render(<List
+    tasks={tasks}
+    onClickDelete={handleClick}
+  />);
+
+  fireEvent.click(container);
+
+  expect(container).toHaveTextContent('완료');
+});
