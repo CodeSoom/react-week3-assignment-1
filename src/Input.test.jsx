554fa1ebb0f0f5385b480bdbc2f22209581f4ff3
emewjin <emewjin@gmail.com>
2021-12-15 23:08:16 +0900

    ref: Page 컴포넌트 테스트에서 Input 컴포넌트 기능 테스트 분리

---
+++ src/Input.test.jsx
@@ -0,0 +1,31 @@
+import { render, fireEvent } from '@testing-library/react';
+
+import Input from './Input';
+
+import { TASK_TITLE } from './stubs';
+
+describe('Input', () => {
+  const onChangeTitle = jest.fn();
+  const onClickAddTask = jest.fn();
+
+  const renderInputComponent = () => render(
+    <Input value={TASK_TITLE} onChange={onChangeTitle} onClick={onClickAddTask} />,
+  );
+  it('투두 인풋의 버튼을 클릭하면 onClickAddTask 이벤트가 호출된다', () => {
+    const { getByText } = renderInputComponent();
+
+    const addTodoButton = getByText('추가');
+    addTodoButton.click();
+
+    expect(onClickAddTask).toBeCalled();
+  });
+
+  it('투두 인풋에 타이핑을 하면 onChangeTitle 이벤트가 호출된다', () => {
+    const { getByPlaceholderText } = renderInputComponent();
+
+    const toDoInput = getByPlaceholderText('할 일을 입력해 주세요');
+    fireEvent.change(toDoInput, { target: { value: '타이핑을 해주세요' } });
+
+    expect(onChangeTitle).toBeCalled();
+  });
+});
--- src/Page.test.jsx
+++ src/Page.test.jsx
@@ -1,4 +1,4 @@
-import { render, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 import Page from './Page';
 
@@ -31,39 +31,11 @@ describe('Page', () => {
     expect(container).toHaveTextContent(TASKS[1].title);
   });
 
-  it('투두 리스트의 완료 버튼을 누르면 onClickDelete 함수가 호출된다', () => {
-    const { getAllByText } = renderPageComponent();
-
-    fireEvent.click(getAllByText('완료')[0]);
-    expect(onClickDeleteTask).toHaveBeenCalledTimes(1);
-
-    fireEvent.click(getAllByText('완료')[1]);
-    expect(onClickDeleteTask).toHaveBeenCalledTimes(2);
-  });
-
   it('투두 인풋을 그려낸다', () => {
     const { getByPlaceholderText } = renderPageComponent();
 
     const toDoInput = getByPlaceholderText('할 일을 입력해 주세요');
     expect(toDoInput).toBeInTheDocument();
     expect(toDoInput).toHaveValue(TASK_TITLE);
-  });
-
-  it('투두 인풋의 버튼을 클릭하면 onClickAddTask 이벤트가 호출된다', () => {
-    const { getByText } = renderPageComponent();
-
-    const addTodoButton = getByText('추가');
-    addTodoButton.click();
-
-    expect(onClickAddTask).toBeCalled();
-  });
-
-  it('투두 인풋에 타이핑을 하면 onChangeTitle 이벤트가 호출된다', () => {
-    const { getByPlaceholderText } = renderPageComponent();
-
-    const toDoInput = getByPlaceholderText('할 일을 입력해 주세요');
-    fireEvent.change(toDoInput, { target: { value: '타이핑을 해주세요' } });
-
-    expect(onChangeTitle).toBeCalled();
   });
 });
