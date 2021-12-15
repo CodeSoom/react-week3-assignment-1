e2cd6fd615d0a8dca64f9372f7bb3c7625618dc6
GUAJEGUICHAN <qkr135qkr@gmail.com>
2021-12-15 20:25:18 +0900

    [test] List컴포넌트에서 task의 length가 1이상 일 때 해당 Item 컴포넌트 출력확인

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -7,3 +7,23 @@ test('task의 length가 0일 때 \'할 일이 없어요!\'', () => {
 
   expect(container).toHaveTextContent('할 일이 없어요!');
 });
+
+test('task의 length가 1이상 일 때 해당 Item 컴포넌트 출력확인', () => {
+  const tasks = [
+    { id: 100, title: '숨 쉬기' },
+    { id: 101, title: '물 마시기' },
+  ];
+  const handleDelete = jest.fn();
+  const { container } = render(<List
+    tasks={tasks}
+    onClickDelete={handleDelete}
+  />);
+
+  // Item 안에있는 title 확인
+  expect(container).toHaveTextContent('숨 쉬기');
+  expect(container).toHaveTextContent('물 마시기');
+
+  // Itme 안에있는 완료버튼 존재 확인
+  expect(container).toHaveTextContent('완료');
+  expect(container).toContainHTML('button');
+});
