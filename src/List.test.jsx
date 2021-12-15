06781a38d7f337fe3f3c090a7f96bad2c172eb4a
emewjin <emewjin@gmail.com>
2021-12-15 23:37:00 +0900

    fix: List 컴포넌트 테스트에서 Item 컴포넌트의 기능인 테스트 삭제

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -17,20 +17,6 @@ describe('List', () => {
     expect(getAllByText('완료')).toHaveLength(2);
   });
 
-  it('"완료" 버튼을 클릭하면 onClickDelete 함수가 호출됩니다', () => {
-    const onClickDelete = jest.fn();
-
-    const { getAllByText } = render(
-      <List tasks={TASKS} onClickDelete={onClickDelete} />,
-    );
-
-    fireEvent.click(getAllByText('완료')[0]);
-    expect(onClickDelete).toHaveBeenCalledTimes(1);
-
-    fireEvent.click(getAllByText('완료')[1]);
-    expect(onClickDelete).toHaveBeenCalledTimes(2);
-  });
-
   it('tasks 배열에 아이템이 없을 경우 List 컴포넌트에 빈 화면용 뷰가 그려집니다', () => {
     const onClickDelete = jest.fn();
 
