696dbb6d79e1252be56da85f23a1e81457b57fc8
GUAJEGUICHAN <qkr135qkr@gmail.com>
2021-12-15 21:30:24 +0900

    [refactor] eslint 적용

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -14,13 +14,12 @@ test('task의 length가 0일 때 \'할 일이 없어요!\' 출력', () => {
 });
 
 test('task의 length가 1이상 일 때 해당 Item 컴포넌트 출력력인', () => {
-
   const handleDelete = jest.fn();
   const { container, getAllByText } = render(
     <List
       tasks={tasks}
       onClickDelete={handleDelete}
-    />
+    />,
   );
 
   // Item 안에있는 title 확인
@@ -28,9 +27,8 @@ test('task의 length가 1이상 일 때 해당 Item 컴포넌트 출력력인', 
   expect(container).toHaveTextContent(tasks[1].title);
 
   // Itme 안에있는 완료버튼 존재 확인
-  expect(getAllByText('완료')[0]).toContainHTML('button')
-  expect(getAllByText('완료')[1]).toContainHTML('button')
-
+  expect(getAllByText('완료')[0]).toContainHTML('button');
+  expect(getAllByText('완료')[1]).toContainHTML('button');
 });
 
 test('onClickDelete 함수가 작동 확인', () => {
@@ -40,7 +38,7 @@ test('onClickDelete 함수가 작동 확인', () => {
     <List
       tasks={tasks}
       onClickDelete={handleClick}
-    />
+    />,
   );
 
   fireEvent.click(getAllByText('완료')[0]);
