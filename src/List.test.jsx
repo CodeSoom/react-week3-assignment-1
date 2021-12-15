742aa1bacda6ec422a01bd8dbbbdf093a14d50aa
emewjin <emewjin@gmail.com>
2021-12-15 23:39:41 +0900

    ref: List 컴포넌트 테스트 수정
    - 반복되는 부분 추출

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -1,28 +1,23 @@
-import { render, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 
 import List from './List';
 import { EMPTY_TASK, TASKS } from './fixture';
 
 describe('List', () => {
+  const onClickDelete = jest.fn();
+  const renderListComponent = (tasks) => render(
+    <List tasks={tasks} onClickDelete={onClickDelete} />,
+  );
   it('tasks 배열의 title이 "완료" 버튼과 함께 List 컴포넌트에 그려집니다', () => {
-    const onClickDelete = jest.fn();
-
-    const { container, getAllByText } = render(
-      <List tasks={TASKS} onClickDelete={onClickDelete} />,
-    );
+    const { container } = renderListComponent(TASKS);
 
     expect(container).toHaveTextContent(TASKS[0].title);
     expect(container).toHaveTextContent(TASKS[1].title);
-
-    expect(getAllByText('완료')).toHaveLength(2);
+    expect(screen.getAllByText('완료')).toHaveLength(2);
   });
 
   it('tasks 배열에 아이템이 없을 경우 List 컴포넌트에 빈 화면용 뷰가 그려집니다', () => {
-    const onClickDelete = jest.fn();
-
-    const { container } = render(
-      <List tasks={EMPTY_TASK} onClickDelete={onClickDelete} />,
-    );
+    const { container } = renderListComponent(EMPTY_TASK);
 
     expect(container).toHaveTextContent('할 일이 없어요!');
   });
