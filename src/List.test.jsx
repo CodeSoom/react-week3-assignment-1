0da86f4e40a1ae449ca3f8f56411dc392aa5a396
GUAJEGUICHAN <qkr135qkr@gmail.com>
2021-12-15 21:28:30 +0900

    [test] List 컴포넌트 테스크 코드 작성

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -1,29 +1,51 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 import List from './List';
 
-test('task의 length가 0일 때 \'할 일이 없어요!\'', () => {
-  const tasks = [];
-  const { container } = render(<List tasks={tasks} />);
+const emptyTasks = [];
+const tasks = [
+  { id: 100, title: '숨 쉬기' },
+  { id: 101, title: '물 마시기' },
+];
+
+test('task의 length가 0일 때 \'할 일이 없어요!\' 출력', () => {
+  const { container } = render(<List tasks={emptyTasks} />);
 
   expect(container).toHaveTextContent('할 일이 없어요!');
 });
 
-test('task의 length가 1이상 일 때 해당 Item 컴포넌트 출력확인', () => {
-  const tasks = [
-    { id: 100, title: '숨 쉬기' },
-    { id: 101, title: '물 마시기' },
-  ];
+test('task의 length가 1이상 일 때 해당 Item 컴포넌트 출력력인', () => {
+
   const handleDelete = jest.fn();
-  const { container } = render(<List
-    tasks={tasks}
-    onClickDelete={handleDelete}
-  />);
+  const { container, getAllByText } = render(
+    <List
+      tasks={tasks}
+      onClickDelete={handleDelete}
+    />
+  );
 
   // Item 안에있는 title 확인
-  expect(container).toHaveTextContent('숨 쉬기');
-  expect(container).toHaveTextContent('물 마시기');
+  expect(container).toHaveTextContent(tasks[0].title);
+  expect(container).toHaveTextContent(tasks[1].title);
 
   // Itme 안에있는 완료버튼 존재 확인
-  expect(container).toHaveTextContent('완료');
-  expect(container).toContainHTML('button');
+  expect(getAllByText('완료')[0]).toContainHTML('button')
+  expect(getAllByText('완료')[1]).toContainHTML('button')
+
+});
+
+test('onClickDelete 함수가 작동 확인', () => {
+  const handleClick = jest.fn();
+
+  const { getAllByText } = render(
+    <List
+      tasks={tasks}
+      onClickDelete={handleClick}
+    />
+  );
+
+  fireEvent.click(getAllByText('완료')[0]);
+  expect(handleClick).toHaveBeenCalledTimes(1);
+
+  fireEvent.click(getAllByText('완료')[1]);
+  expect(handleClick).toHaveBeenCalledTimes(2);
 });
