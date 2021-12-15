45396e1dc07ad6f6b5dd4beba9577da9000da9df
jeongseop.lee <sjl0614@naver.com>
2021-12-15 20:46:20 +0900

    :recycle: Input 테스트 수정

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -2,25 +2,33 @@ import { render, fireEvent } from '@testing-library/react';
 
 import Input from './Input';
 
-test('Input', () => {
+describe('Input', () => {
   const handleClick = jest.fn();
 
   const handleChange = jest.fn();
 
-  const { container, getByText } = render((
-    <Input
-      value="아무거나 하기"
-      onClick={handleClick}
-      onChange={handleChange}
-    />
-  ));
+  const renderInput = () => (
+    render(
+      <Input
+        value="아무거나 하기"
+        onClick={handleClick}
+        onChange={handleChange}
+      />,
+    )
+  );
 
-  expect(container).toHaveTextContent('할 일');
-  expect(container).toHaveTextContent('추가');
+  it('render input', () => {
+    const { container } = renderInput();
 
-  expect(handleClick).not.toBeCalled();
+    expect(container).toHaveTextContent('할 일');
+    expect(container).toHaveTextContent('추가');
+  });
 
-  fireEvent.click(getByText('추가'));
+  it('"추가" button', () => {
+    const { getByText } = renderInput();
 
-  expect(handleClick).toBeCalled();
+    expect(handleClick).not.toBeCalled();
+    fireEvent.click(getByText('추가'));
+    expect(handleClick).toBeCalled();
+  });
 });
