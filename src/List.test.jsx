0f9f8dd27094372701f35ad16fd1087be8157c4e
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2021-12-16 07:01:09 +0900

    [test] List 테스트 코드 리팩토링 - given-when-then 적용 - 가독성을 위한 코드 수정

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -11,13 +11,17 @@ describe('List Component', () => {
     jest.clearAllMocks();
   });
 
-  const renderComponent = (_tasks) => render((
-    <List tasks={_tasks} onClickDelete={onClickDelete} />
+  const renderComponent = (value) => render((
+    <List
+      tasks={value}
+      onClickDelete={onClickDelete}
+    />
   ));
 
   context('task 가 없을 경우', () => {
-    it("task가 없을 경우, '할 일이 없어요!' 출력", () => {
+    it('empty 메시지 출력', () => {
       const { container } = renderComponent([]);
+
       expect(container).toHaveTextContent('할 일이 없어요!');
     });
   });
@@ -29,23 +33,23 @@ describe('List Component', () => {
 
     it('task의 타이틀 및 완료 버튼이 render 된다.', () => {
       const { container, getAllByRole } = renderComponent(tasks);
-
-      expect(container).toHaveTextContent(tasks[0].title);
-      expect(container).toHaveTextContent(tasks[1].title);
-
       const buttons = getAllByRole('button');
+
       expect(buttons).toHaveLength(2);
-      expect(buttons[0]).toHaveTextContent('완료');
+      tasks.forEach(({ title }, index) => {
+        expect(container).toHaveTextContent(title);
+        expect(buttons[index]).toHaveTextContent('완료');
+      });
     });
 
     it('완료 버튼을 클릭할 경우 해당 아이템의 id 값을 호출한다.', () => {
       const { getAllByRole } = renderComponent(tasks);
-
       const buttons = getAllByRole('button');
-      expect(buttons[1]).toHaveTextContent('완료');
-      expect(onClickDelete).not.toHaveBeenCalled();
+
       fireEvent.click(buttons[1]);
-      expect(onClickDelete).toHaveBeenCalledWith(2);
+
+      expect(buttons[1]).toHaveTextContent('완료');
+      expect(onClickDelete).toHaveBeenCalledWith(tasks[1].id);
     });
   });
 });
