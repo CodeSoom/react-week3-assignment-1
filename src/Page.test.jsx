f45e2f8672c44d02277230346e5bbf94fc754274
GUAJEGUICHAN <qkr135qkr@gmail.com>
2021-12-16 23:12:59 +0900

    [test] describe, context, it을 이용한 테스트 코드

--- src/Page.test.jsx
+++ src/Page.test.jsx
@@ -4,43 +4,40 @@ import Page from './Page';
 // Input 컴포넌트 작동 확인 -할일
 
 describe('Page', () => {
-  context('Input 컴포넌트가 그려진다. ', () => {
+  context('언제든지', () => {
+    it('Input 컴포넌트가 그려진다.', () => {
+      const { container, getByRole } = render(<Page tasks={[]} />);
+      expect(container).toHaveTextContent('할 일');
+      expect(container).toContainHTML('input');
+      expect(container).toContainHTML('button');
+      expect(getByRole('button')).toHaveTextContent('추가');
+      expect(container).toHaveTextContent('할 일이 없어요!');
+    });
 
-  });
-  it('Input 컴포넌트 작동 확인', () => {
-    const { container, getByRole } = render(<Page tasks={[]} />);
-
-    // Input 컴포넌트 작동 확인
-    expect(container).toHaveTextContent('할 일');
-    expect(container).toContainHTML('input');
-    expect(container).toContainHTML('button');
-    expect(getByRole('button')).toHaveTextContent('추가');
-
-    // List 컴포넌트 작동 확인
-    expect(container).toHaveTextContent('할 일이 없어요!');
-  });
-  it('task가 비었을 때 List 컴포넌트 작동 확인 ', () => {
+  })
+  context('task가 비어있다.', () => {
     const emptyTasks = [];
-    const { container } = render(<Page tasks={emptyTasks} />);
 
-    // List 컴포넌트 작동 확인
-    expect(container).toHaveTextContent('할 일이 없어요!');
-  });
+    it('"할 일이 없어요"를 그린다.', () => {
+      const { container } = render(<Page tasks={emptyTasks} />);
+      expect(container).toHaveTextContent('할 일이 없어요!');
+    });
+  })
 
-  it('task가 있을 때', () => {
-    // List 컴포넌트 작동 확인
+  context('task가 2개 있다.', () => {
     const tasks = [
       { id: 100, title: '숨 쉬기' },
       { id: 101, title: '물 마시기' },
     ];
-    const { container, getAllByText } = render(<Page tasks={tasks} />);
 
-    // Item 안에있는 title 확인
-    expect(container).toHaveTextContent(tasks[0].title);
-    expect(container).toHaveTextContent(tasks[1].title);
+    it('2개의 task의 각 title과 버튼을 그린다..', () => {
+      const { container, getAllByText } = render(<Page tasks={tasks} />);
 
-    // Itme 안에있는 완료버튼 존재 확인
-    expect(getAllByText('완료')[0]).toContainHTML('button');
-    expect(getAllByText('완료')[1]).toContainHTML('button');
-  });
-});
+      expect(container).toHaveTextContent(tasks[0].title);
+      expect(container).toHaveTextContent(tasks[1].title);
+
+      expect(getAllByText('완료')[0]).toContainHTML('button');
+      expect(getAllByText('완료')[1]).toContainHTML('button');
+    });
+  })
+})
