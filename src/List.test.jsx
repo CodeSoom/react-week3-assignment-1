408cbc4403143ec27f81925241f76ffca4decd2c
leeok <milkteatea0628@gmail.com>
2021-12-16 21:26:16 +0900

    [add] list 테스트 작성!

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -2,13 +2,32 @@ import { render } from '@testing-library/react';
 
 import List from './List';
 
-test('List', () => {
+const handleClick = jest.fn();
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
+// todo가 있을 때 해당 갯수만큼 item이 생겨야한다!
+test('List has tasks', () => {
   const tasks = [
     { id: 1, task: '코드숨 과제하기!' },
     { id: 2, task: '테스트 주도 개발 공부하기!' },
   ];
-  const handleClick = jest.fn();
+  const { getAllByRole } = render((
+    <List
+      tasks={tasks}
+      onClickDelete={handleClick}
+    />
+  ));
 
+  expect(getAllByRole('listitem')).toHaveLength(2);
+});
+
+// 길이가 0인 것도 테스트하려면 별도 배열을 생성해야 ?
+// => test를 분리해서 테스트해준다!
+// todo가 없을 때
+test('List didnt have task', () => {
+  const tasks = [];
   const { container } = render((
     <List
       tasks={tasks}
@@ -16,7 +35,5 @@ test('List', () => {
     />
   ));
 
-  expect(container).toBe('아이템 컴포넌트 호출');
-  // return이 또 다른 컴포넌트일 때
-  // 길이가 0인 것도 테스트하려면 별도 배열을 생성해야 ?
+  expect(container).toHaveTextContent('할 일이 없어요!');
 });
