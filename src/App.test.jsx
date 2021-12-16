adfd4ad6c22e29ac91db47520ebfba12cb3985e3
padawanR0k <padawanR0k@gmail.com>
2021-12-16 22:22:33 +0900

    up: apply describe-context-it pattern to App.test.tsx

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -3,26 +3,36 @@ import { render, fireEvent } from '@testing-library/react';
 import App from './App';
 
 describe('App', () => {
-  let renderResult;
-  beforeEach(() => {
-    renderResult = render(<App />);
-    fireEvent.change(renderResult.getByPlaceholderText('할 일을 입력해 주세요'), {
-      target: { value: '일기쓰기' },
+  describe('할일 추가 버튼 클릭', () => {
+    it('입력한 내용이 리스트에 추가되는지 확인한다.', () => {
+      const { container, getByPlaceholderText, getByText } = render(<App />);
+      fireEvent.change(getByPlaceholderText('할 일을 입력해 주세요'), {
+        target: { value: '일기쓰기' },
+      });
+      fireEvent.click(getByText('추가'));
+      expect(container).toHaveTextContent('일기쓰기');
     });
   });
 
-  it('할일 추가 버튼 클릭시, 입력한 내용이 리스트에 추가되는지 확인한다.', () => {
-    fireEvent.click(renderResult.getByText('추가'));
-    expect(renderResult.container).toHaveTextContent('일기쓰기');
+  describe('완료 버튼 클릭시', () => {
+    it('할 일이 삭제되는지 확인한다', () => {
+      const { container, getByPlaceholderText, getByText } = render(<App />);
+      fireEvent.change(getByPlaceholderText('할 일을 입력해 주세요'), {
+        target: { value: '일기쓰기' },
+      });
+      fireEvent.click(getByText('추가'));
+      fireEvent.click(getByText('완료'));
+      expect(container).not.toHaveTextContent('일기쓰기');
+    });
   });
 
-  it('완료 버튼 클릭시, 할 일이 삭제되는지 확인한다.', () => {
-    fireEvent.click(renderResult.getByText('추가'));
-    fireEvent.click(renderResult.getByText('완료'));
-    expect(renderResult.container).not.toHaveTextContent('일기쓰기');
-  });
-
-  it('더 이상 노출할 할 일이 없을 때, "할 일이 없어요!"문구가 노출되는지 확인한다.', () => {
-    expect(renderResult.container).toHaveTextContent('할 일이 없어요!');
+  describe('더 이상 노출할 목록이 없을 때', () => {
+    it('할 일이 없어요!"문구가 노출되는지 확인한다.', () => {
+      const { container, getByPlaceholderText } = render(<App />);
+      fireEvent.change(getByPlaceholderText('할 일을 입력해 주세요'), {
+        target: { value: '일기쓰기' },
+      });
+      expect(container).toHaveTextContent('할 일이 없어요!');
+    });
   });
 });
