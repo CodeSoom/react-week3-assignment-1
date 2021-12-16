1819b2521aea7844761e1e477540d8d8fbaa7bee
GUAJEGUICHAN <qkr135qkr@gmail.com>
2021-12-16 21:38:23 +0900

    [test] describe, it 이용한 테스트 코드 작성 테스크 코드 내용 수정

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -3,28 +3,41 @@ import { fireEvent, render } from '@testing-library/react';
 
 import Input from './Input';
 
-test('Input', () => {
+describe('Input', () => {
   const handleClick = jest.fn();
   const handleChange = jest.fn();
-  const { container, getByText, getByRole } = render(
+  const value = "test"
+  const InputComponent = () => render(
     <Input
-      value="test"
+      value={value}
       onClick={handleClick}
       onChange={handleChange}
     />,
   );
 
-  // 추가 버튼 작동 확인
-  expect(container).toHaveTextContent('추가');
+  it("할 일을 그린다.", () => {
+    const { container } = InputComponent();
+    expect(container).toHaveTextContent("할 일")
+  })
 
-  expect(handleClick).not.toBeCalled();
-  fireEvent.click(getByText('추가'));
-  expect(handleClick).toBeCalled();
+  it('추가 버튼 누르면 handleClick 작동한다.', () => {
+    const { getByText } = InputComponent();
+    expect(handleClick).not.toBeCalled();
+    fireEvent.click(getByText('추가'));
+    expect(handleClick).toBeCalled();
+  })
 
-  // textbox 입력 작동 확인
-  expect(getByRole('textbox').value).toBe('test');
-  expect(handleChange).not.toBeCalled();
-  fireEvent.change(getByRole('textbox'), { target: { value: 'test2' } });
-  expect(handleChange).toBeCalled();
-  // expect(getByRole('textbox').value).toBe('test2');
+  it('Input 컴포넌트의 프롭의 value가 input 태그의 value와 같다. ', () => {
+    const { getByLabelText } = InputComponent();
+    expect(getByLabelText('할 일').value).toBe(value)
+  })
+
+  it('textbox에 값이 입력될때 handleChange 작동된다.', () => {
+    const { getByLabelText } = InputComponent();
+    expect(handleChange).not.toBeCalled();
+    fireEvent.change(getByLabelText('할 일'), { target: { value: 'test2' } })
+    expect(handleChange).toBeCalled();
+
+  })
+
 });
