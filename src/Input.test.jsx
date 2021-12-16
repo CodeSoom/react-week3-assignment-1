1ccd81d9e1d8b432a3b24240eb0deff30c1c74a1
GUAJEGUICHAN <qkr135qkr@gmail.com>
2021-12-16 22:54:48 +0900

    [refactor]eslint 적용

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -6,7 +6,7 @@ import Input from './Input';
 describe('Input', () => {
   const handleClick = jest.fn();
   const handleChange = jest.fn();
-  const value = "test"
+  const value = 'test';
   const InputComponent = () => render(
     <Input
       value={value}
@@ -15,29 +15,27 @@ describe('Input', () => {
     />,
   );
 
-  it("할 일을 그린다.", () => {
+  it('할 일을 그린다.', () => {
     const { container } = InputComponent();
-    expect(container).toHaveTextContent("할 일")
-  })
+    expect(container).toHaveTextContent('할 일');
+  });
 
   it('추가 버튼 누르면 handleClick 작동한다.', () => {
     const { getByText } = InputComponent();
     expect(handleClick).not.toBeCalled();
     fireEvent.click(getByText('추가'));
     expect(handleClick).toBeCalled();
-  })
+  });
 
   it('Input 컴포넌트의 프롭의 value가 input 태그의 value와 같다. ', () => {
     const { getByLabelText } = InputComponent();
-    expect(getByLabelText('할 일').value).toBe(value)
-  })
+    expect(getByLabelText('할 일').value).toBe(value);
+  });
 
   it('textbox에 값이 입력될때 handleChange 작동된다.', () => {
     const { getByLabelText } = InputComponent();
     expect(handleChange).not.toBeCalled();
-    fireEvent.change(getByLabelText('할 일'), { target: { value: 'test2' } })
+    fireEvent.change(getByLabelText('할 일'), { target: { value: 'test2' } });
     expect(handleChange).toBeCalled();
-
-  })
-
+  });
 });
