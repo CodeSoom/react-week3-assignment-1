1eeb46f37775aa52c588cd4073d30628b67d9b0f
Superduper-India <sunyoung1542@gmail.com>
2021-12-16 20:00:58 +0900

    Update: App테스트 작성

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,2 +1,66 @@
-test('App', () => {
+import { render } from '@testing-library/react';
+
+import App from './App';
+import Page from './Page';
+
+describe('App', () => {
+    const mockFn = jest.fn();
+
+    beforeEach(() => {
+        mockFn.mockImplementation(() => ({
+            newId: 100,
+            taskTitle: '',
+            tasks: [],
+        }));
+    });
+
+    it('App for handleChangeTitle', () => {
+        const handleChange = jest.fn();
+
+        handleChange.mockImplementation((event) => ({
+            taskTitle: event.target.value,
+        }));
+
+        render(<App onChange={handleChange} />);
+    });
+
+    it('App for handleClick', () => {
+        const handleClick = jest.fn();
+        const newId = 100;
+        const taskTitle = '뭐라도 하기';
+        const tasks = [
+            { id: 100, title: '뭐라도 하기' },
+        ];
+
+        handleClick.mockImplementation((id) => ({
+            newId: newId + 1,
+            taskTitle: '',
+            tasks: [{ id: newId, title: taskTitle }],
+            tasks: tasks.filter((task) => task.id !== id),
+        }));
+
+        render(<App onClick={handleClick} />);
+    });
+
+    it('App for Page', () => {
+        const handleChange = jest.fn();
+        const handleClick = jest.fn();
+        const tasks = [];
+        const taskTitle = '';
+
+        const { container } = render(
+            <Page
+                tasks={tasks}
+                onClickDeleteTask={handleClick}
+                taskTitle={taskTitle}
+                onClickAddTask={handleClick}
+                onChangeTitle={handleChange}
+            />,
+        );
+
+        expect(container).toHaveTextContent('To-do');
+        expect(container).toHaveTextContent('할 일');
+        expect(container).toHaveTextContent('추가');
+        expect(container).toHaveTextContent('할 일이 없어요!');
+    });
 });
--- src/InputPractice.test.jsx
+++ src/InputPractice.test.jsx
@@ -3,72 +3,72 @@ import { fireEvent, render } from '@testing-library/react';
 import Input from './Input';
 
 describe('Input', () => {
-  const value = '책 읽기';
-  const handleChange = jest.fn();
-  const handleClick = jest.fn();
+    const value = '책 읽기';
+    const handleChange = jest.fn();
+    const handleClick = jest.fn();
 
-  const renderInput = () => render(
-    <Input
-      value={value}
-      onChange={handleChange}
-      onClick={handleClick}
-    />,
-  );
+    const renderInput = () => render(
+        <Input
+            value={value}
+            onChange={handleChange}
+            onClick={handleClick}
+        />,
+    );
 
-  beforeEach(() => {
-    jest.clearAllMocks();
-  });
-
-  describe('when change input', () => {
-    it('calls onChange handler', () => {
-      // Given
-      const { getByLabelText } = renderInput();
-
-      // When
-      fireEvent.change(getByLabelText('할 일'), {
-        target: { value: '아무것도 안하기' },
-      });
-
-      // Then
-      expect(handleChange).toBeCalled();
+    beforeEach(() => {
+        jest.clearAllMocks();
     });
-  });
 
-  describe('with same text', () => {
-    it('not call onChange handler', () => {
-      // Given
-      const { getByLabelText } = renderInput();
+    describe('when change input', () => {
+        it('calls onChange handler', () => {
+            // Given
+            const { getByLabelText } = renderInput();
 
-      // When
-      fireEvent.change(getByLabelText('할 일'), {
-        target: { value },
-      });
+            // When
+            fireEvent.change(getByLabelText('할 일'), {
+                target: { value: '아무것도 안하기' },
+            });
 
-      // Then
-      expect(handleChange).not.toBeCalled();
+            // Then
+            expect(handleChange).toBeCalled();
+        });
     });
-  });
 
-  it('renders input', () => {
-    const { container, getByLabelText } = renderInput();
+    describe('with same text', () => {
+        it('not call onChange handler', () => {
+            // Given
+            const { getByLabelText } = renderInput();
 
-    const input = getByLabelText('할 일');
+            // When
+            fireEvent.change(getByLabelText('할 일'), {
+                target: { value },
+            });
 
-    expect(input.value).toBe(value);
-    expect(container).toHaveTextContent('할 일');
-    expect(container).toHaveTextContent('추가');
-  });
-
-  describe('when click button', () => {
-    it('calls onClick handler', () => {
-      // Given
-      const { getByText } = renderInput();
-
-      // When
-      fireEvent.click(getByText('추가'));
-
-      // Then
-      expect(handleClick).toBeCalled();
+            // Then
+            expect(handleChange).not.toBeCalled();
+        });
     });
-  });
+
+    it('renders input', () => {
+        const { container, getByLabelText } = renderInput();
+
+        const input = getByLabelText('할 일');
+
+        expect(input.value).toBe(value);
+        expect(container).toHaveTextContent('할 일');
+        expect(container).toHaveTextContent('추가');
+    });
+
+    describe('when click button', () => {
+        it('calls onClick handler', () => {
+            // Given
+            const { getByText } = renderInput();
+
+            // When
+            fireEvent.click(getByText('추가'));
+
+            // Then
+            expect(handleClick).toBeCalled();
+        });
+    });
 });
