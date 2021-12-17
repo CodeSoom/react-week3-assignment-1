e82219d3dc58337bcaf3a3fdc23324a1879333b7
Beomyeon <beomyeon.kim@adriel.ai>
2021-12-18 01:19:49 +0900

    make page test for only necessary field

--- src/Page.test.jsx
+++ src/Page.test.jsx
@@ -3,27 +3,7 @@ import { render } from '@testing-library/react';
 import Page from './Page';
 
 test('Page', () => {
-  const tasks = [
-    {
-      id: 100,
-      title: '테스팅 공부하기',
-    },
-    {
-      id: 101,
-      title: '코드숨 과제하기',
-    },
-  ];
-
-  const taskTitle = '이번주 할 일';
-
-  const {
-    container,
-  } = render((
-    <Page
-      tasks={tasks}
-      taskTitle={taskTitle}
-    />
-  ));
+  const { container } = render((<Page/>));
 
   expect(container).toHaveTextContent('To-do');
 });
