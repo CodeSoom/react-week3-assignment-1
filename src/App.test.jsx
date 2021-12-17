3047a2f9706556ee36b1efec51e10d7e4f1b4c83
yeji <lerry597@gmail.com>
2021-12-17 16:09:08 +0900

    [feat] App.test.jsx 초기상태확인 테스트 추가

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,12 +1,14 @@
 import { render } from '@testing-library/react';
 import App from './App';
 
-describe('App', () => {
-  it('render', () => {
-    const { container } = render((
-      <App />
-    ));
+test('아무 것도 추가하지 않았을 때 초기 상태 확인', () => {
+  const { container, getByPlaceholderText } = render(
+    <App />,
+  );
 
-    expect(container).toBe(1);
-  });
+  const inputTodo = getByPlaceholderText('할 일을 입력해 주세요');
+
+  expect(container).toHaveTextContent('To-do');
+  expect(container).toHaveTextContent('할 일이 없어요!');
+  expect(inputTodo).toHaveAttribute('value', '');
 });
