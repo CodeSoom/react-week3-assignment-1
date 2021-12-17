20095227be22554d17734ef58e115d9d60e561ba
yeji <lerry597@gmail.com>
2021-12-17 14:08:40 +0900

    [feat] Input.test.jsx 할 일 입력, 추가 버튼 클릭 테스트 구현

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,6 +1,11 @@
-import { render, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
 
 describe('App', () => {
+	it('render', () => {
+		const { container } = render((
+			<App />
+		))
 
+	})
 });
--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -1,11 +1,41 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import Input from './Input';
 
-test('Input', () => {
-	const { container, getByPlaceholderText } = render((
+test('할 일 입력값 작성시 변경', () => {
+	const value = '뭐라도 하기';
+  const handleChange = jest.fn();
+
+	const { getByPlaceholderText } = render((
 		<Input 
-		
+			value={value}
+      onChange={handleChange}
 		/>
 	));
-	expect(container).getByPlaceholderText('할 일을 입력해 주세요');
+
+	const inputTodo = getByPlaceholderText('할 일을 입력해 주세요');
+	
+	expect(handleChange).not.toBeCalled();
+
+	fireEvent.change(inputTodo, {value: '뭐라도 하기'});
+	
+	expect(inputTodo).toHaveAttribute('value', '뭐라도 하기');
+});
+
+
+test('추가 버튼 클릭', () => {
+	const handleClick = jest.fn();
+
+	const { container, getByText } = render((
+		<Input 
+      onClick={handleClick}
+		/>
+	));
+
+  expect(container).toHaveTextContent('추가');
+
+  expect(handleClick).not.toBeCalled();
+
+  fireEvent.click(getByText('추가'));
+
+  expect(handleClick).toBeCalled();
 });
