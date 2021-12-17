653d2c132657dd0546699893e14685747d5d43b6
GUAJEGUICHAN <qkr135qkr@gmail.com>
2021-12-17 20:32:10 +0900

    [test] App 컴포넌트 테스트 코드 작성

---
+++ src/App.test.jsx
@@ -0,0 +1,61 @@
+import { render } from '@testing-library/react';
+
+import App from './App';
+import Input from './Input';
+import Item from './Item';
+import List from './List';
+import Page from './Page';
+
+const handleChangeTitle = jest.fn();
+const handleClickAddTask = jest.fn();
+const handleClickDeleteTask = jest.fn();
+const taskTitle = '';
+const tasks = ['test'];
+
+describe('App', () => {
+  context('무조건', () => {
+    it('App이 그려진다.', () => {
+      const { container } = render(<App />);
+      expect(container);
+    });
+    it('Page가 그려진다.', () => {
+      const { container } = render((
+        <Page
+          taskTitle={taskTitle}
+          onChangeTitle={handleChangeTitle}
+          onClickAddTask={handleClickAddTask}
+          tasks={tasks}
+          onClickDeleteTask={handleClickDeleteTask}
+        />));
+      expect(container);
+    });
+    it('Input이 그려진다.', () => {
+      const { container } = render((
+        <Input
+          value={taskTitle}
+          onChange={handleChangeTitle}
+          onClick={handleClickAddTask}
+        />
+      ));
+      expect(container);
+    });
+    it('Item이 그려진다.', () => {
+      const { container } = render((
+        <Item
+          task={tasks[0]}
+          onClickDelete={handleClickDeleteTask}
+        />
+      ));
+      expect(container);
+    });
+    it('List가 그려진다.', () => {
+      const { container } = render((
+        <List
+          tasks={tasks}
+          onClickDelete={handleClickDeleteTask}
+        />
+      ));
+      expect(container);
+    });
+  });
+});
