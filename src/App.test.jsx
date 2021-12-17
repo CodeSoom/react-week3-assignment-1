1fdb0e53ac673f966013319c2272b4248e819b7d
yeji <lerry597@gmail.com>
2021-12-17 16:50:05 +0900

    [feat] App.test.jsx 완료버튼 테스트 작성

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -47,3 +47,28 @@ test('<추가> 누르면 List에 추가 확인', () => {
 
   expect(container).toHaveTextContent('아무 것도 하지 않기');
 });
+
+test('<완료> 누르면 List에서 삭제 확인', () => {
+  const { container, getByText, getByPlaceholderText } = render(
+    <App />,
+  );
+
+  const addButton = getByText('추가');
+  const inputTodo = getByPlaceholderText('할 일을 입력해 주세요');
+
+  fireEvent.change(inputTodo, {
+    target: {
+      value: '재밌는 영화 보기',
+    },
+  });
+
+  fireEvent.click(addButton);
+
+  expect(container).toHaveTextContent('재밌는 영화 보기');
+
+  const doneButton = getByText('완료');
+
+  fireEvent.click(doneButton);
+
+  expect(container).toHaveTextContent('할 일이 없어요!');
+});
