6c698dc9d716762cdfe6020244f7c827c2080902
leeok <milkteatea0628@gmail.com>
2021-12-17 20:31:29 +0900

    [mod] 지적받은 부분 리팩터링

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -2,38 +2,40 @@ import { render } from '@testing-library/react';
 
 import List from './List';
 
-const handleClick = jest.fn();
-beforeEach(() => {
-  jest.clearAllMocks();
-});
+describe('List', () => {
+  const handleClick = jest.fn();
 
-// todo가 있을 때 해당 갯수만큼 item이 생겨야한다!
-test('List has tasks', () => {
-  const tasks = [
-    { id: 1, task: '코드숨 과제하기!' },
-    { id: 2, task: '테스트 주도 개발 공부하기!' },
-  ];
-  const { getAllByRole } = render((
-    <List
-      tasks={tasks}
-      onClickDelete={handleClick}
-    />
-  ));
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+  context('when tasks is exists', () => {
+    it('renders tasks', () => {
+      const tasks = [
+        { id: 1, task: '코드숨 과제하기!' },
+        { id: 2, task: '테스트 주도 개발 공부하기!' },
+      ];
+      const { getAllByRole } = render((
+        <List
+          tasks={tasks}
+          onClickDelete={handleClick}
+        />
+      ));
 
-  expect(getAllByRole('listitem')).toHaveLength(2);
-});
+      expect(getAllByRole('listitem')).toHaveLength(tasks.length);
+    });
+  });
+  
+  context('when tasks is empty', () => {
+    test('renders empty message', () => {
+      const tasks = [];
+      const { container } = render((
+        <List
+          tasks={tasks}
+          onClickDelete={handleClick}
+        />
+      ));
 
-// 길이가 0인 것도 테스트하려면 별도 배열을 생성해야 ?
-// => test를 분리해서 테스트해준다!
-// todo가 없을 때
-test('List didnt have task', () => {
-  const tasks = [];
-  const { container } = render((
-    <List
-      tasks={tasks}
-      onClickDelete={handleClick}
-    />
-  ));
-
-  expect(container).toHaveTextContent('할 일이 없어요!');
+      expect(container).toHaveTextContent('할 일이 없어요!');
+    });
+  });
 });
--- src/Page.test.jsx
+++ src/Page.test.jsx
@@ -1,44 +1,34 @@
 import { render } from '@testing-library/react';
+import { isDOMComponent } from 'react-dom/test-utils';
 
 import Page from './Page';
-import Input from './Input';
-import List from './List';
 
-const handleChange = jest.fn();
-const handleClickAdd = jest.fn();
-const handleClickDelete = jest.fn();
-const taskTitle = '나는 타이틀';
-const tasks = [
-  { id: 1, task: '코드숨 과제하기!' },
-  { id: 2, task: '테스트 주도 개발 공부하기!' },
-];
+describe('Page', () => {
+  const handleChange = jest.fn();
+  const handleClickAdd = jest.fn();
+  const handleClickDelete = jest.fn();
+  const taskTitle = '나는 타이틀';
+  const tasks = [
+    { id: 1, task: '코드숨 과제하기!' },
+    { id: 2, task: '테스트 주도 개발 공부하기!' },
+  ];
 
-// Input과 List는 이미 테스트함. 둘 다 호출하는지 여부를 테스트?
-test('Page', () => {
-  const { container } = render((
-    <Page
-      taskTitle={taskTitle}
-      onChangeTitle={handleChange}
-      onClickAddTask={handleClickAdd}
-      onClickDeleteTask={handleClickDelete}
-      tasks={tasks}
-    />
-  ));
-  const { container: inputContainer } = render((
-    <Input
-      value={taskTitle}
-      onChange={handleChange}
-      onClick={handleClickAdd}
-    />
-  ));
-  const { container: listContainer } = render((
-    <List
-      tasks={tasks}
-      onClickDelete={handleClickDelete}
-    />
-  ));
-
-  expect(container).toHaveTextContent('To-do');
-  expect(container).toContainHTML(inputContainer.innerHTML);
-  expect(container).toContainHTML(listContainer.innerHTML);
+  context('when page render', () => {
+    test('then Input and List render', () => {
+      const { container } = render((
+        <Page
+          taskTitle={taskTitle}
+          onChangeTitle={handleChange}
+          onClickAddTask={handleClickAdd}
+          onClickDeleteTask={handleClickDelete}
+          tasks={tasks}
+        />
+      ));
+      
+      isDOMComponent(container);
+      expect(container).toHaveTextContent('To-do');
+      expect(container).toHaveTextContent('할 일');
+      expect(container).toHaveTextContent('완료');
+    });
+  });
 });
