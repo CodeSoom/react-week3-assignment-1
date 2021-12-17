ce4a9a31b35fd108c99780028d7bf93d5760713d
yeji <lerry597@gmail.com>
2021-12-17 15:55:02 +0900

    [feat] Page.test.jsx List 빈 상태와 추가되었을 때 상태 확인

--- src/Page.test.jsx
+++ src/Page.test.jsx
@@ -1,2 +1,35 @@
-// import { render, fireEvent } from '@testing-library/react';
-// import Page from './Page';
+import { render, fireEvent } from '@testing-library/react';
+import Page from './Page';
+
+test('Input과 List 기본 상태 확인', () => {
+  const tasks = [];
+
+  const { container, getByPlaceholderText, getByText } = render((
+    <Page tasks={tasks} />
+  ));
+
+  const inputTodo = getByPlaceholderText('할 일을 입력해 주세요');
+  const list = getByText('할 일이 없어요!');
+
+  expect(container).toHaveTextContent('To-do');
+  expect(inputTodo).toHaveAttribute('value', '');
+  expect(list).toHaveTextContent('할 일이 없어요!');
+});
+
+test('추가 버튼 클릭 시 입력값 추가 확인', () => {
+  const tasks = [{
+    id: 1,
+    title: '뭐라도 하기',
+  }];
+
+  const { container, getByText } = render((
+    <Page tasks={tasks} />
+  ));
+
+  const addButton = getByText('추가');
+
+  fireEvent.click(addButton);
+
+  expect(container).toHaveTextContent('뭐라도 하기');
+  expect(container).toHaveTextContent('완료');
+});
