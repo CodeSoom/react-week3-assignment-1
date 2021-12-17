b4dd471d5db353c9c102e35458c8d20bf070de6d
Superduper-India <sunyoung1542@gmail.com>
2021-12-17 20:52:14 +0900

    Update: finish App테스트

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -9,10 +9,10 @@ describe('App', () => {
     expect(container).toHaveTextContent('To-do');
     expect(container).toHaveTextContent('할 일이 없어요!');
     expect(getByText(/추가/)).toBeInTheDocument();
-    expect(getByLabelText(/할 일/)).toBeInTheDocument(); // (/이름/)의 의미?
+    expect(getByLabelText(/할 일/)).toBeInTheDocument();
   });
 
-  describe('when put text into input and click button', () => {
+  describe('when put text into input and click add button', () => {
     it('show text and finish button', () => {
       // Given
       const { container, getByLabelText, getByText } = renderApp();
@@ -26,6 +26,29 @@ describe('App', () => {
       // Then
       expect(container).toHaveTextContent('운동하기');
       expect(getByText(/완료/)).toBeInTheDocument();
+    });
+  });
+
+  describe('when click finish button', () => {
+    it('remove text and finish button', () => {
+      // Given
+      const { container, getByLabelText, getByText } = renderApp();
+
+      // When
+      fireEvent.change(getByLabelText(/할 일/), {
+        target: { value: '운동하기' },
+      });
+      fireEvent.click(getByText(/추가/));
+
+      // Then
+      expect(container).toHaveTextContent('운동하기');
+      expect(getByText(/완료/)).toBeInTheDocument();
+
+      // When
+      fireEvent.click(getByText(/완료/));
+
+      // Then
+      expect(container).not.toHaveTextContent('운동하기');
     });
   });
 });
