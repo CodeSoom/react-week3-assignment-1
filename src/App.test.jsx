645fd7863306e1dce695ba2789fe44149fbd099d
GUAJEGUICHAN <qkr135qkr@gmail.com>
2021-12-17 21:51:20 +0900

    [test] App 테스트 코드에 잘 작동할때 테스트하는 코드 추가

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 import App from './App';
 import Input from './Input';
@@ -6,18 +6,16 @@ import Item from './Item';
 import List from './List';
 import Page from './Page';
 
-;
-
 describe('App', () => {
-  const handleChangeTitle = jest.fn();
-  const handleClickAddTask = jest.fn();
-  const handleClickDeleteTask = jest.fn();
-  const taskTitle = '';
+  const taskTitle = 'test';
   const tasks = [
     { id: 100, title: '숨 쉬기' },
     { id: 101, title: '물 마시기' },
   ];
-  context('무조건', () => {
+  context('잘 그려질 때', () => {
+    const handleChangeTitle = jest.fn();
+    const handleClickAddTask = jest.fn();
+    const handleClickDeleteTask = jest.fn();
     it('App이 그려진다.', () => {
       const { container } = render(<App />);
       expect(container);
@@ -60,6 +58,63 @@ describe('App', () => {
         />
       ));
       expect(container);
+    });
+  });
+
+  context('잘 작동할 때', () => {
+    const AppComponent = () => render((<App />));
+
+    it('할 일 1개 입력할 수 있다.', () => {
+      const { container, getByRole, getByLabelText } = AppComponent();
+
+      fireEvent.change(getByLabelText('할 일'), {
+        target: {
+          value: taskTitle,
+        },
+      });
+
+      expect(getByLabelText('할 일').value).toBe(taskTitle);
+
+      fireEvent.click(getByRole('button', { name: '추가' }));
+
+      expect(container).toHaveTextContent(taskTitle);
+    });
+
+    it('할 일을 1개 입력 후 추가 버튼을 클릭하면, 입련 란이 빈 값이 된다.', () => {
+      const { getByLabelText, getByRole } = AppComponent();
+
+      fireEvent.change(getByLabelText('할 일'), {
+        target: {
+          value: taskTitle,
+        },
+      });
+      fireEvent.click(getByRole('button', { name: '추가' }));
+
+      expect(getByLabelText('할 일').value).toBe('');
+    });
+
+    it('할 일을 2개 입력하고 2개 삭제하면 "할 일이 없어요!"가 뜬다. ', () => {
+      const {
+        container, getByRole, getAllByRole, getByLabelText,
+      } = AppComponent();
+
+      tasks.forEach((task) => {
+        fireEvent.change(getByLabelText('할 일'), {
+          target: {
+            value: task,
+          },
+        });
+        fireEvent.click(getByRole('button', { name: '추가' }));
+      });
+      const buttons = getAllByRole('button', { name: '완료' });
+
+      expect(buttons).toHaveLength(2);
+      fireEvent.click(buttons[1]);
+      expect(container).not.toHaveTextContent(tasks[1].title);
+      fireEvent.click(buttons[0]);
+      expect(container).not.toHaveTextContent(tasks[0].title);
+
+      expect(container).toHaveTextContent('할 일이 없어요!');
     });
   });
 });
