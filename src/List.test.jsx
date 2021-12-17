d7baffa42ec6cc24da75b7eef3dbafc8b65aead9
Superduper-India <sunyoung1542@gmail.com>
2021-12-17 20:31:35 +0900

    Update: App테스트 처음부터 다시작성

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -3,22 +3,22 @@ import { render } from '@testing-library/react';
 import List from './List';
 
 it('when tasks are given -> show title of tasks', () => {
-    const tasks = [
-        { id: 100, title: '운동가기' },
-    ];
+  const tasks = [
+    { id: 100, title: '운동가기' },
+  ];
 
-    const { container } = render(<List tasks={tasks} />);
+  const { container } = render(<List tasks={tasks} />);
 
-    expect(container).toHaveTextContent('운동가기');
-    expect(container).toHaveTextContent('완료');
+  expect(container).toHaveTextContent('운동가기');
+  expect(container).toHaveTextContent('완료');
 });
 
 it('when tasks are empty -> show specific content', () => {
-    const tasks = [];
+  const tasks = [];
 
-    const { container } = render(<List
-        tasks={tasks}
-    />);
+  const { container } = render(<List
+    tasks={tasks}
+  />);
 
-    expect(container).toHaveTextContent('할 일이 없어요!');
+  expect(container).toHaveTextContent('할 일이 없어요!');
 });
