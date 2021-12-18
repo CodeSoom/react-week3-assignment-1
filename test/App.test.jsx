ea95bc1fb0c4b8338336522246a6833d688b8e32
PAPICO <ocipap0531@gmail.com>
2021-12-18 19:07:23 +0900

    App test 리뷰 반영

--- test/App.test.jsx
+++ test/App.test.jsx
@@ -1,21 +1,48 @@
 import { fireEvent, render } from '@testing-library/react';
 import App from '../src/App';
 
-test('App', () => {
-  const { container, getByRole } = render(<App />);
+describe('App', () => {
+  const renderComponent = () => {
+    const { container, getByRole } = render(<App />);
+    const input = getByRole('textbox', { name: /할 일/i });
+    return { container, input, getByRole };
+  };
 
-  expect(container).not.toBe(null);
-  expect(container).toHaveTextContent('할 일이 없어요!');
+  it('App 컴포넌트가 렌더링 된다.', () => {
+    const { container } = renderComponent();
 
-  const input = getByRole('textbox', { name: /할 일/i });
+    expect(container).not.toBe(null);
+    expect(container).toHaveTextContent('할 일이 없어요!');
+  });
 
-  fireEvent.change(input, { target: { value: 'Study' } });
-  expect(input).toHaveValue('Study');
+  it('input 에 입력 시 입력값이 input 에 노출된다.', () => {
+    const { input } = renderComponent();
 
-  fireEvent.click(getByRole('button', { name: /추가/i }));
-  expect(input).toHaveValue('');
-  expect(container).toHaveTextContent('Study');
+    fireEvent.change(input, { target: { value: 'Study' } });
+    expect(input).toHaveValue('Study');
+  });
 
-  fireEvent.click(getByRole('button', { name: /완료/i }));
-  expect(container).toHaveTextContent('할 일이 없어요!');
+  context('input 에 "Study" 입력 후, 추가 버튼을 클릭할때', () => {
+    it('input 의 입력 값이 초기화된다.', () => {
+      const { getByRole, input } = renderComponent();
+      fireEvent.change(input, { target: { value: 'Study' } });
+      fireEvent.click(getByRole('button', { name: /추가/i }));
+      expect(input).toHaveValue('');
+    });
+
+    it('입력 값이 화면에 렌더링 된다.', () => {
+      const { container, getByRole, input } = renderComponent();
+      fireEvent.change(input, { target: { value: 'Study' } });
+      fireEvent.click(getByRole('button', { name: /추가/i }));
+      expect(container).toHaveTextContent('Study');
+    });
+  });
+
+  it('리스트 아이템의 완료를 클릭할 때, 해당 리스트 아이템이 삭제된다.', () => {
+    const { container, getByRole, input } = renderComponent();
+    fireEvent.change(input, { target: { value: 'Study' } });
+    fireEvent.click(getByRole('button', { name: /추가/i }));
+    fireEvent.click(getByRole('button', { name: /완료/i }));
+    expect(container).toHaveTextContent('할 일이 없어요!');
+  });
 });
--- test/Input.test.jsx
+++ test/Input.test.jsx
@@ -2,9 +2,14 @@ import { fireEvent, render } from '@testing-library/react';
 import Input from '../src/Input';
 
 describe('Input', () => {
-  test('초기 렌더링 시 "할 일" 과 "추가", value props 가 노출된다.', () => {
+  it('초기 렌더링 시 "할 일" 과 "추가", value props 가 노출된다.', () => {
     const handleChange = jest.fn();
-    const { container, getByRole } = render(<Input value="hello" onChange={handleChange} />);
+    const { container, getByRole } = render(
+      <Input
+        value="hello"
+        onChange={handleChange}
+      />,
+    );
     const input = getByRole('textbox', { name: /할 일/i });
 
     expect(input).toHaveValue('hello');
@@ -12,7 +17,7 @@ describe('Input', () => {
     expect(container).toHaveTextContent('추가');
   });
 
-  test('입력창에 텍스트를 입력할 수 있다.', () => {
+  it('입력창에 텍스트를 입력할 수 있다.', () => {
     const handleChange = jest.fn();
     const { getByRole } = render(<Input onChange={handleChange} />);
     const input = getByRole('textbox', { name: /할 일/i });
@@ -23,7 +28,7 @@ describe('Input', () => {
     expect(handleChange).toBeCalled();
   });
 
-  test('추가 버튼을 클릭 할 수 있다.', () => {
+  it('추가 버튼을 클릭 할 수 있다.', () => {
     const handleClick = jest.fn();
     const { getByRole } = render(<Input onClick={handleClick} />);
     fireEvent.click(getByRole('button', { name: /추가/i }));
