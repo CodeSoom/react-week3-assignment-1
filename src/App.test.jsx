84ce9a11db36d474aa8534ba951e2c53b43d0d2b
jeongseop.lee <sjl0614@naver.com>
2021-12-18 19:05:32 +0900

    :recycle: App mock 제거

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -9,10 +9,6 @@ describe('App', () => {
     ))
   );
 
-  const handleChangeTitle = jest.fn();
-  const handleClickAddTask = jest.fn();
-  const handleClickDeleteTask = jest.fn();
-
   it('renders initial app', () => {
     const { container } = renderApp();
 
@@ -23,32 +19,22 @@ describe('App', () => {
   it('litens input change event', () => {
     const { getByPlaceholderText } = renderApp();
     const input = getByPlaceholderText('할 일을 입력해 주세요');
-    input.addEventListener('change', handleChangeTitle);
 
     fireEvent.change(input, { target: { value: '코드숨' } });
 
-    expect(handleChangeTitle).toBeCalled();
     expect(input).toHaveAttribute('value', '코드숨');
   });
 
   it('litens "추가" and "완료" button click event', () => {
     const { container, getByText, getByPlaceholderText } = renderApp();
-    const addButton = getByText('추가');
-    const input = getByPlaceholderText('할 일을 입력해 주세요');
-    addButton.addEventListener('click', handleClickAddTask);
 
-    fireEvent.change(input, { target: { value: '코드숨' } });
-    fireEvent.click(addButton);
+    fireEvent.change(getByPlaceholderText('할 일을 입력해 주세요'), { target: { value: '코드숨' } });
+    fireEvent.click(getByText('추가'));
 
-    expect(handleClickAddTask).toBeCalled();
     expect(container).toHaveTextContent('코드숨');
 
-    const deleteButton = getByText('완료');
-    deleteButton.addEventListener('click', handleClickDeleteTask);
+    fireEvent.click(getByText('완료'));
 
-    fireEvent.click(deleteButton);
-
-    expect(handleClickDeleteTask).toBeCalled();
     expect(container).not.toHaveTextContent('코드숨');
     expect(container).toHaveTextContent('할 일이 없어요!');
   });
