8a1caf3aea02e4ca38438b1c9b99760ba04d0dbb
wallababa <ansrlansrl@naver.com>
2021-12-18 23:18:32 +0900

    List 관련 테스트 수정
    1. 테스트 메세지 수정
    2. fixture 분리
    3. 리스트 아이템 개별 테스트 추가

--- src/components/List/List.test.jsx
+++ src/components/List/List.test.jsx
@@ -1,31 +1,29 @@
 import { render } from '@testing-library/react';
 import List from './index';
 import { EMPTY_MESSAGE } from '../../constants';
+import { tasks } from './fixtures';
 
 describe('List', () => {
-  it(`tasks의 length가 0이면 "${EMPTY_MESSAGE}"를 보여줍니다.`, () => {
+  it('task가 비어있다면 관련 메세지를 보여줍니다.', () => {
     const sut = render(<List tasks={[]} />);
-    const $emptyElement = sut.queryByText(EMPTY_MESSAGE);
+    const emptyElement = sut.queryByText(EMPTY_MESSAGE);
 
-    expect($emptyElement).not.toBeNull();
+    expect(emptyElement).not.toBeNull();
   });
 
-  it(`tasks의 length가 0이 아니면 "${EMPTY_MESSAGE}"를 보여주지 않습니다.`, () => {
-    const sut = render(<List tasks={[{ id: 1, title: 'foo' }]} />);
-    const $emptyElement = sut.queryByText(EMPTY_MESSAGE);
+  it('task가 비어있지 않다면 빈 메세지를 보여주지 않습니다.', () => {
+    const sut = render(<List tasks={tasks.slice(0, 1)} />);
+    const emptyElement = sut.queryByText(EMPTY_MESSAGE);
 
-    expect($emptyElement).toBeNull();
+    expect(emptyElement).toBeNull();
   });
 
-  it('비어있지 않은 tasks를 전달하면 tasks를 리스트로 보여줍니다.', () => {
-    const tasks = [
-      { id: 1, title: 'foo' },
-      { id: 2, title: 'bar' },
-    ];
+  describe('비어있지 않은 tasks를 전달하면 tasks를 리스트로 보여줍니다.', () => {
     const sut = render(<List tasks={tasks} />);
-    const $list = sut.queryByRole('list');
+    const list = sut.queryByRole('list');
 
-    expect($list).not.toBeNull();
-    expect($list.children).toHaveLength(tasks.length);
+    it.each(tasks)('%o', ({ title }) => {
+      expect(list).toHaveTextContent(title);
+    });
   });
 });
---
+++ src/components/List/fixtures.js
@@ -0,0 +1,4 @@
+export const tasks = [
+  { id: 1, title: 'foo' },
+  { id: 2, title: 'bar' },
+];
