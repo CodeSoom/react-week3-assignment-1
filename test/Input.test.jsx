a7353db75d5afef4c2358bc859c4c652ca34a0c8
PAPICO <ocipap0531@gmail.com>
2021-12-18 19:16:32 +0900

    Input test 리뷰 반영

--- test/Input.test.jsx
+++ test/Input.test.jsx
@@ -2,36 +2,49 @@ import { fireEvent, render } from '@testing-library/react';
 import Input from '../src/Input';
 
 describe('Input', () => {
-  it('초기 렌더링 시 "할 일" 과 "추가", value props 가 노출된다.', () => {
-    const handleChange = jest.fn();
+  const handleChange = jest.fn();
+  const handleClick = jest.fn();
+
+  const renderComponent = (value = '') => {
     const { container, getByRole } = render(
       <Input
-        value="hello"
+        value={value}
         onChange={handleChange}
+        onClick={handleClick}
       />,
     );
     const input = getByRole('textbox', { name: /할 일/i });
+    const addButton = getByRole('button', { name: /추가/i });
 
+    return {
+      container, getByRole, input, addButton,
+    };
+  };
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('초기 렌더링될 때, "할 일" 과 "추가", value props 가 노출된다.', () => {
+    const { container, input } = renderComponent('hello');
+
+    expect(container).not.toBe(null);
     expect(input).toHaveValue('hello');
     expect(container).toHaveTextContent('할 일');
     expect(container).toHaveTextContent('추가');
   });
 
-  it('입력창에 텍스트를 입력할 수 있다.', () => {
-    const handleChange = jest.fn();
-    const { getByRole } = render(<Input onChange={handleChange} />);
-    const input = getByRole('textbox', { name: /할 일/i });
+  it('input 에 입력할 때, onChange 함수가 실행된다.', () => {
+    const { input } = renderComponent();
 
     fireEvent.change(input, { target: { value: 'hello' } });
-
-    expect(input).toHaveValue('hello');
     expect(handleChange).toBeCalled();
   });
 
-  it('추가 버튼을 클릭 할 수 있다.', () => {
-    const handleClick = jest.fn();
-    const { getByRole } = render(<Input onClick={handleClick} />);
-    fireEvent.click(getByRole('button', { name: /추가/i }));
+  it('추가 버튼을 클릭할 때, onClick 함수가 실행된다..', () => {
+    const { addButton } = renderComponent();
+
+    fireEvent.click(addButton);
 
     expect(handleClick).toBeCalled();
   });
