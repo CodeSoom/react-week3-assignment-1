ccf9e63c65453d29e1b45e89a0e979c5e47260fc
CheolWoong Choi <bluesky4381@naver.com>
2021-12-18 17:37:36 +0900

    Page 테스트 코드에 이벤트 핸들러 테스트 코드 추가

--- src/Page.test.jsx
+++ src/Page.test.jsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import Page from './Page';
 
@@ -8,7 +8,7 @@ describe('Page', () => {
     onChangeTitle: jest.fn(),
     onClickAddTask: jest.fn(),
     tasks: [],
-    onClickDeleteTask: jest.fn(),
+    onClickDelete: jest.fn(),
   };
 
   const renderPage = (newState) => {
@@ -28,6 +28,11 @@ describe('Page', () => {
     );
   };
 
+  beforeEach(() => {
+    delete state.taskTitle;
+    state.tasks = [];
+  });
+
   it('taskTitle 값이 제대로 전달되어야 한다.', () => {
     const value = '전달값';
     state.taskTitle = value;
@@ -42,5 +47,35 @@ describe('Page', () => {
     const { queryByText } = renderPage(state);
 
     expect(queryByText(task.title)).not.toBe(null);
+  });
+
+  it('onChangeTitle 이벤트 핸들러 함수가 정상적으로 작동해야 한다.', () => {
+    const { queryByDisplayValue } = renderPage(state);
+    const inputElement = queryByDisplayValue('');
+
+    fireEvent.change(inputElement, { target: { value: 'test' } });
+
+    expect(state.onChangeTitle).toBeCalled();
+  });
+
+  it('onClickAddTask 이벤트 핸들러 함수가 정상적으로 작동해야 한다.', () => {
+    const { queryByText } = renderPage(state);
+    const buttonElement = queryByText('추가');
+
+    fireEvent.click(buttonElement);
+
+    expect(state.onClickAddTask).toBeCalled();
+  });
+
+  it('onClickDelete 이벤트 핸들러 함수가 정상적으로 작동해야 한다.', () => {
+    const task = { id: 1, title: '타이틀' };
+    state.tasks = [task];
+
+    const { queryByText } = renderPage(state);
+    const buttonElement = queryByText('완료');
+
+    fireEvent.click(buttonElement);
+
+    expect(state.onClickDelete).toBeCalled();
   });
 });
