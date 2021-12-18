4ce80da3029ce8593eb481c815798a7a1f21c1bf
jnny1017 <jnny1017@gmail.com>
2021-12-18 13:17:22 +0900

    App테스트 케이스 수정

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -3,22 +3,25 @@ import React from 'react';
 import { render } from '@testing-library/react';
 
 import App from './App';
-import { tasksFixture } from './Fixture';
 
 describe('App Component', () => {
   const { container } = render((
     <App />
   ));
 
-  it('task타이틀을 보여준다', () => {
+  it('입력한 input값에 따른 task타이틀을 보여준다', () => {
     // TODO
   });
 
-  it('task추가를 한다', () => {
+  it('할 일을 입력하고 추가를 누르면 할 일 목록에 추가한 목록이 보인다.', () => {
     // TODO
   });
 
-  it('task삭제를 한다', () => {
+  it('할 일을 입력하고 추가를 누르면 input의 텍스트가 지워진다.', () => {
+    // TODO
+  });
+
+  it('할 일을 완료하면 할 일이 목록에서 보이지 않는다.', () => {
     // TODO
   });
 });
