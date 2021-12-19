2842694cfbf18f85354830f7d19f78c5d081ff0d
PAPICO <ocipap0531@gmail.com>
2021-12-19 15:31:49 +0900

    renderApp 으로 변경하고 context 지우기

--- test/App.test.jsx
+++ test/App.test.jsx
@@ -3,44 +3,38 @@ import { fireEvent, render } from '@testing-library/react';
 import App from '../src/App';
 
 describe('App', () => {
-  const renderComponent = () => {
+  const renderApp = () => {
     const { container, getByRole } = render(<App />);
     const input = getByRole('textbox', { name: /할 일/i });
+
     return { container, input, getByRole };
   };
 
   it('App 컴포넌트가 렌더링 된다.', () => {
-    const { container } = renderComponent();
+    const { container } = renderApp();
 
     expect(container).not.toBe(null);
     expect(container).toHaveTextContent('할 일이 없어요!');
   });
 
   it('input 에 입력 시 입력값이 input 에 노출된다.', () => {
-    const { input } = renderComponent();
+    const { input } = renderApp();
 
     fireEvent.change(input, { target: { value: 'Study' } });
     expect(input).toHaveValue('Study');
   });
 
-  context('input 에 "Study" 입력 후, 추가 버튼을 클릭할때', () => {
-    it('input 의 입력 값이 초기화된다.', () => {
-      const { getByRole, input } = renderComponent();
-      fireEvent.change(input, { target: { value: 'Study' } });
-      fireEvent.click(getByRole('button', { name: /추가/i }));
-      expect(input).toHaveValue('');
-    });
+  it('할 일을 추가할때, 입력 값이 화면에 렌더링 된다.', () => {
+    const { container, getByRole, input } = renderApp();
 
-    it('입력 값이 화면에 렌더링 된다.', () => {
-      const { container, getByRole, input } = renderComponent();
-      fireEvent.change(input, { target: { value: 'Study' } });
-      fireEvent.click(getByRole('button', { name: /추가/i }));
-      expect(container).toHaveTextContent('Study');
-    });
+    fireEvent.change(input, { target: { value: 'Study' } });
+    fireEvent.click(getByRole('button', { name: /추가/i }));
+    expect(input).toHaveValue('');
+    expect(container).toHaveTextContent('Study');
   });
 
   it('리스트 아이템의 완료를 클릭할 때, 해당 리스트 아이템이 삭제된다.', () => {
-    const { container, getByRole, input } = renderComponent();
+    const { container, getByRole, input } = renderApp();
     fireEvent.change(input, { target: { value: 'Study' } });
     fireEvent.click(getByRole('button', { name: /추가/i }));
     fireEvent.click(getByRole('button', { name: /완료/i }));
