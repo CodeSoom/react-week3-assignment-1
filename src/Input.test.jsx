af8f61c7d6ec956890c2343d8b95e286f3425cab
Beomyeon <beomyeon.kim@adriel.ai>
2021-12-19 17:36:55 +0900

    add input test based on lecture

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -6,22 +6,32 @@ describe('Input', () => {
   const handleChange = jest.fn();
   const handleClick = jest.fn();
 
-  const renderInput = () => render((
-    <Input
-      onChange={handleChange}
-      onClick={handleClick}
-    />
-  ));
+  function renderInput(value) {
+    return render((
+      <Input
+        value={value}
+        onChange={handleChange}
+        onClick={handleClick}
+      />
+    ));
+  };
+
+  it('display input value', () => {
+    const { getByDisplayValue } = renderInput('기존 할 일');
+
+    expect(getByDisplayValue('기존 할 일')).not.toBeNull();
+  })
 
   it('change text with input', () => {
     const { getByLabelText } = renderInput();
 
     const textInput = getByLabelText('할 일');
 
-    expect(handleChange).not.toBeCalled();
+    fireEvent.change(textInput, {
+      target: { value: '코드숨 과제하기' },
+    });
 
-    fireEvent.change(textInput, { target: { value: '코드숨 과제하기' } });
-
+    expect(handleChange).toBeCalled();
     expect(textInput.value).toBe('코드숨 과제하기');
   })
 
@@ -29,8 +39,6 @@ describe('Input', () => {
     const { getByText } = renderInput();
 
     const button = getByText('추가');
-
-    expect(handleClick).not.toBeCalled();
 
     fireEvent.click(button);
 
