780dab7cf45b07e0e07ecdc1864a343b799bfe40
free-ko <duddnr654@naver.com>
2022-04-05 20:21:35 +0900

    [3-1] Fix: input 컴포넌트 input 태그, button 태그 랜더링 테스트 코드 작성

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -1,40 +1,17 @@
 import React from 'react';
-import { fireEvent, render, getByRole } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 import Input from './Input';
 
 describe('Input', () => {
   const onClick = jest.fn();
   const onChange = jest.fn();
+  const container = render(<Input onChange={onChange} onClick={onClick} />);
+  const input = container.getByLabelText('할 일');
+  const button = container.getByText('추가');
 
-  it('Renders label, input, button', () => {
-    const { getByText, getByPlaceholderText } = render(<Input />);
-    getByText('할 일');
-    getByPlaceholderText('할 일을 입력해 주세요');
-    getByText('추가');
-  });
-
-  it('Changes input value', () => {
-    const { container } = render(<Input onChange={onChange} />);
-    const input = getByRole(container, 'textbox');
-    expect(onChange).not.toHaveBeenCalled();
-    fireEvent.change(input, {
-      target: { value: 'TDD 배우기' },
-    });
-    expect(onChange).toHaveBeenCalled();
-    expect(input).toHaveValue('TDD 배우기');
-  });
-
-  it('Calls onClick and clears input value', () => {
-    const { container } = render(
-      <Input onChange={onChange} onClick={onClick} />,
-    );
-    const button = getByRole(container, 'button');
-    expect(onClick).not.toHaveBeenCalled();
-    fireEvent.click(button);
-
-    expect(onClick).toHaveBeenCalled();
-    const input = getByRole(container, 'textbox');
-    expect(input).toHaveValue('');
+  it('renders input, button', () => {
+    expect(input).toBeInTheDocument();
+    expect(button).toBeInTheDocument();
   });
 });
