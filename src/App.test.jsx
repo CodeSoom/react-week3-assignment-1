8a79fdd93425811541cc0cf2630c708dec75b05f
yujo <yujo.dev@gmail.com>
2022-04-05 23:19:29 +0900

    test: App 테스트 내용 추가

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,4 +1,4 @@
-import { screen, render } from '@testing-library/react';
+import { screen, render, fireEvent } from '@testing-library/react';
 
 import App from './App';
 
@@ -11,5 +11,27 @@ describe('App Test', () => {
     expect(screen.getByText('To-do')).toBeInTheDocument();
     expect(screen.getByRole('textbox')).toBeInTheDocument();
     expect(screen.getByText('추가')).toBeInTheDocument();
+  });
+
+  test('Input에 입력한 값을 새로운 할 일로 추가할 수 있다.', () => {
+    render(
+      <App />,
+    );
+
+    fireEvent.change(screen.getByRole('textbox'), { target: { value: '뭐라도 하기' } });
+    fireEvent.click(screen.getByText('추가'));
+    expect(screen.getByText('뭐라도 하기')).toBeInTheDocument();
+  });
+
+  test('목록에 존재하는 할 일을 제거할 수 있다.', () => {
+    render(
+      <App />,
+    );
+
+    fireEvent.change(screen.getByRole('textbox'), { target: { value: '뭐라도 하기' } });
+    fireEvent.click(screen.getByText('추가'));
+    expect(screen.getByText('뭐라도 하기')).toBeInTheDocument();
+    fireEvent.click(screen.getByText('완료'));
+    expect(screen.queryByText('뭐라도 하기')).not.toBeInTheDocument();
   });
 });
