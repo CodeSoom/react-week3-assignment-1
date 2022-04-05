61742813dba325175422b0f54709bc6099dd684a
Jiwoo Choi <heyjiwoo@annotation.ai.com>
2022-04-05 21:20:40 +0900

    Refactor App test
    Refactor App test.

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,22 +2,44 @@ import { render, fireEvent } from '@testing-library/react';
 
 import App from './App';
 
-test('App', () => {
-  const { container, getByText, getByRole } = render((
-    <App />
-  ));
+describe('App', () => {
+  it('renders header, add button, and message `할 일이 없어요!`', () => {
+    const { container } = render((
+      <App />
+    ));
 
-  expect(container).toHaveTextContent('To-do');
-  expect(container).toHaveTextContent('추가');
-  expect(container).toHaveTextContent('할 일이 없어요!');
+    expect(container).toHaveTextContent('To-do');
+    expect(container).toHaveTextContent('추가');
+    expect(container).toHaveTextContent('할 일이 없어요!');
+  });
 
-  fireEvent.change(getByRole('textbox'), { target: { value: '신나게 놀기' } });
-  fireEvent.click(getByText('추가'));
+  context('when the task is added', () => {
+    it('renders the task', () => {
+      const { container, getByText, getByRole } = render((
+        <App />
+      ));
 
-  expect(container).toHaveTextContent('신나게 놀기');
-  expect(container).toHaveTextContent('완료');
+      fireEvent.change(getByRole('textbox'), { target: { value: '신나게 놀기' } });
+      fireEvent.click(getByText('추가'));
 
-  fireEvent.click(getByText('완료'));
+      expect(container).toHaveTextContent('신나게 놀기');
+    });
 
-  expect(container).not.toHaveTextContent('신나게 놀기');
+    context('when the done button is clicked', () => {
+      it('removes the task', () => {
+        const { container, getByText, getByRole } = render((
+          <App />
+        ));
+
+        fireEvent.change(getByRole('textbox'), { target: { value: '신나게 놀기' } });
+        fireEvent.click(getByText('추가'));
+
+        expect(container).toHaveTextContent('신나게 놀기');
+
+        fireEvent.click(getByText('완료'));
+
+        expect(container).not.toHaveTextContent('신나게 놀기');
+      });
+    });
+  });
 });
