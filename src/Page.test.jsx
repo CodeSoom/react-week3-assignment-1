5002a3e6fa5fd788e23b90333c3d083b984ef296
free-ko <duddnr654@naver.com>
2022-04-06 20:50:03 +0900

    [3-1] Add: Page 컴포넌트 테스트 코드 추가

---
+++ src/Page.test.jsx
@@ -0,0 +1,34 @@
+import { render } from '@testing-library/react';
+
+import Page from './Page';
+
+const tasks = [
+  { id: 100, title: '운동 하기' },
+  { id: 101, title: '공부 하기' },
+];
+
+const handleChangeTaskTitle = jest.fn();
+const handleClickAddTask = jest.fn();
+const handleClickDeleteTask = jest.fn();
+
+const getRenderPage = () => render((
+  <Page
+    tasks={tasks}
+    taskTitle="tasks"
+    onChangeTitle={handleChangeTaskTitle}
+    onClickAddTask={handleClickAddTask}
+    onClickDeleteTask={handleClickDeleteTask}
+  />
+));
+
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
+describe('Page', () => {
+  it('renders title', () => {
+    const { container } = getRenderPage();
+
+    expect(container).toHaveTextContent('To-do');
+  });
+});
