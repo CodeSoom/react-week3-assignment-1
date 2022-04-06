7f0895d110c5a4bde7bc9e9ee44856dfebb50ff8
yujo <yujo.dev@gmail.com>
2022-04-06 21:14:22 +0900

    test: Input 컴포넌트 테스트 불필요한 코드 제거

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-import { fireEvent, render } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 
 import Input from './Input';
 
@@ -16,29 +16,25 @@ const MockInput = () => {
   );
 };
 
-const setUp = () => {
-  const utils = render(<MockInput />);
-  const input = utils.getByRole('textbox');
-
-  return {
-    input, utils,
-  };
-};
-
 describe('Input Test', () => {
+  beforeEach(() => {
+    render(<MockInput />);
+  });
+
   context('Input 컴포넌트 기능 테스트', () => {
     it('Input에 값을 입력할 수 있어야 한다.', () => {
-      const { input } = setUp();
+      const input = screen.getByRole('textbox');
 
       fireEvent.change(input, { target: { value: '뭐라도 하기' } });
       expect(input.value).toBe('뭐라도 하기');
     });
 
     it('추가 버튼을 눌렀을 때 Input의 입력창이 초기화 되어야 한다.', () => {
-      const { input, utils } = setUp();
+      const input = screen.getByRole('textbox');
+      const button = screen.getByText('추가');
 
       fireEvent.change(input, { target: { value: '뭐라도 하기' } });
-      fireEvent.click(utils.getByText('추가'));
+      fireEvent.click(button);
       expect(input.value).toBe('');
     });
   });
