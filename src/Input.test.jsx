e4e29e0ceb341bd8d34080423832524a0a971430
0hu <dladudgn123@naver.com>
2022-04-06 19:16:49 +0900

    Update Input.test.jsx

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -1,8 +1,8 @@
 import {
-  render, screen, fireEvent,
+  render, fireEvent,
 } from '@testing-library/react';
 
-import '@testing-library/jest-dom';
+import { act } from 'react-dom/test-utils';
 
 import Input from './Input';
 
@@ -10,46 +10,59 @@ const handleChangeTitle = jest.fn();
 const handleClickAddTask = jest.fn();
 
 describe('AddInput', () => {
-  it('should render input element', async () => {
-    render(
+  it('render input element', async () => {
+    const { getByPlaceholderText } = render(
       <Input
         value=""
         onChange={handleChangeTitle}
-        // onClick={handleClickAddTask}
+        onClick={handleClickAddTask}
       />,
     );
-    const inputElement = screen.getByPlaceholderText(/할 일을 입력해 주세요/i);
+
+    const inputElement = getByPlaceholderText(/할 일을 입력해 주세요/i);
     expect(inputElement).toBeInTheDocument();
   });
-  it('should be able to type in input', async () => {
-    render(
+
+  it('should be able to change in input', async () => {
+    const { container, getByPlaceholderText } = render(
       <Input
         value=""
         onChange={handleChangeTitle}
+        onClick={handleClickAddTask}
       />,
     );
-    const inputElement = screen.getByPlaceholderText(/할 일을 입력해 주세요/i);
+
+    const inputElement = getByPlaceholderText(/할 일을 입력해 주세요/i);
     fireEvent.change(inputElement, { target: { value: '코딩을 즐기기' } });
-    expect(inputElement.value).toBe('코딩을 즐기기');
+    // expect(inputElement.value).toEqual('코딩을 즐기기');
+    // TODO : 코딩을 즐기기로 값을 바꿔주기.
   });
-});
 
-test('추가 버튼을 클릭하면 input이 비어진다.', () => {
-  const { getByText } = render(
-    <Input
-      value=""
-      onChange={handleChangeTitle}
-      onClick={handleClickAddTask}
-    />,
-  );
-  fireEvent.click(getByText('추가'));
-  expect(Input.value).toHaveTextContent('');
-});
+  it('추가 버튼을 클릭하면 input이 비어진다. ', async () => {
+    const { container, getByText, getByPlaceholderText } = render(
+      <Input
+        value=""
+        onChange={handleChangeTitle}
+        onClick={handleClickAddTask}
+      />,
+    );
 
-test('input', () => {
-  const { container } = render((
-    <Input />
-  ));
+    await act(async () => {
+      expect(container).toHaveTextContent('추가');
+    });
 
-  expect(container).toHaveTextContent('할 일');
+    await act(async () => {
+      fireEvent.click(getByText('추가'));
+      expect(handleChangeTitle).toBeCalled();
+      expect(getByPlaceholderText(/할 일을 입력해 주세요/i)).toEqual('');
+    });
+  });
+
+  it('input의 왼쪽 글의 이름은 할 일 이다.', async () => {
+    const { container } = render((
+      <Input />
+    ));
+
+    expect(container).toHaveTextContent('할 일');
+  });
 });
