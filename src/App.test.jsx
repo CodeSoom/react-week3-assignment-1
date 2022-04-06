61530cbd8cab72cf6058781ff2c46883fea962c8
hdpyo <hudongpyo.dev@gmail.com>
2022-04-06 15:50:59 +0900

    test: App.jsx 에 대한 테스트 코드를 추가했다.
    - 초기 화면에는 할 일이 없어요! 라는 메시지가 보여야 한다.
    - 추가 버튼을 클릭할 수 있다.

---
+++ src/App.test.jsx
@@ -0,0 +1,15 @@
+import { render, fireEvent } from '@testing-library/react';
+
+import App from './App';
+
+describe('App Test', () => {
+  test('초기 화면에는 할 일이 없어요! 라는 메시지가 보여야 한다.', () => {
+    const { getByText } = render((<App />));
+    expect(getByText('할 일이 없어요!')).toBeVisible();
+  });
+
+  test('추가 버튼을 클릭할 수 있다.', () => {
+    const { getByText } = render((<App />));
+    fireEvent.click(getByText('추가'));
+  });
+});
