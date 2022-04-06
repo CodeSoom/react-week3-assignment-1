0114d83eff744aa6bbfa2c65558a8c0a3bd1409b
yujo <yujo.dev@gmail.com>
2022-04-06 21:10:21 +0900

    test: Input 컴포넌트 동작 위주로 테스트 하도록 변경

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -1,17 +1,45 @@
+import { useState } from 'react';
+
 import { fireEvent, render } from '@testing-library/react';
 
 import Input from './Input';
 
+const MockInput = () => {
+  const [value, setValue] = useState('');
+
+  return (
+    <Input
+      value={value}
+      onChange={({ target }) => setValue(target.value)}
+      onClick={() => setValue('')}
+    />
+  );
+};
+
+const setUp = () => {
+  const utils = render(<MockInput />);
+  const input = utils.getByRole('textbox');
+
+  return {
+    input, utils,
+  };
+};
+
 describe('Input Test', () => {
-  test('추가 버튼을 눌렀을 떄 handleClick 함수가 실행되어야 한다.', () => {
-    const handleClick = jest.fn();
+  context('Input 컴포넌트 기능 테스트', () => {
+    it('Input에 값을 입력할 수 있어야 한다.', () => {
+      const { input } = setUp();
 
-    const { getByText } = render(
-      <Input onClick={handleClick} />,
-    );
+      fireEvent.change(input, { target: { value: '뭐라도 하기' } });
+      expect(input.value).toBe('뭐라도 하기');
+    });
 
-    expect(handleClick).not.toBeCalled();
-    fireEvent.click(getByText('추가'));
-    expect(handleClick).toBeCalled();
+    it('추가 버튼을 눌렀을 때 Input의 입력창이 초기화 되어야 한다.', () => {
+      const { input, utils } = setUp();
+
+      fireEvent.change(input, { target: { value: '뭐라도 하기' } });
+      fireEvent.click(utils.getByText('추가'));
+      expect(input.value).toBe('');
+    });
   });
 });
