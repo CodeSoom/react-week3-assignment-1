2434bdb3d01c466cfb368d1c7af1216f7d511391
yujo <yujo.dev@gmail.com>
2022-04-06 21:29:16 +0900

    test: Page 컴포넌트 테스트 리팩터링

--- src/Page.test.jsx
+++ src/Page.test.jsx
@@ -3,15 +3,13 @@ import { screen, render } from '@testing-library/react';
 import Page from './Page';
 
 describe('Page Test', () => {
-  test('Page를 렌더한다.', () => {
+  it('Page 컴포넌트 렌더 테스트.', () => {
     const tasks = [{
       id: 1,
       title: '뭐라도 하기',
     }];
 
-    render(
-      <Page tasks={tasks} />,
-    );
+    render(<Page tasks={tasks} />);
 
     expect(screen.getByText('To-do')).toBeInTheDocument();
     expect(screen.getByRole('textbox')).toBeInTheDocument();
