7cacd87d3b874fd99a00cb492fe1368d9138fe5a
이찬호 <0407chan@naver.com>
2022-04-07 23:31:44 +0900

    test. List.test추가

---
+++ src/List.test.jsx
@@ -0,0 +1,77 @@
+import { fireEvent, render } from '@testing-library/react';
+import { useState } from 'react';
+import List from './List';
+
+function TestList({ tasks }) {
+  const [stateTasks, setStateTasks] = useState(tasks);
+
+  const handleClickDelete = (id) => {
+    setStateTasks(stateTasks.filter((item) => item.id !== id));
+  };
+
+  return (
+    <List
+      tasks={stateTasks}
+      onClickDelete={handleClickDelete}
+    />
+  );
+}
+
+const setup = () => {
+  const tasks = [
+    {
+      id: 1,
+      title: '배고파요',
+    },
+    {
+      id: 2,
+      title: '치킨을 먹어요',
+    },
+    {
+      id: 3,
+      title: '피자를 먹어요',
+    },
+  ];
+
+  const utils = render(<TestList tasks={tasks} />);
+  return {
+    tasks,
+    ...utils,
+  };
+};
+
+test('할일 리스트 출력', () => {
+  const { container } = setup();
+  expect(container).toHaveTextContent('배고파요');
+  expect(container).toHaveTextContent('치킨을 먹어요');
+  expect(container).toHaveTextContent('피자를 먹어요');
+});
+
+test("'완료' 버튼 출력", () => {
+  const { container } = setup();
+  expect(container).toHaveTextContent('완료');
+});
+
+test("'완료' 버튼 클릭 (배고파요 삭제, 피자를 먹어요 삭제)", () => {
+  const { container, getAllByText } = setup();
+
+  expect(container).toHaveTextContent('배고파요');
+  fireEvent.click(getAllByText('완료')[0]);
+  expect(container).not.toHaveTextContent('배고파요');
+
+  expect(container).toHaveTextContent('피자를 먹어요');
+  fireEvent.click(getAllByText('완료')[1]);
+  expect(container).not.toHaveTextContent('피자를 먹어요');
+
+  expect(container).toHaveTextContent('치킨을 먹어요');
+});
+
+test("빈 배열일 때 '할 일이 없어요!' 출력", () => {
+  const { container, getAllByText } = setup();
+
+  fireEvent.click(getAllByText('완료')[0]);
+  fireEvent.click(getAllByText('완료')[0]);
+  fireEvent.click(getAllByText('완료')[0]);
+
+  expect(container).toHaveTextContent('할 일이 없어요!');
+});
