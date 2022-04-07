caf9dd6853e6fbc494fe238fbbfa3aaca5b1b8c8
0hu <dladudgn123@naver.com>
2022-04-07 14:43:28 +0900

    Update Input.test

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -9,56 +9,53 @@ import Input from './Input';
 const handleChangeTitle = jest.fn();
 const handleClickAddTask = jest.fn();
 
-describe('AddInput', () => {
-  it('render input element', async () => {
-    const { getByPlaceholderText } = render(
+describe('Input', () => {
+  it('renders input element', () => {
+    const { getByPlaceholderText } = render((
       <Input
         value=""
         onChange={handleChangeTitle}
         onClick={handleClickAddTask}
-      />,
-    );
+      />
+    ));
 
     const inputElement = getByPlaceholderText(/할 일을 입력해 주세요/i);
     expect(inputElement).toBeInTheDocument();
   });
 
-  it('should be able to change in input', async () => {
-    const { container, getByPlaceholderText } = render(
+  it('should be able to change in input', () => {
+    const { container, getByPlaceholderText } = render((
       <Input
         value=""
         onChange={handleChangeTitle}
         onClick={handleClickAddTask}
-      />,
-    );
+      />
+    ));
 
     const inputElement = getByPlaceholderText(/할 일을 입력해 주세요/i);
+
     fireEvent.change(inputElement, { target: { value: '코딩을 즐기기' } });
-    // expect(inputElement.value).toEqual('코딩을 즐기기');
+
+    expect(inputElement.getByDisplayValue).toEqual('코딩을 즐기기');
     // TODO : 코딩을 즐기기로 값을 바꿔주기.
   });
 
-  it('추가 버튼을 클릭하면 input이 비어진다. ', async () => {
-    const { container, getByText, getByPlaceholderText } = render(
+  it('추가 버튼을 클릭하면 input이 비어진다. ', () => {
+    const { container, getByText, getByPlaceholderText } = render((
       <Input
         value=""
         onChange={handleChangeTitle}
         onClick={handleClickAddTask}
-      />,
-    );
+      />
+    ));
 
-    await act(async () => {
-      expect(container).toHaveTextContent('추가');
-    });
+    fireEvent.click(getByText('추가'));
 
-    await act(async () => {
-      fireEvent.click(getByText('추가'));
-      expect(handleChangeTitle).toBeCalled();
-      expect(getByPlaceholderText(/할 일을 입력해 주세요/i)).toEqual('');
-    });
+    expect(handleChangeTitle).toBeCalled();
+    expect(getByPlaceholderText(/할 일을 입력해 주세요/i)).toEqual('');
   });
 
-  it('input의 왼쪽 글의 이름은 할 일 이다.', async () => {
+  it('has 할 일 text.', () => {
     const { container } = render((
       <Input />
     ));
--- src/List.test.jsx
+++ src/List.test.jsx
@@ -4,28 +4,30 @@ import '@testing-library/jest-dom';
 
 import List from './List';
 
-test('if there is no task', () => {
-  const tasks = [];
-  const { container } = render((
-    <List
-      tasks={tasks}
-    />
-  ));
-  expect(container).toHaveTextContent('할 일이 없어요!');
-});
+describe('List', () => {
+  it('if there is no task', () => {
+    const tasks = [];
+    const { container } = render((
+      <List
+        tasks={tasks}
+      />
+    ));
+    expect(container).toHaveTextContent('할 일이 없어요!');
+  });
 
-test('if there is task', () => {
-  const tasks = [
-    {
-      id: 1,
-      title: '1',
-    },
-  ];
-  const { container } = render((
-    <List
-      tasks={tasks}
-    />
-  ));
-  expect(container).not.toHaveTextContent('할 일이 없어요!');
-  expect(container).toHaveTextContent('완료');
+  it('if there is task', () => {
+    const tasks = [
+      {
+        id: 1,
+        title: '1',
+      },
+    ];
+    const { container } = render((
+      <List
+        tasks={tasks}
+      />
+    ));
+    expect(container).not.toHaveTextContent('할 일이 없어요!');
+    expect(container).toHaveTextContent('완료');
+  });
 });
