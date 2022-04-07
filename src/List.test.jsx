3961e88c25abf87da0a2d7c52e279a6f4e44ccea
kjoobin <kjoobin>
2022-04-07 10:55:14 +0900

    Refactoring list test-code

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -6,42 +6,68 @@ import List from './List';
 
 const onClickDelete = jest.fn();
 
-describe('List', () => {
-  test('no tasks', () => {
-    const { container } = render((
-      <List
-        tasks={[]}
-        onClickDelete={onClickDelete}
-      />
-    ));
+const tasks = [
+  { id: 100, title: '밥먹기' },
+  { id: 101, title: '테스트 코드 짜기' },
+  { id: 102, title: 'PR 날리기' },
+];
 
-    expect(container).toHaveTextContent('할 일이 없어요!');
+const renderList = ({ tasks: tasksProps }) => render((
+  <List
+    tasks={tasksProps || tasks}
+    onClickDelete={onClickDelete}
+  />
+));
+
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
+describe('List', () => {
+  context('without task', () => {
+    it('renders not have the task', () => {
+      const { container } = renderList({ tasks: [] });
+
+      expect(container).toHaveTextContent('할 일이 없어요!');
+    });
   });
 
-  test('tasks', () => {
-    const tasks = [
-      { id: 100, title: '밥먹기' },
-      { id: 101, title: '테스트 코드 짜기' },
-      { id: 102, title: 'PR 날리기' },
-    ];
+  context('with tasks', () => {
+    it('renders tasks', () => {
+      const { container, getAllByText } = renderList({});
 
-    const { container } = render((
-      <List
-        tasks={tasks}
-        onClickDelete={onClickDelete}
-      />
-    ));
+      expect(container).toHaveTextContent('밥먹기');
+      expect(container).toHaveTextContent('테스트 코드 짜기');
+      expect(container).toHaveTextContent('PR 날리기');
 
-    tasks.forEach(({ id, title }) => {
-      expect(container).toHaveTextContent(title);
+      expect(getAllByText('완료')).toHaveLength(3);
+    });
 
-      const item = getByText(container, title);
+    it('should trigger onClickDelete function with the task id args', () => {
+      const { container } = renderList({});
 
-      const button = getByText(item, '완료');
+      const clickTaskButton = (taskTitle) => {
+        fireEvent.click(
+          getByText(getByText(container, taskTitle), '완료'),
+        );
+      };
 
-      fireEvent.click(button);
+      expect(onClickDelete).not.toBeCalled();
 
-      expect(onClickDelete).toBeCalledWith(id);
+      clickTaskButton('밥먹기');
+
+      expect(onClickDelete).toBeCalledTimes(1);
+      expect(onClickDelete).toBeCalledWith(100);
+
+      clickTaskButton('테스트 코드 짜기');
+
+      expect(onClickDelete).toBeCalledTimes(2);
+      expect(onClickDelete).toBeCalledWith(101);
+
+      clickTaskButton('PR 날리기');
+
+      expect(onClickDelete).toBeCalledTimes(3);
+      expect(onClickDelete).toBeCalledWith(102);
     });
   });
 });
