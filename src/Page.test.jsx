dc3e16e5d861fc7b2ee3b576a22d6fb81b9a33a0
free-ko <duddnr654@naver.com>
2022-04-07 12:42:41 +0900

    [3-1] Fix: Page 컴포넌트 renderPage 함수명 변경

--- src/Page.test.jsx
+++ src/Page.test.jsx
@@ -11,7 +11,7 @@ const handleChangeTaskTitle = jest.fn();
 const handleClickAddTask = jest.fn();
 const handleClickDeleteTask = jest.fn();
 
-const getRenderPage = ({ taskTitle, tasks }) => render((
+const renderPage = ({ taskTitle, tasks }) => render((
   <Page
     taskTitle={taskTitle}
     tasks={tasks}
@@ -27,13 +27,13 @@ beforeEach(() => {
 
 describe('Page', () => {
   it('renders title', () => {
-    const { container } = getRenderPage({ taskTitle: 'TDD공부하기', tasks: sampleTasks });
+    const { container } = renderPage({ taskTitle: 'TDD공부하기', tasks: sampleTasks });
 
     expect(container).toHaveTextContent('To-do');
   });
 
   it('renders tasks', () => {
-    const { container } = getRenderPage({ taskTitle: 'TDD공부하기', tasks: sampleTasks });
+    const { container } = renderPage({ taskTitle: 'TDD공부하기', tasks: sampleTasks });
 
     sampleTasks.forEach((task) => {
       expect(container).toHaveTextContent(task.title);
@@ -41,21 +41,21 @@ describe('Page', () => {
   });
 
   it('render without tasks', () => {
-    const { container } = getRenderPage({ taskTitle: 'TDD공부하기', tasks: [] });
+    const { container } = renderPage({ taskTitle: 'TDD공부하기', tasks: [] });
 
     expect(container).toHaveTextContent('To-do');
     expect(container).toHaveTextContent('할 일이 없어요!');
   });
 
   it('calls handleChangeTaskTitle when change input value', () => {
-    const { getByRole } = getRenderPage({ taskTitle: 'TDD공부하기', tasks: sampleTasks });
+    const { getByRole } = renderPage({ taskTitle: 'TDD공부하기', tasks: sampleTasks });
 
     fireEvent.change(getByRole('textbox'), { target: { value: '운동 하기' } });
     expect(handleChangeTaskTitle).toBeCalled();
   });
 
   it('calls handleClickAddTask when click button', () => {
-    const { getByText } = getRenderPage({ taskTitle: 'TDD공부하기', tasks: sampleTasks });
+    const { getByText } = renderPage({ taskTitle: 'TDD공부하기', tasks: sampleTasks });
     const button = getByText('추가');
 
     fireEvent.click(button);
@@ -63,7 +63,7 @@ describe('Page', () => {
   });
 
   it('calls handleClickDeleteTask when click complete button', () => {
-    const { container, getAllByText } = getRenderPage({ taskTitle: 'TDD공부하기', tasks: sampleTasks });
+    const { container, getAllByText } = renderPage({ taskTitle: 'TDD공부하기', tasks: sampleTasks });
 
     expect(container).toHaveTextContent('운동하기');
     expect(container).toHaveTextContent('공부하기');
