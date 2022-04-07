a8643e78e960d113a40bf7eb6b104868fce53178
이찬호 <0407chan@naver.com>
2022-04-08 00:39:55 +0900

    test. 추가 버튼 클릭시 input value 제거 테스트 추가

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -9,11 +9,16 @@ function TestInput({ onClick }) {
     setValue(newValue);
   };
 
+  const handleClick = () => {
+    setValue('');
+    onClick();
+  };
+
   return (
     <Input
       value={value}
       onChange={(event) => handleChange(event.target.value)}
-      onClick={onClick}
+      onClick={handleClick}
     />
   );
 }
@@ -48,6 +53,19 @@ test('input에 텍스트 입력 테스트', async () => {
   expect(input.value).toBe('할일이 없어');
 
   fireEvent.change(input, { target: { value: '' } });
+  expect(input.value).toBe('');
+});
+
+test('input에 값 입력 후 추가 클릭 시 input에 값 제거', () => {
+  const { input, handleClick, getByText } = setup();
+  expect(handleClick).not.toBeCalled();
+
+  fireEvent.change(input, { target: { value: '할일이 없어' } });
+  expect(input.value).toBe('할일이 없어');
+
+  fireEvent.click(getByText('추가'));
+  expect(handleClick).toBeCalled();
+
   expect(input.value).toBe('');
 });
 
