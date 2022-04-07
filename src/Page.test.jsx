c533d57381824e23e8071aba95740c5a4255a545
kjoobin <kjoobin>
2022-04-07 11:08:02 +0900

    Refactoring page test-code

--- src/Page.test.jsx
+++ src/Page.test.jsx
@@ -1,4 +1,4 @@
-import { render, fireEvent, getByText as getByTextWithContainer } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import Page from './Page';
 
 const onChangeTitle = jest.fn();
@@ -8,11 +8,9 @@ const onClickDeleteTask = jest.fn();
 
 const tasks = [
   { id: 100, title: '밥먹기' },
-  { id: 101, title: '테스트 코드 짜기' },
-  { id: 102, title: 'PR 날리기' },
 ];
 
-const customRender = () => render((
+const renderPage = () => render((
   <Page
     taskTitle="tasks"
     onChangeTitle={onChangeTitle}
@@ -27,55 +25,49 @@ beforeEach(() => {
 });
 
 describe('Page', () => {
-  it('render title', () => {
-    const { container } = customRender();
+  it('renders title', () => {
+    const { container } = renderPage();
 
     expect(container).toHaveTextContent('To-do');
   });
 
-  it('Input', () => {
-    const { getByPlaceholderText, getByText } = customRender();
-    expect(onChangeTitle).not.toHaveBeenCalled();
+  context('Input', () => {
+    it('renders textbox to change the task title', () => {
+      const { getByPlaceholderText } = renderPage();
+      expect(onChangeTitle).not.toHaveBeenCalled();
 
-    fireEvent.change(
-      getByPlaceholderText('할 일을 입력해 주세요'),
-      { target: { value: '할 일을 입력했습니다.' } },
-    );
+      fireEvent.change(
+        getByPlaceholderText('할 일을 입력해 주세요'),
+        { target: { value: '할 일을 입력했습니다.' } },
+      );
 
-    expect(onChangeTitle).toHaveBeenCalledWith('할 일을 입력했습니다.');
+      expect(onChangeTitle).toHaveBeenCalledWith('할 일을 입력했습니다.');
+    });
 
-    expect(onClickAddTask).not.toHaveBeenCalled();
+    it('renders "추가" button to add task', () => {
+      const { container, getByText } = renderPage();
 
-    fireEvent.click(getByText('추가'));
+      expect(container).toHaveTextContent('추가');
 
-    expect(onClickAddTask).toHaveBeenCalled();
+      expect(onClickAddTask).not.toHaveBeenCalled();
+
+      fireEvent.click(getByText('추가'));
+
+      expect(onClickAddTask).toHaveBeenCalled();
+    });
   });
 
-  it('List', () => {
-    const { container, getByText } = customRender();
+  context('List', () => {
+    it('renders "완료" buttons to delete the task', () => {
+      const { container, getByText } = renderPage();
 
-    expect(container).toHaveTextContent('밥먹기');
-    expect(container).toHaveTextContent('테스트 코드 짜기');
-    expect(container).toHaveTextContent('PR 날리기');
+      expect(container).toHaveTextContent('완료');
 
-    expect(onClickDeleteTask).not.toBeCalled();
+      expect(onClickDeleteTask).not.toBeCalled();
 
-    fireEvent.click(
-      getByTextWithContainer(getByText('밥먹기'), '완료'),
-    );
+      fireEvent.click(getByText('완료'));
 
-    expect(onClickDeleteTask).toBeCalledTimes(1);
-
-    fireEvent.click(
-      getByTextWithContainer(getByText('테스트 코드 짜기'), '완료'),
-    );
-
-    expect(onClickDeleteTask).toBeCalledTimes(2);
-
-    fireEvent.click(
-      getByTextWithContainer(getByText('PR 날리기'), '완료'),
-    );
-
-    expect(onClickDeleteTask).toBeCalledTimes(3);
+      expect(onClickDeleteTask).toBeCalled();
+    });
   });
 });
