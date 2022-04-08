07c2e125bb89ec8e4ab2879c30a0c933e0904150
이찬호 <0407chan@naver.com>
2022-04-08 23:54:54 +0900

    refactor. jest.fn 으로 테스트 변경 적용

--- src/Item.test.jsx
+++ src/Item.test.jsx
@@ -1,40 +1,30 @@
 import { fireEvent, render } from '@testing-library/react';
 import Item from './Item';
 
-function TestItem({ task, onClickDelete }) {
-  return (
-    <Item
-      task={task}
-      onClickDelete={onClickDelete}
-    />
-  );
-}
-
-const setup = () => {
-  const handleClick = jest.fn();
+describe('Item', () => {
   const task = {
     id: 1,
     title: '포켓몬빵 사기',
   };
-  const utils = render(<TestItem task={task} onClickDelete={handleClick} />);
 
-  return {
-    handleClick,
-    ...utils,
-  };
-};
-
-describe('Item', () => {
-  it('아이템에 task, 완료버튼 출력 테스트', () => {
-    const { container } = setup();
+  it('1. 아이템에 task title, 완료버튼 출력', () => {
+    const handleClickDelete = jest.fn();
+    const { container } = render(<Item
+      task={task}
+      onClickDelete={handleClickDelete}
+    />);
     expect(container).toHaveTextContent('포켓몬빵 사기');
     expect(container).toHaveTextContent('완료');
   });
 
-  it('완료 버튼 클릭 테스트', () => {
-    const { handleClick, getByText } = setup();
-    expect(handleClick).not.toBeCalled();
+  it('2. 완료 버튼 클릭', () => {
+    const handleClickDelete = jest.fn();
+    const { getByText } = render(<Item
+      task={task}
+      onClickDelete={handleClickDelete}
+    />);
+    expect(handleClickDelete).not.toBeCalled();
     fireEvent.click(getByText('완료'));
-    expect(handleClick).toBeCalledWith(1);
+    expect(handleClickDelete).toBeCalledWith(1);
   });
 });
