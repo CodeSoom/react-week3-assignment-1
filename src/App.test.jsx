1421406c793f3b9b77218b44d952d9c2ea00e36c
이찬호 <0407chan@naver.com>
2022-04-09 00:29:50 +0900

    add. App 3. task 추가 후 완료 동작 테스트

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -21,4 +21,18 @@ describe('App', () => {
     expect(container).toHaveTextContent('할일이 없어');
     expect(container).toHaveTextContent('완료');
   });
+
+  it('3. task 추가 후 완료 처리 테스트', () => {
+    const { container, getByText, getByPlaceholderText } = render(<App />);
+
+    const input = getByPlaceholderText('할 일을 입력해 주세요');
+    fireEvent.change(
+      input,
+      { target: { value: '피카츄 먹이주기' } },
+    );
+    fireEvent.click(getByText('추가'));
+
+    fireEvent.click(getByText('완료'));
+    expect(container).toHaveTextContent('할 일이 없어요!');
+  });
 });
