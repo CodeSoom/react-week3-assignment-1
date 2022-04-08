5785193eefbdb3059da07bd3375ad8e5c8a8b955
kjoobin <kjoobin>
2022-04-08 19:54:58 +0900

    Fix page test scenario

--- src/Page.test.jsx
+++ src/Page.test.jsx
@@ -31,33 +31,44 @@ describe('Page', () => {
     expect(container).toHaveTextContent('To-do');
   });
 
-  context('Input', () => {
-    it('renders textbox to change the task title', () => {
-      const { getByPlaceholderText } = renderPage();
-      expect(onChangeTitle).not.toHaveBeenCalled();
+  it('listens for change event to chagne the task title', () => {
+    const { getByPlaceholderText } = renderPage();
+    expect(onChangeTitle).not.toHaveBeenCalled();
 
-      fireEvent.change(
-        getByPlaceholderText('할 일을 입력해 주세요'),
-        { target: { value: '할 일을 입력했습니다.' } },
-      );
+    fireEvent.change(
+      getByPlaceholderText('할 일을 입력해 주세요'),
+      { target: { value: '할 일을 입력했습니다.' } },
+    );
 
-      expect(onChangeTitle).toHaveBeenCalledWith('할 일을 입력했습니다.');
-    });
-
-    it('renders "추가" button to add task', () => {
-      const { container, getByText } = renderPage();
-
-      expect(container).toHaveTextContent('추가');
-
-      expect(onClickAddTask).not.toHaveBeenCalled();
-
-      fireEvent.click(getByText('추가'));
-
-      expect(onClickAddTask).toHaveBeenCalled();
-    });
+    expect(onChangeTitle).toHaveBeenCalledWith('할 일을 입력했습니다.');
   });
 
-  context('List', () => {
+  it('renders "추가" button to add task', () => {
+    const { container, getByText } = renderPage();
+
+    expect(container).toHaveTextContent('추가');
+
+    expect(onClickAddTask).not.toHaveBeenCalled();
+
+    fireEvent.click(getByText('추가'));
+
+    expect(onClickAddTask).toHaveBeenCalled();
+  });
+
+  context('without the task', () => {
+    it('renders "할 일이 없어요!"');
+    const { container } = renderPage({ tasks: [] });
+
+    expect(container).toHaveTextContent('할 일이 없어요!');
+  });
+
+  context('with tasks', () => {
+    it('renders task title', () => {
+      const { container } = renderPage();
+
+      expect(container).toHaveTextContent('밥먹기');
+    });
+
     it('renders "완료" buttons to delete the task', () => {
       const { container, getByText } = renderPage();
 
