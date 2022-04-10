4a4b9667e4f2c2d9a6c49f79caf5e55a0ec20744
이찬호 <0407chan@naver.com>
2022-04-10 19:27:27 +0900

    refactor. Item.test에 중복 코드 제거

--- src/Item.test.jsx
+++ src/Item.test.jsx
@@ -7,22 +7,30 @@ describe('Item', () => {
     title: '포켓몬빵 사기',
   };
 
+  const handleClickDelete = jest.fn();
+  function renderItem() {
+    return render((
+      <Item
+        task={task}
+        onClickDelete={handleClickDelete}
+      />
+    ));
+  }
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
   it('1. 아이템에 task title, 완료버튼 출력', () => {
-    const handleClickDelete = jest.fn();
-    const { container } = render(<Item
-      task={task}
-      onClickDelete={handleClickDelete}
-    />);
+    const { container } = renderItem();
+
     expect(container).toHaveTextContent('포켓몬빵 사기');
     expect(container).toHaveTextContent('완료');
   });
 
   it('2. 완료 버튼 클릭', () => {
-    const handleClickDelete = jest.fn();
-    const { getByText } = render(<Item
-      task={task}
-      onClickDelete={handleClickDelete}
-    />);
+    const { getByText } = renderItem();
+
     expect(handleClickDelete).not.toBeCalled();
     fireEvent.click(getByText('완료'));
     expect(handleClickDelete).toBeCalledWith(1);
