8507a47c50865ae2c75b74a1716646ea7fda9099
이찬호 <0407chan@naver.com>
2022-04-10 19:35:17 +0900

    refactor. List, 할일 있을때, 없을때 context 분리

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -18,10 +18,10 @@ describe('List', () => {
   ];
 
   const handleClickDelete = jest.fn();
-  function renderList() {
+  function renderList(newTasks = tasks) {
     return render((
       <List
-        tasks={tasks}
+        tasks={newTasks}
         onClickDelete={handleClickDelete}
       />
     ));
@@ -31,38 +31,42 @@ describe('List', () => {
     jest.clearAllMocks();
   });
 
-  it('1. 할일 리스트 출력', () => {
-    const { container } = renderList();
+  context('1. 할일 있을 때', () => {
+    it('1-1. 할일 리스트 출력', () => {
+      const { container } = renderList();
 
-    expect(container).toHaveTextContent('배고파요');
-    expect(container).toHaveTextContent('치킨을 먹어요');
-    expect(container).toHaveTextContent('피자를 먹어요');
+      expect(container).toHaveTextContent('배고파요');
+      expect(container).toHaveTextContent('치킨을 먹어요');
+      expect(container).toHaveTextContent('피자를 먹어요');
+    });
+
+    it("1-2. '완료' 버튼 출력", () => {
+      const { container } = renderList();
+
+      expect(container).toHaveTextContent('완료');
+    });
+
+    it("1-3. '완료' 버튼 클릭 (배고파요 삭제, 피자를 먹어요 삭제)", () => {
+      const { container, getAllByText } = renderList();
+
+      expect(container).toHaveTextContent('완료');
+
+      fireEvent.click(getAllByText('완료')[0]);
+      expect(handleClickDelete).toBeCalledWith(1);
+
+      fireEvent.click(getAllByText('완료')[1]);
+      expect(handleClickDelete).toBeCalledWith(2);
+
+      fireEvent.click(getAllByText('완료')[2]);
+      expect(handleClickDelete).toBeCalledWith(3);
+    });
   });
 
-  test("2. '완료' 버튼 출력", () => {
-    const { container } = renderList();
+  context('2. 빈 배열일 때', () => {
+    it("2-1. '할 일이 없어요!' 출력", () => {
+      const { container } = renderList([]);
 
-    expect(container).toHaveTextContent('완료');
-  });
-
-  test("3. '완료' 버튼 클릭 (배고파요 삭제, 피자를 먹어요 삭제)", () => {
-    const { container, getAllByText } = renderList();
-
-    expect(container).toHaveTextContent('완료');
-
-    fireEvent.click(getAllByText('완료')[0]);
-    expect(handleClickDelete).toBeCalledWith(1);
-
-    fireEvent.click(getAllByText('완료')[1]);
-    expect(handleClickDelete).toBeCalledWith(2);
-
-    fireEvent.click(getAllByText('완료')[2]);
-    expect(handleClickDelete).toBeCalledWith(3);
-  });
-
-  test("4. 빈 배열일 때 '할 일이 없어요!' 출력", () => {
-    const { container } = renderList();
-
-    expect(container).toHaveTextContent('할 일이 없어요!');
+      expect(container).toHaveTextContent('할 일이 없어요!');
+    });
   });
 });
