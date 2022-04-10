96b8652f3fdb180ffa167428c5769a1a91b211f9
이찬호 <0407chan@naver.com>
2022-04-10 19:20:13 +0900

    refactor. render 공통 함수로 분리

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -2,41 +2,39 @@ import { fireEvent, render } from '@testing-library/react';
 import Input from './Input';
 
 describe('Input', () => {
+  const handleChange = jest.fn();
+  const handleClick = jest.fn();
+
+  function renderInput() {
+    return render((
+      <Input
+        value=""
+        onChange={handleChange}
+        onClick={handleClick}
+      />
+    ));
+  }
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
   it("1. '할 일' 텍스트 출력", () => {
-    const value = '';
-    const handleChange = jest.fn();
-    const handleClick = jest.fn();
-    const { container } = render(<Input
-      value={value}
-      onChange={handleChange}
-      onClick={handleClick}
-    />);
+    const { container } = renderInput();
+
     expect(container).toHaveTextContent('할 일');
   });
 
   it("2. '추가' 버튼 클릭", () => {
-    const value = '';
-    const handleChange = jest.fn();
-    const handleClick = jest.fn();
-    const { getByText } = render(<Input
-      value={value}
-      onChange={handleChange}
-      onClick={handleClick}
-    />);
+    const { getByText } = renderInput();
+
     expect(handleClick).not.toBeCalled();
     fireEvent.click(getByText('추가'));
     expect(handleClick).toBeCalled();
   });
 
   it('3. input에 텍스트 입력 테스트', async () => {
-    const value = '';
-    const handleChange = jest.fn();
-    const handleClick = jest.fn();
-    const { getByPlaceholderText } = render(<Input
-      value={value}
-      onChange={handleChange}
-      onClick={handleClick}
-    />);
+    const { getByPlaceholderText } = renderInput();
 
     expect(handleChange).not.toBeCalled();
     fireEvent.change(getByPlaceholderText('할 일을 입력해 주세요'),
@@ -45,14 +43,8 @@ describe('Input', () => {
   });
 
   it('4. input에 placeholder 테스트', async () => {
-    const value = '';
-    const handleChange = jest.fn();
-    const handleClick = jest.fn();
-    const { getByPlaceholderText } = render(<Input
-      value={value}
-      onChange={handleChange}
-      onClick={handleClick}
-    />);
+    const { getByPlaceholderText } = renderInput();
+
     expect(getByPlaceholderText('할 일을 입력해 주세요')
       .getAttribute('placeholder'))
       .toBe('할 일을 입력해 주세요');
