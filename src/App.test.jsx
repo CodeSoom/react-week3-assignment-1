028828c5db717b98228c37b148c3583182d17077
rukeni <rukeni@naver.com>
2022-05-16 21:48:28 +0900

    Add: App.test.jsx추가

---
+++ src/App.test.jsx
@@ -0,0 +1,14 @@
+import { render } from '@testing-library/react';
+
+import App from './App';
+
+test('App', () => {
+  const { container } = render((
+    <App />
+  ));
+
+  expect(container).toHaveTextContent('To-do');
+  expect(container).toHaveTextContent('할 일');
+  expect(container).toHaveTextContent('추가');
+  expect(container).toHaveTextContent('할 일이 없어요!');
+});
