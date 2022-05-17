b244e37ed2fce3c1a7c8a85beb5becce212a7e88
rukeni <rukeni@naver.com>
2022-05-17 20:13:44 +0900

    Refactor: Item 컴포넌트 BDD 적용

--- src/Item.test.jsx
+++ src/Item.test.jsx
@@ -2,27 +2,29 @@ import { render, fireEvent } from '@testing-library/react';
 
 import Item from './Item';
 
-test('Item', () => {
-  const task = {
-    id: 1,
-    title: '뭐라도 하기',
-  };
+describe('Item', () => {
+  describe('Given : task가 주어지면', () => {
+    const task = {
+      id: 1,
+      title: '뭐라도 하기',
+    };
+    const handleClick = jest.fn();
 
-  const handleClick = jest.fn();
+    test('When : 이벤트가 발생했을 때', () => {
+      const { container, getByText } = render((
+        <Item
+          task={task}
+          onClickDelete={handleClick}
+        />
+      ));
 
-  const { container, getByText } = render((
-    <Item
-      task={task}
-      onClickDelete={handleClick}
-    />
-  ));
+      expect(container).toHaveTextContent('뭐라도 하기');
+      expect(container).toHaveTextContent('완료');
 
-  expect(container).toHaveTextContent('뭐라도 하기');
-  expect(container).toHaveTextContent('완료');
+      expect(handleClick).not.toBeCalled();
 
-  expect(handleClick).not.toBeCalled();
-
-  fireEvent.click(getByText('완료'));
-
-  expect(handleClick).toBeCalledWith(1);
+      fireEvent.click(getByText('완료'));
+      expect(handleClick).toBeCalledTimes(1);
+    });
+  });
 });
