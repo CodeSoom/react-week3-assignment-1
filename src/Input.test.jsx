491c3be7dc68ba5f3a2f253f31703db59a1dcb20
rukeni <rukeni@naver.com>
2022-05-17 20:05:00 +0900

    Refactor: Input BDD 반영

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -1,27 +1,32 @@
 import {
-  fireEvent, getByPlaceholderText, getByText, render,
+  fireEvent, render,
 } from '@testing-library/react';
 
 import Input from './Input';
 
-test('Input', () => {
-  const onChange = jest.fn();
-  const onClick = jest.fn();
+describe('Input', () => {
+  describe('Given : 입력값이 주어지지 않고', () => {
+    test('When : 이벤트가 발생하지 않았을 때', () => {
+      const { getByPlaceholderText, container } = render(<Input />);
 
-  const { container } = render((
-    <Input value="" onChange={onChange} onClick={onClick} />
-  ));
+      expect(container).toHaveTextContent('할 일');
+      expect(container).toHaveTextContent('추가');
 
-  expect(container).toHaveTextContent('할 일');
-  expect(container).toHaveTextContent('추가');
+      const input = getByPlaceholderText('할 일을 입력해 주세요');
+      expect(input.value).toBe('');
+    });
+  });
+  describe('Given : 입력값이 주어지면', () => {
+    const textContent = '아리 산책가기';
+    test('When : 값을 입력했을 때', () => {
+      const { getByPlaceholderText, container } = render(<Input />);
 
-  expect(onChange).not.toBeCalled();
+      expect(container).toHaveTextContent('할 일');
+      expect(container).toHaveTextContent('추가');
 
-  const inputText = '뭐라도 하기';
-  fireEvent.change(getByPlaceholderText(container, '할 일을 입력해 주세요'), { target: { value: inputText } });
-  expect(onChange).toBeCalled();
-
-  const button = getByText(container, '추가');
-  fireEvent.click(button);
-  expect(onClick).toBeCalled();
+      const input = getByPlaceholderText('할 일을 입력해 주세요');
+      fireEvent.change(input, { target: { value: textContent } });
+      expect(input.value).toBe(textContent);
+    });
+  });
 });
