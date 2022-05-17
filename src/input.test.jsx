d21137a77d734edce3982fc6674245839da02f53
Ga-Young Song <dolphin2440@naver.com>
2022-05-17 23:09:07 +0900

    Add test case for Input component

---
+++ src/input.test.jsx
@@ -0,0 +1,42 @@
+import { render, fireEvent, screen } from '@testing-library/react';
+
+import Input from './Input';
+
+test('todo input should be empty', () => {
+  render(<Input />);
+
+  const inputTodo = screen.getByPlaceholderText('할 일을 입력해 주세요');
+
+  expect(inputTodo.value).toBe('');
+});
+
+test('추가 button should be displayed', () => {
+  render(<Input />);
+
+  const addButton = screen.getByText('추가');
+
+  expect(addButton).toBeInTheDocument();
+});
+
+test('todo input should be changed', () => {
+  render(<Input />);
+
+  const todo = '뭐라도 하기';
+
+  const inputTodo = screen.getByPlaceholderText('할 일을 입력해 주세요');
+
+  fireEvent.change(inputTodo, { target: { value: todo } });
+  expect(inputTodo.value).toBe('뭐라도 하기');
+});
+
+test('add button should be clicked', () => {
+  const handleClick = jest.fn();
+
+  const { getByText } = render((<Input onClick={handleClick} />));
+
+  expect(handleClick).not.toBeCalled();
+
+  fireEvent.click(getByText('추가'));
+
+  expect(handleClick).toBeCalled();
+});
