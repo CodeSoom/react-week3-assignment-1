7c4dc66899f9150c677803b868b03c25b4c1cd29
rukeni <rukeni@naver.com>
2022-05-17 20:57:13 +0900

    Refactor: Page 컴포넌트 BDD 적용

--- src/Page.test.jsx
+++ src/Page.test.jsx
@@ -4,38 +4,42 @@ import {
 
 import Page from './Page';
 
-test('Page', () => {
-  const taskTitle = '뭐라도 하기';
-  const tasks = [
-    { id: 1, title: '아리 산책가기' },
-    { id: 2, title: '공부하기' },
-  ];
+describe('Page 컴포넌트', () => {
+  describe('Given : 값들이 주어지고', () => {
+    const taskTitle = '뭐라도 하기';
+    const tasks = [
+      { id: 1, title: '아리 산책가기' },
+      { id: 2, title: '공부하기' },
+    ];
 
-  const onChangeTitle = jest.fn();
-  const onClickAddTask = jest.fn();
-  const onClickDeleteTask = jest.fn();
+    const onChangeTitle = jest.fn();
+    const onClickAddTask = jest.fn();
+    const onClickDeleteTask = jest.fn();
 
-  const { container } = render((
-    <Page
-      taskTitle={taskTitle}
-      tasks={tasks}
-      onClickAddTask={onClickAddTask}
-      onClickDeleteTask={onClickDeleteTask}
-      onChangeTitle={onChangeTitle}
-    />
-  ));
+    test('When : 마운트 됐을 때', () => {
+      const { container } = render((
+        <Page
+          taskTitle={taskTitle}
+          tasks={tasks}
+          onClickAddTask={onClickAddTask}
+          onClickDeleteTask={onClickDeleteTask}
+          onChangeTitle={onChangeTitle}
+        />
+      ));
 
-  expect(container).toHaveTextContent('To-do');
+      expect(container).toHaveTextContent('To-do');
 
-  tasks.forEach((task) => {
-    expect(container).toHaveTextContent(task.title);
-  });
+      tasks.forEach((task) => {
+        expect(container).toHaveTextContent(task.title);
+      });
 
-  expect(onClickDeleteTask).not.toBeCalled();
+      expect(onClickDeleteTask).not.toBeCalled();
 
-  tasks.forEach((task, index) => {
-    const button = getAllByText(container, '완료')[index];
-    fireEvent.click(button);
-    expect(onClickDeleteTask).toBeCalledWith(task.id);
+      tasks.forEach((task, index) => {
+        const button = getAllByText(container, '완료')[index];
+        fireEvent.click(button);
+        expect(onClickDeleteTask).toBeCalledWith(index + 1);
+      });
+    });
   });
 });
