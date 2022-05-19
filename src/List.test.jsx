c4455675d49ef00cd3bd303b0497ac61276c9229
eeeesl <wffle0505@gmail.com>
2022-05-19 21:57:22 +0900

    List.test.jsx 수정

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -17,7 +17,7 @@ describe('List', () => {
   context('할 일이 없을 떄', () => {
     const tasks = [];
 
-    it('"할 일이 없어요!"를 표시한다.', () => {
+    it('초기값을 표시한다.', () => {
       const { container } = renderTaskList(tasks);
 
       expect(container).toHaveTextContent('할 일이 없어요!');
@@ -29,19 +29,21 @@ describe('List', () => {
       { id: 1, title: '아무거나 추가하기' },
     ];
 
-    it('할 일 목록을 표시한다.', () => {
+    it('할 일을 목록에 표시한다.', () => {
       const { container } = renderTaskList(tasks);
 
       expect(container).toHaveTextContent('아무거나 추가하기');
       expect(container).toHaveTextContent('완료');
     });
 
-    it('handleClickDelete 함수를 호출한다.', () => {
-      const { getByText } = renderTaskList(tasks);
+    context('할 일이 있고 완료버튼을 눌렀을 때', () => {
+      it('handleClickDelete 함수를 호출한다.', () => {
+        const { getByText } = renderTaskList(tasks);
 
-      expect(handleClickDelete).not.toBeCalled();
-      fireEvent.click(getByText('완료'));
-      expect(handleClickDelete).toBeCalledWith(1);
+        expect(handleClickDelete).not.toBeCalled();
+        fireEvent.click(getByText('완료'));
+        expect(handleClickDelete).toBeCalledWith(1);
+      });
     });
   });
 });
