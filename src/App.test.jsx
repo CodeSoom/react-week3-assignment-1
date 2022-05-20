ffd8f17fa6321a1c0f84fb3019834001c9717205
DaengDo <ring_yoonsuk@naver.com>
2022-05-21 03:12:12 +0900

    State how to test
    - 테스트할 대상 주석으로 작성
    - List 컴포넌트의 테스트 작성
    
    막막하다

---
+++ src/App.test.jsx
@@ -0,0 +1,23 @@
+import { render, fireEvent } from '@testing-library/react';
+
+import App from './App';
+
+/**
+ *
+ * App 컴포넌트 // 랜더하면 // Page 컴포넌트가 랜더됨
+ * App 컴포넌트 // 랜더하면 // Page 컴포넌트의 프롭스로 함수들과 값들이 전달됨
+ *
+ * handleChangeTitle 함수 // html 요소? 이벤트?를 매개변수로 호출하면 // 새로운 state를 반환함
+ * handleClickAddTask 함수 // 호출하면 // 새로운 state를 반환함
+ * handleClickDeleteTask 함수 // number 타입 값을 매개변수로 호출하면 // 해당 숫자에 해당하는 tasks를 필터한 새로운 state 반환함
+ *
+ * useState 같은건 리액트의 함수니까 검증할 필요가 없겠죠?
+ */
+
+describe('', () => {
+  context('', () => {
+    it('', () => {
+      console.log('');
+    });
+  });
+});
--- src/List.test.jsx
+++ src/List.test.jsx
@@ -2,31 +2,50 @@ import { fireEvent, getByText, render } from '@testing-library/react';
 
 import List from './List';
 
-test('List', () => {
-  const task = [
-    // { id: 1, title: '재정신 유지하기' },
-    {
-      id: 2,
-      title: '꿀잠자기',
-    },
-  ];
+/**
+ * Given // When // Then
+ * (describe context it)
+ *
+ * List 컴포넌트 // tasks 빈 배열일 때 // "할 일이 없어요!" 가 출력됨
+ * List 컴포넌트 // tasks 데이터가 있을 때 // "할 일이 없어요!" 가 출력되지 않음
+ * List 컴포넌트 // tasks 데이터가 있을 때 // <Item /> 요소가 반복문으로 출력됨
+ */
 
-  const handleClick = jest.fn();
+describe('List 컴포넌트', () => {
+  const listComponent = (task) => render(<List tasks={task} />);
 
-  const { container, getByText } = render(
-    <List tasks={task} onClickDelete={handleClick} />,
-  );
+  context('할 일이 없으면', () => {
+    const task = [];
 
-  expect(container).toHaveTextContent('꿀잠자기');
-  // expect(container).toHaveTextContent('재정신 유지하기');
+    const { container } = listComponent(task);
 
-  expect(handleClick).not.toBeCalled();
+    it('"할 일이 없어요!" 가 출력 된다.', () => {
+      expect(container).toHaveTextContent('할 일이 없어요!');
+    });
+  });
 
-  // 여러 완료버튼 중 하나를 특정 짓거나 동시에 다 누르고 싶어요...
-  // fireEvent.click(getAllByText('완료'));
-  // fireEvent.click(findAllByText('완료'));
-  fireEvent.click(getByText('완료'));
+  context('할 일이 있으면', () => {
+    const task = [
+      {
+        id: 1,
+        title: '맞춤법 지키기',
+      },
+      {
+        id: 2,
+        title: '꿀잠자기',
+      },
+    ];
 
-  expect('할 일이 없어요!');
-  expect(task.length === 0);
+    const { container } = listComponent(task);
+
+    it('"할 일이 없어요!"가 출력되지 않는다.', () => {
+      expect(container).not.toHaveTextContent('할 일이 없어요!');
+    });
+    it('할 일 목록이 출력된다.', () => {
+      task.forEach((todo) => {
+        // 아래 줄 에러 메시지가 이해가 안갑니다...title이 string 값인걸 명시적으로 알려줘야하나요?
+        expect(container).toHaveTextContent(todo.title);
+      });
+    });
+  });
 });
