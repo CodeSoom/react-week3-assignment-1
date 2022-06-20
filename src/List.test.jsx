d7eb9e4e977e58aba496a961dc14a606f701df2d
seungwoo Kim <kyh196201@gmail.com>
2022-06-21 01:47:27 +0900

    List 컴포넌트 테스트 코드 작성
    props로 전달받은 tasks가 빈 배열일 때 텍스트가 나오는지 검증하는 코드를 작성했습니다.

---
+++ src/List.test.jsx
@@ -0,0 +1,13 @@
+import { render } from '@testing-library/react';
+
+import List from './List';
+
+test('List', () => {
+  const tasks = [];
+
+  const { container } = render((
+    <List tasks={tasks} />
+  ));
+
+  expect(container).toHaveTextContent('할 일이 없어요!');
+});
