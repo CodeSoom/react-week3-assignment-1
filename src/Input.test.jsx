0ce521aaf0c4128a529a4689245e6ac06e3fcccb
Woo Dy <kjop7419@naver.com>
2022-06-21 19:36:23 +0900

    refactor Apply BDD & variable BDD를 적용하여 리팩토링을 진행하였습니다. 중복되지 않는 코드의 변수 선언을 해지하였습니다. 중복되는 부분의 string을 변수화 시켰습니다.

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -5,33 +5,38 @@ import Input from './Input';
 describe('Input', () => {
   const handleChange = jest.fn();
 
-  test('Input이 렌더링 되는지 확인합니다.', () => {
-    const { getByText, getByPlaceholderText } = render((
-      <Input />
-    ));
+  context('Input이 렌더링 됩니다.', () => {
+    it('button엘리먼트와 input엘리먼트가 존재하는지 확인합니다.', () => {
+      const { getByText, getByPlaceholderText } = render((
+        <Input />
+      ));
 
-    const buttonEl = getByText('추가');
-    const inputEl = getByPlaceholderText('할 일을 입력해 주세요');
+      const inputEl = getByPlaceholderText('할 일을 입력해 주세요');
 
-    // input이 존재하는지 체크합니다.
-    expect(inputEl).toBeInTheDocument();
-    // button이 존재하는지 체크합니다.
-    expect(buttonEl).toBeInTheDocument();
+      // input이 존재하는지 체크합니다.
+      expect(inputEl).toBeInTheDocument();
+      // button이 존재하는지 체크합니다.
+      expect(getByText('추가')).toBeInTheDocument();
 
-    expect(inputEl.value).toBe('');
+      expect(inputEl.value).toBe('');
+    });
   });
 
-  test('Input에 있는 폼에 chage event로 입력한 값이 value와 같은지 테스트합니다.', () => {
-    const { getByPlaceholderText } = render(
-      <Input
-        onChange={handleChange}
-      />,
-    );
+  context('input엘리먼트에 새로운 컨텐츠를 입력합니다.', () => {
+    it('input엘리먼트에 chage event로 입력한 값이 value와 같은지 테스트합니다.', () => {
+      const newContents = '숨쉬기';
 
-    const inputEl = getByPlaceholderText('할 일을 입력해 주세요');
+      const { getByPlaceholderText } = render(
+        <Input
+          onChange={handleChange}
+        />,
+      );
 
-    fireEvent.change(inputEl, { target: { value: '추가' } });
+      const inputEl = getByPlaceholderText('할 일을 입력해 주세요');
 
-    expect(inputEl.value).toBe('추가');
+      fireEvent.change(inputEl, { target: { value: newContents } });
+
+      expect(inputEl.value).toBe(newContents);
+    });
   });
 });
