25093eb2ca5159d6d40039a08d1cb322af854181
Woo Dy <kjop7419@naver.com>
2022-06-21 19:42:58 +0900

    refactor BDD BDD를 적용하였습니다.

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -3,32 +3,34 @@ import { render } from '@testing-library/react';
 import List from './List';
 
 describe('List', () => {
-  test('tasks가 있을 때 해당 tasks에 있는 title이 나옵니다.', () => {
-    const tasks = [{
-      id: 1,
-      title: '뭐라도 하기',
-    },
-    {
-      id: 2,
-      title: '숨쉬기',
-    },
-    ];
+  context('tasks의 컨텐츠에 따라 다른 결과가 나오는 지 확인합니다.', () => {
+    it('tasks에 컨텐츠가 있을 시 해당 tasks에 있는 title이 출력됩니다.', () => {
+      const tasks = [{
+        id: 1,
+        title: '뭐라도 하기',
+      },
+      {
+        id: 2,
+        title: '숨쉬기',
+      },
+      ];
 
-    const { container } = render((
-      <List tasks={tasks} />
-    ));
+      const { container } = render((
+        <List tasks={tasks} />
+      ));
 
-    expect(container).toHaveTextContent('뭐라도 하기');
-    expect(container).toHaveTextContent('숨쉬기');
-  });
+      expect(container).toHaveTextContent('뭐라도 하기');
+      expect(container).toHaveTextContent('숨쉬기');
+    });
 
-  test('tasks가 없을 때 "할 일이 없어요!"가 나옵니다', () => {
-    const tasks = [];
+    it('tasks에 컨텐츠가 없을 시 "할 일이 없어요!"가 출력됩니다.', () => {
+      const tasks = [];
 
-    const { container } = render((
-      <List tasks={tasks} />
-    ));
+      const { container } = render((
+        <List tasks={tasks} />
+      ));
 
-    expect(container).toHaveTextContent('할 일이 없어요!');
+      expect(container).toHaveTextContent('할 일이 없어요!');
+    });
   });
 });
