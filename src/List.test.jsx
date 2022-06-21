7a66a3760d3660997e419158541b1e5b7e1af52e
kyh196201 <kyh196201@gmail.com>
2022-06-21 20:29:03 +0900

    List 컴포넌트 테스트 케이스 추가 작성
    List 컴포넌트가 props로 전달받은 tasks의 내용을 렌더링했는지 체크하는 테스트 케이스를 작성했습니다.

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -2,7 +2,7 @@ import { render } from '@testing-library/react';
 
 import List from './List';
 
-test('List', () => {
+test('List is Empty', () => {
   const tasks = [];
 
   const { container } = render((
@@ -10,4 +10,31 @@ test('List', () => {
   ));
 
   expect(container).toHaveTextContent('할 일이 없어요!');
+});
+
+test('List is Rendered', () => {
+  const tasks = [
+    {
+      id: 1,
+      title: '뭐라도 하기',
+    },
+    {
+      id: 2,
+      title: '아무것도 하지 않기',
+    },
+  ];
+
+  const { getAllByRole } = render((
+    <List tasks={tasks} />
+  ));
+
+  const items = getAllByRole('listitem');
+
+  expect(items).toHaveLength(tasks.length);
+
+  items.forEach((item, index) => {
+    const task = tasks[index];
+
+    expect(item).toHaveTextContent(task.title);
+  });
 });
