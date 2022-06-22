6869614f44c134d1f2b7fb072cfe7075d4a0cea5
Woo Dy <kjop7419@naver.com>
2022-06-22 20:14:44 +0900

    refactor BDD BDD에 대해 좀 더 공부 후 리팩토링을 진행하였습니다.

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -4,33 +4,44 @@ import Input from './Input';
 
 describe('Input', () => {
   const handleChange = jest.fn();
+  const handleClick = jest.fn();
+
+  beforeEach(() => {
+    handleChange.mockClear();
+    handleClick.mockClear();
+    render((
+      <Input />
+    ));
+  });
 
   context('Input이 렌더링 됩니다.', () => {
-    it('button엘리먼트와 input엘리먼트가 존재하는지 확인합니다.', () => {
-      const { getByText, getByPlaceholderText } = render((
-        <Input />
-      ));
+    const { container, getByText, getByPlaceholderText } = render((
+      <Input />
+    ));
 
-      const inputEl = getByPlaceholderText('할 일을 입력해 주세요');
+    it('label이 존재합니다.', () => {
+      expect(container).toHaveTextContent('할 일');
+    });
 
-      // input이 존재하는지 체크합니다.
-      expect(inputEl).toBeInTheDocument();
-      // button이 존재하는지 체크합니다.
+    it('button이 존재합니다.', () => {
       expect(getByText('추가')).toBeInTheDocument();
+    });
 
-      expect(inputEl.value).toBe('');
+    it('input이 존재합니다.', () => {
+      expect(getByPlaceholderText('할 일을 입력해 주세요')).toBeInTheDocument();
     });
   });
 
-  context('input엘리먼트에 새로운 컨텐츠를 입력합니다.', () => {
-    it('input엘리먼트에 chage event로 입력한 값이 value와 같은지 테스트합니다.', () => {
-      const newContents = '숨쉬기';
+  context('유저가 "숨쉬기" 라는 할 일을 입력하면', () => {
+    const { getByPlaceholderText } = render((
+      <Input
+        value="아무것도 안하기"
+        onChange={handleChange}
+      />
+    ));
 
-      const { getByPlaceholderText } = render(
-        <Input
-          onChange={handleChange}
-        />,
-      );
+    it('입력창에 "숨쉬기" 라는 문구가 보인다.', () => {
+      const newContents = '숨쉬기';
 
       const inputEl = getByPlaceholderText('할 일을 입력해 주세요');
 
