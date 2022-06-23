2e540eb9652943aadf04fe1ca9a7c3351099f315
minsuk <alstjr0183@naver.com>
2022-06-23 20:39:05 +0900

    fix: Page테스트 context제거 , it 설명 변경 page 테스트 케이스의 의미없는 context를 제거하고 it부분엔 컴포넌트의 동작을 설명하는 뉘앙스로 변경해주었습니다

--- src/Page.test.jsx
+++ src/Page.test.jsx
@@ -13,24 +13,22 @@ describe('Page', () => {
     handleClickDeleteTask.mockClear();
   });
 
-  context('Page가 보인다', () => {
-    const taskTitle = '테스크';
-    const tasks = [{ id: 1, title: '너무 어렵네요' }];
+  const taskTitle = '테스크';
+  const tasks = [{ id: 1, title: '너무 어렵네요' }];
 
-    const { container } = render((<Page
-      taskTitle={taskTitle}
-      onChangeTitle={handleChangeTitle}
-      onClickAddTask={handleClickAddTask}
-      tasks={tasks}
-      onClickDeleteTask={handleClickDeleteTask}
-    />));
+  const { container } = render((<Page
+    taskTitle={taskTitle}
+    onChangeTitle={handleChangeTitle}
+    onClickAddTask={handleClickAddTask}
+    tasks={tasks}
+    onClickDeleteTask={handleClickDeleteTask}
+  />));
 
-    it('페이지의 요소들이 맞는지 확인', () => {
-      expect(container).toHaveTextContent('To-do');
-      expect(container).toHaveTextContent('할 일');
-      expect(container).toHaveTextContent('추가');
-      expect(container).toHaveTextContent('너무 어렵네요');
-      expect(container).toHaveTextContent('완료');
-    });
+  it('page를 렌더링한다', () => {
+    expect(container).toHaveTextContent('To-do');
+    expect(container).toHaveTextContent('할 일');
+    expect(container).toHaveTextContent('추가');
+    expect(container).toHaveTextContent('너무 어렵네요');
+    expect(container).toHaveTextContent('완료');
   });
 });
