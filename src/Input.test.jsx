34b46f2075e6affd50006836126c2c04cea84dae
Woo Dy <kjop7419@naver.com>
2022-06-23 20:04:31 +0900

    refactor Input 잘못된 BDD스타일을 수정하였습니다

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -6,7 +6,7 @@ describe('Input', () => {
   const onChangeTitle = jest.fn();
   const onClickAddTask = jest.fn();
 
-  const RendererInput = () => render(
+  const rendereInput = () => render(
     <Input
       onClick={onClickAddTask}
       onChangeTitle={onChangeTitle}
@@ -16,11 +16,11 @@ describe('Input', () => {
   beforeEach(() => {
     jest.clearAllMocks();
 
-    RendererInput();
+    rendereInput();
   });
 
-  context('Input이 렌더링 되면', () => {
-    const { container, getByText, getByPlaceholderText } = RendererInput();
+  describe('Input이 렌더링 되면', () => {
+    const { container, getByText, getByPlaceholderText } = rendereInput();
 
     it('label이 보입니다.', () => {
       expect(container).toHaveTextContent('할 일');
@@ -35,9 +35,9 @@ describe('Input', () => {
     });
   });
 
-  context('유저가 "숨쉬기" 라는 할 일을 입력하면', () => {
+  describe('유저가 "숨쉬기" 라는 할 일을 입력하면', () => {
     const newContents = '숨쉬기';
-    const { getByPlaceholderText } = RendererInput();
+    const { getByPlaceholderText } = rendereInput();
 
     it('입력창에 "숨쉬기" 라는 문구가 보입니다.', () => {
       const input = getByPlaceholderText('할 일을 입력해 주세요');
@@ -48,17 +48,19 @@ describe('Input', () => {
     });
   });
 
-  context('유저가 "추가"', () => {
-    const { getByText } = RendererInput();
+  describe('유저가 "추가"', () => {
+    const { getByText } = rendereInput();
+    context('버튼을 클릭하면', () => {
+      it('onClickAddTask함수가 호출됩니다.', () => {
+        fireEvent.click(getByText('추가'));
 
-    it('버튼을 1번 클릭하면 1번 호출됩니다.', () => {
-      fireEvent.click(getByText('추가'));
-
-      expect(onClickAddTask).toBeCalledTimes(1);
+        expect(onClickAddTask).toBeCalledTimes(1);
+      });
     });
-
-    it('버튼을 클릭하지 않으면 호출되지 않습니다.', () => {
-      expect(onClickAddTask).not.toBeCalled();
+    context('버튼을 클릭하지 않는다면', () => {
+      it('onClickAddTask함수가 호출되지 않습니다.', () => {
+        expect(onClickAddTask).not.toBeCalled();
+      });
     });
   });
 });
