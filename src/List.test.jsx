7cf6ac06b0ceac720c9e81d12b449f66b5001e56
Woo Dy <kjop7419@naver.com>
2022-06-23 20:00:21 +0900

    refactor BDD BDD 스타일로 리팩토링을 진행하였습니다.

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -1,36 +1,68 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import List from './List';
 
 describe('List', () => {
-  context('tasks의 컨텐츠에 따라 다른 결과가 나오는 지 확인합니다.', () => {
-    it('tasks에 컨텐츠가 있을 시 해당 tasks에 있는 title이 출력됩니다.', () => {
-      const tasks = [{
-        id: 1,
-        title: '뭐라도 하기',
-      },
-      {
-        id: 2,
-        title: '숨쉬기',
-      },
-      ];
+  const onClickDelete = jest.fn();
 
-      const { container } = render((
-        <List tasks={tasks} />
-      ));
+  const tasks = [{
+    id: 1,
+    title: '뭐라도 하기',
+  },
+  ];
 
-      expect(container).toHaveTextContent('뭐라도 하기');
-      expect(container).toHaveTextContent('숨쉬기');
+  const emptyTask = [];
+
+  const rendererList = (task) => render(
+    <List
+      tasks={task}
+      onClickDelete={onClickDelete}
+    />,
+  );
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe('List가 렌더링될 때', () => {
+    describe('task의 할 일 목록에', () => {
+      context('할 일이 있을 시', () => {
+        it('해당 tasks의 할 일 목록만큼 리스트가 보입니다.', () => {
+          const { getAllByRole } = rendererList(tasks);
+
+          const todos = getAllByRole('listitem');
+
+          expect(todos.length).toBe(tasks.length);
+        });
+
+        context('할 일이 없을 시', () => {
+          it('"할 일이 없어요!"가 출력됩니다.', () => {
+            const { container } = rendererList(emptyTask);
+
+            expect(container).toHaveTextContent('할 일이 없어요!');
+          });
+        });
+      });
     });
 
-    it('tasks에 컨텐츠가 없을 시 "할 일이 없어요!"가 출력됩니다.', () => {
-      const tasks = [];
+    describe('유저가 "완료"', () => {
+      context('버튼을 클릭하면', () => {
+        it('onClickDelete가 호출됩니다.', () => {
+          const { getByText } = rendererList(tasks);
 
-      const { container } = render((
-        <List tasks={tasks} />
-      ));
+          fireEvent.click(getByText('완료'));
 
-      expect(container).toHaveTextContent('할 일이 없어요!');
+          expect(onClickDelete).toBeCalledTimes(1);
+        });
+      });
+
+      context('버튼을 클릭하지 않는다면', () => {
+        it('onClickDelete가 호출되지 않습니다.', () => {
+          rendererList(tasks);
+
+          expect(onClickDelete).not.toBeCalled();
+        });
+      });
     });
   });
 });
