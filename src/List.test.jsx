053d7d3b862a91c110a4ce1b617aad5da86576e0
Nakyeon Ko <59277499+nakyeonko3@users.noreply.github.com>
2022-06-23 22:30:04 +0900

    update > Input.test.jsx tasks.length가 0일 때 테스트

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -1,45 +1,32 @@
-import { render, fireEvent } from '@testing-library/react';
-import { useState } from 'react';
+import { render } from '@testing-library/react';
 
 import List from './List';
 
-test('List', () => {
-  // const [tasks, setTasks] = useState([
-  //   {
-  //     id: 101,
-  //     title: '뭐라도 하기',
-  //   },
-  // ]);
+test('List tasks', () => {
   const tasks = [
     {
       id: 101,
       title: '뭐라도 하기',
     },
   ];
-  expect(tasks).toBe([
-    {
-      id: 101,
-      title: '뭐라도 하기',
-    },
-  ]);
   const handleClickDelete = jest.fn();
 
-  const { container, getByText } = render(
+  const { container } = render(
     <List tasks={tasks} onClickDelete={handleClickDelete} />
   );
 
   expect(container).toHaveTextContent('뭐라도 하기');
+});
 
-  // tasks.pop();
-  // expect(tasks.length).toBe(0);
+test('List no tasks', () => {
+  const tasks = [];
+  const handleClickDelete = jest.fn();
 
-  // const { container2 } = render(
-  //   <List tasks={tasks} onClickDelete={handleClickDelete} />
-  // );
+  const { container } = render(
+    <List tasks={tasks} onClickDelete={handleClickDelete} />
+  );
+
+  // expect(container).toHaveTextContent('뭐라도 하기');
 
   expect(container).toHaveTextContent('할 일이 없어요!');
 });
-
-// test('sample', () => {
-//   expect(1 + 1).toBe(2);
-// });
