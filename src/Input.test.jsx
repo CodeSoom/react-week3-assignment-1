26c66f2a6961a061fd71e2a5769c7674062b3513
Nakyeon Ko <59277499+nakyeonko3@users.noreply.github.com>
2022-06-25 15:12:25 +0900

    update > Input.test

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -4,42 +4,51 @@ import userEvent from '@testing-library/user-event';
 
 import Input from './Input';
 
-test('test value on Input', () => {
-  const value = { text: 'react 과제 제출 하기' };
-
-  function onChange() {
-    value.text = '컴퓨터 끄고 자기';
-  }
-  function onClick() {}
-  const { container } = render(
-    <Input value={value.text} onChange={onChange} onClick={onClick} />
-  );
-  expect(container).toHaveTextContent('할 일');
-  expect(container).toHaveTextContent('추가');
-
-  //   expect(container).toHaveTextContent('react 과제 제출 하기');
-});
-
 describe('Input component', () => {
-  const renderInputComponent = (onChange = null, value = null) =>
-    render(<Input onChange={onChange} value={value} />);
+  const handleChange = jest.fn();
+  const handleClick = jest.fn();
 
-  it('label text 할 일', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  const renderInputComponent = (value = '') =>
+    render(
+      <Input onChange={handleChange} onClick={handleClick} value={value} />
+    );
+
+  it("label tag text is '할 일'", () => {
     const { getByText } = renderInputComponent();
     expect(getByText('할 일')).toContainHTML('label');
   });
 
-  it('button text 추가', () => {
+  it("button tag text is '추가' ", () => {
     const { getByText } = renderInputComponent();
-    expect(getByText('추가')).toContainHTML('button');
+    expect(getByText('추가')).toBeInTheDocument();
   });
 
-  it('onChange should be called', () => {
-    const handlechange = jest.fn();
-    const { container } = renderInputComponent(handlechange);
-    expect(handlechange).not.toBeCalled();
+  it('onChange event should be fired when value changed.', () => {
+    const { getByPlaceholderText } = renderInputComponent();
+
+    fireEvent.change(getByPlaceholderText('할 일을 입력해 주세요'), {
+      target: { value: '23' },
+    });
+
+    expect(handleChange).toHaveBeenCalledTimes(1);
   });
-  
-  it('value parameter should be changed', () => {});
-  it('onChange should be called when value changes.', () => {});
+
+  it('value parameter should be changed', () => {
+    const { getByDisplayValue } = renderInputComponent(24);
+    expect(getByDisplayValue('24')).toBeInTheDocument();
+  });
+
+  it('onClick event should be fired when button clicked.', () => {
+    const { getByText } = renderInputComponent();
+
+    expect(handleClick).not.toBeCalled();
+
+    fireEvent.click(getByText('추가'));
+
+    expect(handleClick).toBeCalled();
+  });
 });
