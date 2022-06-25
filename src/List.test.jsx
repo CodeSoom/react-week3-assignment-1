ea0bdeb5e6cb188d61f82f02273601dd934c8ca4
Nakyeon Ko <59277499+nakyeonko3@users.noreply.github.com>
2022-06-25 22:36:36 +0900

    update > List.test.jsx

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -18,9 +18,22 @@ describe('List component', () => {
     expect(getByText('할 일이 없어요!')).toContainHTML('p');
   });
 
-  it('ol html should be existed when tasks.length value is 1 or more ', () => {
+  it('ol html tag should be existed when tasks.length value is 1 or more ', () => {
     const tasks = ['not today'];
     const { container } = renderListComponent(tasks);
     expect(container).toContainHTML('ol');
+  });
+
+  it('tasks array values should be displayed on the screen', () => {
+    const tasks = [
+      { id: 12, title: 'done is better than perfect' },
+      { id: 102, title: '돈은 완벽보다 낫다' },
+      { id: 1, title: '완료하는 것이 완벽보다 낫다' },
+    ];
+    const { container } = renderListComponent(tasks);
+
+    expect(container).toHaveTextContent('done is better than perfect');
+    expect(container).toHaveTextContent('돈은 완벽보다 낫다');
+    expect(container).toHaveTextContent('완료하는 것이 완벽보다 낫다');
   });
 });
