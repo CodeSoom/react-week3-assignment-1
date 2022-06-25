10dc783ca0b71e22cd1cbad94086769ed5df5e60
Nakyeon Ko <59277499+nakyeonko3@users.noreply.github.com>
2022-06-25 11:52:43 +0900

    update > Input.test.jsx, describe it 적용

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -1,15 +1,45 @@
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
+
+import userEvent from '@testing-library/user-event';
 
 import Input from './Input';
 
 test('test value on Input', () => {
-  const value = 'react 과제 제출 하기';
+  const value = { text: 'react 과제 제출 하기' };
 
-  function onChange() {}
+  function onChange() {
+    value.text = '컴퓨터 끄고 자기';
+  }
   function onClick() {}
-  const { container } = render(<Input onChange={onChange} onClick={onClick} />);
+  const { container } = render(
+    <Input value={value.text} onChange={onChange} onClick={onClick} />
+  );
   expect(container).toHaveTextContent('할 일');
   expect(container).toHaveTextContent('추가');
 
   //   expect(container).toHaveTextContent('react 과제 제출 하기');
+});
+
+describe('Input component', () => {
+  const renderInputComponent = (onChange = null, value = null) =>
+    render(<Input onChange={onChange} value={value} />);
+
+  it('label text 할 일', () => {
+    const { getByText } = renderInputComponent();
+    expect(getByText('할 일')).toContainHTML('label');
+  });
+
+  it('button text 추가', () => {
+    const { getByText } = renderInputComponent();
+    expect(getByText('추가')).toContainHTML('button');
+  });
+
+  it('onChange should be called', () => {
+    const handlechange = jest.fn();
+    const { container } = renderInputComponent(handlechange);
+    expect(handlechange).not.toBeCalled();
+  });
+  
+  it('value parameter should be changed', () => {});
+  it('onChange should be called when value changes.', () => {});
 });
