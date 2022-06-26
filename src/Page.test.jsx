8eb6eb22b365770d9b116ad2376b574138bbd9ba
yerin512 <73337811+yerin512@users.noreply.github.com>
2022-06-26 19:22:48 +0900

    Page.jsx의 테스트 코드를 리팩토링 했습니다
    하위 컴포넌트에서 끝낸 중복 테스트 코드를 없애고 Page 화면 내에서 보이는 것들만 수행하도록 수정했습니다

--- src/Page.test.jsx
+++ src/Page.test.jsx
@@ -1,4 +1,4 @@
-import { fireEvent, render } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import Page from './Page';
 import TASKS from './fixtures/Task';
 
@@ -20,84 +20,39 @@ describe('<Page />', () => {
     ));
   }
 
-  it('input이 보인다', () => {
-    const { getByPlaceholderText } = renderPage();
-
-    expect(getByPlaceholderText('할 일을 입력해 주세요')).toBeInTheDocument();
-  });
-
   it('title 이름인 To-do가 보인다', () => {
     const { container } = renderPage();
 
     expect(container).toHaveTextContent('To-do');
   });
 
-  describe('<Input/>', () => {
-    describe('입력할 때', () => {
-      it('handleChangeTitle 함수가 호출됨', () => {
-        const { getByLabelText } = renderPage();
+  it('input이 보인다', () => {
+    const { getByPlaceholderText } = renderPage();
 
-        expect(handleChangeTitle).not.toBeCalled();
+    expect(getByPlaceholderText('할 일을 입력해 주세요')).toBeInTheDocument();
+  });
 
-        fireEvent.change(getByLabelText('할 일'), { target: { value: '뭐라도 하기' } });
+  it('"추가" button이 보인다', () => {
+    const { container } = renderPage();
 
-        expect(handleChangeTitle).toBeCalled();
-      });
-    });
+    expect(container).toHaveTextContent('추가');
+  });
 
-    describe('"추가" 버튼을 누를 때', () => {
-      it('handleClickAddTask 함수가 한 번 호출됨', () => {
-        const { getByText } = renderPage();
+  context('할 일이 있다면', () => {
+    it('할 일 목록 내용이 보임', () => {
+      const { container } = renderPage({ tasks: dummytasks });
 
-        expect(handleClickAddTask).not.toBeCalled();
-
-        fireEvent.click(getByText('추가'));
-
-        expect(handleClickAddTask).toHaveBeenCalledTimes(1);
-      });
-
-      it('입력했던 할 일이 없어지고 placeholder가 보임', () => {
-        const { getByText, getByPlaceholderText } = renderPage();
-
-        fireEvent.click(getByText('추가'));
-
-        expect(getByPlaceholderText('할 일을 입력해 주세요')).toBeInTheDocument();
+      dummytasks.forEach((task) => {
+        expect(container).toHaveTextContent(task.title);
       });
     });
   });
 
-  describe('<List/>', () => {
-    context('할 일이 있다면', () => {
-      it('할 일 목록 내용이 보임', () => {
-        const { container } = renderPage({ tasks: dummytasks });
+  context('할 일이 없다면', () => {
+    it('"할 일이 없어요!" 텍스트가 보임', () => {
+      const { container } = renderPage();
 
-        dummytasks.forEach((task) => {
-          expect(container).toHaveTextContent(task.title);
-        });
-      });
-    });
-
-    context('할 일이 없다면', () => {
-      it('"할 일이 없어요!" 텍스트가 보임', () => {
-        const { container } = renderPage();
-
-        expect(container).toHaveTextContent('할 일이 없어요!');
-      });
-    });
-
-    describe('완료버튼을 누를 때', () => {
-      it('handleClickDelete 함수가 호출됨', () => {
-        const { getAllByRole } = renderPage({
-          tasks: dummytasks,
-        });
-
-        expect(handleClickDeleteTask).not.toBeCalled();
-
-        getAllByRole('button').forEach((button) => {
-          fireEvent.click(button);
-        });
-        expect(handleClickDeleteTask).toBeCalledTimes(dummytasks.length);
-      });
+      expect(container).toHaveTextContent('할 일이 없어요!');
     });
   });
 });
