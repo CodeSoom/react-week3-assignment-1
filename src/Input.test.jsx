1334057d9450167adc7cf9bdf607a17efbf833a7
yerin512 <73337811+yerin512@users.noreply.github.com>
2022-06-26 18:32:30 +0900

    uncontrolled component화 되었던Input.jsx의 테스트 코드를 리팩토링했습니다
    기존에 value 속성 없이 작성했던 코드에 value를 추가했습니다. 또 input에 입력시 기존에는 추가된 값도 보이게끔 하려했지만 hamdleChange만 실행되도록 수정했습니다

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -6,9 +6,10 @@ describe('<Input />', () => {
   const handleChange = jest.fn();
   const handleClick = jest.fn();
 
-  function renderInput() {
+  function renderInput(taskTitle = '') {
     return render((
       <Input
+        value={taskTitle}
         onChange={handleChange}
         onClick={handleClick}
       />
@@ -24,15 +25,17 @@ describe('<Input />', () => {
     expect(container).toHaveTextContent('추가');
   });
 
-  it('입력창에 입력하면 handleChange가 실행되며 입력값이 보여진다.', () => {
-    const { getByLabelText } = renderInput();
+  it('input의 value가 보인다.', () => {
+    const { getByDisplayValue } = renderInput('기존 할 일');
+    expect(getByDisplayValue('기존 할 일')).not.toBeNull();
+  });
 
-    expect(handleChange).not.toBeCalled();
+  it('입력창에 입력하면 handleChange가 실행된다.', () => {
+    const { getByLabelText } = renderInput();
 
     fireEvent.change(getByLabelText('할 일'), { target: { value: '뭐라도 하기' } });
 
     expect(handleChange).toBeCalled();
-    expect(getByLabelText('할 일').value).toBe('뭐라도 하기');
   });
 
   it('"추가" 버튼을 누르면 입력 창에 "할 일을 입력해 주세요" 라는 문구가 보인다', () => {
