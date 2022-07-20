434b96680dea7f53d05c750e83cc1dd2cc8c5cf2
Minjae Kim <innersme@gmail.com>
2022-07-20 17:31:10 +0900

    add List test

---
+++ src/Page.test.jsx
@@ -0,0 +1,13 @@
+import { render, fireEvent } from '@testing-library/react';
+
+import Page from './Page';
+import Input from './Input';
+import List from './List';
+
+test('Page', () => {
+
+
+  const { container } = render(
+    
+  );
+})
