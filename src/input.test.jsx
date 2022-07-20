8d2634d89998cb0f7d23ca4ef4fa27bf27474e46
clapho <clapp301@gmail.com>
2022-07-20 20:38:28 +0900

    Refactor Input test

--- src/input.test.jsx
+++ src/input.test.jsx
@@ -2,24 +2,51 @@ import { render, fireEvent } from '@testing-library/react';
 
 import Input from './Input';
 
-test('Input', () => {
-  const value = '공부하기';
-  const handleClick = jest.fn();
-  const handleChange = jest.fn();
-
-  const { container, getByText, getByPlaceholderText } = render(
-    <Input value={value} onClick={handleClick} onChange={handleChange} />
-  );
-
-  expect(container).toHaveTextContent('할 일');
-  expect(container).toHaveTextContent('추가');
-
-  const input = getByPlaceholderText('할 일을 입력해 주세요');
-  fireEvent.change(input, {
-    target: {
-      value: '공부하기',
-    },
+describe('<Input />', () => {
+  it('has label', () => {
+    const { getByLabelText } = render(<Input />);
+    getByLabelText('할 일');
   });
-  fireEvent.click(getByText('추가'));
-  expect(handleClick).toBeCalled();
+
+  it('has input', () => {
+    const { getByPlaceholderText } = render(<Input />);
+    getByPlaceholderText('할 일을 입력해 주세요');
+  });
+
+  it('has button', () => {
+    const { getByText } = render(<Input />);
+    getByText('추가');
+  });
+
+  it('changes input', () => {
+    const { getByPlaceholderText } = render(<Input />);
+    const input = getByPlaceholderText('할 일을 입력해 주세요');
+
+    fireEvent.change(input, {
+      target: {
+        value: '아무것도 하지 않기',
+      },
+    });
+
+    expect(input.value).toBe('아무것도 하지 않기');
+  });
+
+  it('click add button', () => {
+    const handleClick = jest.fn();
+
+    const { getByText, getByPlaceholderText } = render(
+      <Input onClick={handleClick} />,
+    );
+
+    const input = getByPlaceholderText('할 일을 입력해 주세요');
+    fireEvent.change(input, {
+      target: {
+        value: '아무것도 하지 않기',
+      },
+    });
+
+    fireEvent.click(getByText('추가'));
+    expect(handleClick).toBeCalled();
+    expect(input).toHaveAttribute('value', '');
+  });
 });
