bd6b421f254a3cef4178d2c270ad3f8413132460
hanna244 <shhn0509@gmail.com>
2022-07-21 12:32:19 +0900

    when click 'add' button, test code

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -1,16 +1,23 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 
 import Input from './Input';
 
 test('Input', () => {
   const value = '회고 작성하기';
 
-  render(<Input value={value} />);
+  const handleClick = jest.fn();
+
+  const { getByText } = render(<Input onClick={handleClick} value={value} />);
 
   screen.getByDisplayValue(value);
+
+  expect(handleClick).not.toBeCalled();
+
+  fireEvent.click(getByText('추가'));
+
+  expect(handleClick).toBeCalled();
 });
 
 // Input의 기능
 // - <input>에 텍스트 입력 시 <input>에 작성한 value가 보여지기
-// - <input>에 텍스트 입력 시 추가 버튼 활성화
 // - 추가 버튼 클릭 시 <input>의 텍스트 삭제 및 버튼 비활성화
