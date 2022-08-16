5bad7a854b2f4e6f5531468dfd2fe32e2a3a06d9
0-Chan <kyc4421194@gmail.com>
2022-08-16 19:23:03 +0900

    Apply BDD test code style

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -2,40 +2,66 @@ import { render, fireEvent } from '@testing-library/react';
 
 import List from './List';
 
-test('List', () => {
-  const tasks = [
-    { id: 1, title: '뭐라도 하기' },
-    { id: 2, title: '코드숨 과제' },
-  ];
-
+describe('List', () => {
   const handleClick = jest.fn();
 
-  const { container, getAllByText, rerender } = render((
-    <List
-      tasks={tasks}
-      onClickDelete={handleClick}
-    />
-  ));
+  context('tasks 가 존재한다면', () => {
+    it('title이 보인다.', () => {
+      const tasks = [
+        { id: 1, title: '뭐라도 하기' },
+        { id: 2, title: '코드숨 과제' },
+      ];
 
-  expect(container).toHaveTextContent('뭐라도 하기');
-  expect(container).toHaveTextContent('코드숨 과제');
-  expect(container).toHaveTextContent('완료');
+      const { container } = render((
+        <List
+          tasks={tasks}
+          onClickDelete={handleClick}
+        />
+      ));
 
-  expect(handleClick).not.toBeCalled();
+      expect(container).toHaveTextContent('뭐라도 하기');
+      expect(container).toHaveTextContent('코드숨 과제');
+      expect(container).toHaveTextContent('완료');
+    });
+  });
 
-  const buttons = getAllByText('완료');
-  fireEvent.click(buttons[0]);
-  fireEvent.click(buttons[1]);
+  context('완료 버튼을 누른다면', () => {
+    it('각각의 버튼에 함수가 동작한다.', () => {
+      const tasks = [
+        { id: 1, title: '뭐라도 하기' },
+        { id: 2, title: '코드숨 과제' },
+      ];
 
-  expect(handleClick).toBeCalledWith(1);
-  expect(handleClick).toBeCalledWith(2);
+      const { getAllByText } = render((
+        <List
+          tasks={tasks}
+          onClickDelete={handleClick}
+        />
+      ));
 
-  tasks.length = 0;
-  rerender((
-    <List
-      tasks={tasks}
-      onClickDelete={handleClick}
-    />
-  ));
-  expect(container).toHaveTextContent('할 일이 없어요!');
+      expect(handleClick).not.toBeCalled();
+
+      const buttons = getAllByText('완료');
+      fireEvent.click(buttons[0]);
+      fireEvent.click(buttons[1]);
+
+      expect(handleClick).toBeCalledWith(1);
+      expect(handleClick).toBeCalledWith(2);
+    });
+  });
+
+  context('tasks가 비어있다면', () => {
+    it('할 일이 없어요! 가 보인다', () => {
+      const tasks = [];
+
+      const { container } = render((
+        <List
+          tasks={tasks}
+          onClickDelete={handleClick}
+        />
+      ));
+
+      expect(container).toHaveTextContent('할 일이 없어요!');
+    });
+  });
 });
