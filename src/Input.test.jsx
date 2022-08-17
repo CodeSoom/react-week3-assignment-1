6b8462bc4fe9ba162b24f294d94ef2671f8b4c17
gunhee-jeong <kyle4619@naver.com>
2022-08-17 20:50:06 +0900

    describe-context-it 패턴을 사용
    - text를 입력했을 때와, 추가 버튼을 클릭했을 때의 2가지 테스트를 분리하여 작성해보았습니다.

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -2,22 +2,48 @@ import { render, fireEvent } from '@testing-library/react';
 
 import Input from './Input';
 
-test('Input', () => {
-  const value = '';
+describe('Input', () => {
+  context('할 일이 입력되면', () => {
+    it('input의 value가 변경된다', () => {
+      const handleChange = jest.fn();
+      const handleClick = jest.fn();
 
-  const handleClickAdd = jest.fn();
+      const { container, getByText, getByPlaceholderText } = render((
+        <Input
+          onChange={handleChange}
+          onClick={handleClick}
+        />
+      ));
 
-  const { container, getByText } = render((
-    <Input
-      value={value}
-      onChange={handleClickAdd}
-    />
-  ));
+      expect(container).toHaveTextContent('할 일');
+      expect(container).toHaveTextContent('추가');
 
-  expect(container).toHaveTextContent('할 일');
-  expect(container).toHaveTextContent('추가');
+      expect(handleChange).not.toBeCalled();
+      expect(handleClick).not.toBeCalled();
 
-  expect(handleClickAdd).not.toBeCalled();
+      const input = getByPlaceholderText('할 일을 입력해 주세요');
 
-  fireEvent.click(getByText('추가'));
+      fireEvent.change(input, {
+        target: {
+          value: '잠자기',
+        },
+      });
+      expect(input.value).toBe('잠자기');
+      expect(handleChange).toBeCalled();
+    });
+  });
+
+  context('추가 버튼이 클릭되면', () => {
+    it('handleClick 함수가 실행된다', () => {
+      const handleChange = jest.fn();
+      const handleClick = jest.fn();
+
+      const { container, getByText } = render((
+        <Input
+          onChange={handleChange}
+          onClick={handleClick}
+        />
+      ));
+    });
+  });
 });
