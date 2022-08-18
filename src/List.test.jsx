73ba8f98a97788696d6a3c0178096aff6abb61bc
gunhee-jeong <kyle4619@naver.com>
2022-08-18 20:37:38 +0900

    List 컴포넌트의 'have to-do' 테스트 추가

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -3,8 +3,11 @@ import { render, fireEvent } from '@testing-library/react';
 import List from './List';
 
 describe('List', () => {
+  const handleClick = jest.fn();
+
   it('nothing to-do', () => {
     const tasks = [];
+
     const { container } = render((
       <List
         tasks={tasks}
@@ -12,5 +15,23 @@ describe('List', () => {
     ));
 
     expect(container).toHaveTextContent('할 일이 없어요!');
+  });
+
+  it('have to-do', () => {
+    const tasks = [
+      { id: 1, title: '잠자기' },
+      { id: 2, title: '산책하기' },
+    ];
+
+    const { container } = render((
+      <List
+        tasks={tasks}
+        onClick={handleClick}
+      />
+    ));
+
+    expect(container).toHaveTextContent('잠자기');
+    expect(container).toHaveTextContent('산책하기');
+    expect(container).toHaveTextContent('완료');
   });
 });
