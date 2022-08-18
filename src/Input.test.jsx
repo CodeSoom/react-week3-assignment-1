5810370541287f0632fb3d68d88ab125feaa432b
gunhee-jeong <kyle4619@naver.com>
2022-08-18 14:05:08 +0900

    context를 삭제하고 it문을 이용해 테스트 분리

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -3,47 +3,32 @@ import { render, fireEvent } from '@testing-library/react';
 import Input from './Input';
 
 describe('Input', () => {
-  context('When to-do is entered', () => {
-    it('Input value changed', () => {
-      const handleChange = jest.fn();
-      const handleClick = jest.fn();
+  const handleChange = jest.fn();
+  const handleClick = jest.fn();
 
-      const { container, getByText, getByPlaceholderText } = render((
-        <Input
-          onChange={handleChange}
-          onClick={handleClick}
-        />
-      ));
+  const { container, getByText, getByPlaceholderText } = render((
+    <Input
+      onChange={handleChange}
+      onClick={handleClick}
+    />
+  ));
 
-      expect(container).toHaveTextContent('할 일');
-      expect(container).toHaveTextContent('추가');
+  it('Changed input value', () => {
+    expect(container).toHaveTextContent('할 일');
+    expect(container).toHaveTextContent('추가');
 
-      expect(handleChange).not.toBeCalled();
-      expect(handleClick).not.toBeCalled();
+    expect(handleChange).not.toBeCalled();
+    expect(handleClick).not.toBeCalled();
 
-      const input = getByPlaceholderText('할 일을 입력해 주세요');
+    const input = getByPlaceholderText('할 일을 입력해 주세요');
 
-      fireEvent.change(input, {
-        target: {
-          value: '잠자기',
-        },
-      });
-      expect(input.value).toBe('잠자기');
-      expect(handleChange).toBeCalled();
-    });
+    fireEvent.change(input, { target: { value: '잠자기' } });
+
+    expect(input.value).toBe('잠자기');
+    expect(handleChange).toBeCalled();
   });
 
-  context('When the Add button is Clicked', () => {
-    it('handleClick function is executed', () => {
-      const handleChange = jest.fn();
-      const handleClick = jest.fn();
-
-      const { container, getByText } = render((
-        <Input
-          onChange={handleChange}
-          onClick={handleClick}
-        />
-      ));
-    });
+  it('Click add button', () => {
+    expect();
   });
 });
