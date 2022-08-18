0427426843ea07fcf8fc4b25e1cbe7d844a8b1af
gunhee-jeong <kyle4619@naver.com>
2022-08-18 19:58:17 +0900

    Input 컴포넌트의 'Click add button' 테스트 추가

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -22,18 +22,32 @@ describe('Input', () => {
     const { getByPlaceholderText } = render((
       <Input
         onChange={handleChange}
-        onClick={handleClick}
       />
     ));
 
     expect(handleChange).not.toBeCalled();
-    expect(handleClick).not.toBeCalled();
 
     const input = getByPlaceholderText('할 일을 입력해 주세요');
-
     fireEvent.change(input, { target: { value: '잠자기' } });
 
     expect(input.value).toBe('잠자기');
     expect(handleChange).toBeCalled();
+  });
+
+  it('Click add button', () => {
+    const { getByText, getByPlaceholderText } = render((
+      <Input
+        onClick={handleClick}
+        onChange={handleChange}
+      />
+    ));
+
+    expect(handleClick).not.toBeCalled();
+
+    const input = getByPlaceholderText('할 일을 입력해 주세요');
+    fireEvent.change(input, { target: { value: '잠자기이' } });
+
+    fireEvent.click(getByText('추가'));
+    expect(handleClick).toBeCalled();
   });
 });
