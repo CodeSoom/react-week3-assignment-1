d1d96044982a8d68bba679ce824554b7c7c561c3
0-Chan <kyc4421194@gmail.com>
2022-08-19 19:36:45 +0900

    :sparkles: Add Page.jsx test code template

---
+++ src/Page.test.jsx
@@ -0,0 +1,49 @@
+import { render } from '@testing-library/react';
+
+import Page from './Page';
+
+describe('<Page/>', () => {
+  const handleChange = jest.fn();
+  const handleClickAdd = jest.fn();
+  const handleClickDelete = jest.fn();
+
+  const taskTitle = '테스트 코드 재밌다.';
+  const tasks = [
+    { id: 1, title: '뭐라도 하기' },
+    { id: 2, title: '코드숨 과제' },
+  ];
+
+  const component = (
+    <Page
+      taskTitle={taskTitle}
+      onChangeTitle={handleChange}
+      onClickAddTask={handleClickAdd}
+      tasks={tasks}
+      onClickDeleteTask={handleClickDelete}
+    />
+  );
+
+  it('renders <h1>, <label>, <button> tags', () => {
+    const { container } = render(component);
+
+    expect(container).toHaveTextContent('To-do');
+    expect(container).toHaveTextContent('할 일');
+    expect(container).toHaveTextContent('추가');
+  });
+
+  it('renders changed input value', () => {
+
+  });
+
+  it('clears <input> value after "추가" button clicked', () => {
+
+  });
+
+  context('with tasks', () => {
+
+  });
+
+  context('without tasks', () => {
+
+  });
+});
