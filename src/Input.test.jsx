b92e0aa96cc2ef9efca0e78e9c78e937743a7381
gunhee-jeong <kyle4619@naver.com>
2022-08-19 20:46:41 +0900

    Input 컴포넌트 리뷰반영

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -6,49 +6,47 @@ describe('Input', () => {
   const handleChange = jest.fn();
   const handleClick = jest.fn();
 
+  const renderInput = (value) => render((
+    <Input
+      value={value}
+      onClick={handleClick}
+      onChange={handleChange}
+    />
+  ));
+
   it('Renders a button to add task', () => {
-    const { container, getByPlaceholderText } = render((
-      <Input />
-    ));
+    const value = '';
+    const { container } = renderInput(value);
 
     expect(container).toHaveTextContent('할 일');
     expect(container).toHaveTextContent('추가');
-
-    const input = getByPlaceholderText('할 일을 입력해 주세요');
-    expect(input).toBeEnabled();
   });
 
-  it('The user enters a task in the input box', () => {
-    const { getByPlaceholderText } = render((
-      <Input
-        onChange={handleChange}
-      />
-    ));
+  it('Enter a task and a change event will occur', () => {
+    const { getByPlaceholderText } = renderInput();
 
     expect(handleChange).not.toBeCalled();
 
     const input = getByPlaceholderText('할 일을 입력해 주세요');
     fireEvent.change(input, { target: { value: '잠자기' } });
 
-    expect(input.value).toBe('잠자기');
-    expect(input.getAttribute('value')).toBe('');
     expect(handleChange).toBeCalled();
   });
 
+  it('Reder after change event occurs', () => {
+    const value = '잠자기';
+    const { getByPlaceholderText } = renderInput(value);
+
+    expect(getByPlaceholderText('할 일을 입력해 주세요')).toHaveValue('잠자기');
+  });
+
   it('Click the Add button to add a task', () => {
-    const { getByText, getByPlaceholderText } = render((
-      <Input
-        onClick={handleClick}
-        onChange={handleChange}
-      />
-    ));
+    const { getByText } = renderInput();
 
     expect(handleClick).not.toBeCalled();
 
-    const input = getByPlaceholderText('할 일을 입력해 주세요');
-    fireEvent.change(input, { target: { value: '잠자기' } });
-
     fireEvent.click(getByText('추가'));
+
     expect(handleClick).toBeCalled();
   });
 });
