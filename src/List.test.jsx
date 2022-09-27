ca6d5ca882dc51775e2480d63eea88d52b72e4fb
task11 <task11@naver.com>
2022-09-27 10:51:32 +0900

    refactor: List 컴포넌트 테스트 코드를 describe-context-it 구조로 리팩토링

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -2,27 +2,33 @@ import { render } from '@testing-library/react';
 
 import List from './List';
 
-test('List에 tasks가 있을 때', () => {
-  const tasks = [
-    {
-      id: 1,
-      title: '뭐라도 하기',
-    },
-  ];
+describe('List component test', () => {
+  context('When there task on the list', () => {
+    it('Show Tasks on the list', () => {
+      const tasks = [
+        {
+          id: 1,
+          title: '뭐라도 하기',
+        },
+      ];
 
-  const { container } = render((
-    <List tasks={tasks} />
-  ));
+      const { container } = render((
+        <List tasks={tasks} />
+      ));
 
-  expect(container).toHaveTextContent('뭐라도 하기');
-});
+      expect(container).toHaveTextContent('뭐라도 하기');
+    });
+  });
 
-test('List에 tasks가 없을 떄', () => {
-  const tasks = [];
+  context('When there no task on the list', () => {
+    it('Show Tasks on the list', () => {
+      const tasks = [];
 
-  const { container } = render((
-    <List tasks={tasks} />
-  ));
+      const { container } = render((
+        <List tasks={tasks} />
+      ));
 
-  expect(container).toHaveTextContent('할 일이 없어요!');
+      expect(container).toHaveTextContent('할 일이 없어요!');
+    });
+  });
 });
