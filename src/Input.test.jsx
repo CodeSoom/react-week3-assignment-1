d9a9d408e96ee10c4aeb02f036d7da0ebb279907
sejineeee <sejinee0918@gmail.com>
2022-10-28 20:31:32 +0900

    review: Input test 파일 작성
    BDD 적용(describe, it 키워드 사용)
    중복되는 render 함수 renderInput 변수에 할당
    handleChange, handleClick 테스트 추가
    input과 button 렌더링 테스트 추가

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -2,16 +2,45 @@ import { render, fireEvent } from '@testing-library/react';
 
 import Input from './Input';
 
-test('Input', () => {
+describe('Input', () => {
   const handleChange = jest.fn();
+  const handleClick = jest.fn();
 
-  const { container, getByText } = render((
+  const renderInput = () => render(
     <Input
+      value="운동하기"
       onChange={handleChange}
-    />
-  ));
+      onClick={handleClick}
+    />,
+  );
 
-  fireEvent.click(getByText('추가'));
+  it('Input과 button이 렌더링된다.', () => {
+    const { container } = renderInput();
 
-  expect(handleChange).not.toBeCalled();
+    expect(container).toHaveTextContent('할 일');
+    expect(container).toHaveTextContent('추가');
+  });
+
+  it('input에 입력시 handleChange 함수가 실행된다.', () => {
+    const { getByLabelText } = renderInput();
+
+    const input = getByLabelText('할 일');
+
+    // tdd 하기로 input의 value 값이 잘 바뀌었는지 확인..이 안되네...
+
+    fireEvent.change(input, { target: { value: 'TDD 하기' } });
+
+    expect(handleChange).toBeCalled();
+    // expect(input.value).toBe('TDD');
+  });
+
+  it('추가 버튼을 클릭하면 handleClick 함수가 실행된다.', () => {
+    const { getByText } = renderInput();
+
+    expect(handleClick).not.toBeCalled();
+
+    fireEvent.click(getByText('추가'));
+
+    expect(handleClick).toBeCalled();
+  });
 });
