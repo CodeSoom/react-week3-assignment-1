be63d97720d623a64c5ea53222439cd7c50c1e14
yxxnhx <tjdbsgk514@hanmail.net>
2022-10-28 21:01:05 +0900

    Page test 구현
    Page 컴포넌트에 할일, 추가, 삭제 버튼이 랜더링되는지 여부 확인

---
+++ src/Page.test.jsx
@@ -0,0 +1,33 @@
+import { render, fireEvent, screen } from '@testing-library/react';
+import Page from './Page';
+
+describe('<Page />', () => {
+  const tasks = [{
+    id: 1,
+    content: '잠자기',
+  },
+  {
+    id: 2,
+    content: '누워있기',
+  },
+  ];
+
+  const taskTitle = '';
+
+  const handleDeleteTask = jest.fn();
+  const handleChangeTitle = jest.fn();
+  const handleAddTask = jest.fn();
+
+  it('Page에 할일, 추가, 삭제 버튼이 있는가', () => {
+    const { container } = render((
+      <Page
+        tasks={tasks}
+        taskTitle={taskTitle}
+        onChangeTitle={handleChangeTitle}
+        onClickAddTask={handleAddTask}
+        onClickDeleteTask={handleDeleteTask}
+      />));
+
+    expect(container).toHaveTextContent('완료');
+  });
+});
