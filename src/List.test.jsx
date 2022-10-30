7667159a7aacc6d0af4cded1aa245a532dcb6a9a
yxxnhx <tjdbsgk514@hanmail.net>
2022-10-30 14:54:28 +0900

    review: List test 코드 정리
    자주 쓰는 테스트용 데이터를 fixtures로 빼서 코드 정리

--- src/List.test.jsx
+++ src/List.test.jsx
@@ -2,6 +2,8 @@ import { render } from '@testing-library/react';
 
 import List from './List';
 
+import tasks from '../fixtures/tasks';
+
 describe('List', () => {
   const handleClickDelete = jest.fn();
 
@@ -19,27 +21,18 @@ describe('List', () => {
   context('List에서 할 일이 없을 때', () => {
     it('할 일이 없어요 문구 랜더링된다', () => {
       const task = [];
-      const { getByText } = listElement(task);
+      const { getByText, container } = listElement(task);
+
       getByText('할 일이 없어요!');
 
-      const { container } = listElement(task);
       expect(container).toHaveTextContent('할 일이 없어요!');
     });
   });
 
   context('List에서 할 일이 있을 때', () => {
     it('입력한 할 일이 랜더링된다', () => {
-      const tasks = [{
-        id: 1,
-        title: '잠자기',
-      },
-      {
-        id: 2,
-        title: '누워있기',
-      },
-      ];
-
       const { container } = listElement(tasks);
+
       expect(container).toHaveTextContent('잠자기');
       expect(container).toHaveTextContent('누워있기');
     });
