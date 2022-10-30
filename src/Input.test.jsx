16c0335a0e46efd71f696bce6a8cecbf1f3497bd
yxxnhx <tjdbsgk514@hanmail.net>
2022-10-30 14:44:46 +0900

    review: List test 수정
    함수 이름 역할이 명시적으로 드러날 수 있게 수정
    맥락 구분을 위해 코드 정리
    필요 없는 fireEvent 삭제 및 value 값 변경할 수 있도록 수정

--- src/Input.test.jsx
+++ src/Input.test.jsx
@@ -3,34 +3,30 @@ import { render, fireEvent, screen } from '@testing-library/react';
 import Input from './Input';
 
 describe('<Input />', () => {
-  const handleClick = jest.fn();
-  const handleChange = jest.fn();
-  const InputElement = () => render((<Input
-    value="잠자기"
-    onClick={handleClick}
-    onChange={handleChange}
+  const handleClickAddTask = jest.fn();
+  const handleChangeTitle = jest.fn();
+
+  const InputElement = (taskTitle = '') => render((<Input
+    value={taskTitle}
+    onClick={handleClickAddTask}
+    onChange={handleChangeTitle}
   />));
 
   it('Input 추가 버튼이 호출된다', () => {
     const { getByText } = InputElement();
 
-    expect(handleClick).not.toBeCalled();
+    expect(handleClickAddTask).not.toBeCalled();
 
     fireEvent.click(getByText('추가'));
-    expect(handleClick).toBeCalled();
+
+    expect(handleClickAddTask).toBeCalled();
   });
 
   it('Input에 입력한 값이 제대로 랜더링 된다', () => {
-    InputElement();
+    InputElement('잠자기2');
 
     const input = screen.getByPlaceholderText('할 일을 입력해 주세요');
 
-    fireEvent.change(input, {
-      target: {
-        value: '잠자기',
-      },
-    });
-
-    expect(input).toHaveAttribute('value', '잠자기');
+    expect(input.value).toBe('잠자기2');
   });
 });
